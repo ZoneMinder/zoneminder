d35e09f4d86344cd44b36a5e92ca25ee9deed4d3
stan <stan@e3e1d417-86f3-4887-817a-d78f3d33393f>
2002-09-23 22:08:50 +0000

    Added GPL header.
    git-svn-id: http://svn.zoneminder.com/svn/zm/trunk@53 e3e1d417-86f3-4887-817a-d78f3d33393f

--- src/jmemdst.c
+++ src/jmemdst.c
@@ -1,8 +1,25 @@
-/* this is not a core library module, so it doesn't define JPEG_INTERNALS */
+/*
+ * Zone Monitor JPEG memory encoding, $Date$, $Revision$
+ * Copyright (C) 2002  Philip Coombes
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/ 
+
 #include "jinclude.h"
 #include "jpeglib.h"
 #include "jerror.h"
-
 
 /* Expanded data destination object for memory */
 

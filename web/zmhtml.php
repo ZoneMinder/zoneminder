6bf2f64359dbb6ffa712b2bc1211e9f0eb283515
stan <stan@e3e1d417-86f3-4887-817a-d78f3d33393f>
2003-01-15 10:05:26 +0000

    Added auto email and message options.
    git-svn-id: http://svn.zoneminder.com/svn/zm/trunk@324 e3e1d417-86f3-4887-817a-d78f3d33393f

--- web/zmhtml.php
+++ web/zmhtml.php
@@ -1207,12 +1207,20 @@ window.focus();
 <td align="right" colspan="2" class="text">&nbsp;</td>
 </tr>
 <tr>
+<td align="left" class="text">Automatically delete all matching events:&nbsp;</td>
+<td align="left" class="text"><input type="checkbox" name="auto_delete" value="1"<?php if ( $filter_data[AutoDelete] ) { echo " checked"; } ?>></td>
+</tr>
+<tr>
 <td align="left" class="text">Automatically upload all matching events:&nbsp;</td>
 <td align="left" class="text"><input type="checkbox" name="auto_upload" value="1"<?php if ( $filter_data[AutoUpload] ) { echo " checked"; } ?>></td>
 </tr>
 <tr>
-<td align="left" class="text">Automatically delete all matching events:&nbsp;</td>
-<td align="left" class="text"><input type="checkbox" name="auto_delete" value="1"<?php if ( $filter_data[AutoDelete] ) { echo " checked"; } ?>></td>
+<td align="left" class="text">Automatically email details of all matching events:&nbsp;</td>
+<td align="left" class="text"><input type="checkbox" name="auto_email" value="1"<?php if ( $filter_data[AutoEmail] ) { echo " checked"; } ?>></td>
+</tr>
+<tr>
+<td align="left" class="text">Automatically message details of all matching events:&nbsp;</td>
+<td align="left" class="text"><input type="checkbox" name="auto_message" value="1"<?php if ( $filter_data[AutoMessage] ) { echo " checked"; } ?>></td>
 </tr>
 <tr>
 <td align="right" colspan="2" class="text">&nbsp;</td>

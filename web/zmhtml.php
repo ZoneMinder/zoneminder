20ce8f51a618b415d849c3e6b16edf5da664cbaa
stan <stan@e3e1d417-86f3-4887-817a-d78f3d33393f>
2003-01-16 15:34:50 +0000

    Now doesn't cache even if streaming.
    git-svn-id: http://svn.zoneminder.com/svn/zm/trunk@353 e3e1d417-86f3-4887-817a-d78f3d33393f

--- web/zmhtml.php
+++ web/zmhtml.php
@@ -361,12 +361,12 @@ window.focus();
 			$status = exec( escapeshellcmd( ZMU_PATH." -m $mid -i" ) );
 			chdir( '..' );
 			header("Refresh: ".REFRESH_IMAGE."; URL='$PHP_SELF?view=watchfeed&mid=$mid&mode=still'" );
-			header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");    // Date in the past
-			header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT"); // always modified
-			header("Cache-Control: no-store, no-cache, must-revalidate");  // HTTP/1.1
-			header("Cache-Control: post-check=0, pre-check=0", false);
-			header("Pragma: no-cache");			  // HTTP/1.0
 		}
+		header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");    // Date in the past
+		header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT"); // always modified
+		header("Cache-Control: no-store, no-cache, must-revalidate");  // HTTP/1.1
+		header("Cache-Control: post-check=0, pre-check=0", false);
+		header("Pragma: no-cache");			  // HTTP/1.0
 ?>
 <html>
 <head>

6f8f52a01c4bb7a5c5ae3db0d95069d74e22e707
stan <stan@e3e1d417-86f3-4887-817a-d78f3d33393f>
2003-06-12 14:50:15 +0000

    Added files for v0.9.13
    git-svn-id: http://svn.zoneminder.com/svn/zm/trunk@572 e3e1d417-86f3-4887-817a-d78f3d33393f

---
+++ db/zmalter-0.9.12.sql
@@ -0,0 +1,12 @@
+--
+-- This updates a 0.9.12 database to 0.9.13
+--
+-- Nothing to change
+--
+-- These are optional, it just seemed a good time...
+optimize table Frames;
+optimize table Events;
+optimize table Filters;
+optimize table Zones;
+optimize table Monitors;
+optimize table Stats;

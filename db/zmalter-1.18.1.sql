ac3cd697ed00c36778dcd83af024d993709d59a6
stan <stan@e3e1d417-86f3-4887-817a-d78f3d33393f>
2004-03-09 17:02:17 +0000

    Removed Id from Filters table.
    git-svn-id: http://svn.zoneminder.com/svn/zm/trunk@933 e3e1d417-86f3-4887-817a-d78f3d33393f

--- db/zmalter-1.18.1.sql
+++ db/zmalter-1.18.1.sql
@@ -1,11 +1,16 @@
 --
 -- This updates a 1.18.1 database to 1.19.0
 --
--- Make changes to Zones table
+-- Make changes to Frames table
 --
 alter table Frames add column Type enum('Normal','Bulk','Alarm') NOT NULL default 'Normal' after FrameId;
 update Frames set Type = 'Alarm' where AlarmFrame = 1;
 alter table Frames drop column AlarmFrame;
+--
+-- Make changes to Filters table
+update Filters set Name = concat( Name, '_', Id );
+alter table Filters drop column Id;
+alter table Filters add primary key(Name); 
 --
 -- These are optional, but we might as well
 --

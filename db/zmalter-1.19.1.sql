fa483725d96a3bd3a253d3b1ff0acca321e289ca
stan <stan@e3e1d417-86f3-4887-817a-d78f3d33393f>
2004-04-21 09:18:29 +0000

    Added sample disk purge filter.
    git-svn-id: http://svn.zoneminder.com/svn/zm/trunk@1019 e3e1d417-86f3-4887-817a-d78f3d33393f

--- db/zmalter-1.19.1.sql
+++ db/zmalter-1.19.1.sql
@@ -9,6 +9,10 @@ alter table Events add column Executed tinyint(3) unsigned not null default 0 af
 --
 alter table Filters add column AutoExecute tinytext default '';
 --
+-- Add in a sample filter to purge the oldest 5 events when the disk is 99% full, delete is disabled though
+--
+insert into Filters values ('PurgeWhenFull','trms=2&obr1=&cbr1=&attr1=Archived&op1=&val1=0&cnj2=and&obr2=&cbr2=&attr2=DiskPercent&op2=>=&val2=99&sort_field=Id&sort_asc=1&limit=5',0,0,0,0,0,'');
+--
 -- These are optional, but we might as well
 --
 optimize table Frames;

4a3e254790585cadd43432fffa8c3b8178d5ffed
stan <stan@e3e1d417-86f3-4887-817a-d78f3d33393f>
2004-04-21 13:13:49 +0000

    Added Argentinian Spanush.
    git-svn-id: http://svn.zoneminder.com/svn/zm/trunk@1027 e3e1d417-86f3-4887-817a-d78f3d33393f

---
+++ web/zm_lang_es_ar.php
@@ -0,0 +1,369 @@
+<?php
+//
+// ZoneMinder web Argentinian Spanish language file, $Date$, $Revision$
+// Copyright (C) 2003  Philip Coombes
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//
+
+// ZoneMinder Spanish 'Argentina' Translation by Fernando Diaz.
+
+// Simple String Replacements
+$zmSlang24BitColour          = 'Color 24 bits';
+$zmSlang8BitGrey             = 'Grises 8 bits';
+$zmSlangActual               = 'Actual';
+$zmSlangAddNewMonitor        = 'Agregar Nuevo Monitor';
+$zmSlangAddNewUser           = 'Agregar Nuevo Usuario';
+$zmSlangAddNewZone           = 'Agregar Nueva Zona';
+$zmSlangAlarm                = 'Alarma';
+$zmSlangAlarmBrFrames        = 'Alarma<br/>Cuadros';
+$zmSlangAlarmFrame           = 'Cuadro Alarma';
+$zmSlangAlarmLimits          = 'Alarm Limits';
+$zmSlangAlarmPx              = 'Alarm Px';
+$zmSlangAlert                = 'Alerta';
+$zmSlangAll                  = 'Todo';
+$zmSlangApply                = 'Aplicar';
+$zmSlangApplyingStateChange  = 'Aplicar Cambio Estado';
+$zmSlangArchArchived         = 'Solo Archivados';
+$zmSlangArchive              = 'Archivar';
+$zmSlangArchUnarchived       = 'Solo Sin Archivar';
+$zmSlangAttrAlarmFrames      = 'Alarm Frames';
+$zmSlangAttrArchiveStatus    = 'Estado Archivo';
+$zmSlangAttrAvgScore         = 'Puntaje Prom.';
+$zmSlangAttrDate             = 'Fecha';
+$zmSlangAttrDateTime         = 'Fecha/Hora';
+$zmSlangAttrDiskBlocks       = 'Disk Blocks';
+$zmSlangAttrDiskPercent      = 'Disk Percent';
+$zmSlangAttrDuration         = 'Duración';
+$zmSlangAttrFrames           = 'Cuadros';
+$zmSlangAttrId               = 'Id';
+$zmSlangAttrMaxScore         = 'Puntaje Máximo';
+$zmSlangAttrMontage          = 'Cámara Múltiple';
+$zmSlangAttrName             = 'Name';
+$zmSlangAttrTime             = 'Hora';
+$zmSlangAttrTotalScore       = 'Puntaje Total';
+$zmSlangAttrWeekday          = 'Día Semana';
+$zmSlangAutoArchiveEvents    = 'Automatically archive all matches';
+$zmSlangAutoDeleteEvents     = 'Automatically delete all matches';
+$zmSlangAutoEmailEvents      = 'Automatically email details of all matches';
+$zmSlangAutoExecuteEvents    = 'Automatically execute command on all matches';
+$zmSlangAutoMessageEvents    = 'Automatically message details of all matches';
+$zmSlangAutoUploadEvents     = 'Automatically upload all matches';
+$zmSlangAvgBrScore           = 'Punt.<br/>Promedio';
+$zmSlangBandwidth            = 'Velocidad';
+$zmSlangBlobPx               = 'Blob Px';
+$zmSlangBlobs                = 'Blobs';
+$zmSlangBlobSizes            = 'Blob Sizes';
+$zmSlangBrightness           = 'Brillo';
+$zmSlangBuffers              = 'Buffers';
+$zmSlangCancel               = 'Cancelar';
+$zmSlangCancelForcedAlarm    = 'Cancelar&nbsp;Alarma&nbsp;Forzada';
+$zmSlangCaptureHeight        = 'Captura Alto';
+$zmSlangCapturePalette       = 'Captura Paleta';
+$zmSlangCaptureWidth         = 'Captura Ancho';
+$zmSlangCheckAll             = 'Marcar Todo';
+$zmSlangChooseFilter         = 'Elegir Filtro';
+$zmSlangClose                = 'Cerrar';
+$zmSlangColour               = 'Color';
+$zmSlangConfiguredFor        = 'Configurado Para';
+$zmSlangConfirmPassword      = 'Confirmar Contraseña';
+$zmSlangConjAnd              = 'y';
+$zmSlangConjOr               = 'o';
+$zmSlangConsole              = 'Consola';
+$zmSlangContactAdmin         = 'Contacte el Administrador para detalles.';
+$zmSlangContrast             = 'Contraste';
+$zmSlangCycleWatch           = 'Cycle Watch';
+$zmSlangDay                  = 'Día';
+$zmSlangDeleteAndNext        = 'Borrar &amp; Próximo';
+$zmSlangDeleteAndPrev        = 'Borrar &amp; Anterior';
+$zmSlangDelete               = 'Borrar';
+$zmSlangDeleteSavedFilter    = 'Borrar Filtro Guardado';
+$zmSlangDescription          = 'Descripción';
+$zmSlangDeviceChannel        = 'Canal';
+$zmSlangDeviceFormat         = 'Señal (0=PAL,1=NTSC etc)';
+$zmSlangDeviceNumber         = 'Fuente (/dev/video?)';
+$zmSlangDimensions           = 'Dimensiones';
+$zmSlangDisk                 = 'Disco';
+$zmSlangDuration             = 'Duración';
+$zmSlangEdit                 = 'Editar';
+$zmSlangEmail                = 'Email';
+$zmSlangEnabled              = 'Habilitado';
+$zmSlangEnterNewFilterName   = 'Ingresar Nuevo Nombre De Filtro';
+$zmSlangErrorBrackets        = 'Error, Revisar si tiene la misma cantidad de paréntesis de apertura';
+$zmSlangError                = 'Error';
+$zmSlangErrorValidValue      = 'Error, Revisar si los términos tienen nombres validos';
+$zmSlangEtc                  = 'etc';
+$zmSlangEvent                = 'Evento';
+$zmSlangEventFilter          = 'Filtro de Evento';
+$zmSlangEventId              = 'Event Id';
+$zmSlangEvents               = 'Eventos';
+$zmSlangExclude              = 'Excluir';
+$zmSlangFeed                 = 'Vista';
+$zmSlangFilterPx             = 'Filtro Px';
+$zmSlangFirst                = 'Primero';
+$zmSlangForceAlarm           = 'Forzar&nbsp;Alarma';
+$zmSlangFPS                  = 'fps';
+$zmSlangFPSReportInterval    = 'Intervalo de Reporte FPS';
+$zmSlangFrame                = 'Cuadro';
+$zmSlangFrameId              = 'Id Cuadro';
+$zmSlangFrameRate            = 'Frame Rate';
+$zmSlangFrames               = 'Cuadros';
+$zmSlangFrameSkip            = 'Saltear Cuadro';
+$zmSlangFTP                  = 'FTP';
+$zmSlangFunc                 = 'Func';
+$zmSlangFunction             = 'Función';
+$zmSlangGenerateVideo        = 'Crear Video';
+$zmSlangGeneratingVideo      = 'Creando Video';
+$zmSlangGrey                 = 'Gris';
+$zmSlangHigh                 = 'Alta';
+$zmSlangHighBW               = 'High&nbsp;B/W';
+$zmSlangHour                 = 'Hora';
+$zmSlangHue                  = 'Saturación';
+$zmSlangId                   = 'Id';
+$zmSlangIdle                 = 'Pasivo';
+$zmSlangIgnore               = 'Ignorar';
+$zmSlangImageBufferSize      = 'Tamaño del Buffer de Imagen';
+$zmSlangImage                = 'Imagen';
+$zmSlangInclude              = 'Incluir';
+$zmSlangInverted             = 'Invertido';
+$zmSlangLanguage             = 'Lenguaje';
+$zmSlangLast                 = 'Ultimo';
+$zmSlangLimitResultsPost     = 'results only;'; // This is used at the end of the phrase 'Limit to first N results only'
+$zmSlangLimitResultsPre      = 'Limit to first'; // This is used at the beginning of the phrase 'Limit to first N results only'
+$zmSlangLoad                 = 'Carga';
+$zmSlangLocal                = 'Local';
+$zmSlangLoggedInAs           = 'Registrado Como';
+$zmSlangLoggingIn            = 'Ingresando';
+$zmSlangLogin                = 'Ingresar';
+$zmSlangLogout               = 'Salir';
+$zmSlangLow                  = 'Baja';
+$zmSlangLowBW                = 'Low&nbsp;B/W';
+$zmSlangMark                 = 'Marca';
+$zmSlangMaxBrScore           = 'Puntaje<br/>Max.';
+$zmSlangMaximumFPS           = 'Maximos FPS';
+$zmSlangMax                  = 'Max';
+$zmSlangMediumBW             = 'Medium&nbsp;B/W';
+$zmSlangMedium               = 'Media';
+$zmSlangMinAlarmGeMinBlob    = 'Minimum alarm pixels should be greater than or equal to minimum blob pixels';
+$zmSlangMinAlarmGeMinFilter  = 'Minimum alarm pixels should be greater than or equal to minimum filter pixels';
+$zmSlangMisc                 = 'Otros';
+$zmSlangMonitorIds           = 'Ids&nbsp;Monitor';
+$zmSlangMonitor              = 'Monitor';
+$zmSlangMonitors             = 'Monitores';
+$zmSlangMontage              = 'Cámara Múltiple';
+$zmSlangMonth                = 'Mes';
+$zmSlangMustBeGe             = 'Debe ser mayor o igual que';
+$zmSlangMustBeLe             = 'Debe ser menor o igual que';
+$zmSlangMustConfirmPassword  = 'Debe confirmar la contraseña';
+$zmSlangMustSupplyPassword   = 'Debe ingresar una contraseña';
+$zmSlangName                 = 'Nombre';
+$zmSlangNetwork              = 'Red';
+$zmSlangNew                  = 'Nuevo';
+$zmSlangNewPassword          = 'Nueva Contraseña';
+$zmSlangNewState             = 'Nuevo Estado';
+$zmSlangNewUser              = 'Nuevo Usuario';
+$zmSlangNext                 = 'Siguiente';
+$zmSlangNoFramesRecorded     = 'No hay movimientos grabados para este evento';
+$zmSlangNoneAvailable        = 'Ninguno Disponible';
+$zmSlangNone                 = 'Ninguno';
+$zmSlangNo                   = 'No';
+$zmSlangNormal               = 'Normal';
+$zmSlangNoSavedFilters       = 'FiltrosNoGuardados';
+$zmSlangNoStatisticsRecorded = 'No hay estadisticas guardadas para este evento/marco';
+$zmSlangOpEq                 = 'igual que';
+$zmSlangOpGtEq               = 'mayor o igual que';
+$zmSlangOpGt                 = 'mayor que';
+$zmSlangOpLtEq               = 'menor o igual que';
+$zmSlangOpLt                 = 'menor que';
+$zmSlangOpNe                 = 'distinto que';
+$zmSlangOptionHelp           = 'Ayuda';
+$zmSlangOptionRestartWarning = 'Estos cambios no se guardaran completamente\nmientras el sistema se ejecute. Cuando termine\nde realizar los cambios asegurese de\nreiniciar Zoneminder.';
+$zmSlangOptions              = 'Opciones';
+$zmSlangOrEnterNewName       = 'o introduze nuevo nombre';
+$zmSlangOrientation          = 'Orientación';
+$zmSlangOverwriteExisting    = 'Sobreescribir Exitente';
+$zmSlangPaged                = 'Paged';
+$zmSlangParameter            = 'Parametro';
+$zmSlangPassword             = 'Contraseña';
+$zmSlangPasswordsDifferent   = 'Las contraseñas nueva y de confirmacion son diferentes';
+$zmSlangPaths                = 'Enlaces';
+$zmSlangPhoneBW              = 'Telefono&nbsp;B/N';
+$zmSlangPixels               = 'pixels';
+$zmSlangPleaseWait           = 'Espere por favor';
+$zmSlangPostEventImageBuffer = 'Buffer Imagenes despues evento';
+$zmSlangPreEventImageBuffer  = 'Buffer Imagenes antes evento<';
+$zmSlangPrev                 = 'Prev';
+$zmSlangRate                 = 'Ritmo';
+$zmSlangReal                 = 'Real';
+$zmSlangRecord               = 'Registro';
+$zmSlangRefImageBlendPct     = 'Reference Image Blend %ge';
+$zmSlangRefresh              = 'Actualizar';
+$zmSlangRemoteHostName       = 'Nombre Servidor Remoto';
+$zmSlangRemoteHostPath       = 'Enlace Servidor Remoto';
+$zmSlangRemoteHostPort       = 'Puerto Servidor Remoto';
+$zmSlangRemoteImageColours   = 'Remote Image Colours';
+$zmSlangRemote               = 'Remote';
+$zmSlangRename               = 'Renombrar';
+$zmSlangReplay               = 'Replay';
+$zmSlangResetEventCounts     = 'Borrar Contador Eventos';
+$zmSlangRestarting           = 'Reiniciando';
+$zmSlangRestart              = 'Reiniciar';
+$zmSlangRestrictedCameraIds  = 'Id. Camara restringida';
+$zmSlangRotateLeft           = 'Rotar a la derecha';
+$zmSlangRotateRight          = 'Rotar a la izquierda';
+$zmSlangRunMode              = 'Metodo Ejecucion';
+$zmSlangRunning              = 'Ejecutando';
+$zmSlangRunState             = 'Estado de Ejecución';
+$zmSlangSaveAs               = 'Guardar Como';
+$zmSlangSaveFilter           = 'Guardar Filtro';
+$zmSlangSave                 = 'Guardar';
+$zmSlangScale                = 'Escala';
+$zmSlangScore                = 'Resultado';
+$zmSlangSecs                 = 'Seg';
+$zmSlangSectionlength        = 'Longitud Sección';
+$zmSlangServerLoad           = 'Carga Servidor';
+$zmSlangSetLearnPrefs        = 'Set Learn Prefs'; // This can be ignored for now
+$zmSlangSetNewBandwidth      = 'Nuevo Ancho de Banda';
+$zmSlangSettings             = 'Configuracion';
+$zmSlangShowFilterWindow     = 'Abrir Filtro';
+$zmSlangSortAsc              = 'Asc';
+$zmSlangSortBy               = 'Sort by';
+$zmSlangSortDesc             = 'Desc';
+$zmSlangSource               = 'Origen';
+$zmSlangSourceType           = 'Tipo Origen';
+$zmSlangStart                = 'Iniciar';
+$zmSlangState                = 'Estado';
+$zmSlangStats                = 'Est.';
+$zmSlangStatus               = 'Estado';
+$zmSlangStills               = 'Fotos';
+$zmSlangStop                 = 'Desactivar';
+$zmSlangStopped              = 'Apagado';
+$zmSlangStream               = 'Video';
+$zmSlangSystem               = 'Sistema';
+$zmSlangTimeDelta            = 'Hora Delta';
+$zmSlangTime                 = 'Hora';
+$zmSlangTimestamp            = 'Etiqueta Hora';
+$zmSlangTimeStamp            = 'Etiqueta Hora';
+$zmSlangTimestampLabelFormat = 'Formato Etiqueta Hora';
+$zmSlangTimestampLabelX      = 'Eje X Etiqueta Hora';
+$zmSlangTimestampLabelY      = 'Eje Y Etiqueta Hora';
+$zmSlangTools                = 'Herramientas';
+$zmSlangTotalBrScore         = 'Total<br/>puntuación';
+$zmSlangTriggers             = 'Gatillos';
+$zmSlangType                 = 'Tipo';
+$zmSlangUnarchive            = 'Desarchivar';
+$zmSlangUnits                = 'Unidades';
+$zmSlangUnknown              = 'Desconocido';
+$zmSlangUseFilterExprsPost   = '&nbsp;filtrar&nbsp;sentencias'; // This is used at the end of the phrase 'use N filter expressions'
+$zmSlangUseFilterExprsPre    = 'Utilizar&nbsp;'; // This is used at the beginning of the phrase 'use N filter expressions'
+$zmSlangUseFilter            = 'Usar Filtro';
+$zmSlangUsername             = 'Nombre';
+$zmSlangUsers                = 'Usuarios';
+$zmSlangUser                 = 'Usuario';
+$zmSlangValue                = 'Valor';
+$zmSlangVideoGenFailed       = 'Fallo la creacion del video!';
+$zmSlangVideoGenParms        = 'Parametros Generacion Video';
+$zmSlangVideoSize            = 'Tamaño Video';
+$zmSlangVideo                = 'Video';
+$zmSlangViewAll              = 'Ver Todo';
+$zmSlangViewPaged            = 'View Paged';
+$zmSlangView                 = 'Ver';
+$zmSlangWarmupFrames         = 'Avisos Movimiento';
+$zmSlangWatch                = 'Monitor';
+$zmSlangWeb                  = 'Web';
+$zmSlangWeek                 = 'Semana';
+$zmSlangX10ActivationString  = 'X10 Comando Activación';
+$zmSlangX10InputAlarmString  = 'X10 Comando Entrada Alarma';
+$zmSlangX10OutputAlarmString = 'X10 Output Alarm String';
+$zmSlangX10                  = 'X10';
+$zmSlangYes                  = 'Si';
+$zmSlangYouNoPerms           = 'No tiene permisos para acceder a este recurso.';
+$zmSlangZoneAlarmColour      = 'Color Alarma (RGB)';
+$zmSlangZoneAlarmThreshold   = 'Alarm Threshold (0>=?<=255)';
+$zmSlangZoneFilterHeight     = 'Altura Filtro (pixels)';
+$zmSlangZoneFilterWidth      = 'Ancho FIltro (pixels)';
+$zmSlangZoneMaxAlarmedArea   = 'Maximo Area Alarma';
+$zmSlangZoneMaxBlobArea      = 'Maximo Blob Area';
+$zmSlangZoneMaxBlobs         = 'Maximo Blobs';
+$zmSlangZoneMaxFilteredArea  = 'Maximo Area Filtrada';
+$zmSlangZoneMaxX             = 'Maximo X (Derecha)';
+$zmSlangZoneMaxY             = 'Maximo Y (Abajo)';
+$zmSlangZoneMinAlarmedArea   = 'Minimo Area Alarma';
+$zmSlangZoneMinBlobArea      = 'Minimum Blob Area';
+$zmSlangZoneMinBlobs         = 'Minimum Blobs';
+$zmSlangZoneMinFilteredArea  = 'Minimo Area Filtrada';
+$zmSlangZoneMinX             = 'Minimo X (Izquierda)';
+$zmSlangZoneMinY             = 'Minimo Y (Arriba)';
+$zmSlangZones                = 'Zonas';
+$zmSlangZone                 = 'Zona';
+
+// Complex replacements with formatting and/or placements, must be passed through sprintf
+// Complex replacements with formatting and/or placements, must be passed through sprintf
+$zmClangCurrentLogin         = 'Usuario actual es \'%1$s\'';
+$zmClangEventCount           = '%1$s %2$s';
+$zmClangLastEvents           = 'Ultimo %1$s %2$s';
+$zmClangMonitorCount         = '%1$s %2$s';
+$zmClangMonitorFunction      = 'Monitor %1$s Funcion';
+
+
+// Variable arrays expressing plurality, see the zmVlang description above
+$zmVlangEvent                = array( 0=>'Eventos', 1=>'Evento', 2=>'Eventos' );
+$zmVlangMonitor              = array( 0=>'Monitores', 1=>'Monitor', 2=>'Monitores' );
+
+
+function zmVlang( $lang_var_array, $count )
+{
+	krsort( $lang_var_array );
+	foreach ( $lang_var_array as $key=>$value )
+	{
+		if ( abs($count) >= $key )
+		{
+			return( $value );
+		}
+	}
+	die( 'Error, unable to correlate variable language string' );
+}
+
+
+
+$zmSlangGoToZoneMinder       = 'Go to Zoneminder.com';
+$zmSlangMinAlarmPixelsLtMax  = 'Minimum alarm pixels should be less than maximum alarm pixels';
+$zmSlangMinBlobAreaLtMax     = 'Minimum blob area should be less than maximum blob area';
+$zmSlangMinBlobsLtMax        = 'Minimum blobs should be less than maximum blobs';
+$zmSlangMinFilterPixelsLtMax = 'Minimum filter pixels should be less than maximum filter pixels';
+$zmSlangMinPixelThresLtMax   = 'Minimum pixel threshold should be less than maximum pixel threshold';
+$zmSlangUpdateNotNecessary   = 'No update is necessary';
+$zmSlangUpdateAvailable      = 'An update to Zoneminder is available';
+$zmSlangVersion              = 'Version';
+$zmSlangVersionIgnore        = 'Ignore this version';
+$zmSlangVersionRemindHour    = 'Remind again in 1 hour';
+$zmSlangVersionRemindDay     = 'Remind again in 1 day';
+$zmSlangVersionRemindWeek    = 'Remind again in 1 week';
+$zmSlangVersionRemindNever   = 'Don\'t remind about new versions';
+$zmSlangZoneMaxPixelThres    = 'Maximum Pixel Threshold (0>=?<=255)';
+$zmSlangZoneMinPixelThres    = 'Minimum Pixel Threshold (0>=?<=255)';
+$zmClangLatestRelease        = 'The latest release is v%1$s, you have v%2$s.';
+$zmClangRunningRecentVer     = 'You are running the most recent version of Zoneminder, v%s.';
+
+$zmSlangAttrMonitorId        = 'Monitor Id';
+$zmSlangAttrMonitorName      = 'Monitor Name';
+$zmSlangBadMonitorChars      = 'Monitor Names may only contain alphanumeric characters plus hyphen and underscore';
+$zmSlangCheckMethod          = 'Alarm Check Method';
+$zmSlangOpIn                 = 'in set';
+$zmSlangOpMatches            = 'matches';
+$zmSlangOpNotIn              = 'not in set';
+$zmSlangOpNotMatches         = 'does not match';
+
+?>

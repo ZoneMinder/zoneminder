e3c5d3567458443fbf034709806108753a0201c1
stan <stan@e3e1d417-86f3-4887-817a-d78f3d33393f>
2004-10-13 10:58:37 +0000

    Added Groups table and ratonalised some name columns.
    git-svn-id: http://svn.zoneminder.com/svn/zm/trunk@1155 e3e1d417-86f3-4887-817a-d78f3d33393f

--- db/zmalter-1.19.5.sql
+++ db/zmalter-1.19.5.sql
@@ -1,9 +1,25 @@
 --
 -- This updates a 1.19.5 database to 1.19.6
 --
+-- Create the Groups table
+--
+CREATE TABLE Groups (
+  Id int(10) unsigned NOT NULL auto_increment,
+  Name varchar(64) NOT NULL,
+  MonitorIds tinytext NOT NULL,
+  PRIMARY KEY  (Id)
+) TYPE=MyISAM;
+--
 -- Add a new index to the Events table
 --
 alter table Events add index (Frames);
+--
+-- Rationalise some of the name columns
+alter table Events modify column Name varchar(64) not null;
+alter table Filters modify column Name varchar(64) not null;
+alter table Monitors modify column Name varchar(64) not null;
+alter table States modify column Name varchar(64) not null;
+alter table Zones modify column Name varchar(64) not null;
 --
 -- These are optional, but we might as well do it now
 --

d3eed68ee58c66d33958adeb220fc564c1425642
stan <stan@e3e1d417-86f3-4887-817a-d78f3d33393f>
2005-02-23 12:06:18 +0000

    Replaced fixed text with token.
    git-svn-id: http://svn.zoneminder.com/svn/zm/trunk@1338 e3e1d417-86f3-4887-817a-d78f3d33393f

--- web/zm_html_view_console.php
+++ web/zm_html_view_console.php
@@ -87,7 +87,7 @@ $montage_height = ZM_WEB_MONTAGE_HEIGHT?ZM_WEB_MONTAGE_HEIGHT:$max_height;
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
 <head>
-<title>ZM - Console</title>
+<title>ZM - <?= $zmSlangConsole ?></title>
 <link rel="stylesheet" href="zm_html_styles.css" type="text/css">
 <link rel="shortcut icon" href="favicon.ico">
 <link rel="icon" type="image/ico" href="favicon.ico">

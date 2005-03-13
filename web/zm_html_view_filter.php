86b6b9f9192a5c3b94fb270f11356119a3d49a40
stan <stan@e3e1d417-86f3-4887-817a-d78f3d33393f>
2005-03-13 20:52:01 +0000

    Replaced hardcoded text with token.
    git-svn-id: http://svn.zoneminder.com/svn/zm/trunk@1369 e3e1d417-86f3-4887-817a-d78f3d33393f

--- web/zm_html_view_filter.php
+++ web/zm_html_view_filter.php
@@ -273,7 +273,7 @@ else
 <td class="text"><?php if ( $trms > 2 ) { echo buildSelect( $obracket_name, $obracket_types ); } else { ?>&nbsp;<?php } ?></td>
 <td class="text"><?= buildSelect( $attr_name, $attr_types, "$value_name.value = ''; submitToFilter( document.filter_form, 0 );" ); ?></td>
 <?php if ( $$attr_name == "Archived" ) { ?>
-<td class="text"><center>is equal to</center></td>
+<td class="text"><center><?= $zmSlangIsEqualTo ?></center></td>
 <td class="text"><?= buildSelect( $value_name, $archive_types ); ?></td>
 <?php } elseif ( $$attr_name ) { ?>
 <td class="text"><?= buildSelect( $op_name, $op_types ); ?></td>

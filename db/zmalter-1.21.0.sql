136a115184b9af786e972b1bae6e058c949ecf43
stan <stan@e3e1d417-86f3-4887-817a-d78f3d33393f>
2005-05-16 15:37:46 +0000

    Bug 3 - Made zone blobs limits full ints.
    git-svn-id: http://svn.zoneminder.com/svn/zm/trunk@1411 e3e1d417-86f3-4887-817a-d78f3d33393f

--- db/zmalter-1.21.0.sql
+++ db/zmalter-1.21.0.sql
@@ -7,8 +7,13 @@ alter table Monitors modify column Orientation enum('0','90','180','270','hori',
 --
 -- Make changes to Stats table
 --
-alter table Sats modify column MinBlobSize int(10) unsigned NOT NULL default '0';
-alter table Sats modify column MaxBlobSize int(10) unsigned NOT NULL default '0';
+alter table Stats modify column MinBlobSize int(10) unsigned NOT NULL default '0';
+alter table Stats modify column MaxBlobSize int(10) unsigned NOT NULL default '0';
+--
+-- Make changes to Zones table
+--
+alter table Zones modify column MinBlobPixels int(10) unsigned default NULL;
+alter table Zones modify column MaxBlobPixels int(10) unsigned default NULL;
 --
 -- These are optional, but we might as well do it now
 --

bfdbc232424c0196e6cb7b7908875f748ffa3ff3
stan <stan@e3e1d417-86f3-4887-817a-d78f3d33393f>
2005-12-05 12:19:26 +0000

    Moved form tages outside of table.
    git-svn-id: http://svn.zoneminder.com/svn/zm/trunk@1654 e3e1d417-86f3-4887-817a-d78f3d33393f

--- web/zm_html_view_login.php
+++ web/zm_html_view_login.php
@@ -36,15 +36,15 @@ window.resizeTo( <?= $jws['login']['w'] ?>, <?= $jws['login']['h'] ?> );
 ?>
 </head>
 <body>
-<table align="center" border="0" cellspacing="2" cellpadding="2" width="96%">
 <form name="login_form" method="post" action="<?= $PHP_SELF ?>">
 <input type="hidden" name="action" value="login">
 <input type="hidden" name="view" value="postlogin">
+<table align="center" border="0" cellspacing="2" cellpadding="2" width="96%">
 <tr><td colspan="2" class="smallhead" align="center">ZoneMinder <?= $zmSlangLogin ?></td></tr>
 <tr><td width="50%" class="text" align="right"><?= $zmSlangUsername ?></td><td width="50%" align="left" class="text"><input type="text" class="form" name="username" value="<?= isset($username)?$username:"" ?>" size="12"></tr>
 <tr><td width="50%" class="text" align="right"><?= $zmSlangPassword ?></td><td width="50%" align="left" class="text"><input type="password" class="form" name="password" value="" size="12"></tr>
 <tr><td colspan="2" align="center"><input type="submit" value="<?= $zmSlangLogin ?>" class="form"></td></tr>
-</form>
 </table>
+</form>
 </body>
 </html>

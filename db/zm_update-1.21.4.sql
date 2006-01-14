365d49734e2e82e271414c6af4fb829650c0d12f
stan <stan@e3e1d417-86f3-4887-817a-d78f3d33393f>
2006-01-14 13:34:24 +0000

    Bug 245 - Added progress bar options.
    git-svn-id: http://svn.zoneminder.com/svn/zm/trunk@1799 e3e1d417-86f3-4887-817a-d78f3d33393f

--- db/zm_update-1.21.4.sql
+++ db/zm_update-1.21.4.sql
@@ -18,6 +18,7 @@ alter table Zones add column NumCoords tinyint(3) unsigned NOT NULL default '0' 
 alter table Zones add column Coords tinytext NOT NULL after NumCoords;
 alter table Zones add column Area int(10) unsigned not null default 0 after Coords;
 alter table Zones modify column AlarmRGB int(10) unsigned default '0';
+alter table Zones add index MonitorId (MonitorId);  
 --
 insert into Controls values ('','Neu-Fusion NCS370','Remote','zmcontrol-ncs370.pl',0,0,0,0,0,0,0,0,NULL,NULL,NULL,NULL,0,NULL,NULL,0,0,0,0,0,NULL,NULL,NULL,NULL,0,NULL,NULL,0,0,0,0,0,NULL,NULL,NULL,NULL,0,NULL,NULL,0,0,0,0,0,NULL,NULL,NULL,NULL,0,NULL,NULL,0,0,0,0,0,NULL,NULL,NULL,NULL,0,NULL,NULL,1,24,1,0,1,1,0,0,0,1,1,NULL,NULL,NULL,NULL,0,NULL,NULL,0,NULL,1,NULL,NULL,NULL,NULL,0,NULL,NULL,0,NULL,0,0);
 

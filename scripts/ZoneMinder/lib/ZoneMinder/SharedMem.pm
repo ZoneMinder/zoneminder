ba8dd7e374f9577f5dea0799ca3c3fece64a98c0
stan <stan@e3e1d417-86f3-4887-817a-d78f3d33393f>
2006-01-14 14:11:50 +0000

    Added documentation, simplified write spec.
    git-svn-id: http://svn.zoneminder.com/svn/zm/trunk@1809 e3e1d417-86f3-4887-817a-d78f3d33393f

--- scripts/ZoneMinder/lib/ZoneMinder/SharedMem.pm
+++ scripts/ZoneMinder/lib/ZoneMinder/SharedMem.pm
@@ -62,14 +62,12 @@ our %EXPORT_TAGS = (
 		zmShmRead
 		zmShmWrite
 		zmGetMonitorState
-		zmGetLastEventId
 		zmGetAlarmLocation
 		zmIsAlarmed
 		zmInAlarm
 		zmHasAlarmed
 		zmGetLastEvent
 		zmGetLastImageTime
-		zmGetMonitorActions
 		zmMonitorEnable
 		zmMonitorDisable
 		zmMonitorSuspend
@@ -327,7 +325,7 @@ sub zmShmRead( $$;$ )
 		{
 			( $value ) = unpack( "c", $data );
 		}
-		elsif ( $type =~ /^char[\d+]$/ )
+		elsif ( $type =~ /^char\[\d+\]$/ )
 		{
 			( $value ) = unpack( "Z".$size, $data );
 		}
@@ -347,7 +345,7 @@ sub zmShmRead( $$;$ )
 sub zmShmWrite( $$;$ )
 {
 	my $monitor = shift;
-	my $field_values_ref = shift;
+	my $field_values = shift;
 	my $nocheck = shift;
 
 	if ( !$nocheck && !zmShmVerify( $monitor ) )
@@ -358,12 +356,7 @@ sub zmShmWrite( $$;$ )
 	my $shm_key = $monitor->{ShmKey};
 	my $shm_id = $monitor->{ShmId};
 	
-	if ( ref($field_values_ref) eq "HASH" )
-	{
-		$field_values_ref = [ %$field_values_ref ];
-	}
-	my %field_values = @$field_values_ref;
-	while ( my ( $field, $value ) = each( %field_values ) )
+	while ( my ( $field, $value ) = each( %$field_values ) )
 	{
 		my ( $section, $element ) = split( /[\/:.]/, $field );
 		Fatal( "Invalid shm selector '$field'" ) if ( !$section || !$element );
@@ -389,7 +382,7 @@ sub zmShmWrite( $$;$ )
 		{
 			$data = pack( "c", $value );
 		}
-		elsif ( $type =~ /^char[\d+]$/ )
+		elsif ( $type =~ /^char\[\d+\]$/ )
 		{
 			$data = pack( "Z".$size, $value );
 		}
@@ -412,13 +405,6 @@ sub zmGetMonitorState( $ )
 	my $monitor = shift;
 
 	return( zmShmRead( $monitor, "shared_data:state" ) );
-}
-
-sub zmGetLastEventId( $ )
-{
-	my $monitor = shift;
-
-	return( zmShmRead( $monitor, "shared_data:last_event" ) );
 }
 
 sub zmGetAlarmLocation( $ )
@@ -536,45 +522,45 @@ sub zmTriggerEventOn( $$$;$$ )
 	my $text = shift;
 	my $showtext = shift;
 
-	my @values = (
-		( "trigger_data:trigger_score" => $score ),
-		( "trigger_data:trigger_cause" => $cause ),
-	);
-	push( @values, ( "trigger_data:trigger_text" => $text ) ) if ( defined($text) );
-	push( @values, ( "trigger_data:trigger_showtext" => $showtext ) ) if ( defined($showtext) );
-	push( @values, ( "trigger_data:trigger_state" => TRIGGER_ON ) ); # Write state last so event not read incomplete
+	my $values = {
+		"trigger_data:trigger_score" => $score,
+		"trigger_data:trigger_cause" => $cause,
+	};
+	$values->{"trigger_data:trigger_text"} = $text if ( defined($text) );
+	$values->{"trigger_data:trigger_showtext"} = $showtext if ( defined($showtext) );
+	$values->{"trigger_data:trigger_state"} = TRIGGER_ON; # Write state last so event not read incomplete
 
-	zmShmWrite( $monitor, \@values );
+	zmShmWrite( $monitor, $values );
 }
 
 sub zmTriggerEventOff( $ )
 {
 	my $monitor = shift;
 
-	my @values = (
-		( "trigger_data:trigger_state"    => TRIGGER_OFF ),
-		( "trigger_data:trigger_score"    => 0 ),
-		( "trigger_data:trigger_cause"    => "" ),
-		( "trigger_data:trigger_text"     => "" ),
-		( "trigger_data:trigger_showtext" => "" ),
-	);
+	my $values = {
+		"trigger_data:trigger_state"    => TRIGGER_OFF,
+		"trigger_data:trigger_score"    => 0,
+		"trigger_data:trigger_cause"    => "",
+		"trigger_data:trigger_text"     => "",
+		"trigger_data:trigger_showtext" => "",
+	};
 
-	zmShmWrite( $monitor, \@values );
+	zmShmWrite( $monitor, $values );
 }
 
 sub zmTriggerEventCancel( $ )
 {
 	my $monitor = shift;
 
-	my @values = (
-		( "trigger_data:trigger_state"    => TRIGGER_CANCEL ),
-		( "trigger_data:trigger_score"    => 0 ),
-		( "trigger_data:trigger_cause"    => "" ),
-		( "trigger_data:trigger_text"     => "" ),
-		( "trigger_data:trigger_showtext" => "" ),
-	);
+	my $values = {
+		"trigger_data:trigger_state"    => TRIGGER_CANCEL,
+		"trigger_data:trigger_score"    => 0,
+		"trigger_data:trigger_cause"    => "",
+		"trigger_data:trigger_text"     => "",
+		"trigger_data:trigger_showtext" => "",
+	};
 
-	zmShmWrite( $monitor, \@values );
+	zmShmWrite( $monitor, $values );
 }
 
 sub zmTriggerShowtext( $$ )
@@ -582,50 +568,169 @@ sub zmTriggerShowtext( $$ )
 	my $monitor = shift;
 	my $showtext = shift;
 
-	my @values = (
-		( "trigger_data:trigger_showtext" => $showtext ),
-	);
+	my $values = {
+		"trigger_data:trigger_showtext" => $showtext,
+	};
 
-	zmShmWrite( $monitor, \@values );
+	zmShmWrite( $monitor, $values );
 }
 
 1;
 __END__
-# Below is stub documentation for your module. You'd better edit it!
 
 =head1 NAME
 
-ZoneMinder - Perl extension for blah blah blah
+ZoneMinder::SharedMem - ZoneMinder Shared Memory access module
 
 =head1 SYNOPSIS
 
-  use ZoneMinder;
-  blah blah blah
+  use ZoneMinder::SharedMem;
+  use ZoneMinder::SharedMem qw(:all);
+
+  if ( zmShmVerify( $monitor ) )
+  {
+    $state = zmGetMonitorState( $monitor );
+    if ( $state == STATE_ALARM )
+    {
+      ...
+    }
+  }
+
+  ( $lri, $lwi ) = zmShmRead( $monitor, [ "shared_data:last_read_index", "shared_data:last_write_index" ] );
+  zmShmWrite( $monitor, { "trigger_data:trigger_showtext" => "Some Text" } );
 
 =head1 DESCRIPTION
 
-Stub documentation for ZoneMinder, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
+The ZoneMinder:SharedMem module contains methods for accessing and writing to shared memory as well as helper methods for common operations.
 
-Blah blah blah.
+The core elements of ZoneMinder used shared memory to allow multiple access to resources. Although ZoneMinder scripts have used this information before, up until now it was difficult to access and prone to errors. This module introduces a common API for shared memory access (both reading and writing) making it a lot easier to customise scripts or even create your own.
 
-=head2 EXPORT
+All the methods listed below require a 'monitor' parameter. This must be a reference to a hash with at least the 'Id' field set to the monitor id of the shared memory you wish to access. Using database methods to select the monitor details will also return this kind of data. Some of the shared memory methods will add and amend new fields to this hash.
+
+=over 4
+
+=head1 METHODS
+
+=item zmShmVerify ( $monitor );
+
+Verify that the shared memory of the monitor given exists and is valid. It will return an undefined value if it is not valid. You should generally call this method first before using any of the other methods, but most of the remaining methods will also do so if the memory has not already been verified.
+
+=item zmShmRead ( $monitor, $readspec );
+
+This method is used to read data from shared memory attached to the given monitor. The shared memory will be verified if it has not already been. The 'readspec' must either be a string of the form "<section>:<field>" or a reference to an array of strings of the same format. In the first case a single value is returned, in the latter case a list of values is return. Errors will cause undefined to be returned. The allowable sections and field names are described below.
+
+=item zmShmWrite ( $monitor, $writespec );
+
+This method is used to write data to shared memory attached to the given monitor. The shared memory will be verified if it has not already been. The 'writespec' must be a reference to a hash with keys of the form "<section>:<field>" and values as the data to be written. Errors will cause undefined to be returned, otherwise a non-undefined value will be returned. The allowable sections and field names are described below.
+
+=item $state = zmGetMonitorState ( $monitor );
+
+Return the current state of the given monitor. This is an integer value and can be compared with the STATE constants given below.
+
+=item $event_id = zmGetLastEvent ( $monitor );
+
+Return the event id of the last event that the monitor generated, or 0 if no event has been generated by the current monitor process.
+
+=item zmIsAlarmed ( $monitor );
+
+Return 1 if the monitor given is currently in an alarm state, 0 otherwise.
+
+=item zmInAlarm ( $monitor );
+
+Return 1 if the monitor given is currently in an alarm or alerted state, 0 otherwise.
+
+=item zmHasAlarmed ( $monitor );
+
+Return 1 if the given monitor is in an alarm state, or has been in an alarm state since the last call to this method.
+
+=item ( $x, $y ) = zmGetAlarmLocation ( $monitor );
+
+Return an x,y pair indicating the image co-ordinates of the centre of the last motion event generated by the given monitor. If no event has been generated by the current monitor process, or the alarm was not motion related, returns -1,-1.
+
+=item zmGetLastImageTime ( $monitor );
+
+Returns the time (in utc seconds) since the last image was captured by the given monitor, or 0 otherwise.
+
+=item zmMonitorSuspend ( $monitor );
+
+Suspend the given monitor from generating events caused by motion. This method can be used to prevent camera actions such as panning or zooming from causing events. If configured to do so, the monitor may automatically resume after a defined period.
+
+=item zmMonitorResume ( $monitor );
+
+Allow the given monitor to resume generating events caused by motion.
+
+=item zmTriggerEventOn ( $monitor, $score, $cause [, $text, $showtext ] );
+
+Trigger the given monitor to generate an event. You must supply an event score and a cause string indicating the reason for the event. You may also supply a text string containing further details about the event and a showtext string which may be included in the timestamp annotation on any images captured during the event, if configured to do so.
+
+=item zmTriggerEventOff ( $monitor );
+
+Trigger the given monitor to not generate any events. This method does not cancel zmTriggerEventOn, but is exclusive to it. This method is intended to allow external triggers to prevent normal events being generated by monitors in the same way as zmMonitorSuspend but applies to all events and not just motion, and is intended for longer timescales than are appropriate for suspension.
+
+=item zmTriggerEventCancel ( $monitor );
+
+Cancel any previous trigger on or off requests. This stops a triggered alarm if it exists from a previous 'on' and allows events to be generated once more following a previous 'off'.
+
+=item zmTriggerShowtext ( $monitor, $showtest );
+
+Indicate that the given text should be displayed in the timestamp annotation on any images captured, if the format of the annotation string defined for the monitor permits.
+
+=head1 DATA
+
+The data fields in shared memory that may be accessed are as follows. There are two main sections, shared_data which is general data and trigger_data which is used for event triggering. Whilst reading from these fields is harmless, extreme care must be taken when writing to shared memory, especially in the shared_data section as this is normally written to only by monitor capture and analysis processes.
+
+  shared_data         The general shared memory section
+    size              The size, in bytes, of this section
+    valid             Flag indicating whether this section has been initialised
+    active            Flag indicatinf whether this monitor is active
+    state             The current monitor state, see the STATE constants below
+    last_write_index  The last index, in the image buffer, that an image has been saved to
+    last_read_index   The last index, in the image buffer, that an image has been analysed from
+    last_image_time   The time (in utc seconds) when the last image was captured
+    last_event        The id of the last event generated by the monitor analysis process, 0 if none
+    action            The monitor actions bitmask, see the ACTION constants below
+    brightness        Read/write location for the current monitor brightness
+    hue               Read/write location for the current monitor hue
+    colour            Read/write location for the current monitor colour
+    contrast          Read/write location for the current monitor contrast
+    alarm_x           Image x co-ordinate (from left) of the centre of the last motion event, -1 if none
+    alarm_y           Image y co-ordinate (from top) of the centre of the last motion event, -1 if none
+
+  trigger_data        The triggered event shared memory section
+    size              The size, in bytes of this section
+    trigger_state     The current trigger state, see the TRIGGER constants below
+    trigger_score     The current triggered event score
+    trigger_cause     The current triggered event cause string
+    trigger_text      The current triggered event descriptive text string
+    trigger_showtext  The triggered text that will be displayed on captured image timestamps
+
+=head1 CONSTANTS
+
+The following constants are used by the methods above, but can also be used by user scripts if required.
+
+=item STATE_IDLE STATE_PREALARM STATE_ALARM STATE_ALERT STATE_TAPE
+
+These constants define the state of the monitor with respect to alarms and events. They are used in the shared_data:state field.
+
+=item ACTION_GET ACTION_SET ACTION_RELOAD ACTION_SUSPEND ACTION_RESUME
+
+These constants defines the various values that can exist in the shared_data:action field. This is a bitmask which when non-zero defines an action that an executing monitor process should take. ACTION_GET requires that the current values of brightness, contrast, colour and hue are taken from the camera and written to the equivalent shared memory fields. ACTION_SET implies the reverse, that the values in shared memory should be written to the camera. ACTION_RELOAD signal that the monitor process should reload itself from the database in case any settings have changed there. ACTION_SUSPEND signals that a monitor should stop exaiming images for motion, though other alarms may still occur. ACTION_RESUME sigansl that a monitor should resume motion detectiom.
+
+=item TRIGGER_CANCEL TRIGGER_ON TRIGGER_OFF 
+
+These constants are used in the definition of external triggers. TRIGGER_CANCEL is used to indicated that any previous trigger settings should be cancelled, TRIGGER_ON signals that an alarm should be created (or continued)) as a result of the current trigger and TRIGGER_OFF signals that the trigger should prevent any alarms from being generated. See the trigger methods above for further details.
+
+=head1 EXPORT
 
 None by default.
-
+The :constants tag will export the shared memory constants which mostly define enumerations for the variables held in memory
+The :functions tag will export the shared memory access functions.
+The :all tag will export all above symbols.
 
 
 =head1 SEE ALSO
 
-Mention other useful documentation such as the documentation of
-related modules or operating system documentation (such as man pages
-in UNIX), or any relevant external documentation such as RFCs or
-standards.
-
-If you have a mailing list set up for your module, mention it here.
-
-If you have a web site set up for your module, mention it here.
+http://www.zoneminder.com
 
 =head1 AUTHOR
 

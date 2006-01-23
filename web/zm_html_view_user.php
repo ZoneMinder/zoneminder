28175b0e5298ff2cca56290f3910409af0b5a3d7
stan <stan@e3e1d417-86f3-4887-817a-d78f3d33393f>
2006-01-23 12:14:57 +0000

    Bug 269 - Added popup monitor id selector
    git-svn-id: http://svn.zoneminder.com/svn/zm/trunk@1855 e3e1d417-86f3-4887-817a-d78f3d33393f

--- web/zm_html_view_user.php
+++ web/zm_html_view_user.php
@@ -95,6 +95,16 @@ if ( !$new_user['Password'] )
 	return( true );
 }
 
+function selectRestrictedMonitors()
+{
+    newWindow( "<?= $PHP_SELF ?>?view=monitorselect&callForm=<?= urlencode( 'user_form' ) ?>&callField=<?= urlencode( 'new_user[MonitorIds]' ) ?>", "zmRestrictedMonitors", <?= $jws['monitorselect']['w'] ?>, <?= $jws['monitorselect']['h'] ?> );
+}
+
+function newWindow(Url,Name,Width,Height)
+{
+	var Win = window.open(Url,Name,"resizable,width="+Width+",height="+Height);
+}
+
 function closeWindow()
 {
 	window.close();
@@ -102,14 +112,14 @@ function closeWindow()
 </script>
 </head>
 <body>
-<table border="0" cellspacing="0" cellpadding="4" width="100%">
-<tr>
-<td colspan="2" align="left" class="head"><?= $zmSlangUser ?> - &quot;<?= $new_user['Username'] ?>&quot;</td>
-</tr>
 <form name="user_form" method="post" action="<?= $PHP_SELF ?>" onsubmit="return validateForm( document.user_form )">
 <input type="hidden" name="view" value="<?= $view ?>">
 <input type="hidden" name="action" value="user">
 <input type="hidden" name="uid" value="<?= $uid ?>">
+<table border="0" cellspacing="0" cellpadding="4" width="100%">
+<tr>
+<td colspan="2" align="left" class="head"><?= $zmSlangUser ?> - &quot;<?= $new_user['Username'] ?>&quot;</td>
+</tr>
 <tr><td align="right" class="text"><?= $zmSlangUsername ?></td><td align="left" class="text"><input type="text" name="new_user[Username]" value="<?= $new_user['Username'] ?>" size="16" class="form"></td></tr>
 <tr><td align="right" class="text"><?= $zmSlangNewPassword ?></td><td align="left" class="text"><input type="password" name="new_user[Password]" value="" size="16" class="form"></td></tr>
 <tr><td align="right" class="text"><?= $zmSlangConfirmPassword ?></td><td align="left" class="text"><input type="password" name="conf_password" value="" size="16" class="form"></td></tr>
@@ -121,10 +131,11 @@ function closeWindow()
 <tr><td align="right" class="text"><?= $zmSlangMonitors ?></td><td align="left" class="text"><?= buildSelect( "new_user[Monitors]", $nve ) ?></td></tr>
 <tr><td align="right" class="text"><?= $zmSlangSystem ?></td><td align="left" class="text"><?= buildSelect( "new_user[System]", $nve ) ?></td></tr>
 <tr><td align="right" class="text"><?= $zmSlangMaxBandwidth ?></td><td align="left" class="text"><?= buildSelect( "new_user[MaxBandwidth]", $bandwidths ) ?></td></tr>
-<tr><td align="right" class="text"><?= $zmSlangRestrictedCameraIds ?></td><td align="left" class="text"><input type="text" name="new_user[MonitorIds]" value="<?= $new_user['MonitorIds'] ?>" size="16" class="form"></td></tr>
+<tr><td align="right" class="text"><?= $zmSlangRestrictedMonitors ?></td><td align="left" class="text"><input type="text" name="new_user[MonitorIds]" value="<?= $new_user['MonitorIds'] ?>" size="16" class="form">&nbsp;<a href="#" onClick="selectRestrictedMonitors()"><?= $zmSlangSelect ?></a></td></tr>
 <tr>
 <td align="right"><input type="submit" value="<?= $zmSlangSave ?>" class="form"></td><td align="left"><input type="button" value="<?= $zmSlangClose ?>" class="form" onClick="closeWindow()"></td>
 </tr>
 </table>
+</form>
 </body>
 </html>

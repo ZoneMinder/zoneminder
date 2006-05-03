b7cd9dc258eaade031969eb36c78e2df78f0d148
stan <stan@e3e1d417-86f3-4887-817a-d78f3d33393f>
2006-05-03 14:38:17 +0000

    Bug 322 - Added missing zone preset
    git-svn-id: http://svn.zoneminder.com/svn/zm/trunk@1951 e3e1d417-86f3-4887-817a-d78f3d33393f

---
+++ db/zm_update-1.22.1.sql
@@ -0,0 +1,18 @@
+--
+-- This updates a 1.22.1 database to 1.22.2
+--
+
+--
+-- Add missing zone preset
+--
+insert into ZonePresets values (6,'Best, high sensitivity','Active','Percent','Blobs',20,NULL,8,NULL,3,3,6,NULL,5,NULL,1,NULL);
+
+--
+-- These are optional, but we might as well do it now
+--
+optimize table Frames;
+optimize table Events;
+optimize table Filters;
+optimize table Zones;
+optimize table Monitors;
+optimize table Stats;

c29b436e54f18337517453fbcd4a89c302579b23
stan <stan@e3e1d417-86f3-4887-817a-d78f3d33393f>
2006-05-08 12:34:44 +0000

    Removed redundant zone extent columns
    git-svn-id: http://svn.zoneminder.com/svn/zm/trunk@1961 e3e1d417-86f3-4887-817a-d78f3d33393f

--- db/zm_update-1.22.1.sql
+++ db/zm_update-1.22.1.sql
@@ -3,9 +3,17 @@
 --
 
 --
--- Add missing zone preset
+-- Add missing Zone Preset
 --
 insert into ZonePresets values (6,'Best, high sensitivity','Active','Percent','Blobs',20,NULL,8,NULL,3,3,6,NULL,5,NULL,1,NULL);
+
+--
+-- Remove redundant Zone columns
+--
+alter table Zones drop column LoX;
+alter table Zones drop column HiX;
+alter table Zones drop column LoY;
+alter table Zones drop column HiY;
 
 --
 -- These are optional, but we might as well do it now

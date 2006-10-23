501ded0f5d211464979b6055d695e2d4dce97dc4
stan <stan@e3e1d417-86f3-4887-817a-d78f3d33393f>
2006-10-23 14:06:21 +0000

    Updated for 1.22.3
    git-svn-id: http://svn.zoneminder.com/svn/zm/trunk@2025 e3e1d417-86f3-4887-817a-d78f3d33393f

---
+++ db/zm_update-1.22.2.sql
@@ -0,0 +1,23 @@
+--
+-- This updates a 1.22.2 database to 1.22.3
+--
+
+--
+-- Add new Background column into Filters
+--
+alter table Filters add column Background tinyint(1) unsigned not null default 0;
+
+--
+-- Set the Background flag for any filters currently saved with Auto tasks
+--
+update Filters set Background = 1 where (AutoArchive = 1 or AutoVideo = 1 or AutoUpload = 1 or AutoEmail = 1 or AutoMessage = 1 or AutoExecute = 1 or AutoDelete = 1);
+
+--
+-- These are optional, but we might as well do it now
+--
+optimize table Frames;
+optimize table Events;
+optimize table Filters;
+optimize table Zones;
+optimize table Monitors;
+optimize table Stats;

76345804e150531fa23c300fb0ff3f78fc5bd165
stan <stan@e3e1d417-86f3-4887-817a-d78f3d33393f>
2008-07-16 08:39:34 +0000

    Fixed incorrect use of global
    git-svn-id: http://svn.zoneminder.com/svn/zm/trunk@2536 e3e1d417-86f3-4887-817a-d78f3d33393f

--- web/skins/classic/views/monitorpreset.php
+++ web/skins/classic/views/monitorpreset.php
@@ -43,7 +43,7 @@ xhtmlHeaders(__FILE__, $SLANG['MonitorPreset'] );
     <div id="content">
       <form name="contentForm" id="contentForm" method="post" action="<?= $_SERVER['PHP_SELF'] ?>">
         <input type="hidden" name="view" value="none"/>
-        <input type="hidden" name="mid" value="<?= $mid ?>/">
+        <input type="hidden" name="mid" value="<?= $_REQUEST['mid'] ?>"/>
         <p>
           <?= $SLANG['MonitorPresetIntro'] ?>
         </p>

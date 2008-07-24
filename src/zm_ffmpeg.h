8d503408f8e7f47296c7e814c4f97d5537938a47
stan <stan@e3e1d417-86f3-4887-817a-d78f3d33393f>
2008-07-24 11:30:18 +0000

    Fixe dproblem with detection of FFMPEG header locations
    git-svn-id: http://svn.zoneminder.com/svn/zm/trunk@2597 e3e1d417-86f3-4887-817a-d78f3d33393f

--- src/zm_ffmpeg.h
+++ src/zm_ffmpeg.h
@@ -26,10 +26,28 @@
 
 extern "C" {
 #define __STDC_CONSTANT_MACROS
+#if HAVE_LIBAVCODEC_AVCODEC_H
+#include <libavcodec/avcodec.h>
+#elif HAVE_FFMPEG_AVCODEC_H
 #include <ffmpeg/avcodec.h>
+#else
+#error "No location for avcodec.h found"
+#endif
+#if HAVE_LIBAVFORMAT_AVFORMAT_H
+#include <libavformat/avformat.h>
+#elif HAVE_FFMPEG_AVFORMAT_H
 #include <ffmpeg/avformat.h>
+#else
+#error "No location for avformat.h found"
+#endif
 #if HAVE_LIBSWSCALE
+#if HAVE_LIBSWSCALE_SWSCALE_H
+#include <libswscale/swscale.h>
+#elif HAVE_FFMPEG_SWSCALE_H
 #include <ffmpeg/swscale.h>
+#else
+#error "No location for swscale.h found"
+#endif
 #endif // HAVE_LIBSWSCALE
 }
 

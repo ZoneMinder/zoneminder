d0e787f957ea3200ebe9405eafff7e9ad6cd111f
stan <stan@e3e1d417-86f3-4887-817a-d78f3d33393f>
2008-07-25 09:14:36 +0000

    Test revision for properties
    git-svn-id: http://svn.zoneminder.com/svn/zm/trunk@2609 e3e1d417-86f3-4887-817a-d78f3d33393f

--- src/zm.h
+++ src/zm.h
@@ -1,6 +1,6 @@
 //
 // ZoneMinder Core Interfaces, $Date$, $Revision$
-// Copyright (C) 2003, 2004, 2005, 2006  Philip Coombes
+// $copyright$
 // 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License

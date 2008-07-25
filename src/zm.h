0e88a85980ef4d958498fcef95c9965ad1b0c44f
stan <stan@e3e1d417-86f3-4887-817a-d78f3d33393f>
2008-07-25 09:23:20 +0000

    Updated propeties
    git-svn-id: http://svn.zoneminder.com/svn/zm/trunk@2610 e3e1d417-86f3-4887-817a-d78f3d33393f

--- src/zm.h
+++ src/zm.h
@@ -1,6 +1,6 @@
 //
 // ZoneMinder Core Interfaces, $Date$, $Revision$
-// $copyright$
+// $Copyright$
 // 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License

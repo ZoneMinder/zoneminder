1dd9aa5ddf7061a650e1914ef33fc57c9098f5f2
stan <stan@e3e1d417-86f3-4887-817a-d78f3d33393f>
2008-10-14 12:14:14 +0000

    Fixed typo in zmdc.pl
    git-svn-id: http://svn.zoneminder.com/svn/zm/trunk@2663 e3e1d417-86f3-4887-817a-d78f3d33393f

--- scripts/zmdc.pl
+++ scripts/zmdc.pl
@@ -560,7 +560,7 @@ if ( !$server_up )
 
 				if ( $process->{keepalive} )
 				{
-					if ( !$process->{delay} || ($process->{runtime} > ZM_MAX_RESTART_DELAY)) )
+					if ( !$process->{delay} || ($process->{runtime} > ZM_MAX_RESTART_DELAY) )
 					{
 						#start( $process->{daemon}, @{$process->{args}} );
 						# Schedule for immediate restart

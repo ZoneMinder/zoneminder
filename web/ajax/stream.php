b8781904ca7addf1c274affed2ca1142d926ae40
viseon <viseon@e3e1d417-86f3-4887-817a-d78f3d33393f>
2008-12-05 23:42:35 +0000

    Baselined rates and scales to base, replay_rate now short int.
    git-svn-id: http://svn.zoneminder.com/svn/zm/trunk@2706 e3e1d417-86f3-4887-817a-d78f3d33393f

--- web/ajax/stream.php
+++ web/ajax/stream.php
@@ -22,26 +22,26 @@ switch ( $_REQUEST['command'] )
 {
     case CMD_VARPLAY :
         //error_log( "Varplaying to ".$_REQUEST['rate'] );
-        $msg =  pack( "lcc", MSG_CMD, $_REQUEST['command'], $_REQUEST['rate'] );
+        $msg = pack( "lcn", MSG_CMD, $_REQUEST['command'], $_REQUEST['rate']-32768 );
         break;
     case CMD_ZOOMIN :
         //error_log( "Zooming to ".$_REQUEST['x'].",".$_REQUEST['y'] );
-        $msg =  pack( "lcnn", MSG_CMD, $_REQUEST['command'], $_REQUEST['x'], $_REQUEST['y'] );
+        $msg = pack( "lcnn", MSG_CMD, $_REQUEST['command'], $_REQUEST['x'], $_REQUEST['y'] );
         break;
     case CMD_PAN :
         //error_log( "Panning to ".$_REQUEST['x'].",".$_REQUEST['y'] );
-        $msg =  pack( "lcnn", MSG_CMD, $_REQUEST['command'], $_REQUEST['x'], $_REQUEST['y'] );
+        $msg = pack( "lcnn", MSG_CMD, $_REQUEST['command'], $_REQUEST['x'], $_REQUEST['y'] );
         break;
     case CMD_SCALE :
         //error_log( "Scaling to ".$_REQUEST['scale'] );
-        $msg =  pack( "lcn", MSG_CMD, $_REQUEST['command'], $_REQUEST['scale'] );
+        $msg = pack( "lcn", MSG_CMD, $_REQUEST['command'], $_REQUEST['scale'] );
         break;
     case CMD_SEEK :
         //error_log( "Seeking to ".$_REQUEST['offset'] );
-        $msg =  pack( "lcN", MSG_CMD, $_REQUEST['command'], $_REQUEST['offset'] );
+        $msg = pack( "lcN", MSG_CMD, $_REQUEST['command'], $_REQUEST['offset'] );
         break;
     default :
-        $msg =  pack( "lc", MSG_CMD, $_REQUEST['command'] );
+        $msg = pack( "lc", MSG_CMD, $_REQUEST['command'] );
         break;
 }
 
@@ -93,9 +93,9 @@ switch ( $data['type'] )
     {
         $data =  unpack( "ltype/imonitor/dfps/istate/ilevel/Cdelayed/Cpaused/C/C/irate/ddelay/izoom/Cenabled/Cforced", $msg );
         $data['fps'] = sprintf( "%.2f", $data['fps'] );
-        $data['rate'] /= 100;
+        $data['rate'] /= RATE_BASE;
         $data['delay'] = sprintf( "%.2f", $data['delay'] );
-        $data['zoom'] = sprintf( "%.1f", $data['zoom']/100 );
+        $data['zoom'] = sprintf( "%.1f", $data['zoom']/SCALE_BASE );
         ajaxResponse( array( 'status'=>$data ) );
         break;
     }
@@ -103,8 +103,8 @@ switch ( $data['type'] )
     {
         $data =  unpack( "ltype/ievent/Cpaused/C/C/C/iprogress/irate/izoom", $msg );
         //$data['progress'] = sprintf( "%.2f", $data['progress'] );
-        $data['rate'] /= 100;
-        $data['zoom'] = sprintf( "%.1f", $data['zoom']/100 );
+        $data['rate'] /= RATE_BASE;
+        $data['zoom'] = sprintf( "%.1f", $data['zoom']/SCALE_BASE );
         ajaxResponse( array( 'status'=>$data ) );
         break;
     }

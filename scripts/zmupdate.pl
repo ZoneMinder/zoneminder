a018876744c017be807164e19614c1aa3b5b64c8
stan <stan@e3e1d417-86f3-4887-817a-d78f3d33393f>
2009-02-23 20:54:36 +0000

    Updated to version 1.24.1
    git-svn-id: http://svn.zoneminder.com/svn/zm/trunk@2788 e3e1d417-86f3-4887-817a-d78f3d33393f

--- scripts/zmupdate.pl
+++ scripts/zmupdate.pl
@@ -861,6 +861,12 @@ if ( $version )
         patchDB( $dbh, "1.23.3" );
         $cascade = !undef;
     }
+    if ( $cascade || $version eq "1.24.0" )
+    {
+        # Patch the database
+        patchDB( $dbh, "1.24.0" );
+        $cascade = !undef;
+    }
     if ( $cascade )
     {
         my $installed_version = ZM_VERSION;

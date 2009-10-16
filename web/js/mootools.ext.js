c2e251405e87833cfd91b3fc4496288d10cf57a3
stan <stan@e3e1d417-86f3-4887-817a-d78f3d33393f>
2009-10-16 17:11:29 +0000

    Tweaked request timeout handling
    git-svn-id: http://svn.zoneminder.com/svn/zm/trunk@2985 e3e1d417-86f3-4887-817a-d78f3d33393f

--- web/js/mootools.ext.js
+++ web/js/mootools.ext.js
@@ -20,6 +20,8 @@ else
         alert( "MooTools version "+MooTools.version+" found.\nVersion "+zmMooToolsVersion+" required, please upgrade." );
     }
 
+    var requestTimeoutCount = 0;
+    var debugRequestTimeouts = false;
     /*
      * Ajax class extention to allow for request timeouts
      */
@@ -37,27 +39,51 @@ else
                 if ( this.timeoutTimer )
                     this.removeTimer();
                 this.timeoutTimer = this.onTimeout.delay( this.options.timeout, this );
+                if ( debugRequestTimeouts )
+                    console.log( "Setting timer "+this.timeoutTimer+", "+data+", "+requestTimeoutCount+" running" );
+                requestTimeoutCount++;
                 this.addEvent( 'onComplete', this.removeTimer );
             }
             this.previous( data );
             return( this );
         },
 
+        cancel: function()
+        {
+            if ( debugRequestTimeouts )
+                console.log( "Cancelling timer "+this.timeoutTimer );
+            if ( debugRequestTimeouts )
+                console.log( "Running "+this.running );
+            this.previous();
+            this.removeTimer();
+            return( this );
+        },
+
         timeout: function()
         {
             this.onTimeout();
+            return( this );
         },
 
         onTimeout: function ()
         {
+            if ( debugRequestTimeouts )
+                console.log( "Timer "+this.timeoutTimer+" timed out" );
             this.cancel();
             this.fireEvent('complete').fireEvent('failure').fireEvent('timeout');
+            return( this );
         },
 
         removeTimer: function()
         {
-            $clear( this.timeoutTimer );
-            this.timeoutTimer = null;
+            if ( this.timeoutTimer )
+            {
+                requestTimeoutCount--;
+                if ( debugRequestTimeouts )
+                    console.log( "Clearing timer "+this.timeoutTimer+", "+requestTimeoutCount+" running" );
+                $clear( this.timeoutTimer );
+                this.timeoutTimer = null;
+            }
             return( this );
         }
     });

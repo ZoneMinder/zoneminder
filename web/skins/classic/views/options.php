79e24aef444294805ac478aea518d0d1bdb8c2d0
stagecoach <stagecoach@e3e1d417-86f3-4887-817a-d78f3d33393f>
2010-11-24 01:35:12 +0000

    updated /web/skins/classic/views/options.php: added tab for skin selection updated /web/lang/en_gb.php: added Display entry and SkinDescription entry
    git-svn-id: http://svn.zoneminder.com/svn/zm/trunk@3206 e3e1d417-86f3-4887-817a-d78f3d33393f

--- web/skins/classic/views/options.php
+++ web/skins/classic/views/options.php
@@ -27,6 +27,7 @@ if ( !canView( 'System' ) )
 $canEdit = canEdit( 'System' );
 
 $tabs = array();
+$tabs['skins'] = $SLANG['Display']; // change me to be supported by SLANG...
 $tabs['system'] = $SLANG['System'];
 $tabs['config'] = $SLANG['Config'];
 $tabs['paths'] = $SLANG['Paths'];
@@ -81,7 +82,42 @@ foreach ( $tabs as $name=>$value )
       </ul>
       <div class="clear"></div>
 <?php 
-if ( $tab == "users" )
+if($tab == 'skins') {
+	$current_skin = $_COOKIE['zmSkin'];
+	if (isset($_GET['skin-choice'])) {
+		setcookie('zmSkin',$_GET['skin-choice']);
+		//header("Location: index.php?view=options&tab=skins&reset_parent=1");
+		echo '<script type="text/javascript">window.opener.location.reload();window.location.reload();</script>';
+	}
+
+?>
+	<form name="optionsForm" method="get" action="<?= $_SERVER['PHP_SELF'] ?>">
+        <input type="hidden" name="view" value="<?= $view ?>"/>
+        <input type="hidden" name="tab" value="<?= $tab ?>"/>
+		<table class="contentTable major optionTable" cellspacing="0">
+			<thead><tr><th><?= $SLANG['Name'] ?></th><th><?= $SLANG['Description'] ?></th> <th><?= $SLANG['Value'] ?></th></tr></thead>
+			<tbody>
+			<td>ZM_SKIN</td>
+			<td><?php echo $SLANG['SkinDescription']; ?></td>
+			<td><select name="skin-choice">
+				<?php
+					foreach(glob('skins/*',GLOB_ONLYDIR) as $dir) {
+						$dir = basename($dir);
+						echo '<option value="'.$dir.'" '.($current_skin==$dir ? 'SELECTED' : '').'>'.$dir.'</option>';
+					}
+				?>
+				</select>
+			</td>
+		</table>
+        <div id="contentButtons">
+          <input type="submit" value="<?= $SLANG['Save'] ?>"<?= $canEdit?'':' disabled="disabled"' ?>/>
+		  <input type="button" value="<?= $SLANG['Cancel'] ?>" onclick="closeWindow();"/>
+        </div>
+     </form>
+	
+      <?php
+}
+elseif ( $tab == "users" )
 {
 ?>
       <form name="userForm" method="post" action="<?= $_SERVER['PHP_SELF'] ?>">

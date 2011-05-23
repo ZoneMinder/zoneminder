58c77d2b40b3b0086f2d3d21cbf6d1a72bb9ddcf
stan <stan@e3e1d417-86f3-4887-817a-d78f3d33393f>
2011-05-23 16:19:12 +0000

    Updates for version 1.24.4
    git-svn-id: http://svn.zoneminder.com/svn/zm/trunk@3358 e3e1d417-86f3-4887-817a-d78f3d33393f

---
+++ db/zm_update-1.24.3.sql
@@ -0,0 +1,13 @@
+--
+-- This updates a 1.24.3 database to the next version
+--
+
+--
+-- These are optional, but we might as well do it now
+--
+optimize table Frames;
+optimize table Events;
+optimize table Filters;
+optimize table Zones;
+optimize table Monitors;
+optimize table Stats;

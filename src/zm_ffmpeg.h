02831ddcb17c7246256a3a8352e8452d2539055a
stan <stan@e3e1d417-86f3-4887-817a-d78f3d33393f>
2011-06-24 16:05:06 +0000

    Removed support for old locations of ffmpeg headers
    git-svn-id: http://svn.zoneminder.com/svn/zm/trunk@3468 e3e1d417-86f3-4887-817a-d78f3d33393f

--- src/zm_ffmpeg.h
+++ src/zm_ffmpeg.h
@@ -27,32 +27,16 @@ extern "C" {
 #endif
 #if HAVE_LIBAVUTIL_AVUTIL_H
 #include <libavutil/avutil.h>
-#elif HAVE_FFMPEG_AVUTIL_H
-#include <ffmpeg/avutil.h>
-#else
-#error "No location for avutils.h found"
 #endif
 #if HAVE_LIBAVCODEC_AVCODEC_H
 #include <libavcodec/avcodec.h>
-#elif HAVE_FFMPEG_AVCODEC_H
-#include <ffmpeg/avcodec.h>
-#else
-#error "No location for avcodec.h found"
 #endif
 #if HAVE_LIBAVFORMAT_AVFORMAT_H
 #include <libavformat/avformat.h>
-#elif HAVE_FFMPEG_AVFORMAT_H
-#include <ffmpeg/avformat.h>
-#else
-#error "No location for avformat.h found"
 #endif
 #if HAVE_LIBSWSCALE
 #if HAVE_LIBSWSCALE_SWSCALE_H
 #include <libswscale/swscale.h>
-#elif HAVE_FFMPEG_SWSCALE_H
-#include <ffmpeg/swscale.h>
-#else
-#error "No location for swscale.h found"
 #endif
 #endif // HAVE_LIBSWSCALE
 #ifdef __cplusplus

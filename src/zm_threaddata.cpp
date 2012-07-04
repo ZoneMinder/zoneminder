f9022778f7e3e6eeab617729e104ce032b44c509
stan <stan@e3e1d417-86f3-4887-817a-d78f3d33393f>
2012-07-04 09:45:54 +0000

    Fixed template instantiation
    git-svn-id: http://svn.zoneminder.com/svn/zm/trunk@3661 e3e1d417-86f3-4887-817a-d78f3d33393f

--- src/zm_threaddata.cpp
+++ src/zm_threaddata.cpp
@@ -17,19 +17,5 @@
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 // 
 
-void neverCalled()
-{
-    ThreadData<bool> dummy1;
-    dummy1.setValue( false );
-    dummy1.getUpdatedValue();
-    dummy1.getUpdatedValue( 1 );
-    dummy1.getUpdatedValue( 0.1 );
-    dummy1.updateValueSignal( true );
-    dummy1.updateValueBroadcast( true );
-
-    ThreadData<int> dummy2;
-    dummy2.getValue();
-    dummy2.setValue( 1 );
-    dummy2.getUpdatedValue( 1 );
-    dummy2.updateValueBroadcast( true );
-}
+template class ThreadData<bool>;
+template class ThreadData<int>;

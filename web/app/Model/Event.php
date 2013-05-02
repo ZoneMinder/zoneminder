e782fbe38d369e75e369674a2797548426abc36d
Kyle Johnson <kjohnson@gnulnx.net>
2013-05-02 08:55:59 -0400

    Defined associations between the Monitor and Event models

--- web/app/Model/Event.php
+++ web/app/Model/Event.php
@@ -1,5 +1,11 @@
 <?php
 class Event extends AppModel {
   public $useTable = 'Events';
+  public $belongsTo = array(
+	'Monitor' => array(
+		'className' => 'Monitor',
+		'foreignKey' => 'MonitorId'
+	)
+  );
 }
 ?>
--- web/app/Model/Monitor.php
+++ web/app/Model/Monitor.php
@@ -1,5 +1,11 @@
 <?php
   class Monitor extends AppModel {
     public $useTable = 'Monitors';
+    public $hasMany = array(
+	'Event' => array(
+		'className' => 'Event',
+		'foreignKey' => 'MonitorId'
+	)
+    );
   }
 ?>

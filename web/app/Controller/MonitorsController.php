cff58441fc258ff5761a172e9564c8b799f0ba08
Kyle Johnson <kjohnson@gnulnx.net>
2013-05-02 21:31:05 -0400

    Fixed some indents

--- web/app/Controller/MonitorsController.php
+++ web/app/Controller/MonitorsController.php
@@ -1,5 +1,4 @@
 <?php
-
 	class MonitorsController extends AppController {
 		public $helpers = array('Html', 'Form');
   
@@ -19,30 +18,30 @@
 			$this->set('monitor', $monitor);
 		}
 
-public function edit($id = null) {
-    if (!$id) {
-        throw new NotFoundException(__('Invalid monitor'));
-    }
-
-    $monitor = $this->Monitor->findById($id);
-    if (!$monitor) {
-        throw new NotFoundException(__('Invalid monitor'));
-    }
-
-    if ($this->request->is('put') || $this->request->is('post')) {
-        $this->Monitor->id = $id;
-        if ($this->Monitor->save($this->request->data)) {
-            $this->Session->setFlash('Your monitor has been updated.');
-            $this->redirect(array('action' => 'index'));
-        } else {
-            $this->Session->setFlash('Unable to update your monitor.');
-        }
-    }
-
-    if (!$this->request->data) {
-        $this->request->data = $monitor;
-    }
-}
+		public function edit($id = null) {
+	    if (!$id) {
+	        throw new NotFoundException(__('Invalid monitor'));
+	    }
+	
+	    $monitor = $this->Monitor->findById($id);
+	    if (!$monitor) {
+	        throw new NotFoundException(__('Invalid monitor'));
+	    }
+	
+	    if ($this->request->is('put') || $this->request->is('post')) {
+	        $this->Monitor->id = $id;
+	        if ($this->Monitor->save($this->request->data)) {
+	            $this->Session->setFlash('Your monitor has been updated.');
+	            $this->redirect(array('action' => 'index'));
+	        } else {
+	            $this->Session->setFlash('Unable to update your monitor.');
+	        }
+	    }
+	
+	    if (!$this->request->data) {
+	        $this->request->data = $monitor;
+	    }
+		}
 
 	}
 

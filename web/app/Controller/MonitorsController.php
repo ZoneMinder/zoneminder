39e15b48a15eb2ef10abfd3407dc8629a8bb5b2d
Kyle Johnson <kjohnson@gnulnx.net>
2013-05-14 22:52:47 -0400

    Replaced use of querybuilder functionality via find() with prebuilt queries via query().  This fixes #1.

--- web/app/Controller/MonitorsController.php
+++ web/app/Controller/MonitorsController.php
@@ -13,31 +13,11 @@
 				}
 			}
 
-			$this->set('eventsLastHour', $this->Event->find('all', array(
-				'fields' => 'COUNT(Event.Id) AS count',
-				'group' => 'Event.MonitorId',
-				'conditions' => 'Event.StartTime > DATE_SUB(NOW(), INTERVAL 1 HOUR)'
-			)));
-			$this->set('eventsLastDay', $this->Event->find('all', array(
-				'fields' => 'COUNT(Event.Id) AS count',
-				'group' => 'Event.MonitorId',
-				'conditions' => 'Event.StartTime > DATE_SUB(NOW(), INTERVAL 1 DAY)'
-			)));
-			$this->set('eventsLastWeek', $this->Event->find('all', array(
-				'fields' => 'COUNT(Event.Id) AS count',
-				'group' => 'Event.MonitorId',
-				'conditions' => 'Event.StartTime > DATE_SUB(NOW(), INTERVAL 1 WEEK)'
-			)));
-			$this->set('eventsLastMonth', $this->Event->find('all', array(
-				'fields' => 'COUNT(Event.Id) AS count',
-				'group' => 'Event.MonitorId',
-				'conditions' => 'Event.StartTime > DATE_SUB(NOW(), INTERVAL 1 MONTH)'
-			)));
-			$this->set('eventsArchived', $this->Event->find('all', array(
-				'fields' => 'COUNT(Event.Id) AS count',
-				'group' => 'Event.MonitorId',
-				'conditions' => array('Event.Archived' => 1)
-			)));
+			$this->set('eventsLastHour', $this->Monitor->query('SELECT COUNT(Event.Id) AS count FROM Monitors AS Monitor LEFT JOIN Events as Event ON Monitor.Id = Event.MonitorId AND Event.StartTime > DATE_SUB(NOW(), INTERVAL 1 HOUR) GROUP BY Monitor.Id'));
+			$this->set('eventsLastDay', $this->Monitor->query('SELECT COUNT(Event.Id) AS count FROM Monitors AS Monitor LEFT JOIN Events as Event ON Monitor.Id = Event.MonitorId AND Event.StartTime > DATE_SUB(NOW(), INTERVAL 1 DAY) GROUP BY Monitor.Id'));
+			$this->set('eventsLastWeek', $this->Monitor->query('SELECT COUNT(Event.Id) AS count FROM Monitors AS Monitor LEFT JOIN Events as Event ON Monitor.Id = Event.MonitorId AND Event.StartTime > DATE_SUB(NOW(), INTERVAL 1 WEEK) GROUP BY Monitor.Id'));
+			$this->set('eventsLastMonth', $this->Monitor->query('SELECT COUNT(Event.Id) AS count FROM Monitors AS Monitor LEFT JOIN Events as Event ON Monitor.Id = Event.MonitorId AND Event.StartTime > DATE_SUB(NOW(), INTERVAL 1 MONTH) GROUP BY Monitor.Id'));
+			$this->set('eventsArchived', $this->Monitor->query('SELECT COUNT(Event.Id) AS count FROM Monitors AS Monitor LEFT JOIN Events as Event ON Monitor.Id = Event.MonitorId AND Event.Archived = 1 GROUP BY Monitor.Id'));
 
 		}
 

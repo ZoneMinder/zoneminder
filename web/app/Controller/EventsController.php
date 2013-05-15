0d004fb58894b698a953ac81f1fc7c789f279cca
Kyle Johnson <kjohnson@gnulnx.net>
2013-05-15 17:51:53 -0400

    Changed some minor indents

--- web/app/Controller/EventsController.php
+++ web/app/Controller/EventsController.php
@@ -4,12 +4,12 @@ class EventsController extends AppController {
     public $helpers = array('Paginator');
     public $components = array('Paginator');
 
+public function index() {
 	$this->loadModel('Monitor');
 	$this->loadModel('Config');
 
 	$events_per_page = $this->Config->find('first', array('conditions' => array('Name' => 'ZM_WEB_EVENTS_PER_PAGE'), 'fields' => 'Value'));
 
-    public function index() {
 	$this->paginate = array(
 		'fields' => array('Event.Name', 'Event.Length', 'Event.MonitorId', 'Event.Id', 'Monitor.Name'),
 		'limit' => $events_per_page['Config']['Value'],
@@ -19,7 +19,6 @@ class EventsController extends AppController {
 	$this->set('events', $data);
 	$options = array('fields' => array('DISTINCT Monitor.Id'));
 	$this->set('monitors', $this->Event->Monitor->Find('all', $options));
-    }
 
 	$this->set('monitors', $this->Monitor->find('all', array('fields' => array('Monitor.Name') )));
 
@@ -28,6 +27,7 @@ class EventsController extends AppController {
 	$this->set('eventsLastWeek', $this->Monitor->query('SELECT COUNT(Event.Id) AS count FROM Monitors AS Monitor LEFT JOIN Events as Event ON Monitor.Id = Event.MonitorId AND Event.StartTime > DATE_SUB(NOW(), INTERVAL 1 WEEK) GROUP BY Monitor.Id'));
 	$this->set('eventsLastMonth', $this->Monitor->query('SELECT COUNT(Event.Id) AS count FROM Monitors AS Monitor LEFT JOIN Events as Event ON Monitor.Id = Event.MonitorId AND Event.StartTime > DATE_SUB(NOW(), INTERVAL 1 MONTH) GROUP BY Monitor.Id'));
 	$this->set('eventsArchived', $this->Monitor->query('SELECT COUNT(Event.Id) AS count FROM Monitors AS Monitor LEFT JOIN Events as Event ON Monitor.Id = Event.MonitorId AND Event.Archived = 1 GROUP BY Monitor.Id'));
+}
 
   public function view($id = null) {
     if (!$id) {

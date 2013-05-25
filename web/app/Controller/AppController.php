07ac7aaeb4f4ae5d134193844f5a0ade2e196e07
Kyle Johnson <kjohnson@gnulnx.net>
2013-05-25 11:44:53 -0400

    Use the jQuery javascript engine

--- web/app/Controller/AppController.php
+++ web/app/Controller/AppController.php
@@ -32,7 +32,7 @@ App::uses('Controller', 'Controller');
  * @link		http://book.cakephp.org/2.0/en/controllers.html#the-app-controller
  */
 class AppController extends Controller {
-  public $helpers = array('Html', 'Form');
+  public $helpers = array('Html', 'Form', 'Js' => array('jQuery'));
   public $components = array('Cookie', 'Session');
 
   public function beforeFilter() {

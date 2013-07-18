ad0aaa6e93ec142e208c1351b98db1f50d96f0ec
Kyle Johnson <kjohnson@gnulnx.net>
2013-07-18 08:08:07 -0400

    Initial commit of Zone MVC

--- web/app/Model/Zone.php
+++ web/app/Model/Zone.php
@@ -1,11 +1,11 @@
 <?php
 class Zone extends AppModel {
-  public $useTable = 'Zones';
-  public $belongsTo = array(
-	'Monitor' => array(
-		'className' => 'Monitor',
-		'foreignKey' => 'MonitorId'
-	)
-  );
+	public $useTable = 'Zones';
+	public $primaryKey = 'Id';
+	public $belongsTo = array(
+		'Monitor' => array(
+			'className' => 'Monitor',
+			'foreignKey' => 'MonitorId')
+		);
 }
 ?>

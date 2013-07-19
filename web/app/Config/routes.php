f1d9de75b06e7f848ed9a6c8f88faad6a53b1b54
Kyle Johnson <kjohnson@gnulnx.net>
2013-07-19 09:30:08 -0400

    Point root to the Monitors index action

--- web/app/Config/routes.php
+++ web/app/Config/routes.php
@@ -26,7 +26,7 @@
  * its action called 'display', and we pass a param to select the view file
  * to use (in this case, /app/View/Pages/home.ctp)...
  */
-	Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
+	Router::connect('/', array('controller' => 'Monitors', 'action' => 'index'));
 /**
  * ...and connect the rest of 'Pages' controller's urls.
  */

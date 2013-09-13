e42afcd76a462421a3335cb5a3dc40a8a047f6e5
Kyle Johnson <kjohnson@gnulnx.net>
2013-09-12 20:01:31 -0400

    Load all available CakePHP plugins

--- web/app/Config/bootstrap.php
+++ web/app/Config/bootstrap.php
@@ -26,6 +26,7 @@
 
 // Setup a 'default' cache configuration for use in the application.
 Cache::config('default', array('engine' => 'File'));
+CakePlugin::loadAll();
 
 /**
  * The settings below can be used to set additional paths to models, views and controllers.

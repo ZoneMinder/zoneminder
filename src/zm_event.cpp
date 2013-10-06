ZoneMinder/zoneminder

commit e12a9dc4083a47d43e5ffb531ec2a3903bd82938

Fixed compilation failure when zlib is not available

src/zm_event.cpp
@@ -1257,14 +1257,18 @@ bool EventStream::sendFrame( int delta_us )
                 case STREAM_JPEG :
                     send_image->EncodeJpeg( img_buffer, &img_buffer_size );
                     break;
-                case STREAM_RAW :
-                    img_buffer = (uint8_t*)(send_image->Buffer());
-                    img_buffer_size = send_image->Size();
-                    break;
                 case STREAM_ZIP :
+#if HAVE_ZLIB_H
                     unsigned long zip_buffer_size;
                     send_image->Zip( img_buffer, &zip_buffer_size );
                     img_buffer_size = zip_buffer_size;
+                    break;
+#else
+		    Error("zlib is required for zipped images. Falling back to raw image");
+#endif // HAVE_ZLIB_H
+                case STREAM_RAW :
+                    img_buffer = (uint8_t*)(send_image->Buffer());
+                    img_buffer_size = send_image->Size();
                     break;
                 default:
                     Fatal( "Unexpected frame type %d", type );

commit a9e886aea2912d401f07ca2d5eba2cfaa497fa53

Fix small zipped images bug introduced by commit e12a9dc408

src/zm_event.cpp
@@ -1264,7 +1264,8 @@ bool EventStream::sendFrame( int delta_us )
                     img_buffer_size = zip_buffer_size;
                     break;
 #else
-		    Error("zlib is required for zipped images. Falling back to raw image");
+                    Error("zlib is required for zipped images. Falling back to raw image");
+                    type = STREAM_RAW;
 #endif // HAVE_ZLIB_H
                 case STREAM_RAW :
                     img_buffer = (uint8_t*)(send_image->Buffer());
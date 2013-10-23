9fecfb45c8cbcba9d1483824044effb3f2355bbf
Kyle Johnson <kjohnson@gnulnx.net>
2013-10-23 13:52:58 -0400

    Check for and error on invalid or non-exsistent zones

--- web/app/Controller/ZonesController.php
+++ web/app/Controller/ZonesController.php
@@ -8,7 +8,15 @@ class ZonesController extends AppController {
     }
 
 	public function edit($zid = null) {
+		if (!$zid) {
+			throw new NotFoundException(__('Invalid Zone'));
+		}
+
 		$zone = $this->Zone->findById($zid);
+		if (!$zone) {
+			throw new NotFoundException(__('Invalid Zone'));
+		}
+
 		$mid = $zone['Monitor']['Id'];
 		$this->Zone->createSnapshot($mid, $zid);
 		$this->set('zoneImage', '/img'.'/Zones'.$mid.'.jpg?'.time());

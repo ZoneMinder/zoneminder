3fcb7e9ff5ec9ebc60d3501b12c7e40ead93b480
Kyle Johnson <kjohnson@gnulnx.net>
2013-10-23 21:39:38 -0400

    Made some of the zone.js code "better", also draw existing zones

--- web/app/webroot/js/zone.js
+++ web/app/webroot/js/zone.js
@@ -5,16 +5,43 @@ $(document).ready(function()
     oCanvas = $('#c1').offset(),
     ctx = canvas.getContext("2d"),
     zone = document.getElementById("imgZone"),
-    vertices = [];
+    vertices = [], coords = $("#ZoneCoords").val().split(" ");
 
+  window.onload = function() {
+    drawMonitor();
+  }
 
   function drawMonitor() {
     canvas.width = zone.width;
     canvas.height = zone.height;
     ctx.drawImage(zone, 0, 0);
+
+    if (coords) {
+      drawZone(coords);
+    }
   }
 
-  function drawZone(vertex) {
+  function drawZone(coords) {
+    var length = coords.length;
+    for (var i = 0; i < length; i++) {
+      var coord = coords[i].split(",");
+      var left = coord[0];
+      var top = coord[1];
+      vertices.push([left, top]);
+      drawVertex(vertices[vertices.length - 1]); // Draw the most recent click
+    }
+    finishZone();
+  }
+
+  function finishZone() {
+    ctx.lineTo(vertices[0][0], vertices[0][1]);
+    ctx.fillStyle = "rgba(158, 217, 50, 0.5)";
+    ctx.fill();
+    ctx.stroke();
+    vertices = [];
+  }
+
+  function drawVertex(vertex) {
     if (vertices.length > 1) {
       ctx.lineTo(vertex[0], vertex[1]);
     } else {
@@ -24,19 +51,13 @@ $(document).ready(function()
     ctx.stroke();
   }
 
-  drawMonitor();
-
   $(canvas).click(function(e){
-    vertices.push([e.pageX - oCanvas.left, e.pageY - oCanvas.top]);
-    drawZone(vertices[vertices.length - 1]);
+    vertices.push([e.pageX - oCanvas.left, e.pageY - oCanvas.top]); // Add the most recent click to the vertices array
+    drawVertex(vertices[vertices.length - 1]); // Draw the most recent click
   });
 
   $("#done").click(function() {
-    ctx.lineTo(vertices[0][0], vertices[0][1]);
-    ctx.fillStyle = "rgba(158, 217, 50, 0.5)";
-    ctx.fill();
-    ctx.stroke();
-    vertices = [];
+    finishZone();
   });
 
   $("#reset").click(function() {

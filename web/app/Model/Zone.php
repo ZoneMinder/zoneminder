82f0d0ed8b7fcdeafb96e1556cce18c68caea8fb
Kyle Johnson <kjohnson@gnulnx.net>
2013-11-01 16:03:44 -0400

    Save the zones image to the cakephp webroot

--- web/app/Model/Zone.php
+++ web/app/Model/Zone.php
@@ -9,7 +9,8 @@ class Zone extends AppModel {
 		);
 
 	public function createSnapshot($mid = null, $zid = null) {
-		chdir(Configure::read('ZM_PATH_WEB') . '/' . Configure::read('ZM_DIR_IMAGES'));
+		# chdir(Configure::read('ZM_PATH_WEB') . '/' . Configure::read('ZM_DIR_IMAGES'));
+		chdir(WWW_ROOT . '/img');
 		$command = Configure::read('ZM_PATH_BIN');
                 $command .= "/zmu -m $mid -z$zid";
 		exec( escapeshellcmd( $command ), $output, $status );

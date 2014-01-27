ab923ee96055838aadd374475d2e1dfaa44124c9
Kyle Johnson <kjohnson@gnulnx.net>
2014-01-27 10:20:20 -0500

    Added lock and alter perms for the zm user to docker start script

--- start.sh
+++ start.sh
@@ -8,7 +8,7 @@ sleep 5
 mysql -u root < db/zm_create.sql
 
 # Add the ZoneMinder DB user
-mysql -u root -e "grant insert,select,update,delete on zm.* to 'zm'@'localhost' identified by 'zm'"
+mysql -u root -e "grant insert,select,update,delete,lock tables,alter on zm.* to 'zm'@'localhost' identified by 'zm'"
 
 # Install the ZoneMinder apache vhost file
 wget --quiet https://raw.github.com/kylejohnson/puppet-zoneminder/master/files/zoneminder -O /etc/apache2/sites-enabled/000-default

9ce51755ff2dc9ef2d4f5e3349d5ea8de093789e
m-bene <hub@bene.priv.at>
2014-03-20 10:26:48 +0100

    add ExtendAlarmFrames Fields to Zones and ZonePresets

--- db/zm_update-1.27.1.sql
+++ db/zm_update-1.27.1.sql
@@ -7,3 +7,8 @@
 --
 
 INSERT INTO Controls VALUES (NULL,'WanscamPT','Remote','Wanscam',1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,16,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,16,0,0,0,0,0,1,16,1,1,1,1,0,0,0,1,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0);
+
+-- Add extend alarm frame count to zone definition and Presets
+ALTER TABLE `Zones` ADD `ExtendAlarmFrames` smallint(5) unsigned not null default 0 AFTER `OverloadFrames`;
+ALTER TABLE `ZonePresets` ADD `ExtendAlarmFrames` smallint(5) unsigned not null default 0 AFTER `OverloadFrames`;
+

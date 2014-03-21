9b935fd7670551414a96e60325a788c639c25a94
Sune1337 <root@zm01.medialaget.se>
2014-03-21 17:50:49 +0100

    - Timeout when opening stream after 10 seconds Sometimes when restarting the camera ffmpeg hung itself in some state, when calling avformat_open_input, which seemed to last forever. - Reopen stream if av_read_frame returns EOF Sometimes ffmpeg starts returning an EOF error when calling av_read_frame. Once this happens it seems no more images will ever be captured. - Reopen stream if av_read_frame returns -110 Means something like Connection failed; cant remember. Anyway. Once this happens it seems no more images will ever be captured.

--- src/zm_ffmpeg_camera.cpp
+++ src/zm_ffmpeg_camera.cpp
@@ -41,6 +41,10 @@ FfmpegCamera::FfmpegCamera( int p_id, const std::string &p_path, const std::stri
 	mRawFrame = NULL;
 	mFrame = NULL;
 	frameCount = 0;
+    mIsOpening = false;
+    mCanCapture = false;
+    mOpenStart = 0;
+    mOpenTimeout = 10;
 	
 #if HAVE_LIBSWSCALE    
 	mConvertContext = NULL;
@@ -63,31 +67,7 @@ FfmpegCamera::FfmpegCamera( int p_id, const std::string &p_path, const std::stri
 
 FfmpegCamera::~FfmpegCamera()
 {
-    av_freep( &mFrame );
-    av_freep( &mRawFrame );
-    
-#if HAVE_LIBSWSCALE
-    if ( mConvertContext )
-    {
-        sws_freeContext( mConvertContext );
-        mConvertContext = NULL;
-    }
-#endif
-
-    if ( mCodecContext )
-    {
-       avcodec_close( mCodecContext );
-       mCodecContext = NULL; // Freed by av_close_input_file
-    }
-    if ( mFormatContext )
-    {
-#if LIBAVFORMAT_VERSION_INT >= AV_VERSION_INT(53, 17, 0)
-        avformat_close_input( &mFormatContext );
-#else
-        av_close_input_file( mFormatContext );
-#endif
-        mFormatContext = NULL;
-    }
+    CloseFfmpeg();
 
 	if ( capture )
 	{
@@ -112,116 +92,11 @@ void FfmpegCamera::Terminate()
 int FfmpegCamera::PrimeCapture()
 {
     Info( "Priming capture from %s", mPath.c_str() );
-    
 
-
-    // Open the input, not necessarily a file
-#if LIBAVFORMAT_VERSION_INT < AV_VERSION_INT(53, 4, 0)
-    if ( av_open_input_file( &mFormatContext, mPath.c_str(), NULL, 0, NULL ) !=0 )
-#else
-    // Handle options
-    AVDictionary *opts = 0;
-    StringVector opVect = split(Options(), ",");
-    
-    // Set transport method as specified by method field, rtpUni is default
-    if ( Method() == "rtpMulti" )
-    	opVect.push_back("rtsp_transport=udp_multicast");
-    else if ( Method() == "rtpRtsp" )
-        opVect.push_back("rtsp_transport=tcp");
-    else if ( Method() == "rtpRtspHttp" )
-        opVect.push_back("rtsp_transport=http");
-    
-  	Debug(2, "Number of Options: %d",opVect.size());
-    for (size_t i=0; i<opVect.size(); i++)
-    {
-    	StringVector parts = split(opVect[i],"=");
-    	if (parts.size() > 1) {
-    		parts[0] = trimSpaces(parts[0]);
-    		parts[1] = trimSpaces(parts[1]);
-    	    if ( av_dict_set(&opts, parts[0].c_str(), parts[1].c_str(), 0) == 0 ) {
-    	        Debug(2, "set option %d '%s' to '%s'", i,  parts[0].c_str(), parts[1].c_str());
-    	    }
-    	    else
-    	    {
-    	        Warning( "Error trying to set option %d '%s' to '%s'", i, parts[0].c_str(), parts[1].c_str() );
-    	    }
-    		  
-    	}
+    if (OpenFfmpeg() != 0){
+        ReopenFfmpeg();
     }
-
-    if ( avformat_open_input( &mFormatContext, mPath.c_str(), NULL, &opts ) !=0 )
-#endif
-        Fatal( "Unable to open input %s due to: %s", mPath.c_str(), strerror(errno) );
-
-    // Locate stream info from input
-#if LIBAVFORMAT_VERSION_INT < AV_VERSION_INT(53, 4, 0)
-    if ( av_find_stream_info( mFormatContext ) < 0 )
-#else
-    if ( avformat_find_stream_info( mFormatContext, 0 ) < 0 )
-#endif
-        Fatal( "Unable to find stream info from %s due to: %s", mPath.c_str(), strerror(errno) );
-    
-    // Find first video stream present
-    mVideoStreamId = -1;
-    for (unsigned int i=0; i < mFormatContext->nb_streams; i++ )
-    {
-#if LIBAVUTIL_VERSION_INT >= AV_VERSION_INT(51,2,1)
-        if ( mFormatContext->streams[i]->codec->codec_type == AVMEDIA_TYPE_VIDEO )
-#else
-        if ( mFormatContext->streams[i]->codec->codec_type == CODEC_TYPE_VIDEO )
-#endif
-        {
-            mVideoStreamId = i;
-            break;
-        }
-    }
-    if ( mVideoStreamId == -1 )
-        Fatal( "Unable to locate video stream in %s", mPath.c_str() );
-
-    mCodecContext = mFormatContext->streams[mVideoStreamId]->codec;
-
-    // Try and get the codec from the codec context
-    if ( (mCodec = avcodec_find_decoder( mCodecContext->codec_id )) == NULL )
-        Fatal( "Can't find codec for video stream from %s", mPath.c_str() );
-
-    // Open the codec
-#if LIBAVFORMAT_VERSION_INT < AV_VERSION_INT(53, 7, 0)
-    if ( avcodec_open( mCodecContext, mCodec ) < 0 )
-#else
-    if ( avcodec_open2( mCodecContext, mCodec, 0 ) < 0 )
-#endif
-        Fatal( "Unable to open codec for video stream from %s", mPath.c_str() );
-
-    // Allocate space for the native video frame
-    mRawFrame = avcodec_alloc_frame();
-
-    // Allocate space for the converted video frame
-    mFrame = avcodec_alloc_frame();
-    
-	if(mRawFrame == NULL || mFrame == NULL)
-		Fatal( "Unable to allocate frame for %s", mPath.c_str() );
-	
-	int pSize = avpicture_get_size( imagePixFormat, width, height );
-	if( (unsigned int)pSize != imagesize) {
-		Fatal("Image size mismatch. Required: %d Available: %d",pSize,imagesize);
-	}
-	
-#if HAVE_LIBSWSCALE
-#if LIBSWSCALE_VERSION_INT >= AV_VERSION_INT(0, 8, 0)
-	if(!sws_isSupportedInput(mCodecContext->pix_fmt)) {
-		Fatal("swscale does not support the codec format: %c%c%c%c",(mCodecContext->pix_fmt)&0xff,((mCodecContext->pix_fmt>>8)&0xff),((mCodecContext->pix_fmt>>16)&0xff),((mCodecContext->pix_fmt>>24)&0xff));
-	}
-	
-	if(!sws_isSupportedOutput(imagePixFormat)) {
-		Fatal("swscale does not support the target format: %c%c%c%c",(imagePixFormat)&0xff,((imagePixFormat>>8)&0xff),((imagePixFormat>>16)&0xff),((imagePixFormat>>24)&0xff));
-	}
-#endif
-	
-#else // HAVE_LIBSWSCALE
-    Fatal( "You must compile ffmpeg with the --enable-swscale option to use ffmpeg cameras" );
-#endif // HAVE_LIBSWSCALE
-
-    return( 0 );
+    return 0;
 }
 
 int FfmpegCamera::PreCapture()
@@ -232,6 +107,10 @@ int FfmpegCamera::PreCapture()
 
 int FfmpegCamera::Capture( Image &image )
 {
+    if (!mCanCapture){
+        return -1;
+    }
+
 	AVPacket packet;
 	uint8_t* directbuffer;
    
@@ -248,7 +127,16 @@ int FfmpegCamera::Capture( Image &image )
         int avResult = av_read_frame( mFormatContext, &packet );
         if ( avResult < 0 )
         {
-            Error( "Unable to read packet from stream %d: error %d", packet.stream_index, avResult );
+            if (avResult == AVERROR_EOF || (mFormatContext->pb && mFormatContext->pb->eof_reached))
+            {
+                Info( "av_read_frame returned EOF. Reopening stream." );
+                ReopenFfmpeg();
+            } else if (avResult == -110) {
+                Info( "av_read_frame returned \"%s\". Reopening stream.", av_err2str(avResult)) ;
+                ReopenFfmpeg();
+            } else {
+                Error( "Unable to read packet from stream %d: error %d \"%s\".", packet.stream_index, avResult, av_err2str(avResult) );
+            }
             return( -1 );
         }
         Debug( 5, "Got packet from stream %d", packet.stream_index );
@@ -298,6 +186,238 @@ int FfmpegCamera::PostCapture()
 {
     // Nothing to do here
     return( 0 );
+}
+
+int FfmpegCamera::OpenFfmpeg() {
+
+    Debug ( 2, "OpenFfmpeg called." );
+
+    mOpenStart = time(NULL);
+    mIsOpening = true;
+
+    // Open the input, not necessarily a file
+#if LIBAVFORMAT_VERSION_INT < AV_VERSION_INT(53, 4, 0)
+    Debug ( 1, "Calling av_open_input_file" );
+    if ( av_open_input_file( &mFormatContext, mPath.c_str(), NULL, 0, NULL ) !=0 )
+#else
+	    // Handle options
+    AVDictionary *opts = 0;
+    StringVector opVect = split(Options(), ",");
+    
+    // Set transport method as specified by method field, rtpUni is default
+    if ( Method() == "rtpMulti" )
+    	opVect.push_back("rtsp_transport=udp_multicast");
+    else if ( Method() == "rtpRtsp" )
+        opVect.push_back("rtsp_transport=tcp");
+    else if ( Method() == "rtpRtspHttp" )
+        opVect.push_back("rtsp_transport=http");
+    
+  	Debug(2, "Number of Options: %d",opVect.size());
+    for (size_t i=0; i<opVect.size(); i++)
+    {
+    	StringVector parts = split(opVect[i],"=");
+    	if (parts.size() > 1) {
+    		parts[0] = trimSpaces(parts[0]);
+    		parts[1] = trimSpaces(parts[1]);
+    	    if ( av_dict_set(&opts, parts[0].c_str(), parts[1].c_str(), 0) == 0 ) {
+    	        Debug(2, "set option %d '%s' to '%s'", i,  parts[0].c_str(), parts[1].c_str());
+    	    }
+    	    else
+    	    {
+    	        Warning( "Error trying to set option %d '%s' to '%s'", i, parts[0].c_str(), parts[1].c_str() );
+    	    }
+    		  
+    	}
+    }    
+	Debug ( 1, "Calling avformat_open_input" );
+
+    mFormatContext = avformat_alloc_context( );
+    mFormatContext->interrupt_callback.callback = FfmpegInterruptCallback;
+    mFormatContext->interrupt_callback.opaque = this;
+
+    if ( avformat_open_input( &mFormatContext, mPath.c_str(), NULL, NULL ) !=0 )
+#endif
+    {
+        mIsOpening = false;
+        Error( "Unable to open input %s due to: %s", mPath.c_str(), strerror(errno) );
+        return -1;
+    }
+
+    mIsOpening = false;
+    Debug ( 1, "Opened input" );
+
+    // Locate stream info from avformat_open_input
+#if LIBAVFORMAT_VERSION_INT < AV_VERSION_INT(53, 4, 0)
+    Debug ( 1, "Calling av_find_stream_info" );
+    if ( av_find_stream_info( mFormatContext ) < 0 )
+#else
+    Debug ( 1, "Calling avformat_find_stream_info" );
+    if ( avformat_find_stream_info( mFormatContext, 0 ) < 0 )
+#endif
+        Fatal( "Unable to find stream info from %s due to: %s", mPath.c_str(), strerror(errno) );
+    
+    Debug ( 1, "Got stream info" );
+
+    // Find first video stream present
+    mVideoStreamId = -1;
+    for (unsigned int i=0; i < mFormatContext->nb_streams; i++ )
+    {
+#if LIBAVUTIL_VERSION_INT >= AV_VERSION_INT(51,2,1)
+        if ( mFormatContext->streams[i]->codec->codec_type == AVMEDIA_TYPE_VIDEO )
+#else
+        if ( mFormatContext->streams[i]->codec->codec_type == CODEC_TYPE_VIDEO )
+#endif
+        {
+            mVideoStreamId = i;
+            break;
+        }
+    }
+    if ( mVideoStreamId == -1 )
+        Fatal( "Unable to locate video stream in %s", mPath.c_str() );
+
+    Debug ( 1, "Found video stream" );
+
+    mCodecContext = mFormatContext->streams[mVideoStreamId]->codec;
+
+    // Try and get the codec from the codec context
+    if ( (mCodec = avcodec_find_decoder( mCodecContext->codec_id )) == NULL )
+        Fatal( "Can't find codec for video stream from %s", mPath.c_str() );
+
+    Debug ( 1, "Found decoder" );
+
+    // Open the codec
+#if LIBAVFORMAT_VERSION_INT < AV_VERSION_INT(53, 7, 0)
+    Debug ( 1, "Calling avcodec_open" );
+    if ( avcodec_open( mCodecContext, mCodec ) < 0 )
+#else
+    Debug ( 1, "Calling avcodec_open2" );
+    if ( avcodec_open2( mCodecContext, mCodec, 0 ) < 0 )
+#endif
+        Fatal( "Unable to open codec for video stream from %s", mPath.c_str() );
+
+    Debug ( 1, "Opened codec" );
+
+    // Allocate space for the native video frame
+    mRawFrame = avcodec_alloc_frame();
+
+    // Allocate space for the converted video frame
+    mFrame = avcodec_alloc_frame();
+    
+    if(mRawFrame == NULL || mFrame == NULL)
+        Fatal( "Unable to allocate frame for %s", mPath.c_str() );
+
+    Debug ( 1, "Allocated frames" );
+    
+    int pSize = avpicture_get_size( imagePixFormat, width, height );
+    if( (unsigned int)pSize != imagesize) {
+        Fatal("Image size mismatch. Required: %d Available: %d",pSize,imagesize);
+    }
+
+    Debug ( 1, "Validated imagesize" );
+    
+#if HAVE_LIBSWSCALE
+    Debug ( 1, "Calling sws_isSupportedInput" );
+    if(!sws_isSupportedInput(mCodecContext->pix_fmt)) {
+        Fatal("swscale does not support the codec format: %c%c%c%c",(mCodecContext->pix_fmt)&0xff,((mCodecContext->pix_fmt>>8)&0xff),((mCodecContext->pix_fmt>>16)&0xff),((mCodecContext->pix_fmt>>24)&0xff));
+    }
+    
+    if(!sws_isSupportedOutput(imagePixFormat)) {
+        Fatal("swscale does not support the target format: %c%c%c%c",(imagePixFormat)&0xff,((imagePixFormat>>8)&0xff),((imagePixFormat>>16)&0xff),((imagePixFormat>>24)&0xff));
+    }
+    
+#else // HAVE_LIBSWSCALE
+    Fatal( "You must compile ffmpeg with the --enable-swscale option to use ffmpeg cameras" );
+#endif // HAVE_LIBSWSCALE
+
+    mCanCapture = true;
+
+    return 0;
+}
+
+int FfmpegCamera::ReopenFfmpeg() {
+
+    Debug(2, "ReopenFfmpeg called.");
+
+    mCanCapture = false;
+    pthread_t thread1;
+    if (pthread_create( &thread1, NULL, ReopenFfmpegThreadCallback, (void*) this) != 0){
+        Error( "ReopenFfmpeg failed to create worker thread." );
+        return -1;
+    }
+
+    return 0;
+}
+
+int FfmpegCamera::CloseFfmpeg(){
+
+    Debug(2, "CloseFfmpeg called.");
+
+    mCanCapture = false;
+
+    av_freep( &mFrame );
+    av_freep( &mRawFrame );
+    
+#if HAVE_LIBSWSCALE
+    if ( mConvertContext )
+    {
+        sws_freeContext( mConvertContext );
+        mConvertContext = NULL;
+    }
+#endif
+
+    if ( mCodecContext )
+    {
+       avcodec_close( mCodecContext );
+       mCodecContext = NULL; // Freed by av_close_input_file
+    }
+    if ( mFormatContext )
+    {
+#if LIBAVFORMAT_VERSION_INT < AV_VERSION_INT(53, 4, 0)
+        av_close_input_file( mFormatContext );
+#else
+        avformat_close_input( &mFormatContext );
+#endif
+        mFormatContext = NULL;
+    }
+
+    return 0;
+}
+
+int FfmpegCamera::FfmpegInterruptCallback(void *ctx) 
+{ 
+    FfmpegCamera* camera = reinterpret_cast<FfmpegCamera*>(ctx);
+    if (camera->mIsOpening){
+        int now = time(NULL);
+        if ((now - camera->mOpenStart) > camera->mOpenTimeout) {
+            Error ( "Open video took more than %d seconds.", camera->mOpenTimeout );
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+void *FfmpegCamera::ReopenFfmpegThreadCallback(void *ctx){
+    if (ctx == NULL) return NULL;
+
+    FfmpegCamera* camera = reinterpret_cast<FfmpegCamera*>(ctx);
+
+    // Close current stream.
+    camera->CloseFfmpeg();
+
+    // Sleep if neccessary to not reconnect too fast.
+    int wait = camera->mOpenTimeout - (time(NULL) - camera->mOpenStart);
+    wait = wait < 0 ? 0 : wait;
+    if (wait > 0){
+        Debug( 1, "Sleeping %d seconds before reopening stream.", wait );
+        sleep(wait);
+    }
+    
+    if (camera->OpenFfmpeg() != 0){
+        camera->ReopenFfmpeg();
+    }
+
+    return NULL;
 }
 
 #endif // HAVE_LIBAVFORMAT

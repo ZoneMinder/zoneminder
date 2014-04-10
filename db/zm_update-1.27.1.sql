6c637947e2b5e37b9aedf1848418090a471073e5
Sune1337 <kristoffer.lindahl@gmail.com>
2014-04-10 13:56:14 +0200

    Ability to skip frames in motion detection. The reason for this is to make it possible to reduce CPU-load if split-second motion detection is not required.

--- db/zm_update-1.27.1.sql
+++ db/zm_update-1.27.1.sql
@@ -7,3 +7,21 @@
 --
 
 INSERT INTO Controls VALUES (NULL,'WanscamPT','Remote','Wanscam',1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,16,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,16,0,0,0,0,0,1,16,1,1,1,1,0,0,0,1,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0);
+
+--
+-- Add MotionSkipFrame field for controlling how many frames motion detection should skip.
+--
+
+SET @s = (SELECT IF(
+	(SELECT COUNT(*)
+	FROM INFORMATION_SCHEMA.COLUMNS
+	WHERE table_name = 'Monitors'
+	AND table_schema = DATABASE()
+	AND column_name = 'MotionFrameSkip'
+	) > 0,
+"SELECT 1",
+"ALTER TABLE `Monitors` ADD `MotionFrameSkip` smallint(5) unsigned NOT NULL default '0' AFTER `FrameSkip`"
+));
+
+PREPARE stmt FROM @s;
+EXECUTE stmt;

1f29aa432a9298cfb08df5b660ea938eb6186bd3
Kfir Itzhak <mastertheknife@gmail.com>
2014-04-23 12:12:41 +0300

    Minor corrections to the cURL source type in the web interface

--- web/skins/classic/views/monitor.php
+++ web/skins/classic/views/monitor.php
@@ -178,7 +178,7 @@ $sourceTypes = array(
     'File'   => $SLANG['File'],
     'Ffmpeg' => $SLANG['Ffmpeg'],
     'Libvlc' => $SLANG['Libvlc'],
-    'cURL'   => "cURL (HTTP only)"
+    'cURL'   => "cURL (HTTP(S) only)"
 );
 if ( !ZM_HAS_V4L )
     unset($sourceTypes['Local']);
@@ -731,7 +731,7 @@ switch ( $tab )
         elseif ( $newMonitor['Type'] == "cURL" )
         {
 ?>
-            <tr><td><?= "Address" ?></td><td><input type="text" name="newMonitor[Path]" value="<?= validHtmlStr($newMonitor['Path']) ?>" size="36"/></td></tr>
+            <tr><td><?= "URL" ?></td><td><input type="text" name="newMonitor[Path]" value="<?= validHtmlStr($newMonitor['Path']) ?>" size="36"/></td></tr>
             <tr><td><?= "Username" ?></td><td><input type="text" name="newMonitor[User]" value="<?= validHtmlStr($newMonitor['User']) ?>" size="12"/></td></tr>
             <tr><td><?= "Password" ?></td><td><input type="text" name="newMonitor[Pass]" value="<?= validHtmlStr($newMonitor['Pass']) ?>" size="12"/></td></tr>
 <?php

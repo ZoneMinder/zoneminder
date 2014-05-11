fb7ea142bdc1aa5bf5a86dbd40161112f2c4e7de
Will W <nodenet@hotmail.com>
2014-05-11 17:35:27 +0100

    Audio support including fixes for dts/pts, split on keyframe and update to mkv extension to prevent ffmpeg problems writing rtsp audio to mp4 containter (header problem)

--- src/zm_videostore.h
+++ src/zm_videostore.h
@@ -10,6 +10,7 @@ private:
 	AVOutputFormat *fmt;
 	AVFormatContext *oc;
 	AVStream *video_st;
+	AVStream *audio_st;
     
 	const char *filename;
 	const char *format;
@@ -17,15 +18,30 @@ private:
     bool keyframeMessage;
     int keyframeSkipNumber;
     
+    int64_t startTime;
     int64_t startPts;
     int64_t startDts;
+    int64_t filter_in_rescale_delta_last;
     
 public:
-	VideoStore(const char *filename_in, const char *format_in, AVStream *input_st);
+	VideoStore(const char *filename_in, const char *format_in, AVStream *input_st, AVStream *inpaud_st, int64_t nStartTime);
 	~VideoStore();
 
-    int writeVideoFramePacket(AVPacket *pkt, AVStream *input_st, AVPacket *lastKeyframePkt);
+    int writeVideoFramePacket(AVPacket *pkt, AVStream *input_st);//, AVPacket *lastKeyframePkt);
+    int writeAudioFramePacket(AVPacket *pkt, AVStream *input_st);
 };
+
+/*
+class VideoEvent {
+public:
+	VideoEvent(unsigned int	eid);
+	~VideoEvent();
+
+    int createEventImage(unsigned int fid, char *&pBuff);
+    
+private:
+    unsigned int	m_eid;
+};*/
 
 #endif //havelibav
 #endif //zm_videostore_h

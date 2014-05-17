82447ebb2798c5c663850190a53fc7f0f0e028a9
m-bene <hub@bene.priv.at>
2014-05-17 22:05:57 +0200

    remove unwanted comment change

--- web/skins/classic/views/monitor.php
+++ web/skins/classic/views/monitor.php
@@ -1,7 +1,7 @@
 <?php
 //
 // ZoneMinder web monitor view file, $Date$, $Revision$
-// Copyright (C) 2001-2008 Philip CoombesMethod
+// Copyright (C) 2001-2008 Philip Coombes
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License

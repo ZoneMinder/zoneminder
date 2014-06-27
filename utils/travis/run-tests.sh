78d91264149c6d373081910c17ce79896ecc6264
Kyle Johnson <kjohnson@gnulnx.net>
2014-06-27 08:23:53 -0400

    First shot at testing an individual monitor

--- utils/travis/run-tests.sh
+++ utils/travis/run-tests.sh
@@ -11,6 +11,11 @@ with_timestamps() {
 
 run_tests() {
 	mysql -uzmuser -pzmpass zm < ../../db/test.monitor.sql
+	sudo zmu -l
+	sudo zmc -m1 &
+	sudo zma -m1 &
+	sudo zmu -l
+	sudo grep ERR /var/log/syslog
 	sudo zmpkg.pl start
 	sudo zmfilter.pl -f purgewhenfull
 }

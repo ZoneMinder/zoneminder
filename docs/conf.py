bf1fd272dfdd5e4abf304474d56e338a7747061d
stevegilvarry <SteveGilvarry@users.noreply.github.com>
2014-07-30 21:55:57 +1000

    Turning on sticky sidebar

--- docs/conf.py
+++ docs/conf.py
@@ -103,7 +103,9 @@ html_theme = 'default'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+	"stickysidebar": "true"
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []

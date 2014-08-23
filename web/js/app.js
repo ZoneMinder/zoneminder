62df1303f9e2ea0d9ab78684c5e210a9d544db78
Kyle Johnson <kjohnson@gnulnx.net>
2014-08-23 17:54:35 +0000

    Move http queries into a factory

--- web/js/app.js
+++ web/js/app.js
@@ -15,3 +15,23 @@ ZoneMinder.config(['$routeProvider',
 
 }]);
 
+
+
+ZoneMinder.factory('Monitors', function ($http) {
+	return {
+		getMonitors: function(callback) {
+			$http.get('/api/monitors.json').success(callback);
+		}
+	};
+});
+
+ZoneMinder.factory('Config', function($http) {
+	return {
+		getCategories: function(callback) {
+			$http.get('/api/configs/categories.json').success(callback);
+		},
+		getCategory: function(category, callback) {
+			$http.get('/api/configs/categories/' + category + '.json').success(callback);
+		}
+	};
+});
--- web/js/controllers.js
+++ web/js/controllers.js
@@ -1,11 +1,38 @@
 var ZoneMinder = angular.module('ZoneMinderControllers', []);
 
-function MonitorController($scope, $http) {
-	$http.get('/api/monitors.json').success(function(data, status, headers, config) {
-		$scope.monitors = data['monitors'];
+function MonitorController($scope, Monitors) {
+	Monitors.getMonitors(function(results) {
+		$scope.monitors = results.monitors;
 	});
 }
 
 function NavigationController($scope) {
 
+}
+
+function ConfigController($scope, $http, Config) {
+
+	Config.getCategories(function(results) {
+		// List of category names for the tabs
+		$scope.categories = results['categories'];
+
+		// For each category, add all config options belonging to it to the categories array
+		for (var key in results['categories']) {
+			var category = results['categories'][key]['Config']['Category'];
+			buildCats(category);
+		}
+	});
+
+	// Get config options belonging to a given category and push them into categories array
+	function buildCats(cat) {
+		Config.getCategory(cat, function(results) {
+			$scope[cat] = results['config'];
+		});
+	}
+	
+
+
+	$http.get('/api/configs.json').success(function(data, status, headers, config) {
+		$scope.config = data['configs'];
+	});
 }

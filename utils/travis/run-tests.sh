ba14cc136e2340c1ae99850c6ba365de90b395db
SteveGilvarry <steve.gilvarry@gmail.com>
2014-10-06 21:15:41 +1100

    More Logging of build

--- utils/travis/run-tests.sh
+++ utils/travis/run-tests.sh
@@ -13,11 +13,6 @@ run_tests() {
 	printf '%s\n' "${PWD##*/}"
 	mysql -uzmuser -pzmpass < ${TRAVIS_BUILD_DIR}/db/zm_create.sql
 	mysql -uzmuser -pzmpass zm < ${TRAVIS_BUILD_DIR}/db/test.monitor.sql
-	sudo zmu -l
-	sudo zmc -m1 &
-	sudo zma -m1 &
-	sudo zmu -l
-	sudo grep ERR /var/log/syslog
 	sudo zmpkg.pl start
 	sudo zmfilter.pl -f purgewhenfull
 }

f2c419b685aa9e174bcf24e95f67e98c75c7a89b
Isaac Connor <iconnor@pseudo.connortechnology.com>
2014-10-23 09:00:53 -0400

    Fix segfault on zmu -l due to not connectig to the shared mem

--- src/zmu.cpp
+++ src/zmu.cpp
@@ -715,7 +715,7 @@ int main( int argc, char *argv[] )
 					if ( function > 1 )
 					{
 						Monitor *monitor = Monitor::Load( mon_id, false, Monitor::QUERY );
-						if ( monitor )
+						if ( monitor && monitor->connect() )
 						{
 							struct timeval tv = monitor->GetTimestamp();
 							printf( "%4d%5d%6d%9d%11ld.%02ld%6d%6d%8d%8.2f\n",

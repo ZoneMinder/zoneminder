bcbd73fe44200961059c0301b188c10df1c8cb25
Kyle Johnson <kjohnson@gnulnx.net>
2014-11-01 15:39:11 +0000

    Split nav / header out into own file

--- web/skins/bootstrap/views/console.php
+++ web/skins/bootstrap/views/console.php
@@ -189,31 +189,8 @@ xhtmlHeaders( __FILE__, $SLANG['Console'] );
     <form name="monitorForm" method="get" action="<?= $_SERVER['PHP_SELF'] ?>">
     <input type="hidden" name="view" value="<?= $view ?>"/>
     <input type="hidden" name="action" value=""/>
-    <nav class="navbar navbar-default" role="navigation">
-			<div class="container-fluid">
-				<a href="?view=state" class="btn btn-<?= $statusClass ?> btn-md navbar-btn pull-right" onclick="createPopup( '?view=state', 'zmState', 'state' ); return( false );">
-					<span class="glyphicon glyphicon-off"></span>
-				</a>
-				<ul class="nav navbar-nav pull-right">
-      		<li>
-						<?= makePopupLink( '?view=options', 'zmOptions', 'options', $SLANG['Options'] ) ?>
-					</li>
-					<?php if ( logToDatabase() > Logger::NOLOG ) { ?>
-						<li>
-							<?= makePopupLink( '?view=log', 'zmLog', 'log', '<span class="'.logState().'">'.$SLANG['Log'].'</span>' ) ?>
-						</li>
-					<?php } ?>
-      		<li>
-						<?= makePopupLink( '?view=groups', 'zmGroups', 'groups', sprintf( $CLANG['MonitorCount'], count($displayMonitors), zmVlang( $VLANG['Monitor'], count($displayMonitors) ) ).($group?' ('.$group['Name'].')':''), canView( 'System' ) ); ?>
-					</li>
-					<?php if ( ZM_OPT_X10 && canView( 'Devices' ) ) { ?>
-						<li><?= makePopupLink( '?view=devices', 'zmDevices', 'devices', $SLANG['Devices'] ) ?></li>
-					<?php } ?>
-      		<li><?= makePopupLink( '?view=cycle&amp;group='.$cycleGroup, 'zmCycle'.$cycleGroup, array( 'cycle', $cycleWidth, $cycleHeight ), $SLANG['Cycle'], $running ) ?></li>
-					<li><?= makePopupLink( '?view=montage&amp;group='.$cycleGroup, 'zmMontage'.$cycleGroup, 'montage', $SLANG['Montage'], $running ) ?></li>
-				</ul>
-			</div>
-    </nav>
+
+		<?php include("header.php"); ?>
 
     <div id="content" class="container-fluid">
 			<div class="row">

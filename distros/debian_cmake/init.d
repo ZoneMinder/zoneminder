972dedd15a4fca89dde2d6a42e4e5e31325651e4
Cosme Domínguez Díaz <cosme.ddiaz@gmail.com>
2014-11-11 22:47:28 +0100

    * distros/debian -> Autotools * distros/debian_cmake -> CMake

--- distros/debian_cmake/init.d
+++ distros/debian_cmake/init.d
@@ -23,12 +23,10 @@ command="$ZM_PATH_BIN/zmpkg.pl"
 
 start() {
 	echo -n "Starting $prog: "
-	mkdir -p $RUNDIR && chown www-data:www-data $RUNDIR
-	mkdir -p $TMPDIR && chown www-data:www-data $TMPDIR
+	mkdir -p $RUNDIR $TMPDIR && chown www-data:www-data $RUNDIR $TMPDIR
 	$command start
 	RETVAL=$?
-	[ $RETVAL = 0 ] && echo success
-	[ $RETVAL != 0 ] && echo failure
+	[ $RETVAL = 0 ] && echo success || echo failure
 	echo
 	[ $RETVAL = 0 ] && touch /var/lock/zm
 	return $RETVAL
@@ -51,8 +49,7 @@ stop() {
 	else
 		$command stop
 		RETVAL=$?
-		[ $RETVAL = 0 ] && echo success
-		[ $RETVAL != 0 ] && echo failure
+		[ $RETVAL = 0 ] && echo success || echo failure
 		echo
 		[ $RETVAL = 0 ] && rm -f /var/lock/zm
 	fi

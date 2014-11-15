b04d970132882154a119a0df10ad69cfd1d097ab
Kyle Johnson <kjohnson@gnulnx.net>
2014-11-15 17:34:51 +0000

    Add charset and other meta tags to head

--- web/skins/bootstrap/includes/functions.php
+++ web/skins/bootstrap/includes/functions.php
@@ -40,6 +40,9 @@ function xhtmlHeaders( $file, $title )
 <html lang="en">
 <head>
   <title><?= ZM_WEB_TITLE_PREFIX ?> - <?= validHtmlStr($title) ?></title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" type="image/ico" href="graphics/favicon.ico"/>
   <link rel="shortcut icon" href="graphics/favicon.ico"/>
   <link rel="stylesheet" href="css/reset.css" type="text/css"/>

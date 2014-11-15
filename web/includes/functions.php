58fbc6c8336ae87f70f4150e0a8344a89d399ce0
Kyle Johnson <kjohnson@gnulnx.net>
2014-11-15 19:38:45 +0000

    Wrap pagination links in <li> instead of <p>

--- web/includes/functions.php
+++ web/includes/functions.php
@@ -1533,9 +1533,9 @@ function getPagination( $pages, $page, $maxShortcuts, $query, $querySep='&amp;' 
             {
                 if ( false && $page > 2 )
                 {
-                    $pageText .= '<a href="?view='.$view.$querySep.'page=1'.$query.'">&lt;&lt;</a>';
+                    $pageText .= '<li><a href="?view='.$view.$querySep.'page=1'.$query.'">&lt;&lt;</a></li>';
                 }
-                $pageText .= '<a href="?view='.$view.$querySep.'page='.($page-1).$query.'">&lt;</a>';
+                $pageText .= '<li><a href="?view='.$view.$querySep.'page='.($page-1).$query.'">&lt;</a></li>';
 
                 $newPages = array();
                 $pagesUsed = array();
@@ -1555,11 +1555,11 @@ function getPagination( $pages, $page, $maxShortcuts, $query, $querySep='&amp;' 
 
                 foreach ( $newPages as $newPage )
                 {
-                    $pageText .= '<a href="?view='.$view.$querySep.'page='.$newPage.$query.'">'.$newPage.'</a>&nbsp;';
+                    $pageText .= '<li><a href="?view='.$view.$querySep.'page='.$newPage.$query.'">'.$newPage.'</a></li>';
                 }
 
             }
-            $pageText .= '-&nbsp;'.$page.'&nbsp;-';
+                    $pageText .= '<li class="active"><a href="?view='.$view.$querySep.'page='.$page.$query.'">'.$page.'</a></li>';
             if ( $page < $pages )
             {
                 $newPages = array();
@@ -1580,12 +1580,12 @@ function getPagination( $pages, $page, $maxShortcuts, $query, $querySep='&amp;' 
 
                 foreach ( $newPages as $newPage )
                 {
-                    $pageText .= '&nbsp;<a href="?view='.$view.$querySep.'page='.$newPage.$query.'">'.$newPage.'</a>';
+                    $pageText .= '<li><a href="?view='.$view.$querySep.'page='.$newPage.$query.'">'.$newPage.'</a></li>';
                 }
-                $pageText .= '<a href="?view='.$view.$querySep.'page='.($page+1).$query.'">&gt;</a>';
+                $pageText .= '<li><a href="?view='.$view.$querySep.'page='.($page+1).$query.'">&gt;</a></li>';
                 if ( false && $page < ($pages-1) )
                 {
-                    $pageText .= '<a href="?view='.$view.$querySep.'page='.$pages.$query.'">&gt;&gt;</a>';
+                    $pageText .= '<li><a href="?view='.$view.$querySep.'page='.$pages.$query.'">&gt;&gt;</a></li>';
                 }
             }
         }

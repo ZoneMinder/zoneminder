4364c731432f5520ca64b451fe8a6ceac549dcd0
Kyle Johnson <kjohnson@gnulnx.net>
2014-11-20 04:11:33 +0000

    Remake Options view with angular.  Uses API.

--- web/skins/bootstrap/js/controllers.js
+++ web/skins/bootstrap/js/controllers.js
@@ -53,4 +53,40 @@ ZoneMinder.controller('ConfigController', function($scope, $http, Config) {
     $scope.myModel = {configData: results.data.keyValues};
   }); 
 
+	Config.getCategories().then(function(results) {
+		// List of category names for the tabs
+		$scope.categories = results.data.categories;
+
+		// For each category, add all config options belonging to it to the categories array
+		angular.forEach(results['data']['categories'], function(value, key) {
+			console.log(key);
+			var cat = results.data.categories[key].Config.Category;
+			catman(cat);
+		});
+
+	});
+
+	function catman(category) {
+			Config.getCategory(category).then(function(results) {
+				$scope[category] = results.data.config;
+			});
+	}
+
+	$scope.updateConfig = function(configId, configName) {
+		var newValue = $scope.myModel.configData[configName];
+		var i = document.getElementById(configName).parentNode.parentNode;
+		var s = i.getElementsByTagName("span");
+		s = s[0];
+
+		Config.updateOption(configId, newValue).then(function(results) {
+			if (results.statusText == 'OK') {
+				i.className = i.className + " has-success has-feedback";
+				s.className = s.className + " glyphicon glyphicon-ok";
+			} else {
+				i.className = i.className + " has-failure has-feedback";
+				s.className = s.className + " glyphicon glyphicon-ok";
+			}
+		});
+	}
+
 });

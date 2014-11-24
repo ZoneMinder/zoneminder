fe87ca7bd7bbb8806547604c8afb61d247fd5dfc
Kyle Johnson <kjohnson@gnulnx.net>
2014-11-24 20:02:59 +0000

    Initial commit of Host view

---
+++ web/skins/bootstrap/views/host.php
@@ -0,0 +1,16 @@
+<?php
+xhtmlHeaders( __FILE__, 'Host' );
+?>
+
+<body>
+
+	<?php include("header.php"); ?>
+
+	<div class="container-fluid">
+
+	</div>
+
+	<?php include("footer.php"); ?>
+
+</body>
+</html>

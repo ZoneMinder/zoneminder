735c14ea2d96bed53ab53c5f73cc96d0afeffd73
Kyle Johnson <kjohnson@gnulnx.net>
2014-12-16 14:18:54 -0500

    Replace 'name' with 'ng-model' in General monitor tab

--- web/skins/bootstrap/views/tab-monitor-general.php
+++ web/skins/bootstrap/views/tab-monitor-general.php
@@ -2,11 +2,11 @@
 
 	<div class="form-group">
 		<label for="Name"><?= $SLANG['Name'] ?></label>
-		<input type="text" id="Name" class="form-control" name="newMonitor[Name]" placeholder="Monitor-Name" />
+		<input type="text" id="Name" class="form-control" ng-model="newMonitor[Name]" placeholder="Monitor-Name" />
 	</div>
 	<div class="form-group">
 		<label for="SourceType"><?= $SLANG['SourceType'] ?></label>
-		<select name="newMonitor[Type]" id="SourceType" class="form-control">
+		<select ng-model="newMonitor[Type]" id="SourceType" class="form-control">
 			<option value="Local">Local</option>
 			<option value="Remote">Remote</option>
 			<option value="File">File</option>
@@ -17,7 +17,7 @@
 	</div>
 	<div class="form-group">
 		<label for="Function"><?= $SLANG['Function'] ?></label>
-		<select class="form-control" id="Function" name="newMonitor[Function]">
+		<select class="form-control" id="Function" ng-model="newMonitor[Function]">
 			<option value="None">None</option>
 			<option value="Monitor" selected="selected">Monitor</option>
 			<option value="Modect">Modect</option>
@@ -28,17 +28,17 @@
 	</div>
 	<div class="checkbox">
 		<label>
-			<input type="checkbox" id="Enabled" class="form-control" name="newMonitor[Enabled]" value="1"<?php if ( !empty($newMonitor['Enabled']) ) { ?> checked="checked"<?php } ?>/>
+			<input type="checkbox" id="Enabled" ng-model="newMonitor[Enabled]" value="1"<?php if ( !empty($newMonitor['Enabled']) ) { ?> checked="checked"<?php } ?>/>
 			Enabled
 		</label>
 	</div>
 	<div class="form-group">
 		<label for="MaxFPS"><?= $SLANG['MaximumFPS'] ?></label>
-		<input type="number" id="MaxFPS" class="form-control" name="newMonitor[MaxFPS]" value="<?= validHtmlStr($newMonitor['MaxFPS']) ?>" />
+		<input type="number" id="MaxFPS" class="form-control" ng-model="newMonitor[MaxFPS]" value="<?= validHtmlStr($newMonitor['MaxFPS']) ?>" />
 	</div>
 	<div class="form-group">
 		<label for="AlarmMaxFPS"><?= $SLANG['AlarmMaximumFPS'] ?></label>
-		<input type="number" id="AlarmMaxFPS" class="form-control" name="newMonitor[AlarmMaxFPS]" value="<?= validHtmlStr($newMonitor['AlarmMaxFPS']) ?>" />
+		<input type="number" id="AlarmMaxFPS" class="form-control" ng-model="newMonitor[AlarmMaxFPS]" value="<?= validHtmlStr($newMonitor['AlarmMaxFPS']) ?>" />
 	</div>
 <?php
 	if ( ZM_FAST_IMAGE_BLENDS )
@@ -46,18 +46,18 @@
 ?>
 	<div class="form-group">
 		<label for=""><?= $SLANG['RefImageBlendPct'] ?></label>
-<select class="form-control" name="newMonitor[RefBlendPerc]"><?php foreach ( $fastblendopts as $name => $value ) { ?><option value="<?= $value ?>"<?php if ( $value == $newMonitor['RefBlendPerc'] ) { ?> selected="selected"<?php } ?>><?= $name ?></option><?php } ?></select></div>
+<select class="form-control" ng-model="newMonitor[RefBlendPerc]"><?php foreach ( $fastblendopts as $name => $value ) { ?><option value="<?= $value ?>"<?php if ( $value == $newMonitor['RefBlendPerc'] ) { ?> selected="selected"<?php } ?>><?= $name ?></option><?php } ?></select></div>
 	<div class="form-group"><?= "Alarm " . $SLANG['RefImageBlendPct'] ?></label>
-<select class="form-control" name="newMonitor[AlarmRefBlendPerc]"><?php foreach ( $fastblendopts_alarm as $name => $value ) { ?><option value="<?= $value ?>"<?php if ( $value == $newMonitor['AlarmRefBlendPerc'] ) { ?> selected="selected"<?php } ?>><?= $name ?></option><?php } ?></select></div>
+<select class="form-control" ng-model="newMonitor[AlarmRefBlendPerc]"><?php foreach ( $fastblendopts_alarm as $name => $value ) { ?><option value="<?= $value ?>"<?php if ( $value == $newMonitor['AlarmRefBlendPerc'] ) { ?> selected="selected"<?php } ?>><?= $name ?></option><?php } ?></select></div>
 <?php
 	} else {
 ?>
 	<div class="form-group">
 		<label for=""><?= $SLANG['RefImageBlendPct'] ?></label>
-		<input type="text" class="form-control" name="newMonitor[RefBlendPerc]" value="<?= validHtmlStr($newMonitor['RefBlendPerc']) ?>" />
+		<input type="text" class="form-control" ng-model="newMonitor[RefBlendPerc]" value="<?= validHtmlStr($newMonitor['RefBlendPerc']) ?>" />
 	</div>
 	<div class="form-group"><?= "Alarm " . $SLANG['RefImageBlendPct'] ?></label>
-		<input type="text" class="form-control" name="newMonitor[AlarmRefBlendPerc]" value="<?= validHtmlStr($newMonitor['AlarmRefBlendPerc']) ?>" />
+		<input type="text" class="form-control" ng-model="newMonitor[AlarmRefBlendPerc]" value="<?= validHtmlStr($newMonitor['AlarmRefBlendPerc']) ?>" />
 	</div>
 <?php
         }

d5c8de6cb7060176f765f4325fd85fed689a0ec3
Kyle Johnson <kjohnson@gnulnx.net>
2014-12-16 21:05:07 -0500

    Set each input's ng-model for remote monitor type

--- web/skins/bootstrap/views/tab-monitor-remote.php
+++ web/skins/bootstrap/views/tab-monitor-remote.php
@@ -1,38 +1,37 @@
 		<div ng-show="sourceType == 'Remote'">
 			<div class="form-group">
-				<label for=""><?= $SLANG['RemoteProtocol'] ?></label>
-				<select name="newMonitor[Protocol]" id="newMonitor[Protocol]" onchange="updateMethods( this )">
+				<label for="newMonitor[Protocol]"><?= $SLANG['RemoteProtocol'] ?></label>
+				<select ng-model="Protocol" id="newMonitor[Protocol]" onchange="updateMethods( this )">
 					<option value="http">HTTP</option>
 					<option value="rtsp">RTSP</option>
 				</select>
 			</div>
 			<div class="form-group">
-				<label for=""><?= $SLANG['RemoteMethod'] ?></label>
-<?php
-            if ( empty($newMonitor['Protocol']) || $newMonitor['Protocol'] == "http" )
-            {
-?>
-            <?= buildSelect( "newMonitor[Method]", $httpMethods ); ?>
-<?php
-            }
-            else
-            {
-?>
-            <?= buildSelect( "newMonitor[Method]", $rtspMethods ); ?>
-<?php
-            }
-?>
+				<label for="newMonitor[Method]"><?= $SLANG['RemoteMethod'] ?></label>
+<?php if ( empty($newMonitor['Protocol']) || $newMonitor['Protocol'] == "http" ) { ?>
+				<select ng-model="Method" id="newMonitor[Method]">
+					<option value="simple">Simple</option>
+					<option value="regexp">Regexp</option>
+				</select>
+<?php } else { ?>
+				<select ng-model="Method" id="newMonitor[Method]">
+					<option value="rtpUni">RTP/Unicast</option>
+					<option value="rtpMulti">RTP/Multicast</option>
+					<option value="rtpRtsp">RTP/RTSP</option>
+					<option value="rtpRtspHttp">RTP/RTSP/HTTP</option>
+				</select>
+<?php } ?>
 			</div>
 			<div class="form-group">
 				<label><?= $SLANG['RemoteHostName'] ?></label>
-				<input type="text" class="form-control" name="newMonitor[Host]" value="<?= validHtmlStr($newMonitor['Host']) ?>" size="36"/>
+				<input type="text" class="form-control" ng-model="Host" placeholder="HostName or ip.add.re.ss" />
 			</div>
 			<div class="form-group">
 				<label for=""><?= $SLANG['RemoteHostPort'] ?></label>
-				<input type="number" class="form-control" name="newMonitor[Port]" value="<?= validHtmlStr($newMonitor['Port']) ?>" size="6"/>
+				<input type="number" class="form-control" ng-model="Port" placeholder="Usually 80 for http, 554 for rtsp" />
 			</div>
 			<div class="form-group">
 				<label><?= $SLANG['RemoteHostPath'] ?></label>
-				<input type="text" class="form-control" name="newMonitor[Path]" value="<?= validHtmlStr($newMonitor['Path']) ?>" size="36"/>
+				<input type="text" class="form-control" ng-model="Path" placeholder="/path/to/stream.mpg" />
 			</div>
 	</div> <!-- End Remote -->

4e0e8664b1aabb8c94cd0faede050f2b3b359e61
Kyle Johnson <kjohnson@gnulnx.net>
2015-01-21 17:26:46 -0500

    Fix some tabbing in controllers.js

--- web/skins/bootstrap/js/controllers.js
+++ web/skins/bootstrap/js/controllers.js
@@ -145,19 +145,19 @@ ZoneMinder.controller('EventController', function($scope, Event, $modalInstance,
 
 	Event.get(eventId).then(function(results) {
 		$scope.eventId			= eventId;
-		$scope.name 				= results.data.event.Event.Name;
-		$scope.cause 				= results.data.event.Event.Cause;
+		$scope.name 			= results.data.event.Event.Name;
+		$scope.cause 			= results.data.event.Event.Cause;
 		$scope.startTime 		= results.data.event.Event.StartTime;
 		$scope.endTime 			= results.data.event.Event.EndTime;
-		$scope.width 				= results.data.event.Event.Width;
+		$scope.width 			= results.data.event.Event.Width;
 		$scope.length 			= results.data.event.Event.Length;
-		$scope.frames				= results.data.event.Event.Frames;
-		$scope.alarmFrames	= results.data.event.Event.AlarmFrames;
-		$scope.totScore				= results.data.event.Event.TotScore;
-		$scope.avgScore				= results.data.event.Event.AvgScore;
-		$scope.maxScore				= results.data.event.Event.MaxScore;
-		$scope.notes				= results.data.event.Event.Notes;
-		$scope.archived				= results.data.event.Event.Archived;
+		$scope.frames			= results.data.event.Event.Frames;
+		$scope.alarmFrames		= results.data.event.Event.AlarmFrames;
+		$scope.totScore			= results.data.event.Event.TotScore;
+		$scope.avgScore			= results.data.event.Event.AvgScore;
+		$scope.maxScore			= results.data.event.Event.MaxScore;
+		$scope.notes			= results.data.event.Event.Notes;
+		$scope.archived			= results.data.event.Event.Archived;
 		$scope.archive_text		= $scope.archived == 0 ? 'Archive' : 'Unarchive';
 	});
 

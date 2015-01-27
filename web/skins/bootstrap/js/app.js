786009407c6712be40f4df2fe3aabe33f2c76213
Kyle Johnson <kjohnson@gnulnx.net>
2015-01-27 11:15:10 -0500

    Add Angular filter for padding a number
    This is needed because of the way Frames are stored on the
    filesystem, such as 00051-capture.jpg, vs. how they're stored
    in the database, such as 51

--- web/skins/bootstrap/js/app.js
+++ web/skins/bootstrap/js/app.js
@@ -187,3 +187,14 @@ ZoneMinder.filter('DateDiff', function() {
 	}
 	};
 });
+
+ZoneMinder.filter('zpad', function() {
+	return function(input, n) {
+		if(input === undefined)
+			input = ""
+		if(input.length >= n)
+			return input
+		var zeros = "0".repeat(n);
+		return (zeros + input).slice(-1 * n)
+	}; 
+});

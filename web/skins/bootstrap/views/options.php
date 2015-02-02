8ba365b42fba5faf85c184b74a4069fd68bad898
Kyle Johnson <kjohnson@gnulnx.net>
2015-02-02 15:33:40 -0500

    Display Options tabs vertically.
    In the 'Options' view, show the tabs horizontally via bootstrap's
    grid layout.

--- web/skins/bootstrap/views/options.php
+++ web/skins/bootstrap/views/options.php
@@ -6,15 +6,21 @@
 
 <div class="container-fluid">
 	<div class="container" ng-controller="ConfigController">
-		<ul class="nav nav-tabs" role="tablist" id="myTab">
-			<li role="presentation" ng-repeat="category in categories">
-				<a href="#{{category.Config.Category}}" aria-controls="{{category.Config.Category}}" role="tab" data-toggle="tab">{{category.Config.Category}}</a>
-			</li>
-		</ul>
+		<div class="row">
+			<div class="col-md-2">
+				<ul class="nav nav-pills nav-stacked" role="tablist" id="myTab">
+					<li role="presentation" ng-repeat="category in categories" ng-class="{ 'active': (category.Config.Category == 'system') }">
+						<a href="#{{category.Config.Category}}" aria-controls="{{category.Config.Category}}" role="tab" data-toggle="tab">{{category.Config.Category}}</a>
+					</li>
+				</ul>
+			</div>
 		
-		<div class="tab-content">
-			<div role="tabpanel" class="form-horizontal tab-pane" id="{{category.Config.Category}}" ng-repeat="category in categories" angular-html-bind="{{ category.Config.Category }}"></div>
-		</div>
+			<div class="col-md-10">
+				<div class="tab-content">
+					<div role="tabpanel" class="form-horizontal tab-pane" ng-class="{ 'active': (category.Config.Category == 'system') }" id="{{category.Config.Category}}" ng-repeat="category in categories" angular-html-bind="{{ category.Config.Category }}"></div>
+				</div>
+			</div>
+		</div> <!-- End .row -->
 	</div> <!-- End ConfigController -->
 </div>
 

29bcbb2af9138095e78e8d3201e7720be64d4978
Kyle Johnson <kjohnson@gnulnx.net>
2015-02-18 11:39:43 -0500

    Set html5Mode to false until refreshing page works
    If with html5Mode true, freshing a page fails because state is not
    preserved.

--- web/js/app.js
+++ web/js/app.js
@@ -8,7 +8,7 @@ var ZoneMinder = angular.module('ZoneMinder', [
 ]);
 
 ZoneMinder.config(['$locationProvider', function($locationProvider) {
-	$locationProvider.html5Mode(true);    
+	$locationProvider.html5Mode(false);
 }]);
 
 ZoneMinder.config(function($stateProvider, $urlRouterProvider) {

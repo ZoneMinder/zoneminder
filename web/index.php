0af7d0cc0b144d742b4eacb7520c33995fc38955
Isaac Connor <iconnor@pseudo.connortechnology.com>
2015-02-19 16:04:06 -0500

    check defined(ZM_DEFAULT_SKIN) otherwise php will turn it into a string

--- web/index.php
+++ web/index.php
@@ -63,7 +63,7 @@ if ( isset($_GET['skin']) )
     $skin = $_GET['skin'];
 elseif ( isset($_COOKIE['zmSkin']) )
     $skin = $_COOKIE['zmSkin'];
-elseif ( ZM_SKIN_DEFAULT )
+elseif ( defined(ZM_SKIN_DEFAULT) )
 	$skin = ZM_SKIN_DEFAULT;
 else
     $skin = "classic";

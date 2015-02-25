80b1a61a127c52cfb0d35832d84d5e91de56b716
Kyle Johnson <kjohnson@gnulnx.net>
2015-02-25 10:28:24 -0500

    Add CakePHP Crud plugin to the API
    I wish I knew about this plugin months ago.
    
    By default PDO and CakePHP cast int as string.  This prevents me
    from using `<input type="number">`, as the modle that the input
    is bound to is actually a string, not an int.
    
    Crud by default casts int back to int via ApiTransformationListener
    
    It also has a bunch of other features which makes the life of a
    REST / CRUD developer easier.

--- web/api/app/Config/bootstrap.php
+++ web/api/app/Config/bootstrap.php
@@ -69,7 +69,7 @@ Cache::config('default', array('engine' => 'File'));
  * CakePlugin::load('DebugKit'); //Loads a single plugin named DebugKit
  *
  */
-
+CakePlugin::load('Crud');
 /**
  * You can attach event listeners to the request lifecycle as Dispatcher Filter. By default CakePHP bundles two filters:
  *

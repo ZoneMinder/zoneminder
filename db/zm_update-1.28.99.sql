2819585cdbfa26c498c4d544cf093db06869ef04
Isaac Connor <iconnor@pseudo.connortechnology.com>
2015-07-14 16:09:01 -0400

    move add ServerId to Monitors to zm_update-1.28.100.sql

--- db/zm_update-1.28.99.sql
+++ db/zm_update-1.28.99.sql
@@ -357,21 +357,6 @@ SET @s = (SELECT IF(
 )"
 ));
 
---
--- Add ServerId column to Monitors
---
-
-SET @s = (SELECT IF(
-    (SELECT COUNT(*)
-    FROM INFORMATION_SCHEMA.COLUMNS
-    WHERE table_name = 'Monitors'
-    AND table_schema = DATABASE()
-    AND column_name = 'ServerId'
-    ) > 0,
-"SELECT 'Column ServerId exists in Monitors'",
-"ALTER TABLE Monitors ADD `ServerId` int(10) unsigned AFTER `Name`"
-));
-
 PREPARE stmt FROM @s;
 EXECUTE stmt;
 

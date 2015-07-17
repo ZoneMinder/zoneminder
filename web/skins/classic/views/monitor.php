02ee745167b0ee2bcf2459d35f6c5ff3c2218c62
Isaac Connor <iconnor@pseudo.connortechnology.com>
2015-07-17 10:09:22 -0400

    parameter to defined should be in quotes

--- web/skins/classic/views/monitor.php
+++ web/skins/classic/views/monitor.php
@@ -40,7 +40,7 @@ if ( isset($_REQUEST['tab']) )
 else
     $tab = "general";
 
-    if ( defined( ZM_SERVER_ID ) ) {
+    if ( defined( 'ZM_SERVER_ID' ) ) {
         $Server = dbFetchOne( 'SELECT * FROM Servers WHERE Id=?', NULL, array( ZM_SERVER_ID ) );
     } else {
         $Server = array();

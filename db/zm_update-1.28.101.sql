f4aba0de1fe07eb84a8b48644e10b4eaa077ff3f
Andrew Bauer <knnniggett@users.sourceforge.net>
2015-08-09 15:36:13 -0500

    add sql update script

---
+++ db/zm_update-1.28.101.sql
@@ -0,0 +1,18 @@
+--
+-- Add Monitor RTSPDescribe field
+-- Used to enable or disable processing of the remote camera RTSP DESCRIBE response header
+--
+SET @s = (SELECT IF(
+	(SELECT COUNT(*)
+	FROM INFORMATION_SCHEMA.COLUMNS
+	WHERE table_name = 'Monitors'
+	AND table_schema = DATABASE()
+	AND column_name = 'RTSPDescribe'
+	) > 0,
+"SELECT 'Column Options already exists in Monitors'",
+"ALTER TABLE `Monitors` ADD `RTSPDescribe` tinyint(1) unsigned NOT NULL default '0' AFTER `Deinterlacing`"
+));
+
+PREPARE stmt FROM @s;
+EXECUTE stmt;
+

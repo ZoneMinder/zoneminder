9117e2949a6359adc003d99a39ff274321be7ccd
Isaac Connor <iconnor@pseudo.connortechnology.com>
2015-08-16 10:52:47 -0400

    add server.php.

---
+++ web/skins/classic/views/server.php
@@ -0,0 +1,99 @@
+<?php
+//
+// ZoneMinder web user view file, $Date$, $Revision$
+// Copyright (C) 2001-2008 Philip Coombes
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//
+
+if ( !canEdit( 'System' ) )
+{
+    $view = "error";
+    return;
+}
+
+if ( $_REQUEST['id'] ) {
+	if ( !($newServer = dbFetchOne( 'SELECT * FROM Servers WHERE Id = ?', NULL, ARRAY($_REQUEST['id'])) ) ) {
+		$view = "error";
+		return;
+	}
+} else {
+	$newServer = array();
+	$newServer['Name'] = translate('NewServer');
+}
+
+$yesno = array( 0=>translate('No'), 1=>translate('Yes') );
+$nv = array( 'None'=>translate('None'), 'View'=>translate('View') );
+$nve = array( 'None'=>translate('None'), 'View'=>translate('View'), 'Edit'=>translate('Edit') );
+
+$sql = "select Id,Name from Monitors order by Sequence asc";
+$monitors = array();
+foreach( dbFetchAll( $sql ) as $monitor )
+{
+    $monitors[] = $monitor;
+}
+
+$focusWindow = true;
+
+xhtmlHeaders(__FILE__, translate('Server')." - ".$newServer['Name'] );
+?>
+<body>
+  <div id="page">
+    <div id="header">
+      <h2><?php echo translate('Server')." - ".$newServer['Name'] ?></h2>
+    </div>
+    <div id="content">
+      <form name="contentForm" method="post" action="<?php echo $_SERVER['PHP_SELF'] ?>" onsubmit="return validateForm( this, <?php echo empty($newServer['Name'])?'true':'false' ?> )">
+        <input type="hidden" name="view" value="<?php echo $view ?>"/>
+        <input type="hidden" name="object" value="server"/>
+        <input type="hidden" name="id" value="<?php echo validHtmlStr($_REQUEST['id']) ?>"/>
+        <input type="hidden" name="newServer[MonitorIds]" value="<?php echo $newServer['MonitorIds'] ?>"/>
+        <table id="contentTable" class="major" cellspacing="0">
+          <tbody>
+            <tr>
+              <th scope="row"><?php echo translate('ServerName') ?></th>
+              <td><input type="text" name="newServer[Name]" value="<?php echo $newServer['Name'] ?>"/></td>
+            </tr>
+			<tr>
+              <th scope="row"><?php echo translate('RestrictedMonitors') ?></th>
+              <td>
+                <select name="monitorIds" size="4" multiple="multiple">
+<?php
+    foreach ( $monitors as $monitor )
+    {
+        if ( visibleMonitor( $monitor['Id'] ) )
+        {
+?>
+                  <option value="<?php echo $monitor['Id'] ?>"<?php if ( array_key_exists( $monitor['Id'], $monitorIds ) ) { ?> selected="selected"<?php } ?>><?php echo htmlentities($monitor['Name']) ?></option>
+<?php
+        }
+    }
+?>
+                </select>
+              </td>
+            </tr>
+<?php
+}
+?>
+          </tbody>
+        </table>
+        <div id="contentButtons">
+          <input type="submit" value="<?php echo translate('Save') ?>"/><input type="button" value="<?php echo translate('Cancel') ?>" onclick="closeWindow()"/>
+        </div>
+      </form>
+    </div>
+  </div>
+</body>
+</html>

c48bd3ebca9b14057e3b200a3e39a3fa877c8014
Isaac Connor <iconnor@pseudo.connortechnology.com>
2015-08-27 11:37:33 -0400

    restore

--- web/api/app/Config/core.php
+++ web/api/app/Config/core.php
@@ -223,12 +223,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', '4AlwR5ePsf7gY8OxBCp1ftqw5pHhY7bQIJWwSRSB');
+	Configure::write('Security.salt', 'Q0MjGG2xRQEhJVQR85WhFJKI7f2St8RYMlVR7GNQ');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '65025553361572663298232797852');
+	Configure::write('Security.cipherSeed', '02670120062639232092038865362');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).

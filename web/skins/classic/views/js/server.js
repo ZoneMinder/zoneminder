4fa6948e5c22f93e81fab214c02dc48ba6855ee8
Isaac Connor <iconnor@pseudo.connortechnology.com>
2015-09-16 09:12:21 -0400

    add form validation for server editing.  Currently just requires a name

---
+++ web/skins/classic/views/js/server.js
@@ -0,0 +1,14 @@
+function validateForm( form, newServer )
+{
+    var errors = new Array();
+    if ( !form.elements['newServer[Name]'].value )
+    {
+        errors[errors.length] = "You must supply a name";
+    }
+    if ( errors.length )
+    {
+        alert( errors.join( "\n" ) );
+        return( false );
+    }
+    return( true );
+}

b25958df142eede8c81350f4e58e77b7d4f09e99
Isaac Connor <iconnor@pseudo.connortechnology.com>
2015-09-17 16:07:04 -0400

    add a check for StateId

--- db/zm_update-1.28.107.sql
+++ db/zm_update-1.28.107.sql
@@ -3,6 +3,23 @@
 --
 
 --
+-- Add Monitor StateId field
+--
+SET @s = (SELECT IF(
+    (SELECT COUNT(*)
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE table_name = 'Servers'
+    AND table_schema = DATABASE()
+    AND column_name = 'StateId'
+    ) > 0,
+"SELECT 'Column StateId already exists in Servers'",
+"ALTER TABLE `Servers` ADD `StateId` int(10) unsigned AFTER `Name`"
+));
+
+PREPARE stmt FROM @s;
+EXECUTE stmt;
+
+--
 -- Add Monitor RTSPDescribe field
 -- Used to enable or disable processing of the remote camera RTSP DESCRIBE response header
 --

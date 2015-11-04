5b68656602231ec20c93e9f3fe50feb34ca8403f
SteveGilvarry <steve.gilvarry@gmail.com>
2015-11-05 01:09:39 +1100

    Try "STDC_FORMAT_MACROS" to see if that helps Travis on gcc 4.6.3

--- src/zm_videostore.cpp
+++ src/zm_videostore.cpp
@@ -19,6 +19,8 @@
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 //
 
+#define __STDC_FORMAT_MACROS 1
+
 #include <stdlib.h>
 #include <string.h>
 #include <inttypes.h>

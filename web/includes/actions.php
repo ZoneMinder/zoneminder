e6c983072864c43cd5279d24f2df29258e29c068
Isaac Connor <iconnor@pseudo.connortechnology.com>
2015-12-09 09:23:36 -0500

    Use Monitor class instead of old sql

--- web/includes/actions.php
+++ web/includes/actions.php
@@ -246,13 +246,14 @@ if ( !empty($action) )
     if ( !empty($_REQUEST['mid']) && canView( 'Control', $_REQUEST['mid'] ) )
     {
         require_once( 'control_functions.php' );
+        require_once( 'Monitor.php' );
         $mid = validInt($_REQUEST['mid']);
         if ( $action == "control" )
         {
-            $monitor = dbFetchOne( "select C.*,M.* from Monitors as M inner join Controls as C on (M.ControlId = C.Id) where M.Id = ?", NULL, array($mid) );
+            $monitor = new Monitor( $mid );
 
             $ctrlCommand = buildControlCommand( $monitor );
-			sendControlCommand( $monitor['Id'], $ctrlCommand );
+			sendControlCommand( $monitor->Id(), $ctrlCommand );
         }
         elseif ( $action == "settings" )
         {

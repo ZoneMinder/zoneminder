c4bdb127ce4b4c98ea988dfb025a79425e82b1c4
arjunrc <pliablepixels@gmail.com>
2015-12-19 16:53:30 -0500

    fixed custom style breaking rtd theme

--- docs/conf.py
+++ docs/conf.py
@@ -15,6 +15,9 @@
 import sys
 import os
 
+def setup(app):
+	app.add_stylesheet('zmstyle.css')
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -130,7 +133,7 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-#html_static_path = ['_static']
+html_static_path = ['_static']
 #html_style='zmstyles.css'
 
 # Add any extra paths that contain custom files (such as robots.txt or

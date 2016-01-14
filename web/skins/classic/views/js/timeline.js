f16fb5c5f6933a60d7c9fc009f82490097209aff
Isaac Connor <iconnor@15akron1.point-one.com>
2016-01-14 13:34:12 -0500

    use eid and fid instead of specifying the path to the image

--- web/skins/classic/views/js/timeline.js
+++ web/skins/classic/views/js/timeline.js
@@ -112,7 +112,7 @@ function previewEvent( eventId, frameId )
 function loadEventImage( imagePath, eid, fid, width, height )
 {
     var imageSrc = $('imageSrc');
-    imageSrc.setProperty( 'src', imagePrefix+imagePath );
+    imageSrc.setProperty( 'src', imagePrefix + '&eid='+eid+'&fid='+fid );
     imageSrc.removeEvent( 'click' );
     imageSrc.addEvent( 'click', showEvent.pass( [ eid, fid, width, height ] ) );
     var eventData = $('eventData');

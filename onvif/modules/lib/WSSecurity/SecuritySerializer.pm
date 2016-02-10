61932a6470dba2c2a2e1724f29d1204d6efe89f7
Jan M. Hochstein <jan@altaroca.com>
2016-02-10 16:36:21 +0100

    Corrected file copyrights

--- onvif/modules/lib/WSSecurity/SecuritySerializer.pm
+++ onvif/modules/lib/WSSecurity/SecuritySerializer.pm
@@ -1,7 +1,7 @@
 # ==========================================================================
 #
 # Perl WS-Security header for SOAP::WSDL
-# Copyright (C) Jan M. Hochstein
+# Copyright (C) 2014  Jan M. Hochstein
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License

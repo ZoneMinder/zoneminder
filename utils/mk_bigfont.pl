b28224cacb04d399a1c9b66016fd45e5eb5f9c52
Robin Daermann <robin.daermann@ruhr-uni-bochum.de>
2016-02-11 07:35:21 +0100

    Added copyright information to mk_bigfont.pl

--- utils/mk_bigfont.pl
+++ utils/mk_bigfont.pl
@@ -1,4 +1,31 @@
 #!/usr/bin/env perl
+#
+# ==========================================================================
+#
+# ZoneMinder Big Font Generate Script
+# Copyright (C) 2015 Robin Därmann
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+# ==========================================================================
+
+# Basically, this script reads src/zm_font.h and duplicates every bit of every
+# character found in that file. It then duplicates every generated line, so
+# that every character effectively gets double sized.
+# Output goes to STDOUT so it would be useful to redirect it to a file, i.e. to
+# src/zm_bigfont.c
 
 use strict;
 use warnings;

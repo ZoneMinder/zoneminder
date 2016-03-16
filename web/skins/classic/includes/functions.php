6c1f53cbdc5adb4ffe2e9c9c149c6ca6972ed158
Kyle Johnson <kjohnson@gnulnx.net>
2016-03-16 00:49:51 -0400

    Load jquery and bootstrap js in classic skin

--- web/skins/classic/includes/functions.php
+++ web/skins/classic/includes/functions.php
@@ -74,6 +74,8 @@ function xhtmlHeaders( $file, $title )
   <script type="text/javascript" src="js/mootools.ext.js"></script>
   <script type="text/javascript" src="js/logger.js"></script>
   <script type="text/javascript" src="js/overlay.js"></script>
+  <script type="text/javascript" src="/skins/<?php echo $css; ?>/js/jquery-1.11.3.js"></script>
+  <script type="text/javascript" src="/skins/<?php echo $css; ?>/js/bootstrap.min.js"></script>
 <?php if ( $title == 'Login' && (defined('ZM_OPT_USE_GOOG_RECAPTCHA') && ZM_OPT_USE_GOOG_RECAPTCHA) ) { ?>
   <script src='https://www.google.com/recaptcha/api.js'></script>
 <?php

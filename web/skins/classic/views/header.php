d698f2af1463c499960af15bc3bbfcb04793c179
Kyle Johnson <kjohnson@gnulnx.net>
2016-03-16 00:51:13 -0400

    Restyle the header.  Full width, dark background, collapseable.

--- web/skins/classic/views/header.php
+++ web/skins/classic/views/header.php
@@ -184,12 +184,19 @@ $versionClass = (ZM_DYN_DB_VERSION&&(ZM_DYN_DB_VERSION!=ZM_VERSION))?'errorText'
 
 
 ?>
-<div class="navbar navbar-default">
-	<div class="container">
+<div class="navbar navbar-inverse">
+	<div class="container-fluid">
 		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-header-nav" aria-expanded="false">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
 			<a class="navbar-brand" href="http://www.zoneminder.com" target="ZoneMinder">ZoneMinder</a>
 		</div>
 
+		<div class="collapse navbar-collapse" id="main-header-nav">
 		<ul class="nav navbar-nav">
 			<li><a href="?view=console"><?php echo translate('Console') ?></a></li>
 <?php if ( canView( 'System' ) ) { ?>
@@ -229,5 +236,6 @@ $versionClass = (ZM_DYN_DB_VERSION&&(ZM_DYN_DB_VERSION!=ZM_VERSION))?'errorText'
 <?php } ?>
 </div>
 
+		</div><!-- End .navbar-collapse -->
 	</div> <!-- End .container-fluid -->
 </div> <!-- End .navbar .navbar-default -->

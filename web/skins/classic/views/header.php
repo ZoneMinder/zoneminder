92e0552f8e48dcc2a9a969d076e8d83ed641f624
Kyle Johnson <kjohnson@gnulnx.net>
2016-04-01 14:02:19 -0400

    Add side tab list to options page, and style it.

--- web/skins/classic/views/header.php
+++ web/skins/classic/views/header.php
@@ -184,7 +184,7 @@ $versionClass = (ZM_DYN_DB_VERSION&&(ZM_DYN_DB_VERSION!=ZM_VERSION))?'errorText'
 
 
 ?>
-<div class="navbar navbar-inverse">
+<div class="navbar navbar-inverse navbar-fixed-top">
 	<div class="container-fluid">
 		<div class="navbar-header">
 			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-header-nav" aria-expanded="false">

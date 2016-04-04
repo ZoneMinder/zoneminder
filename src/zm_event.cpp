de5b9c6daeb841ce8d6583d5e173a5e9f33f6bca
Isaac Connor <iconnor@pseudo.connortechnology.com>
2016-04-04 16:56:40 -0400

    I don't know what the Name of an event is, but the column count and value count are off

--- src/zm_event.cpp
+++ src/zm_event.cpp
@@ -85,7 +85,7 @@ Event::Event( Monitor *p_monitor, struct timeval p_start_time, const std::string
     static char sql[ZM_SQL_MED_BUFSIZ];
 
     struct tm *stime = localtime( &start_time.tv_sec );
-    snprintf( sql, sizeof(sql), "insert into Events ( MonitorId, StorageId, Name, StartTime, Width, Height, Cause, Notes, Videoed ) values ( %d, %d, 'New Event', from_unixtime( %ld ), %d, %d, '%s', '%s' )", monitor->Id(), storage->Id(), start_time.tv_sec, monitor->Width(), monitor->Height(), cause.c_str(), notes.c_str(), videoEvent );
+    snprintf( sql, sizeof(sql), "insert into Events ( MonitorId, StorageId, StartTime, Width, Height, Cause, Notes, Videoed ) values ( %d, %d, 'New Event', from_unixtime( %ld ), %d, %d, '%s', '%s' )", monitor->Id(), storage->Id(), start_time.tv_sec, monitor->Width(), monitor->Height(), cause.c_str(), notes.c_str(), videoEvent );
     if ( mysql_query( &dbconn, sql ) )
     {
         Error( "Can't insert event: %s", mysql_error( &dbconn ) );

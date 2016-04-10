39a30ff51b4106fa9956de4b2736740b2992e9fb
Andy Bauer <knnniggett@hotmail.com>
2016-04-10 15:59:13 -0500

    refresh parent when cancel is clicked

--- web/skins/classic/views/js/zones.js
+++ web/skins/classic/views/js/zones.js
@@ -1,6 +1,5 @@
 var streamCmdParms = "view=request&request=stream&connkey="+connKey;
 var streamCmdReq = new Request.JSON( { url: monitorUrl+thisUrl, method: 'post', timeout: AJAX_TIMEOUT, link: 'cancel' } );
-var streamCmdTimer = null;
 
 function streamCmdQuit( action ) {
     if ( action )
--- web/skins/classic/views/zone.php
+++ web/skins/classic/views/zone.php
@@ -267,7 +267,7 @@ for ( $i = 0; $i < $pointCols; $i++ )
               </tr>
             </tbody>
           </table>
-          <input id="pauseBtn" type="button" value="<?php echo translate('Pause') ?>" onclick="streamCmdPauseToggle()"/><input type="submit" id="submitBtn" name="submitBtn" value="<?php echo translate('Save') ?>" onclick="return saveChanges( this )"<?php if (!canEdit( 'Monitors' ) || (false && $selfIntersecting)) { ?> disabled="disabled"<?php } ?>/><input type="button" value="<?php echo translate('Cancel') ?>" onclick="closeWindow()"/>
+          <input id="pauseBtn" type="button" value="<?php echo translate('Pause') ?>" onclick="streamCmdPauseToggle()"/><input type="submit" id="submitBtn" name="submitBtn" value="<?php echo translate('Save') ?>" onclick="return saveChanges( this )"<?php if (!canEdit( 'Monitors' ) || (false && $selfIntersecting)) { ?> disabled="disabled"<?php } ?>/><input type="button" value="<?php echo translate('Cancel') ?>" onclick="refreshParentWindow(); closeWindow();"/>
         </div>
       </form>
     </div>

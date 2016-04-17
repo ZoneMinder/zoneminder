98fa65310b5105ea88deb7a986b0eac2cf85591a
Isaac Connor <iconnor@pseudo.connortechnology.com>
2016-04-17 09:29:00 -0400

    whitespace changes

--- src/zm_videostore.cpp
+++ src/zm_videostore.cpp
@@ -96,12 +96,13 @@ VideoStore::VideoStore(const char *filename_in, const char *format_in,
     }
     
     if (inpaud_st) {
+
         audio_st = avformat_new_stream(oc, inpaud_st->codec->codec);
         if (!audio_st) {
             Error("Unable to create audio out stream\n");
             audio_st = NULL;
         } else {
-            ret=avcodec_copy_context(audio_st->codec, inpaud_st->codec);
+            ret = avcodec_copy_context(audio_st->codec, inpaud_st->codec);
             if (ret < 0) {
                 Fatal("Unable to copy audio context %s\n", av_make_error_string(ret).c_str());
             }   

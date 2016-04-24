6a48b9041a38876ab5cdfd8a19e9dcdb9ca40075
Kyle Johnson <kjohnson@gnulnx.net>
2016-04-24 00:52:19 -0400

    Revert "Include Chart.js"
    This reverts commit 33d6d311b05c7a70fa8b01cdc62d0a1175f1474f.
    
    Didn't mean to add this code in yet.  For a future feature.

--- web/skins/classic/includes/functions.php
+++ web/skins/classic/includes/functions.php
@@ -76,7 +76,6 @@ function xhtmlHeaders( $file, $title )
   <script type="text/javascript" src="js/overlay.js"></script>
   <script type="text/javascript" src="/skins/<?php echo $css; ?>/js/jquery-1.11.3.js"></script>
   <script type="text/javascript" src="/skins/<?php echo $css; ?>/js/bootstrap.min.js"></script>
-  <script type="text/javascript" src="/skins/<?php echo $css; ?>/js/Chart.js"></script>
   <script type="text/javascript" src="/skins/<?php echo $css; ?>/views/js/state.js.php"></script>
   <script type="text/javascript" src="/skins/<?php echo $css; ?>/views/js/state.js"></script>
 <?php if ( $title == 'Login' && (defined('ZM_OPT_USE_GOOG_RECAPTCHA') && ZM_OPT_USE_GOOG_RECAPTCHA) ) { ?>

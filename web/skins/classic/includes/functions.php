85f950d80a538c492ac8b0ed4452e3720f1f2f64
Isaac Connor <iconnor@testing.internal.point-one.com>
2016-05-13 09:45:24 -0400

    put jquery into no conflict mode so it doesn't overwite mootools use of $

--- web/skins/classic/includes/functions.php
+++ web/skins/classic/includes/functions.php
@@ -85,6 +85,9 @@ function xhtmlHeaders( $file, $title )
   <script type="text/javascript">
   //<![CDATA[
   <!--
+var $j = jQuery.noConflict();
+// $j is now an alias to the jQuery function; creating the new alias is optional.
+
 <?php include("skins/$skin/views/js/state.js.php")?>
   //-->
   //]]>

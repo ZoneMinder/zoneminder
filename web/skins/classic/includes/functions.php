09ebcfb7f889444fe5654b4bf9c4d837d90ed148
Isaac Connor <iconnor@pseudo.connortechnology.com>
2016-08-22 11:42:44 -0400

    only put in the development h3 tag if the value has been set

--- web/skins/classic/includes/functions.php
+++ web/skins/classic/includes/functions.php
@@ -243,7 +243,9 @@ function getNavBarHTML() {
   </div>
   <div class="pull-right">
 	  <?php echo makePopupLink( '?view=version', 'zmVersion', 'version', '<span class="'.$versionClass.'">v'.ZM_VERSION.'</span>', canEdit( 'System' ) ) ?>
-      <h3 id="development"><center><?php echo ZM_WEB_CONSOLE_BANNER ?></center></h3>
+  <?php if ( defined('ZM_WEB_CONSOLE_BANNER') ) { ?>
+      <h3 id="development"><?php echo ZM_WEB_CONSOLE_BANNER ?></h3>
+  <?php } ?>
   </div>
   <ul class="list-inline">
 	  <li><?php echo translate('Load') ?>: <?php echo getLoad() ?></li>

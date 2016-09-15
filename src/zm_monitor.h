8f33ed6ca6b26e722865faaf33f96969808b31c1
Isaac Connor <iconnor@connortechnology.com>
2016-09-15 11:16:05 -0400

    introduce another sharedmem variable called startup_time, which is set by zmc indicating when the process started.

--- src/zm_monitor.h
+++ src/zm_monitor.h
@@ -128,14 +128,18 @@ class Monitor
      ** Shared memory layout should be identical for both 32bit and 64bit and is multiples of 16.
      */  
     union {                     /* +64   */
-      time_t last_write_time;
+      time_t startup_time;			/* When the zmc process started.  zmwatch uses this to see how long the process has been running without getting any images */
       uint64_t extrapad1;
     };
-    union {            /* +72   */
-      time_t last_read_time;
+    union {                     /* +72   */
+      time_t last_write_time;
       uint64_t extrapad2;
     };
-    uint8_t control_state[256];  /* +80   */
+    union {            /* +80   */
+      time_t last_read_time;
+      uint64_t extrapad3;
+    };
+    uint8_t control_state[256];  /* +88   */
 
   } SharedData;
 
@@ -216,6 +220,12 @@ class Monitor
       inline time_t getLastConnectTime() const {
         return( last_connect_time );
       }
+			inline time_t getStartupTime() const {
+				return( shared_data->startup_time );
+			}
+			inline void setStartupTime( time_t p_time ) {
+				shared_data->startup_time = p_time;
+			}
 
       bool connect();
       bool disconnect();

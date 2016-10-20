017085aee73ed2e2767c855da59c4307f36f4193
Isaac Connor <iconnor@pseudo.connortechnology.com>
2016-10-20 11:49:35 -0400

    Use proper rounding instead of sprintf based rounding for fps

--- web/ajax/stream.php
+++ web/ajax/stream.php
@@ -104,7 +104,7 @@ switch ( $data['type'] )
     case MSG_DATA_WATCH :
     {
         $data =  unpack( "ltype/imonitor/istate/dfps/ilevel/irate/ddelay/izoom/Cdelayed/Cpaused/Cenabled/Cforced", $msg );
-        $data['fps'] = sprintf( "%.2f", $data['fps'] );
+        $data['fps'] = round( $data['fps'], 2 );
         $data['rate'] /= RATE_BASE;
         $data['delay'] = round( $data['delay'], 2 );
         $data['zoom'] = round( $data['zoom']/SCALE_BASE, 1 );

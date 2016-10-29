3e3956f45dde0093f2b235ba92f6aca5b316a10f
Kyle Johnson <kjohnson@gnulnx.net>
2016-10-29 11:46:01 -0600

    Update doctype to HTML5, add meta tags for mobile browsers

--- web/skins/classic/includes/functions.php
+++ web/skins/classic/includes/functions.php
@@ -36,9 +36,12 @@ function xhtmlHeaders( $file, $title )
 
     extract( $GLOBALS, EXTR_OVERWRITE );
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html lang="en">
 <head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maxiumum-scale=1.0, user-scalable=no">
   <title><?php echo ZM_WEB_TITLE_PREFIX ?> - <?php echo validHtmlStr($title) ?></title>
   <link rel="icon" type="image/ico" href="graphics/favicon.ico"/>
   <link rel="shortcut icon" href="graphics/favicon.ico"/>

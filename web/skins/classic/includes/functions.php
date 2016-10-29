eacf6cd415d65476223d1ade55626c3831acb910
Kyle Johnson <kjohnson@gnulnx.net>
2016-10-29 11:19:32 -0600

    Restyle login page, move recaptcha js to <head>
    The way it was handled previously resulted in
    invalid html, with an extra <head> tag being
    inserteed inside the <body>.

--- web/skins/classic/includes/functions.php
+++ web/skins/classic/includes/functions.php
@@ -72,6 +72,9 @@ function xhtmlHeaders( $file, $title )
   <script type="text/javascript" src="js/logger.js"></script>
   <script type="text/javascript" src="js/overlay.js"></script>
 <?php
+<?php if ( $title == 'Login' && (defined('ZM_OPT_USE_GOOG_RECAPTCHA') && ZM_OPT_USE_GOOG_RECAPTCHA) ) { ?>
+  <script src='https://www.google.com/recaptcha/api.js'></script>
+<?php }
     if ( $skinJsPhpFile )
     {
 ?>

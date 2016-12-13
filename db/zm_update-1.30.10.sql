eb5f0b370e82a564a6669d37d7fc3ad9b9a3e214
Isaac Connor <iconnor@testing.internal.point-one.com>
2016-12-12 22:23:56 -0500

    fix type of Config.DefaultValue and add NOT NULL to Message

--- db/zm_update-1.30.10.sql
+++ db/zm_update-1.30.10.sql
@@ -3,4 +3,7 @@
 -- Alter type of Messages column from VARCHAR(255) to TEXT
 --
 
-ALTER TABLE Logs MODIFY Message TEXT;
+-- ALTER TABLE Logs ALTER  Message DROP DEFAULT;
+ALTER TABLE Logs MODIFY Message TEXT NOT NULL;
+
+ALTER TABLE Config MODIFY DefaultValue TEXT;

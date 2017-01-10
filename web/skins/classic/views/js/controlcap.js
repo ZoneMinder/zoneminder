7ef7a36f394d5ebe74dafe2467691a99fefde363
Andy Bauer <knnniggett@hotmail.com>
2017-01-10 17:53:05 -0600

    fix conditional logic in controlcap.js

--- web/skins/classic/views/js/controlcap.js
+++ web/skins/classic/views/js/controlcap.js
@@ -3,7 +3,7 @@ function validateForm( form ) {
 
     // If "Can Move" is enabled, then the end user must also select at least one of the other check boxes (excluding Can Move Diagonally)
     if ( form.elements['newControl[CanMove]'].checked ) {
-        if ( !form.elements['newControl[CanMoveCon]'].checked || !form.elements['newControl[CanMoveRel]'].checked || !form.elements['newControl[CanMoveAbs]'].checked || !form.elements['newControl[CanMoveMap]'].checked ) {
+        if ( !(form.elements['newControl[CanMoveCon]'].checked || form.elements['newControl[CanMoveRel]'].checked || form.elements['newControl[CanMoveAbs]'].checked || form.elements['newControl[CanMoveMap]'].checked) ) {
             errors[errors.length] = "In addition to \"Can Move\", you also must select at least one of: \"Can Move Mapped\", \"Can Move Absolute\", \"Can Move Relative\", or \"Can Move Continuous\"";
         }
     } else {

9fd52c231ad2a4925c6181ec784ca56a3ddbd8a7
Andrew Bauer <knnniggett@users.sourceforge.net>
2017-01-16 08:39:54 -0600

    remove packpack build files

--- build/userwrapper.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-useradd -u 1000 abauer
-usermod -a -G sudo abauer 2>/dev/null || :
-usermod -a -G wheel abauer 2>/dev/null || :
-usermod -a -G adm abauer 2>/dev/null || :
-export HOME=/home/abauer
-exec chroot --userspec=abauer / $@

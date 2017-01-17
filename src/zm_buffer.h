7116d66500c5d036aa038ab377d423dc600b6767
Isaac Connor <isaac@zoneminder.com>
2017-01-16 21:57:21 -0500

    use the correct pointer casts

--- src/zm_buffer.h
+++ src/zm_buffer.h
@@ -153,7 +153,7 @@ public:
         mHead = mTail = mStorage;
       else if ( level )
       {
-        if ( ((unsigned int)mHead-(unsigned int)mStorage) > mSize )
+        if ( ((uintptr_t)mHead-(uintptr_t)mStorage) > mSize )
         {
           memcpy( mStorage, mHead, mSize );
           mHead = mStorage;

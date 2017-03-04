02ea25e8a045dd8030c3ea218f0fd26d992dc562
Andy Bauer <knnniggett@hotmail.com>
2017-03-04 10:49:14 -0600

    set group ownership on zm.conf

--- utils/packpack/startpackpack.sh
+++ utils/packpack/startpackpack.sh
@@ -74,6 +74,7 @@ installtrusty () {
     # Install and test the zoneminder package (only) for Ubuntu Trusty
     sudo gdebi --non-interactive build/zoneminder_*amd64.deb
     sudo chmod 644 /etc/zm/zm.conf 
+    sudo chgrp www-data /etc/zm/zm.conf
     mysql -uzmuser -pzmpass zm < db/test.monitor.sql
     sudo /usr/bin/zmpkg.pl start
     sudo /usr/bin/zmfilter.pl -f purgewhenfull

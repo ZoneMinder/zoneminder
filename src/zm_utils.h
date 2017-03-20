3b15ba91d104bbf5b884549420d9571e927ff570
Isaac Connor <isaac@zoneminder.com>
2017-03-20 11:16:21 -0400

    Rough in a CURL based UriDecode

--- src/zm_utils.h
+++ src/zm_utils.h
@@ -60,4 +60,6 @@ void timespec_diff(struct timespec *start, struct timespec *end, struct timespec
 
 extern unsigned int sseversion;
 
+std::string UriDecode( const std::string &encoded );
+
 #endif // ZM_UTILS_H

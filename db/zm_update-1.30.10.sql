10732f039da9f22ee54b4c2dd80f1cb5c32aa962
Isaac Connor <isaac@zoneminder.com>
2017-04-21 12:45:44 -0400

    update to 1.31.0

--- db/zm_update-1.30.10.sql
+++
@@ -1,9 +0,0 @@
--- This updates a 1.30.9 database to 1.30.10
---
--- Alter type of Messages column from VARCHAR(255) to TEXT
---
-
--- ALTER TABLE Logs ALTER  Message DROP DEFAULT;
-ALTER TABLE Logs MODIFY Message TEXT NOT NULL;
-
-ALTER TABLE Config MODIFY DefaultValue TEXT;

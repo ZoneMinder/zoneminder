998aa2199b82d0e6c97c9d943764f4a655b094f9
Isaac Connor <isaac@zoneminder.com>
2017-04-24 16:55:08 -0400

    fix message for consistency

--- utils/do_debian_package.sh
+++ utils/do_debian_package.sh
@@ -65,7 +65,7 @@ fi;
 
 if [ "$DISTRO" == "" ]; then
   DISTRO=`lsb_release -a 2>/dev/null | grep Codename | awk '{print $2}'`;
-  echo "Guessed distro as $DISTRO";
+  echo "Defaulting to $DISTRO for distribution";
 fi;
 
 # Release is a special mode...  it uploads to the release ppa and cannot have a snapshot

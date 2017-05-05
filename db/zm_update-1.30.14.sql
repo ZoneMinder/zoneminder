6dcad09c6559490edf661887a4914c9e946a89f9
Isaac Connor <iconnor@testing.internal.point-one.com>
2017-05-05 16:53:49 -0400

    this was never commited and got lost.  Merge it's content into zm_update-1.31.0.sql

--- db/zm_update-1.30.14.sql
+++
@@ -1,4 +0,0 @@
-ALTER TAble Monitors MODIFY V4LMultiBuffer tinyint(1) unsigned;
-ALTER TAble Monitors MODIFY ControlId int(10) unsigned;
-ALTER TAble Monitors MODIFY TrackDelay smallint(5) unsigned;
-ALTER TAble Monitors MODIFY ReturnDelay smallint(5) unsigned;

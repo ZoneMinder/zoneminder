d1c61a252f2f3a49bd101d5efae03f5215ce32f6
Isaac Connor <isaac@zoneminder.com>
2017-05-18 13:50:56 -0400

    fix whitspace/braces

--- web/skins/classic/views/js/controlpreset.js
+++ web/skins/classic/views/js/controlpreset.js
@@ -1,14 +1,10 @@
-function updateLabel()
-{
-    var presetIndex = $('contentForm').preset.getValue();
-    if ( labels[presetIndex] )
-    {
-        $('contentForm').newLabel.value = labels[presetIndex];
-    }
-    else
-    {
-        $('contentForm').newLabel.value = "";
-    }
+function updateLabel() {
+  var presetIndex = $('contentForm').preset.getValue();
+  if ( labels[presetIndex] ) {
+    $('contentForm').newLabel.value = labels[presetIndex];
+  } else {
+    $('contentForm').newLabel.value = "";
+  }
 }
 
 window.addEvent( 'domready', updateLabel );

e2df0b74d410caac4de6f13e49911b8f2b62bf7c
Isaac Connor <isaac@zoneminder.com>
2017-05-29 09:51:31 -0400

    remove including state.js.php as it has nothing to do with this pr

--- web/skins/classic/includes/functions.php
+++ web/skins/classic/includes/functions.php
@@ -85,7 +85,6 @@ function xhtmlHeaders( $file, $title )
 var $j = jQuery.noConflict();
 // $j is now an alias to the jQuery function; creating the new alias is optional.
 
-<?php include("skins/$skin/views/js/state.js.php")?>
   //-->
   //]]>
 </script>

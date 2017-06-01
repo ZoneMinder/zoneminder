85c19532cfd40a834b8d32515dc42e24ff18a277
Andrew Bauer <zonexpertconsulting@outlook.com>
2017-05-31 19:04:14 -0500

    Update startpackpack.sh
    fix copy/paste error

--- utils/packpack/startpackpack.sh
+++ utils/packpack/startpackpack.sh
@@ -119,8 +119,8 @@ setdebpkgver () {
 
     # DEBUG
     git describe --long --always
-    sed -n 's/^\([0-9\.]*\)-\([0-9]*\)-\([a-z0-9]*\)/\1/p')
-    sed -n 's/^\([0-9\.]*\)-\([0-9]*\)-\([a-z0-9]*\)/\2/p')
+    git describe --long --always | sed -n 's/^\([0-9\.]*\)-\([0-9]*\)-\([a-z0-9]*\)/\1/p'
+    git describe --long --always | sed -n 's/^\([0-9\.]*\)-\([0-9]*\)-\([a-z0-9]*\)/\2/p'
     
     # Set VERSION to x.xx.x+x e.g. 1.30.2+15
     # the last x is number of commits since release

be8be3b0fde750ec0592c5c06747396d260d9386
Isaac Connor <iconnor@testing.internal.point-one.com>
2017-06-19 13:40:32 -0400

    Put back Lost Storageareas code

--- scripts/ZoneMinder/lib/ZoneMinder/Filter.pm
+++ scripts/ZoneMinder/lib/ZoneMinder/Filter.pm
@@ -99,7 +99,7 @@ sub Execute {
   my $sql = $self->Sql();
 
   if ( $self->{HasDiskPercent} ) {
-    my $disk_percent = getDiskPercent();
+		my $disk_percent = getDiskPercent( $$self{Storage} ? $$self{Storage}->Path() : () );
     $sql =~ s/zmDiskPercent/$disk_percent/g;
   }
   if ( $self->{HasDiskBlocks} ) {
@@ -195,6 +195,9 @@ sub Sql {
                 # This gets used later, I forget for what
                 $$self{Server} = new ZoneMinder::Server( $temp_value );
               }
+						} elsif ( $term->{attr} eq 'StorageId' ) {
+							$value = "'$temp_value'";
+							$$self{Storage} = new ZoneMinder::Storage( $temp_value );
             } elsif ( $term->{attr} eq 'Name'
                 || $term->{attr} eq 'Cause'
                 || $term->{attr} eq 'Notes'

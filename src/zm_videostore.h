732ebf6df4c42e5dd192dd93832a037c652af6c3
Isaac Connor <isaac@zoneminder.com>
2017-08-23 10:37:32 -0400

    shorted some variable names and correct spacing

--- src/zm_videostore.h
+++ src/zm_videostore.h
@@ -18,35 +18,35 @@ class VideoStore {
 private:
   unsigned int packets_written;
 
-	AVOutputFormat *output_format;
+	AVOutputFormat *out_format;
 	AVFormatContext *oc;
-	AVStream *video_output_stream;
-	AVStream *audio_output_stream;
-  AVCodecContext *video_output_context;
+	AVStream *video_out_stream;
+	AVStream *audio_out_stream;
+  AVCodecContext *video_out_ctx;
 
-	AVStream *video_input_stream;
-	AVStream *audio_input_stream;
+	AVStream *video_in_stream;
+	AVStream *audio_in_stream;
 
   // Move this into the object so that we aren't constantly allocating/deallocating it on the stack
   AVPacket opkt;
   // we are transcoding
-  AVFrame *input_frame;
-  AVFrame *output_frame;
+  AVFrame *in_frame;
+  AVFrame *out_frame;
 
-  AVCodecContext *video_input_context;
-  AVCodecContext *audio_input_context;
+  AVCodecContext *video_in_ctx;
+  AVCodecContext *audio_in_ctx;
   int ret;
 
   // The following are used when encoding the audio stream to AAC
-  AVCodec *audio_output_codec;
-  AVCodecContext *audio_output_context;
+  AVCodec *audio_out_codec;
+  AVCodecContext *audio_out_ctx;
   int data_present;
   AVAudioFifo *fifo;
-  int output_frame_size;
+  int out_frame_size;
 #ifdef HAVE_LIBAVRESAMPLE
-AVAudioResampleContext* resample_context;
+AVAudioResampleContext* resample_ctx;
 #endif
-  uint8_t *converted_input_samples;
+  uint8_t *converted_in_samples;
     
 	const char *filename;
 	const char *format;
@@ -54,13 +54,13 @@ AVAudioResampleContext* resample_context;
   bool keyframeMessage;
   int keyframeSkipNumber;
     
-  // These are for input
+  // These are for in
   int64_t video_last_pts;
   int64_t video_last_dts;
   int64_t audio_last_pts;
   int64_t audio_last_dts;
 
-  // These are for output, should start at zero.  We assume they do not wrap because we just aren't going to save files that big.
+  // These are for out, should start at zero.  We assume they do not wrap because we just aren't going to save files that big.
   int64_t video_next_pts;
   int64_t video_next_dts;
   int64_t audio_next_pts;
@@ -71,7 +71,13 @@ AVAudioResampleContext* resample_context;
   bool setup_resampler();
 
 public:
-	VideoStore(const char *filename_in, const char *format_in, AVStream *video_input_stream, AVStream *audio_input_stream, int64_t nStartTime, Monitor * p_monitor );
+	VideoStore(
+      const char *filename_in,
+      const char *format_in,
+      AVStream *video_in_stream,
+      AVStream *audio_in_stream,
+      int64_t nStartTime,xi
+      Monitor * p_monitor);
 	~VideoStore();
 
   int writeVideoFramePacket( AVPacket *pkt );

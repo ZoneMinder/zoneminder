ac6964b159cc3e62adc43cbedef68d27c82a758e
Isaac Connor <isaac@zoneminder.com>
2017-11-01 11:56:18 -0400

    add position clearing to other Layouts

--- db/zm_update-1.31.12.sql
+++ db/zm_update-1.31.12.sql
@@ -22,6 +22,8 @@ SET @s = (SELECT IF(
 PREPARE stmt FROM @s;
 EXECUTE stmt;
 
+DELETE FROM MontageLayouts WHERE Name IN ('Freeform','2 Wide','3 Wide','4 Wide','5 Wide');
+
 SET @s = ( SELECT IF(
     (SELECT COUNT(*) FROM MontageLayouts WHERE Name='Freeform') > 0,
       "SELECT 'Freeform already in layouts'",
@@ -33,7 +35,7 @@ EXECUTE stmt;
 SET @s = ( SELECT IF(
     (SELECT COUNT(*) FROM MontageLayouts WHERE Name='2 Wide') > 0,
 "SELECT '2 Wide already in layouts'",
-'INSERT INTO MontageLayouts (`Name`,`Positions`) VALUES (\'2 Wide\', \'{"default":{"float":"left","position":"relative","width":"49%"}}\');'
+'INSERT INTO MontageLayouts (`Name`,`Positions`) VALUES (\'2 Wide\', \'{"default":{"float":"left","position":"relative","width":"49%","left":"0px","right":"0px","top":"0px","bottom":"0px"}}\');'
 ) );
 PREPARE stmt FROM @s;
 EXECUTE stmt;
@@ -41,7 +43,7 @@ EXECUTE stmt;
 SET @s = ( SELECT IF(
     (SELECT COUNT(*) FROM MontageLayouts WHERE Name='3 Wide') > 0,
       "SELECT '3 Wide already in layouts'",
-'INSERT INTO MontageLayouts (`Name`,`Positions`) VALUES (\'3 Wide\', \'{"default":{"float":"left","position":"relative","width":"33%"}}\');'
+'INSERT INTO MontageLayouts (`Name`,`Positions`) VALUES (\'3 Wide\', \'{"default":{"float":"left","position":"relative","width":"33%","left":"0px","right":"0px","top":"0px","bottom":"0px"}}\');'
 ) );
 PREPARE stmt FROM @s;
 EXECUTE stmt;
@@ -49,7 +51,7 @@ EXECUTE stmt;
 SET @s = ( SELECT IF(
     (SELECT COUNT(*) FROM MontageLayouts WHERE Name='4 Wide') > 0,
       "SELECT '4 Wide already in layouts'",
-'INSERT INTO MontageLayouts (`Name`,`Positions`) VALUES (\'4 Wide\', \'{"default":{"float":"left","position":"relative","width":"24.5%"}}\');'
+'INSERT INTO MontageLayouts (`Name`,`Positions`) VALUES (\'4 Wide\', \'{"default":{"float":"left","position":"relative","width":"24.5%","left":"0px","right":"0px","top":"0px","bottom":"0px"}}\');'
 ) );
 
 PREPARE stmt FROM @s;

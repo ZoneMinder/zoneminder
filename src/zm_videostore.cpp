476ed4c9e08141f8414b9eeb5717dd35a8a7463d
APHW2 MFGENG <nvidia@aphw2>
2017-11-09 06:16:41 -0800

    add a quick test to load omx

--- src/zm_videostore.cpp
+++ src/zm_videostore.cpp
@@ -150,7 +150,11 @@ VideoStore::VideoStore(
       Error("Could not allocate in frame");
       return;
     }
-    video_out_codec = avcodec_find_encoder(AV_CODEC_ID_H264);
+    video_out_codec = avcodec_find_encoder_by_name("h264_omx");
+    if ( ! video_out_codec ) {
+      Debug(1, "Didn't find omx");
+      video_out_codec = avcodec_find_encoder(AV_CODEC_ID_H264);
+    }
     if (!video_out_codec) {
       Fatal("Could not find codec for H264");
     }

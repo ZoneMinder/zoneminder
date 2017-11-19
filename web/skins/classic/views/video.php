b6a1885167c40e67f13f86c4a61216e83c04adcb
digital-gnome <31593470+digital-gnome@users.noreply.github.com>
2017-11-18 19:02:56 -0500

    fix generated video download (#2002)
    Fixes out of memory php error when downloading large generated video files

--- web/skins/classic/views/video.php
+++ web/skins/classic/views/video.php
@@ -101,6 +101,9 @@ if ( isset($_REQUEST['downloadIndex']) )
     header( "Content-Transfer-Encoding: binary" );
     header( "Content-Type: application/force-download" );
     header( "Content-Length: ".filesize($videoFiles[$downloadIndex]) ); 
+    while ( ob_get_level() > 0 ) {
+     ob_end_clean();
+    }
     readfile( $videoFiles[$downloadIndex] );
     exit;
 }

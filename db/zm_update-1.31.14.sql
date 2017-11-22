1fe421ed9d1b540c8a23b430ec7f4582cc98c774
Isaac Connor <iconnor@tesla.com>
2017-11-22 14:22:12 -0800

    add DiskSpace columns to Monitors

--- db/zm_update-1.31.14.sql
+++ db/zm_update-1.31.14.sql
@@ -14,12 +14,36 @@ EXECUTE stmt;
 SET @s = (SELECT IF(
     (SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS WHERE table_schema = DATABASE()
      AND table_name = 'Monitors'
+     AND column_name = 'TotalEventDiskSpace'
+    ) > 0,
+"SELECT 'Column TotalEventDiskSpace already exists in Monitors'",
+"ALTER TABLE `Monitors` ADD `TotalEventDiskSpace` BIGINT AFTER `TotalEvents`"
+));
+
+PREPARE stmt FROM @s;
+EXECUTE stmt;
+
+SET @s = (SELECT IF(
+    (SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS WHERE table_schema = DATABASE()
+     AND table_name = 'Monitors'
      AND column_name = 'HourEvents'
     ) > 0,
 "SELECT 'Column HourEvents already exists in Monitors'",
 "ALTER TABLE `Monitors` ADD `HourEvents` INT(10) AFTER `TotalEvents`"
 ));
 
+
+PREPARE stmt FROM @s;
+EXECUTE stmt;
+
+SET @s = (SELECT IF(
+    (SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS WHERE table_schema = DATABASE()
+     AND table_name = 'Monitors'
+     AND column_name = 'HourEventDiskSpace'
+    ) > 0,
+"SELECT 'Column HourEventDiskSpace already exists in Monitors'",
+"ALTER TABLE `Monitors` ADD `HourEventDiskSpace` BIGINT AFTER `HourEvents`"
+));
 
 PREPARE stmt FROM @s;
 EXECUTE stmt;
@@ -40,6 +64,17 @@ EXECUTE stmt;
 SET @s = (SELECT IF(
     (SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS WHERE table_schema = DATABASE()
      AND table_name = 'Monitors'
+     AND column_name = 'DayEventDiskSpace'
+    ) > 0,
+"SELECT 'Column DayEventDiskSpace already exists in Monitors'",
+"ALTER TABLE `Monitors` ADD `DayEventDiskSpace` BIGINT AFTER `DayEvents`"
+));
+
+PREPARE stmt FROM @s;
+EXECUTE stmt;
+SET @s = (SELECT IF(
+    (SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS WHERE table_schema = DATABASE()
+     AND table_name = 'Monitors'
      AND column_name = 'WeekEvents'
     ) > 0,
 "SELECT 'Column WeekEvents already exists in Monitors'",
@@ -53,7 +88,18 @@ EXECUTE stmt;
 SET @s = (SELECT IF(
     (SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS WHERE table_schema = DATABASE()
      AND table_name = 'Monitors'
-     AND column_name = 'WeekEvents'
+     AND column_name = 'WeekEventDiskSpace'
+    ) > 0,
+"SELECT 'Column WeekEventDiskSpace already exists in Monitors'",
+"ALTER TABLE `Monitors` ADD `WeekEventDiskSpace` BIGINT AFTER `WeekEvents`"
+));
+
+PREPARE stmt FROM @s;
+EXECUTE stmt;
+SET @s = (SELECT IF(
+    (SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS WHERE table_schema = DATABASE()
+     AND table_name = 'Monitors'
+     AND column_name = 'MonthEvents'
     ) > 0,
 "SELECT 'Column MonthEvents already exists in Monitors'",
 "ALTER TABLE `Monitors` ADD `MonthEvents` INT(10) AFTER `WeekEvents`"
@@ -63,3 +109,14 @@ SET @s = (SELECT IF(
 PREPARE stmt FROM @s;
 EXECUTE stmt;
 
+SET @s = (SELECT IF(
+    (SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS WHERE table_schema = DATABASE()
+     AND table_name = 'Monitors'
+     AND column_name = 'MonthEventDiskSpace'
+    ) > 0,
+"SELECT 'Column MonthEventDiskSpace already exists in Monitors'",
+"ALTER TABLE `Monitors` ADD `MonthEventDiskSpace` BIGINT AFTER `MonthEvents`"
+));
+
+PREPARE stmt FROM @s;
+EXECUTE stmt;

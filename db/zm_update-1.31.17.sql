0e2d2ce6119e7a549fc67295d5d8d92e8469f026
Isaac Connor <isaac@zoneminder.com>
2017-12-18 13:05:04 -0500

    fix typos

--- db/zm_update-1.31.17.sql
+++ db/zm_update-1.31.17.sql
@@ -1,7 +1,7 @@
 alter table Events modify Id int(10) unsigned;
 alter table Events DROP Primary key;
 alter table Events Add Primary key(Id);
-alter table Events modify Id int(10) unsigned auto_incremement;
+alter table Events modify Id int(10) unsigned auto_increment;
 
 SET @s = (SELECT IF(
     (SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS WHERE table_schema = DATABASE()
@@ -9,7 +9,7 @@ SET @s = (SELECT IF(
       AND column_name = 'Scheme'
     ) > 0,
     "SELECT 'Column Scheme already exists in Storage'",
-    "ALTER TABLE Storage ADD `Scheme   enum('Deep','Medium','Shallow') NOT NULL default 'Medium' AFTER `DiskSpace`"
+    "ALTER TABLE Storage ADD `Scheme`   enum('Deep','Medium','Shallow') NOT NULL default 'Medium' AFTER `DiskSpace`"
     ));
 
 PREPARE stmt FROM @s;
@@ -18,10 +18,10 @@ EXECUTE stmt;
 SET @s = (SELECT IF(
     (SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS WHERE table_schema = DATABASE()
       AND table_name = 'Events'
-      AND column_name = 'StorageScheme'
+      AND column_name = 'Scheme'
     ) > 0,
-    "SELECT 'Column StorageScheme already exists in Events'",
-    "ALTER TABLE Events ADD `StorageScheme   enum('Deep','Medium','Shallow') NOT NULL default 'Deep' AFTER `DiskSpace`"
+    "SELECT 'Column Scheme already exists in Events'",
+    "ALTER TABLE Events ADD `Scheme`   enum('Deep','Medium','Shallow') NOT NULL default 'Deep' AFTER `DiskSpace`"
     ));
 
 PREPARE stmt FROM @s;

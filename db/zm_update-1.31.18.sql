6c07347721065789470c570e151b44a6818a499f
Isaac Connor <isaac@zoneminder.com>
2018-01-01 13:19:16 -0500

    Fix Update DiskSpace Filter adding

--- db/zm_update-1.31.18.sql
+++ db/zm_update-1.31.18.sql
@@ -13,10 +13,10 @@ EXECUTE stmt;
 
 SET @s = (SELECT IF(
     (SELECT COUNT(*) FROM Filters WHERE Name = 'Update DiskSpace'
-      AND Query = '{"terms":[{"attr":"DiskSpace","op":"IS","val":"NULL"}],"sort_field":"Id","sort_asc":"1","limit":"100"}'
+      AND Query = '{"terms":[{"attr":"DiskSpace","op":"IS","val":"NULL"}]}'
     ) > 0,
     "SELECT 'Update Disk Space Filter already exists.'",
-    "INSERT INTO Filters (Name,Query,UpdateDiskSpace,Background) values ('Update DiskSpace','{\"terms\":[{\"attr\":\"DiskSpace\",\"op\":\"IS\",\"val\":\"NULL\"}],\"sort_field\":\"Id\",\"sort_asc\":\"1\",\"limit\":\"100\"}',1,1)"
+    "INSERT INTO Filters (Name,Query,UpdateDiskSpace,Background) values ('Update DiskSpace','{\"terms\":[{\"attr\":\"DiskSpace\",\"op\":\"IS\",\"val\":\"NULL\"}]}',1,1)"
     ));
 
 PREPARE stmt FROM @s;

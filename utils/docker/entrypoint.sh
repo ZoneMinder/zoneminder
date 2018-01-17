7b3f72d7a27487ede6b5a3c56286e788dd32c6ba
Andrew Bauer <zonexpertconsulting@outlook.com>
2018-01-16 20:02:18 -0600

    update service startup text

--- utils/docker/entrypoint.sh
+++ utils/docker/entrypoint.sh
@@ -61,7 +61,9 @@ fi
 service apache2 start
 
 # Start ZoneMinder
-/usr/local/bin/zmpkg.pl start && echo "ZoneMinder started"
+echo "* Starting ZoneMinder video surveillance recorder"
+/usr/local/bin/zmpkg.pl start
+echo "  ...done."
 
 # Stay in a loop to keep the container running
 while :

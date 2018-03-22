11cf3ce482cc14d5b66d9bdadef702f5d7f29421
Isaac Connor <isaac@zoneminder.com>
2018-03-22 11:33:24 -0400

    whitespace

--- src/zm_monitor.cpp
+++ src/zm_monitor.cpp
@@ -1571,12 +1571,12 @@ bool Monitor::Analyse() {
                 // lets construct alarm cause. It will contain cause + names of zones alarmed
                 std::string alarm_cause="";
                 for ( int i=0; i < n_zones; i++) {
-                    if (zones[i]->Alarmed()) {
-                        alarm_cause += std::string(zones[i]->Label());
-                        if (i < n_zones-1) {
-                            alarm_cause +=",";
-                        }
+                  if (zones[i]->Alarmed()) {
+                    alarm_cause += std::string(zones[i]->Label());
+                    if (i < n_zones-1) {
+                      alarm_cause +=",";
                     }
+                  }
                 } 
                 alarm_cause = cause+" "+alarm_cause;
                 strncpy( shared_data->alarm_cause,alarm_cause.c_str() , sizeof(shared_data->alarm_cause) );

49aeb4b467b635eee49fe067064b94b665b16176
Isaac Connor <iconnor@tesla.com>
2018-04-03 10:38:38 -0700

    whitespace

--- web/skins/classic/views/monitor.php
+++ web/skins/classic/views/monitor.php
@@ -18,8 +18,8 @@
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 //
 
-require_once( 'includes/Server.php');
-require_once( 'includes/Storage.php');
+require_once('includes/Server.php');
+require_once('includes/Storage.php');
 
 if ( !canView( 'Monitors' ) ) {
   $view = 'error';

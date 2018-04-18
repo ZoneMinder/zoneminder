bbee7fd0f29804ed47b20545644d56bacdec2e09
Isaac Connor <iconnor@tesla.com>
2018-04-18 14:17:41 -0700

    fix array_key_exists

--- web/includes/database.php
+++ web/includes/database.php
@@ -166,7 +166,7 @@ function dbFetchOne( $sql, $col=false, $params=NULL ) {
 
   if ( $result && $dbRow = $result->fetch(PDO::FETCH_ASSOC) ) {
     if ( $col ) {
-      if ( ! array_key_exists($col, $dbRow[$col]) ) {
+      if ( ! array_key_exists($col, $dbRow) ) {
         Warning("$col does not exist in the returned row " . print_r($dbRow, true));
       }
       return $dbRow[$col];

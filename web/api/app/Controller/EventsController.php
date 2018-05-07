1a012c62ff2a2436eb1e77eb4f120ab0e298efa8
Isaac Connor <iconnor@tesla.com>
2018-05-07 14:07:03 -0700

    Add fileExists to event view

--- web/api/app/Controller/EventsController.php
+++ web/api/app/Controller/EventsController.php
@@ -125,6 +125,8 @@ class EventsController extends AppController {
     $event['Event']['Next'] = $event_neighbors['next']['Event']['Id'];
     $event['Event']['Prev'] = $event_neighbors['prev']['Event']['Id'];
 
+    $event['Event']['fileExists'] = $this->Event->fileExists($event['Event']);
+
     # Also get the previous and next events for the same monitor
     $event_monitor_neighbors = $this->Event->find('neighbors', array(
       'conditions'=>array('Event.MonitorId'=>$event['Event']['MonitorId'])

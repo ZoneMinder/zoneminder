780cb693c25dcb5c42dd2b251ee7a33f5430e575
Isaac Connor <isaac@zoneminder.com>
2018-05-16 11:14:11 -0400

    google code style

--- web/skins/classic/views/js/controlpreset.js.php
+++ web/skins/classic/views/js/controlpreset.js.php
@@ -1,9 +1,8 @@
 var labels = new Array();
 <?php
-foreach ( $labels as $index=>$label )
-{
+foreach ( $labels as $index=>$label ) {
 ?>
-labels[<?php echo validInt($index) ?>] = "<?php echo validJsStr($label) ?>";
+labels[<?php echo validInt($index) ?>] = '<?php echo validJsStr($label) ?>';
 <?php
 }
 ?>

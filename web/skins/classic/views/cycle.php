184d6fa2ef9d4cd5a96e7b06c7f4d5082b7a0d92
Isaac Connor <isaac@zoneminder.com>
2018-07-11 15:51:04 -0400

    remove extra spaces

--- web/skins/classic/views/cycle.php
+++ web/skins/classic/views/cycle.php
@@ -18,7 +18,7 @@
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 //
 
-if ( !canView( 'Stream' ) ) {
+if ( !canView('Stream') ) {
   $view = 'error';
   return;
 }

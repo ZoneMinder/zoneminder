bfe20e444abca7daed88903b74e127bb07649a69
Pliable Pixels <pliablepixels@gmail.com>
2018-07-13 10:33:42 -0400

    add auth check back but leave out login/out

--- web/api/app/Controller/AppController.php
+++ web/api/app/Controller/AppController.php
@@ -27,51 +27,64 @@ App::uses('CrudControllerTrait', 'Crud.Lib');
  * Add your application-wide methods in the class below, your controllers
  * will inherit them.
  *
- * @package        app.Controller
- * @link        http://book.cakephp.org/2.0/en/controllers.html#the-app-controller
+ * @package   app.Controller
+ * @link    http://book.cakephp.org/2.0/en/controllers.html#the-app-controller
  */
-class AppController extends Controller
-{
-    use CrudControllerTrait;
+class AppController extends Controller {
+  use CrudControllerTrait;
 
-    public $components = [
-        'Session', //  We are going to use SessionHelper to check PHP session vars
-        'RequestHandler',
-        'Crud.Crud' => [
-            'actions' => [
-                'index' => 'Crud.Index',
-                'add' => 'Crud.Add',
-                'edit' => 'Crud.Edit',
-                'view' => 'Crud.View',
-                'keyvalue' => 'Crud.List',
-                'category' => 'Crud.Category',
-            ],
-            'listeners' => ['Api', 'ApiTransformation'],
-            #],
-            #'DebugKit.Toolbar' => [
-            #  'bootstrap' => true, 'routes' => true
-        ],
-    ];
+  public $components = [
+    'Session', //  We are going to use SessionHelper to check PHP session vars
+    'RequestHandler',
+    'Crud.Crud' => [
+      'actions' => [
+        'index' => 'Crud.Index',
+        'add'   => 'Crud.Add',
+        'edit'  => 'Crud.Edit',
+        'view'  => 'Crud.View',
+        'keyvalue' => 'Crud.List',
+        'category' => 'Crud.Category'
+      ],
+      'listeners' => ['Api', 'ApiTransformation']
+    #],
+    #'DebugKit.Toolbar' => [
+    #  'bootstrap' => true, 'routes' => true
+    ]
+  ];
 
-    // Global beforeFilter function
-    //Zoneminder sets the username session variable
-    // to the logged in user. If this variable is set
-    // then you are logged in
-    // its pretty simple to extend this to also check
-    // for role and deny API access in future
-    // Also checking to do this only if ZM_OPT_USE_AUTH is on
-    public function beforeFilter()
-    {
-        $this->loadModel('Config');
+  // Global beforeFilter function
+  //Zoneminder sets the username session variable
+  // to the logged in user. If this variable is set
+  // then you are logged in
+  // its pretty simple to extend this to also check
+  // for role and deny API access in future 
+  // Also checking to do this only if ZM_OPT_USE_AUTH is on
+  public function beforeFilter() {
+    $this->loadModel('Config');
+    
+    $options = array('conditions' => array('Config.' . $this->Config->primaryKey => 'ZM_OPT_USE_API'));
+    $config = $this->Config->find('first', $options);
+    $zmOptApi = $config['Config']['Value'];
 
-        $options = array('conditions' => array('Config.' . $this->Config->primaryKey => 'ZM_OPT_USE_API'));
-        $config = $this->Config->find('first', $options);
-        $zmOptApi = $config['Config']['Value'];
+    if ($zmOptApi !='1') {
+      throw new UnauthorizedException(__('API Disabled'));
+      return; 
+    }
+  // We need to reject methods that are not authenticated
+  // besides login and logout
+    if (strcasecmp($this->params->controller, "host") && 
+        strcasecmp($this->params->action, "login") &&
+        strcasecmp($this->params->action,"logout")) {
 
-        if ($zmOptApi != '1') {
-            throw new UnauthorizedException(__('API Disabled'));
-            return;
-        }
+          if (!$this->Session->read('user.Username')) {
+                throw new UnauthorizedException(__('Not Authenticated'));
+                return;
+            } else if (!$this->Session->read('user.Enabled')) {
+                throw new UnauthorizedException(__('User is not enabled'));
+                return;
+            } 
 
-    } # end function beforeFilter()
+    }
+   
+  } # end function beforeFilter()
 }

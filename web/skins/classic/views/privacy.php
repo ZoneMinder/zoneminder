f6bbc8b0a46a3c7163e320e8e94777b1fd654438
Andy Bauer <knnniggett@hotmail.com>
2018-08-05 14:52:17 -0500

    split the privacy text and run it through translate

--- web/skins/classic/views/privacy.php
+++ web/skins/classic/views/privacy.php
@@ -43,65 +43,26 @@ xhtmlHeaders(__FILE__, translate('Privacy') );
       <form name="contentForm" id="contentForm" method="post" action="<?php echo $_SERVER['PHP_SELF'] ?>">
         <input type="hidden" name="view" value="none"/>
         <input type="hidden" name="action" value="privacy"/>
-        <p>
-          <p><u>About</u></p>
-          <p>Since 2002, ZoneMinder has been the premier free and open-source Video Management System (VMS) solution for Linux platforms. ZoneMinder is supported by the community and is managed by those who choose to volunteer their spare time to the project. The best way to improve ZoneMinder is to get involved.</p>
-          <p><br/>
+        <p><u><?php echo translate('PrivacyAbout') ?></u></p>
+        <p><?php echo translate('PrivacyAboutText') ?></p>
+        <br>
 
-          </p>
-          <p><u>Contact</u></p>
-          <p>There are three primary ways to engage with the community:</p>
-          <p>    • The user forum at forums.zoneminder.com</p>
-          <p>    • The ZoneMinder Slack channel</p>
-          <p>    • A github forum exists for bug reporting only</p>
-          <p><br/>
+        <p><u><?php echo translate('PrivacyContact') ?></u></p>
+        <p><?php echo translate('PrivacyContactText') ?></p>
+        <br>
 
-          </p>
-          <p><u>Cookies</u></p>
-          <p>Whether you use a web browser or a mobile app to communicate with the ZoneMinder server, a ZMSESSID cookie is created on the client to uniquely identify a session with the ZoneMinder server. ZmCSS and zmSkin cookies are created to remember your style and skin choices. 
-          </p>
-          <p><br/>
+        <p><u><?php echo translate('PrivacyCookies') ?></u></p>
+        <p><?php echo translate('PrivacyCookiesText') ?></p>
+        <br>
 
-          </p>
-          <p><u>Telemetry</u></p>
-          <p>Because ZoneMinder is open-source, anyone can install it without registering. This makes it difficult to  answer questions such as: how many systems are out there, what is the largest system out there, what kind of systems are our there, or where are these systems located? Knowing the answers to these questions, helps new users who ask these questions, and it helps us set priorities based on the majority user base.  
-          </p>
-          <p><br/>
+        <p><u><?php echo translate('PrivacyTelemetry') ?></u></p>
+        <p><?php echo translate('PrivacyTelemetryText') ?></p>
+        <br>
 
-          </p>
-          <p>The ZoneMinder Telemetry daemon collects the following data about your system:</p>
-          <p>    • A unique identifier</p>
-          <p>    • City based location including city, region, country, latitude, and longitude</p>
-          <p>    • Current time</p>
-          <p>    • Total number of monitors</p>
-          <p>    • Total number of events</p>
-          <p>    • System architecture</p>
-          <p>    • Operating system kernel, distro, and distro version</p>
-          <p>    • Version of ZoneMinder</p>
-          <p>    • Total amount of memory</p>
-          <p>    • Number of cpu cores</p>
-          <p>    • The following configuration parameters from each monitor are collected:</p>
-          <p>        ◦ Id</p>
-          <p>        ◦ Name</p>
-          <p>        ◦ Type</p>
-          <p>        ◦ Function</p>
-          <p>        ◦ Width</p>
-          <p>        ◦ Height</p>
-          <p>        ◦ Colours</p>
-          <p>        ◦ MaxFPS</p>
-          <p>        ◦ AlarmMaxFPS</p>
-          <p><br/>
+        <p><?php echo translate('PrivacyTelemetryText') ?></p>
+        <p><?php echo translate('PrivacyConclusionText') ?></p>
+        <p><?php echo buildSelect( "option", $options ); ?></p>
 
-          </p>
-          <p>We are <u>NOT</u> collecting any image specific data from your cameras. We don’t know what your cameras are watching. This data will not be sold or used for any purpose not stated herein.</p>
-          <p><br/>
-
-          </p>
-          <p>By clicking accept, you agree to send us this data to help make ZoneMinder a better product. By clicking decline, you can still freely use ZoneMinder and all its features.</p>
-        </p>
-        <p>
-          <?php echo buildSelect( "option", $options ); ?>
-        </p>
         <div id="contentButtons">
           <input type="submit" value="<?php echo translate('Apply') ?>" onclick="submitForm( this )">
           <input type="button" value="<?php echo translate('Cancel') ?>" onclick="closeWindow()">

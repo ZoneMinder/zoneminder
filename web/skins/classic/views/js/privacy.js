56f4d768c2783081e5918f9c5128a1b630d67c2c
Andrew Bauer <zonexpertconsulting@outlook.com>
2018-08-13 14:23:44 -0500

    Add Privacy Statement (#2176)
    * initial implementation of privacy popup
    
    * split the privacy text and run it through translate
    
    * change style of toggle button, validate the form
    
    * fix copy/paste error
    
    * fix typos
    
    * display privacy view inline rather than popup
    
    * display privacy inline if show_privacy flag set
    
    * redirect to console after selection is made
    
    * typo
    
    * css formatting
    
    * update privacy verbiage
    
    * push privacy text to all language files

---
+++ web/skins/classic/views/js/privacy.js
@@ -0,0 +1,9 @@
+function submitForm( element ) {
+  var form = element.form;
+  if ( form.option.selectedIndex == 0 )
+    form.view.value = currentView;
+  else
+    form.view.value = 'none';
+  form.submit();
+}
+

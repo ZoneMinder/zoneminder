bb70c9f14e3a7a62261678a52614eca8d51967c9
Isaac Connor <isaac@zoneminder.com>
2018-08-15 08:28:44 -0400

    Cleanup Licenses. Fixes #1276

--- scripts/ZoneMinder/lib/ZoneMinder/Control/Trendnet.pm
+++ scripts/ZoneMinder/lib/ZoneMinder/Control/Trendnet.pm
@@ -1,8 +1,7 @@
 #===========================================================================
 #
-# ZoneMinder Trendnet IP Control Protocol Module, $Date: $, $Revision: $
-# Copyright (C) 2018 ZoneMinder LLC8 ZoneMinder LLC8 ZoneMinder LLC8 ZoneMinder LLC8 ZoneMinder LLC8 ZoneMinder LLC8 ZoneMinder LLC8 ZoneMinder LLC
-#
+# ZoneMinder Trendnet IP Control Protocol Module
+# Copyright (C) 2018 ZoneMinder ZoneMinder LLC
 #
 # ==========================================================================
 #
@@ -422,7 +421,6 @@ sub reset {
 
 1;
 __END__
-# Below is stub documentation for your module. You'd better edit it!
 
 =head1 NAME
 
@@ -435,19 +433,6 @@ ZoneMinder::Control::Trendnet - Perl module for Trendnet cameras
 
 =head1 DESCRIPTION
 
-Stub documentation for Trendnet PTZ Sctrol
-
-=head2 EXPORT
-
-None by default.
-
-=head1 COPYRIGHT AND LICENSE
-
-Copyright (C) 2018 by ZoneMinder LLC
-
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself, either Perl version 5.8.3 or,
-at your option, any later version of Perl 5 you may have available.
-
+Module for accessing the PTZ functions of Trendnet PTZ IP Cameras
 
 =cut

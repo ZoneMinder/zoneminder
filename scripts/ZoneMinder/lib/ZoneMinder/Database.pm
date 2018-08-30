b1fe80a0bd6683c3672f7e046f0242451cdad917
Isaac Connor <isaac@zoneminder.com>
2018-08-30 18:30:33 -0400

    Add JSON/MaybeXS to dependencies, replacing JSON/Any

--- scripts/ZoneMinder/lib/ZoneMinder/Database.pm
+++ scripts/ZoneMinder/lib/ZoneMinder/Database.pm
@@ -232,11 +232,11 @@ sub start_transaction {
 
 sub end_transaction {
 	#my ( $caller, undef, $line ) = caller;
-#$openprint::log->debug("Called end_transaction from $caller : $line");
-	my ( $d, $ac ) = @_;
-if ( ! defined $ac ) {
-	Error("Undefined ac");
-}
+  #$openprint::log->debug("Called end_transaction from $caller : $line");
+  my ( $d, $ac ) = @_;
+  if ( ! defined $ac ) {
+    Error("Undefined ac");
+  }
 	$d = $dbh if ! $d;
 	if ( $ac ) {
 		#$log->debug("Committing");
@@ -244,55 +244,31 @@ if ( ! defined $ac ) {
 	} # end if
 	$d->{AutoCommit} = $ac;
 } # end sub end_transaction
+
 1;
 __END__
-# Below is stub documentation for your module. You'd better edit it!
 
 =head1 NAME
 
-ZoneMinder::Database - Perl extension for blah blah blah
+ZoneMinder::Database - Perl module containing database functions used in ZM
 
 =head1 SYNOPSIS
 
 use ZoneMinder::Database;
-blah blah blah
 
 =head1 DESCRIPTION
 
-Stub documentation for ZoneMinder, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
-
-Blah blah blah.
 
 =head2 EXPORT
 
-None by default.
-
-
-
-=head1 SEE ALSO
-
-Mention other useful documentation such as the documentation of
-related modules or operating system documentation (such as man pages
-in UNIX), or any relevant external documentation such as RFCs or
-standards.
-
-If you have a mailing list set up for your module, mention it here.
-
-If you have a web site set up for your module, mention it here.
+zmDbConnect
+zmDbDisconnect
+zmDbGetMonitors
+zmDbGetMonitor
+zmDbGetMonitorAndControl
 
 =head1 AUTHOR
 
 Philip Coombes, E<lt>philip.coombes@zoneminder.comE<gt>
-
-=head1 COPYRIGHT AND LICENSE
-
-Copyright (C) 2001-2008  Philip Coombes
-
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself, either Perl version 5.8.3 or,
-at your option, any later version of Perl 5 you may have available.
-
 
 =cut

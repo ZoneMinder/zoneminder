77e0ebc424e0f61e8292d252b9bdccfac5531eab
Jonathan Lassoff <jof@planet.com>
2018-09-04 10:47:58 +0000

    Remove license from top of file

--- scripts/ZoneMinder/lib/ZoneMinder/Control/Dahua.pm
+++ scripts/ZoneMinder/lib/ZoneMinder/Control/Dahua.pm
@@ -1,23 +1,3 @@
-# =========================================================================
-#
-# Dahua IP Camera HTTP API Control Module, $Date: $, $Revision: $
-# Jonathan Lassoff (jof@thejof.com) -- 2018
-#
-#
-# ==========================================================================
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-#
-# ==========================================================================
-#
-
 package ZoneMinder::Control::Dahua;
 
 use 5.8.0;

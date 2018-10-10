48e805c09fbfe0bf8912e5b14b012fb571e1b81a
Isaac Connor <isaac@zoneminder.com>
2018-10-10 13:11:27 -0400

    Make header flips perisisent based on cookie

--- web/skins/classic/includes/functions.php
+++ web/skins/classic/includes/functions.php
@@ -124,6 +124,7 @@ echo output_link_if_exists( array(
     jQuery("#flip").click(function(){
       jQuery("#panel").slideToggle("slow");
       jQuery("#flip").toggleClass('glyphicon-menu-down').toggleClass('glyphicon-menu-up');
+      Cookie.write( 'zmHeaderFlip', jQuery('#flip').hasClass('glyphicon-menu-up') ? 'up' : 'down', { duration: 10*365 } );
     });
   });
   </script>
@@ -303,7 +304,7 @@ if (isset($_REQUEST['filter']['Query']['terms']['attr'])) {
   }
 ?>
       <li><a href="?view=report_event_audit"<?php echo $view=='report_event_audit'?' class="selected"':''?>><?php echo translate('ReportEventAudit') ?></a></li>
-      <li><a href="#"><span id="flip" class="glyphicon glyphicon-menu-up pull-right"></span></a></li>
+      <li><a href="#"><span id="flip" class="glyphicon glyphicon-menu-<?php echo ( isset($_COOKIE['zmHeaderFlip']) and $_COOKIE['zmHeaderFlip'] == 'down') ? 'down' : 'up' ?> pull-right"></span></a></li>
 		</ul>
 <?php } // end if canView('Monitors') ?>
 
@@ -321,7 +322,7 @@ if (isset($_REQUEST['filter']['Query']['terms']['attr'])) {
 </div>
 		</div><!-- End .navbar-collapse -->
 	</div> <!-- End .container-fluid -->
-    <div id="panel">
+  <div id="panel"<?php echo ( isset($_COOKIE['zmHeaderFlip']) and $_COOKIE['zmHeaderFlip'] == 'down' ) ? 'style="display:none;"' : '' ?>>
 <?php
 }//end reload null.  Runs on full page load
 

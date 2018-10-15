1cf512fc8b42cd835cf8c70380b51882fefe4adc
Verdi R-D <verdi@azend.org>
2018-10-15 13:16:10 -0400

    moved update to new database update version

--- db/zm_update-1.32.2.sql
+++ db/zm_update-1.32.2.sql
@@ -1,9 +1,5 @@
 --
 -- This updates a 1.32.1 database to 1.32.2
 --
-
+-- No changes required
 --
--- Add some additional monitor preset values
---
-
-INSERT INTO MonitorPresets VALUES (NULL,'D-link DCS-930L, 640x480, mjpeg','Remote','http',0,0,'http','simple','<ip-address>',80,'/mjpeg.cgi',NULL,640,480,3,NULL,0,NULL,NULL,NULL,100,100);

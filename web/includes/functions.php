91d83a89fa1909d27ad248e901151584823c877b
Isaac Connor <isaac@zoneminder.com>
2018-10-25 15:40:12 -0400

    include semaphore function replacements

--- web/includes/functions.php
+++ web/includes/functions.php
@@ -2273,4 +2273,30 @@ function unparse_url($parsed_url, $substitutions = array() ) {
   $fragment = isset($parsed_url['fragment']) ? '#' . $parsed_url['fragment'] : ''; 
   return "$scheme$user$pass$host$port$path$query$fragment"; 
 }
+
+// The following works around php not being built with semaphore functions.
+if (!function_exists('sem_get')) {
+  function sem_get($key) {
+    return fopen(__FILE__ . '.sem.' . $key, 'w+');
+  }
+  function sem_acquire($sem_id) {
+    return flock($sem_id, LOCK_EX);
+  }
+  function sem_release($sem_id) {
+    return flock($sem_id, LOCK_UN);
+  }
+}
+
+if( !function_exists('ftok') ) {
+  function ftok($filename = "", $proj = "") {
+    if ( empty($filename) || !file_exists($filename) ) {
+      return -1;
+    } else {
+      $filename = $filename . (string) $proj;
+      for($key = array(); sizeof($key) < strlen($filename); $key[] = ord(substr($filename, sizeof($key), 1)));
+      return dechex(array_sum($key));
+    }
+  }
+}
+
 ?>

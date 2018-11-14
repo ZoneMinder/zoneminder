490fb40917eef7b9fc47935bc1e17f09fcc6ee36
Isaac Connor <isaac@zoneminder.com>
2018-11-14 09:22:38 -0500

    DefaultView in the past was used to control whether the watch view starts up with ptz controls visible or the events list.  Isaac changed the watch view to always have the controls visible... so this setting is no longer useful.

--- db/zm_update-1.32.3.sql
+++ db/zm_update-1.32.3.sql
@@ -7,3 +7,5 @@
 --
 
 INSERT INTO MonitorPresets VALUES (NULL,'D-link DCS-930L, 640x480, mjpeg','Remote','http',0,0,'http','simple','<ip-address>',80,'/mjpeg.cgi',NULL,640,480,3,NULL,0,NULL,NULL,NULL,100,100);
+
+ALTER TABLE Monitors DROP COLUMN DefaultView;

4ca7bce4ea43a5f07d326bb64716be54351f8088
regularnmae <z.asanishvili@gmail.com>
2018-11-20 03:29:57 +0400

    modified:   web/skins/classic/views/events.php

--- web/skins/classic/views/events.php
+++ web/skins/classic/views/events.php
@@ -291,7 +291,7 @@ if ( $pagination ) {
           <?php echo translate('DownloadVideo') ?>
           </button>
           <button type="button" name="generateBtn" value="GenerateVideo" onclick="generateVideo(this, 'markEids')" disabled="disabled">
-          <?php echo 'generate video' ?>
+          <?php echo translate('GenerateVideo') ?>
           </button>
           <button type="button" name="deleteBtn" value="Delete" onclick="deleteEvents(this, 'markEids');" disabled="disabled">
           <?php echo translate('Delete') ?>

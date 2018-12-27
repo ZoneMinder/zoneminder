a51aaf6055a0bf5efd942d37548c6eb369a4095d
Isaac Connor <isaac@zoneminder.com>
2018-12-27 15:20:51 -0500

    include includes/functions.php so that we have access to all it's contents

--- web/api/app/Controller/AppController.php
+++ web/api/app/Controller/AppController.php
@@ -101,6 +101,7 @@ class AppController extends Controller {
         }
       } # end if ! login or logout
     } # end if ZM_OPT_AUTH
+      require_once '../../../includes/functions.php';
    
   } # end function beforeFilter()
 }

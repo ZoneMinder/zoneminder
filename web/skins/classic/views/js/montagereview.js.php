ac270059448571d1078f35799e02a729f9600e79
Isaac Connor <isaac@zoneminder.com>
2019-01-15 11:38:43 -0500

    remove debug

--- web/skins/classic/views/js/montagereview.js.php
+++ web/skins/classic/views/js/montagereview.js.php
@@ -127,7 +127,6 @@ echo "\nvar Servers = [];\n";
 $Server = new Server();
 echo 'Servers[0] = new Server(' . json_encode($Server). ");\n";
 foreach ( Server::find() as $Server ) {
-  echo '//'.$Server->PathToIndex()."\n"; // Otherwise get null when json_encoding
   echo 'Servers[' . $Server->Id() . '] = new Server(' . $Server->to_json(). ");\n";
 }
 

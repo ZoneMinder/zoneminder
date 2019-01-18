84a7e480932db19a9f65ba8e0826f763ac096acb
Matthew Noorenberghe <github@matthew.noorenberghe.com>
2019-01-17 23:38:09 -0800

    Add linting of JS in .php files

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,19 @@ module.exports = {
     "browser": true,
   },
   "extends": ["google"],
+  "overrides": [{
+    // eslint-plugin-html handles eol-last slightly different - it applies to
+    // each set of script tags, so we turn it off here.
+    "files": "**/*.*php",
+    "rules": {
+      "eol-last": "off",
+      "indent": "off",
+    },
+  }],
+  "plugins": [
+    "html",
+    "php-markup",
+  ],
   "rules": {
     "camelcase": "off",
     "comma-dangle": "off",
@@ -25,5 +38,13 @@ module.exports = {
     "quotes": "off",
     "require-jsdoc": "off",
     "spaced-comment": "off",
+  },
+  "settings": {
+    "php/php-extensions": [".php"],
+    "php/markup-replacement": {"php": "", "=": "0"},
+    "php/keep-eol": false,
+    "php/remove-whitespace": false,
+    "php/remove-empty-line": false,
+    "php/remove-php-lint": false
   },
 };

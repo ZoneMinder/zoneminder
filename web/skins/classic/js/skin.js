d3f8037e5868e51d53bf4c1d0b56773569f46df6
Matt N <github@matthew.noorenberghe.com>
2019-01-19 06:42:12 -0800

    Replace onclick='submitTab(...' with a click listener (#2424)

--- web/skins/classic/js/skin.js
+++ web/skins/classic/js/skin.js
@@ -143,6 +143,8 @@ $j(document).ready(function() {
     evt.preventDefault();
   });
 
+  $j(".tabList a").click(submitTab);
+
   // 'data-on-click-this' calls the global function in the attribute value with the element when a click happens.
   document.querySelectorAll("a[data-on-click-this], button[data-on-click-this], input[data-on-click-this]").forEach(function attachOnClick(el) {
     var fnName = el.getAttribute("data-on-click-this");
@@ -309,11 +311,13 @@ function secsToTime( seconds ) {
   return( timeString );
 }
 
-function submitTab( tab ) {
+function submitTab(evt) {
+  var tab = this.getAttribute("data-tab-name");
   var form = $('contentForm');
   form.action.value = "";
   form.tab.value = tab;
   form.submit();
+  evt.preventDefault();
 }
 
 function submitThisForm() {

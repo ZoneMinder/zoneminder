c8e41bfee75cb0a70e69d8f1488aa948fc31831a
Matthew Noorenberghe <github@matthew.noorenberghe.com>
2019-02-10 00:10:39 -0800

    log.php: Ensure 'line' is an integer. Helps with #2466

--- web/ajax/log.php
+++ web/ajax/log.php
@@ -15,7 +15,7 @@ switch ( $_REQUEST['task'] ) {
 
       $file = !empty($_POST['file']) ? preg_replace( '/\w+:\/\/[\w.:]+\//', '', $_POST['file'] ) : '';
       if ( !empty( $_POST['line'] ) )
-        $line = $_POST['line'];
+        $line = validInt($_POST['line']);
       else
         $line = NULL;
 

ef0e5f453a4e60a5bdd6bc347e517a87182b6cad
Matthew Noorenberghe <github@matthew.noorenberghe.com>
2019-02-09 17:11:53 -0800

    monitor.php: Fix XSS from LinkedMonitors. Fixes #2463

--- web/skins/classic/views/monitor.php
+++ web/skins/classic/views/monitor.php
@@ -522,7 +522,7 @@ foreach ( $tabs as $name=>$value ) {
       <input type="hidden" name="tab" value="<?php echo $tab ?>"/>
       <input type="hidden" name="action" value="monitor"/>
       <input type="hidden" name="mid" value="<?php echo $monitor->Id()?>"/>
-      <input type="hidden" name="newMonitor[LinkedMonitors]" value="<?php echo (null !== $monitor->LinkedMonitors())?$monitor->LinkedMonitors():'' ?>"/>
+      <input type="hidden" name="newMonitor[LinkedMonitors]" value="<?php echo (null !== $monitor->LinkedMonitors())?validHtmlStr($monitor->LinkedMonitors()):'' ?>"/>
       <input type="hidden" name="origMethod" value="<?php echo ( null !== $monitor->Method())?validHtmlStr($monitor->Method()):'' ?>"/>
 <?php
 if ( $tab != 'general' ) {

3b1f9779fa37bc325fb30157f3212fe0eb62b181
Isaac Connor <isaac@zoneminder.com>
2019-02-12 14:45:30 -0500

    experiment with namespaces on the Server class

--- web/skins/classic/views/_monitor_filters.php
+++ web/skins/classic/views/_monitor_filters.php
@@ -18,7 +18,7 @@
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 //
 
-$servers = Server::find(null, array('order'=>'lower(Name)'));
+$servers = ZM\Server::find(null, array('order'=>'lower(Name)'));
 $ServersById = array();
 foreach ( $servers as $S ) {
   $ServersById[$S->Id()] = $S;

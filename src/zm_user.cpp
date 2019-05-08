3a67217972d73d2ddcfe5a222604cdbd04b084ff
Pliable Pixels <pliablepixels@gmail.com>
2019-05-08 11:29:34 -0400

    move decode inside try/catch

--- src/zm_user.cpp
+++ src/zm_user.cpp
@@ -150,56 +150,58 @@ User *zmLoadTokenUser (std::string jwt_token_str, bool use_remote_addr ) {
     }
     key += remote_addr;
   }
-    
 
   Info ("Inside zmLoadTokenUser, formed key=%s", key.c_str());
 
-  auto decoded = jwt::decode(jwt_token_str);
-
-  auto verifier = jwt::verify()
-                      .allow_algorithm(jwt::algorithm::hs256{ key })
-                      .with_issuer("ZoneMinder");
   try {
+
+    auto decoded = jwt::decode(jwt_token_str);
+    auto verifier = jwt::verify()
+                        .allow_algorithm(jwt::algorithm::hs256{ key })
+                        .with_issuer("ZoneMinder");
+  
     verifier.verify(decoded);
-  } 
+    
+    // token is valid and not expired
+    if (decoded.has_payload_claim("user")) {
+
+      // We only need to check if user is enabled in DB and pass on
+      // correct access permissions
+      std::string username = decoded.get_payload_claim("user").as_string();
+      Info ("Got %s as user claim from token", username.c_str());
+      char sql[ZM_SQL_MED_BUFSIZ] = "";
+      snprintf(sql, sizeof(sql),
+        "SELECT Id, Username, Enabled, Stream+0, Events+0, Control+0, Monitors+0, System+0, MonitorIds"
+        " FROM Users where Username = '%s' and Enabled = 1", username.c_str() );
+
+      MYSQL_RES *result = mysql_store_result(&dbconn);
+      if ( !result ) {
+        Error("Can't use query result: %s", mysql_error(&dbconn));
+        exit(mysql_errno(&dbconn));
+      }
+      int n_users = mysql_num_rows(result);
+
+      if ( n_users != 1 ) {
+        mysql_free_result(result);
+        Warning("Unable to authenticate user %s", username);
+        return NULL;
+      }
+
+      MYSQL_ROW dbrow = mysql_fetch_row(result);
+      User *user = new User(dbrow);
+      Info ("Authenticated user '%s' via token", username.c_str());
+      return user;
+
+    }
+    else {
+      Error ("User not found in claim");
+      return 0;
+    }
+
+  }
   catch (const Exception &e) {
       Error( "Unable to verify token: %s", e.getMessage().c_str() );
       return 0;
-  }
-  // token is valid and not expired
-  if (decoded.has_payload_claim("user")) {
-
-    // We only need to check if user is enabled in DB and pass on
-    // correct access permissions
-    std::string username = decoded.get_payload_claim("user").as_string();
-    Info ("Got %s as user claim from token", username.c_str());
-    char sql[ZM_SQL_MED_BUFSIZ] = "";
-    snprintf(sql, sizeof(sql),
-      "SELECT Id, Username, Enabled, Stream+0, Events+0, Control+0, Monitors+0, System+0, MonitorIds"
-      " FROM Users where Username = '%s' and Enabled = 1", username.c_str() );
-
-    MYSQL_RES *result = mysql_store_result(&dbconn);
-    if ( !result ) {
-      Error("Can't use query result: %s", mysql_error(&dbconn));
-      exit(mysql_errno(&dbconn));
-    }
-    int n_users = mysql_num_rows(result);
-
-    if ( n_users != 1 ) {
-      mysql_free_result(result);
-      Warning("Unable to authenticate user %s", username);
-      return NULL;
-    }
-
-    MYSQL_ROW dbrow = mysql_fetch_row(result);
-    User *user = new User(dbrow);
-    Info ("Authenticated user '%s' via token", username.c_str());
-    return user;
-
-  }
-  else {
-    Error ("User not found in claim");
-    return 0;
   }
 }
 

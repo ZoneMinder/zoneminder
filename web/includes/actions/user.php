628760d5b9ad63ed7abefeab2d1ddd00beb4adee
Isaac Connor <isaac@zoneminder.com>
2019-05-29 10:29:03 -0400

    Spacing and braces cleanup from asker's code

--- web/includes/actions/user.php
+++ web/includes/actions/user.php
@@ -32,16 +32,15 @@ if ( $action == 'user' ) {
       $pass_hash = '"'.password_hash($pass, PASSWORD_BCRYPT).'"';
     } else {
       $pass_hash = ' PASSWORD('.dbEscape($_REQUEST['newUser']['Password']).') ';
-      ZM\Info ('Cannot use bcrypt as you are using PHP < 5.5');
+      ZM\Info('Cannot use bcrypt as you are using PHP < 5.5');
     }
    
     if ( $_REQUEST['newUser']['Password'] ) {
       $changes['Password'] = 'Password = '.$pass_hash;
-      ZM\Info ("PASS CMD=".$changes['Password']);
-    }
-      
-    else
+      ZM\Info('PASS CMD='.$changes['Password']);
+    } else {
       unset($changes['Password']);
+    }
 
     if ( count($changes) ) {
       if ( !empty($_REQUEST['uid']) ) {
@@ -69,10 +68,9 @@ if ( $action == 'user' ) {
       $pass_hash = ' PASSWORD('.dbEscape($_REQUEST['newUser']['Password']).') ';
       ZM\Info ('Cannot use bcrypt as you are using PHP < 5.3');
     }
-   
 
     if ( !empty($_REQUEST['newUser']['Password']) ) {
-      ZM\Info ("PASS CMD=".$changes['Password']);
+      ZM\Info('PASS CMD='.$changes['Password']);
       $changes['Password'] = 'Password = '.$pass_hash;
     }
       

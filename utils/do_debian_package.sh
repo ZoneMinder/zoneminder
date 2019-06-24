7914583a9e982a7623aea7f0c01bf02481ac07e7
Isaac Connor <isaac@zoneminder.com>
2019-06-24 11:50:58 -0400

    Add support for specifying PPA

--- utils/do_debian_package.sh
+++ utils/do_debian_package.sh
@@ -30,6 +30,10 @@ case $i in
     INTERACTIVE="${i#*=}"
     shift # past argument=value
     ;;
+    -p=*|--ppa=*)
+    PPA="${i#*=}"
+    shift # past argument=value
+    ;;
     -r=*|--release=*)
     RELEASE="${i#*=}"
     shift
@@ -120,20 +124,21 @@ else
   fi;
 fi
 
-PPA="";
-if [ "$RELEASE" != "" ]; then
-  # We need to use our official tarball for the original source, so grab it and overwrite our generated one.
-  IFS='.' read -r -a VERSION <<< "$RELEASE"
-  if [ "${VERSION[0]}.${VERSION[1]}" == "1.30" ]; then
-    PPA="ppa:iconnor/zoneminder-stable"
+if [ "$PPA" == "" ]; then
+  if [ "$RELEASE" != "" ]; then
+    # We need to use our official tarball for the original source, so grab it and overwrite our generated one.
+    IFS='.' read -r -a VERSION <<< "$RELEASE"
+    if [ "${VERSION[0]}.${VERSION[1]}" == "1.30" ]; then
+      PPA="ppa:iconnor/zoneminder-stable"
+    else
+      PPA="ppa:iconnor/zoneminder-${VERSION[0]}.${VERSION[1]}"
+    fi;
   else
-    PPA="ppa:iconnor/zoneminder-${VERSION[0]}.${VERSION[1]}"
-  fi;
-else
-  if [ "$BRANCH" == "" ]; then
-    PPA="ppa:iconnor/zoneminder-master";
-  else 
-    PPA="ppa:iconnor/zoneminder-$BRANCH";
+    if [ "$BRANCH" == "" ]; then
+      PPA="ppa:iconnor/zoneminder-master";
+    else
+      PPA="ppa:iconnor/zoneminder-$BRANCH";
+    fi;
   fi;
 fi;
 
@@ -327,6 +332,7 @@ EOF
         dput $PPA $SC
       fi;
     else
+      echo "dputting to $PPA";
         dput $PPA $SC
     fi;
   fi;

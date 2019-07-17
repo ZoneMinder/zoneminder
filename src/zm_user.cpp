f98bca5849977c8fbd239c8ad412e27844b933b3
Pliable Pixels <pliablepixels@gmail.com>
2019-07-17 15:56:21 -0400

    clearer debug logs for tokens

--- src/zm_user.cpp
+++ src/zm_user.cpp
@@ -157,6 +157,7 @@ User *zmLoadTokenUser (std::string jwt_token_str, bool use_remote_addr ) {
   std::pair<std::string, unsigned int> ans = verifyToken(jwt_token_str, key);
   std::string username = ans.first;
   unsigned int iat = ans.second;
+  Debug (1,"retrieved user '%s'from token", username.c_str());
 
   if (username != "") {
     char sql[ZM_SQL_MED_BUFSIZ] = "";
@@ -178,7 +179,7 @@ User *zmLoadTokenUser (std::string jwt_token_str, bool use_remote_addr ) {
 
     if ( n_users != 1 ) {
       mysql_free_result(result);
-      Error("Unable to authenticate user %s", username.c_str());
+      Error("Unable to authenticate user '%s'", username.c_str());
       return NULL;
     }
 
@@ -188,7 +189,7 @@ User *zmLoadTokenUser (std::string jwt_token_str, bool use_remote_addr ) {
 
     if (stored_iat > iat ) { // admin revoked tokens
       mysql_free_result(result);
-      Error("Token was revoked for %s", username.c_str());
+      Error("Token was revoked for '%s'", username.c_str());
       return NULL;
     }
 

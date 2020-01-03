e951b0af1cb097dc58d322c19b45680a07b39bbc
Isaac Connor <isaac@zoneminder.com>
2020-01-03 16:35:04 -0500

    add code to set default value for checkboxes

--- web/includes/actions/controlcap.php
+++ web/includes/actions/controlcap.php
@@ -28,6 +28,65 @@ if ( $action == 'controlcap' ) {
   require_once('includes/Control.php');
   $Control = new ZM\Control( !empty($_REQUEST['cid']) ? $_REQUEST['cid'] : null );
 
+  $field_defaults = array(
+    'CanWake' =>  0,
+    'CanSleep'  =>  0,
+    'CanReset'  =>  0,
+    'CanReboot' =>  0,
+    'CanMove' => 0,
+    'CanMoveDiag' => 0,
+    'CanMoveMap' => 0,
+    'CanMoveRel' => 0,
+    'CanMoveAbs' => 0,
+    'CanMoveCon' => 0,
+    'CanPan'    =>  0,
+    'HasPanSpeed' =>  0,
+    'HasTurboPan' =>  0,
+    'CanTilt'     =>  0,
+    'HasTiltSpeed'  =>  0,
+    'HasTurboTilt'  =>  0,
+    'CanZoom' => 0,
+    'CanZoomRel' => 0,
+    'CanZoomAbs' => 0,
+    'CanZoomCon' => 0,
+    'HasZoomSpeed'  =>  0,
+    'CanFocus' => 0,
+    'CanAutoFocus' => 0,
+    'CanFocusRel' => 0,
+    'CanFocusAbs' => 0,
+    'CanFocusCon' => 0,
+    'HasFocusSpeed'  =>  0,
+    'CanGain' => 0,
+    'CanAutoGain' => 0,
+    'CanGainRel' => 0,
+    'CanGainAbs' => 0,
+    'CanGainCon' => 0,
+    'HasGainSpeed'  =>  0,
+    'CanWhite' => 0,
+    'CanAutoWhite' => 0,
+    'CanWhiteRel' => 0,
+    'CanWhiteAbs' => 0,
+    'CanWhiteCon' => 0,
+    'HasWhiteSpeed'  =>  0,
+    'CanIris' => 0,
+    'CanAutoIris' => 0,
+    'CanIrisRel' => 0,
+    'CanIrisAbs' => 0,
+    'CanIrisCon' => 0,
+    'HasIrisSpeed'  =>  0,
+    'HasPresets'    =>  0,
+    'HasHomePreset' =>  0,
+    'CanSetPresets' =>  0,
+  );
+
+  # Checkboxes don't return an element in the POST data, so won't be present in newControl.
+  # So force a value for these fields
+  foreach ( $field_defaults as $field => $value ) {
+    if ( ! (isset($_REQUEST['newControl'][$field]) and $_REQUEST['newControl'][$field]) ) {
+      $_REQUEST['newControl'][$field] = $value;
+    }
+  } # end foreach type
+
   //$changes = getFormChanges( $control, $_REQUEST['newControl'], $types, $columns );
   $Control->save($_REQUEST['newControl']);
   $refreshParent = true;

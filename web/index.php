6345ecc479a17ef3b5469f2aae1d8e86761ceb9a
Isaac Connor <isaac@zoneminder.com>
2020-01-23 17:04:33 -0500

    remove debug

--- web/index.php
+++ web/index.php
@@ -245,7 +245,6 @@ if ( ZM_OPT_USE_AUTH and (!isset($user)) and ($view != 'login') and ($view != 'n
   if ( ! $request ) {
     zm_session_start();
     $_SESSION['postLoginQuery'] = $_SERVER['QUERY_STRING'];
-    ZM\Error("postLoginQuery " . $_SESSION['postLoginQuery']);
     session_write_close();
   }
   $request = null;

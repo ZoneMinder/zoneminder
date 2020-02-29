81d329af2f644d92caa1bd1dcb14bca9f95392fd
Isaac Connor <iconnor@connortechnology.com>
2020-02-29 09:28:04 -0500

    spacing, eslint

--- web/skins/classic/views/js/video.js
+++ web/skins/classic/views/js/video.js
@@ -1,11 +1,11 @@
-function deleteVideo( e ) {
+function deleteVideo(e) {
   index = e.getAttribute('data-file-index');
-  window.location.replace( thisUrl+'?view='+currentView+'&eid='+eventId+'&deleteIndex='+index );
+  window.location.replace(thisUrl+'?view='+currentView+'&eid='+eventId+'&deleteIndex='+index);
 }
 
-function downloadVideo( e ) {
+function downloadVideo(e) {
   index = e.getAttribute('data-file-index');
-  window.location.replace( thisUrl+'?view='+currentView+'&eid='+eventId+'&downloadIndex='+index );
+  window.location.replace(thisUrl+'?view='+currentView+'&eid='+eventId+'&downloadIndex='+index);
 }
 
 var generateVideoTimer = null;
@@ -13,25 +13,25 @@ var generateVideoTimer = null;
 function generateVideoProgress() {
   var tickerText = $('videoProgressTicker').get('text');
   if ( tickerText.length < 1 || tickerText.length > 4 ) {
-    $('videoProgressTicker').set( 'text', '.' );
+    $('videoProgressTicker').set('text', '.');
   } else {
-    $('videoProgressTicker').appendText( '.' );
+    $('videoProgressTicker').appendText('.');
   }
 }
 
 function generateVideoResponse( respObj, respText ) {
-  window.location.replace( thisUrl+'?view='+currentView+'&eid='+eventId+'&generated='+((respObj.result=='Ok')?1:0) );
+  window.location.replace(thisUrl+'?view='+currentView+'&eid='+eventId+'&generated='+((respObj.result=='Ok')?1:0));
 }
 
 function generateVideo() {
   form = $j('#contentForm')[0];
   var parms = 'view=request&request=event&action=video';
   parms += '&'+$(form).toQueryString();
-  var query = new Request.JSON( {url: thisUrl, method: 'post', data: parms, onSuccess: generateVideoResponse} );
+  var query = new Request.JSON({url: thisUrl, method: 'post', data: parms, onSuccess: generateVideoResponse});
   query.send();
-  $('videoProgress').removeClass( 'hidden' );
-  $('videoProgress').setProperty( 'class', 'warnText' );
-  $('videoProgressText').set( 'text', videoGenProgressString );
+  $('videoProgress').removeClass('hidden');
+  $('videoProgress').setProperty('class', 'warnText');
+  $('videoProgressText').set('text', videoGenProgressString);
   generateVideoProgress();
-  generateVideoTimer = generateVideoProgress.periodical( 500 );
+  generateVideoTimer = generateVideoProgress.periodical(500);
 }

402c0c6a1bb7ac28a57b448769a39660b8143346
Isaac Connor <isaac@zoneminder.com>
2020-05-14 12:01:03 -0400

    Fix build on older ffmpeg

--- src/zm_ffmpeg_camera.cpp
+++ src/zm_ffmpeg_camera.cpp
@@ -975,10 +975,12 @@ int FfmpegCamera::CaptureAndRecord(
             Error("Error count over 100, going to close and re-open stream");
             return -1;
           }
+#if HAVE_LIBAVUTIL_HWCONTEXT_H
           if ( (ret == AVERROR_INVALIDDATA ) && (hw_pix_fmt != AV_PIX_FMT_NONE) ) {
             use_hwaccel = false;
             return -1;
           }
+#endif
         }
         zm_av_packet_unref(&packet);
         continue;

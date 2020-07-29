fd5adc66234cff83262a289b7af18af94ec9e308
Andrew Bauer <zonexpertconsulting@outlook.com>
2020-07-29 12:49:32 -0500

    remove blockquote from options.php
    It makes the vertical navbar font unnaturally large

--- web/skins/classic/views/options.php
+++ web/skins/classic/views/options.php
@@ -59,7 +59,7 @@ xhtmlHeaders(__FILE__, translate('Options'));
   <div class="container-fluid">
     <div class="row flex-nowrap">
       <nav id="sidebar" style="background-color:#f5f5f5">
-        <ul class="nav nav-pills flex-column blockquote">
+        <ul class="nav nav-pills flex-column">
 <?php
 foreach ( $tabs as $name=>$value ) {
 ?>

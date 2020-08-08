8533ce7018729c1c65377422d6edd5f444310c6b
Isaac Connor <isaac@zoneminder.com>
2020-08-08 11:38:54 -0400

    fix CSP on polygons

--- web/skins/classic/views/zones.php
+++ web/skins/classic/views/zones.php
@@ -62,7 +62,7 @@ xhtmlHeaders(__FILE__, translate('Zones'));
 <?php
       foreach( array_reverse($zones) as $zone ) {
 ?>
-          <polygon points="<?php echo $zone['AreaCoords'] ?>" class="popup-link <?php echo $zone['Type']?>" onclick="streamCmdQuit(true); return false;"
+          <polygon points="<?php echo $zone['AreaCoords'] ?>" class="popup-link <?php echo $zone['Type']?>" data-on-click-true="streamCmdQuit"
                    data-url="?view=zone&amp;mid=<?php echo $mid ?>&amp;zid=<?php echo $zone['Id'] ?>"
                    data-window-name="zmZone<?php echo $zone['Id'] ?>"
                    data-window-tag="zone"

b626e0de1c4ef5e10b0dd5d41a42d627ca226d45
Andrew Bauer <zonexpertconsulting@outlook.com>
2020-08-08 11:19:29 -0500

    eslint nanny strikes again

--- web/skins/classic/js/skin.js
+++ web/skins/classic/js/skin.js
@@ -588,14 +588,14 @@ function setButtonState(element_id, butClass) {
   }
 }
 
-function setCookie(name,value,days) {
-    var expires = "";
-    if (days) {
-      var date = new Date();
-      date.setTime(date.getTime() + (days*24*60*60*1000));
-      expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+function setCookie(name, value, days) {
+  var expires = "";
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + (days*24*60*60*1000));
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "") + expires + "; path=/";
 }
 
 function delCookie(name) {

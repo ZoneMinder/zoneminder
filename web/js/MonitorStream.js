6f5eee6c0f428578f934673cd6eac7b33d06003a
Isaac Connor <isaac@zoneminder.com>
2020-09-09 12:09:23 -0400

    Add show_analyze_frames function to tell zms to send analysis frames

--- web/js/MonitorStream.js
+++ web/js/MonitorStream.js
@@ -276,4 +276,10 @@ function MonitorStream(monitorData) {
           .fail(this.onFailure.bind(this));
     };
   }
+  this.analyze_frames = true;
+  this.show_analyze_frames = function(toggle) {
+    this.analyze_frames = toggle;
+    console.log("Sending command " + (this.analyze_frames?CMD_ANALYZE_ON:CMD_ANALYZE_OFF));
+    this.streamCmdReq.send(this.streamCmdParms+"&command="+(this.analyze_frames?CMD_ANALYZE_ON:CMD_ANALYZE_OFF));
+  }
 } // end function MonitorStream

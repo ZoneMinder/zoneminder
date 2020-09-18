467e75a11c297c3bf3780172b7c205328480bdba
Andrew Bauer <zonexpertconsulting@outlook.com>
2020-09-18 11:03:20 -0500

    convert eventdetail function to php file

--- web/ajax/modal.php
+++ web/ajax/modal.php
@@ -8,24 +8,16 @@ if ( empty($_REQUEST['modal']) ) {
 $modal = validJsStr($_REQUEST['modal']);
 $data = array();
 
-switch ( $modal ) {
-  case 'eventdetail' :
-    $eid = isset($_REQUEST['eid']) ? $_REQUEST['eid'] : '';
-    $eids = isset($_REQUEST['eids']) ? $_REQUEST['eids'] : '';
-    $data['html'] = getEventDetailHTML($eid, $eids);
-    break;
-  default :
-    ZM\Logger::Debug("Including modals/$modal.php");
-    # Shouldn't be necessary but at the moment we have last .conf file contents
-    ob_start();
-    @$result = include('modals/'.$modal.'.php');
-    $data['html'] = ob_get_contents();
-    ob_end_clean();
-    if ( !$result ) {
-      ajaxError("Unknown modal '".$modal."'");
-      return;
-    }
-} # end switch $modal
+ZM\Logger::Debug("Including modals/$modal.php");
+# Shouldn't be necessary but at the moment we have last .conf file contents
+ob_start();
+@$result = include('modals/'.$modal.'.php');
+$data['html'] = ob_get_contents();
+ob_end_clean();
+if ( !$result ) {
+  ajaxError("Unknown modal '".$modal."'");
+  return;
+}
 
 ajaxResponse($data);
 return;

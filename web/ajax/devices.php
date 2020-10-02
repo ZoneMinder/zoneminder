6c0f61ebbd7a564541b7ce97f68caa51b1eb7352
Andrew Bauer <zonexpertconsulting@outlook.com>
2020-10-02 12:39:05 -0500

    rework devices view, remove inline onclick

---
+++ web/ajax/devices.php
@@ -0,0 +1,38 @@
+<?php
+//
+// ZoneMinder web action file
+// Copyright (C) 2019 ZoneMinder LLC
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+
+// Device view actions
+if ( !canEdit('Devices') ) {
+  ajaxError('Insufficient permissions for user '.$user['Username']);
+  return;
+}
+
+if ( $action == 'delete' ) {
+  if ( isset($_REQUEST['markDids']) ) {
+    foreach( $_REQUEST['markDids'] as $markDid ) {
+      dbQuery('DELETE FROM Devices WHERE Id=?', array($markDid));
+    }
+  }
+  ajaxResponse();
+} else {
+  ajaxError('Unrecognised action '.$_REQUEST['action']);
+}
+
+?>
--- web/skins/classic/views/js/devices.js
+++ web/skins/classic/views/js/devices.js
@@ -1,43 +1,6 @@
 var newDeviceBtn = $j('#newDeviceBtn');
-
-function switchDeviceOn( element, key ) {
-  var form = element.form;
-  form.view.value = currentView;
-  form.action.value = 'device';
-  form.command.value = 'on';
-  form.key.value = key;
-  form.submit();
-}
-
-function switchDeviceOff( element, key ) {
-  var form = element.form;
-  form.view.value = currentView;
-  form.action.value = 'device';
-  form.command.value = 'off';
-  form.key.value = key;
-  form.submit();
-}
-
-function deleteDevice( element ) {
-  var form = element.form;
-  form.view.value = currentView;
-  form.action.value = 'delete';
-  form.submit();
-}
-
-function configureButtons( element, name ) {
-  var form = element.form;
-  var checked = false;
-  for (var i = 0; i < form.elements.length; i++) {
-    if ( form.elements[i].name.indexOf(name) == 0) {
-      if ( form.elements[i].checked ) {
-        checked = true;
-        break;
-      }
-    }
-  }
-  form.deleteBtn.disabled = !checked;
-}
+var table = $j('#devicesTable');
+var deleteBtn = $j('#deleteBtn');
 
 // Load the Device Modal HTML via Ajax call
 function getDeviceModal(did) {
@@ -70,7 +33,72 @@ function enableDeviceModal() {
   });
 }
 
+// Load the Delete Confirmation Modal HTML via Ajax call
+function getDelConfirmModal(key) {
+  $j.getJSON(thisUrl + '?request=modal&modal=delconfirm&key=' + key)
+      .done(function(data) {
+        if ( $j('#deleteConfirm').length ) {
+          $j('#deleteConfirm').replaceWith(data.html);
+        } else {
+          $j("body").append(data.html);
+        }
+        manageDelConfirmModalBtns();
+      })
+      .fail(logAjaxFail);
+}
+
+// Manage the DELETE CONFIRMATION modal button
+function manageDelConfirmModalBtns() {
+  document.getElementById("delConfirmBtn").addEventListener("click", function onDelConfirmClick(evt) {
+    if ( ! canEditDevice ) {
+      enoperm();
+      return;
+    }
+
+    var selections = getIdSelections();
+
+    evt.preventDefault();
+    $j.getJSON(thisUrl + '?request=devices&action=delete&markDids[]='+selections.join('&markDids[]='))
+        .done( function(data) {
+          $j('#devicesTable').bootstrapTable('refresh');
+          window.location.reload(true);
+        })
+        .fail(logAjaxFail);
+  });
+
+  // Manage the CANCEL modal button
+  document.getElementById("delCancelBtn").addEventListener("click", function onDelCancelClick(evt) {
+    $j('#deleteConfirm').modal('hide');
+  });
+}
+
+// Returns the event id's of the selected rows
+function getIdSelections() {
+  return $j.map(table.bootstrapTable('getSelections'), function(row) {
+    return row.Id.replace(/(<([^>]+)>)/gi, ''); // strip the html from the element before sending
+  });
+}
+
+// Take the appropriate action when the user clicks on cells in the table
+function processClicks(event, field, value, row, $element) {
+  if ( field == 'On' || field == 'Off' ) {
+    var key = row.KeyString.replace(/(<([^>]+)>)/gi, '');
+    var url = '?request=device&action=device&command=' + field + '&key=' + key;
+    console.log('Url sent to device: ' + url);
+    $j.getJSON(thisUrl + url)
+      .done(function(data) {
+        // TODO - verify if either of these are needed
+        $j('#devicesTable').bootstrapTable('refresh');
+        window.location.reload(true);
+      })
+      .fail(logAjaxFail);
+  }
+}
+
 function initPage() {
+  // Init the bootstrap-table
+  table.bootstrapTable({icons: icons});
+
   if ( canEditDevice ) enableDeviceModal();
 
   newDeviceBtn.prop('disabled', !canEditDevice);
@@ -89,6 +117,32 @@ function initPage() {
     evt.preventDefault();
     window.location.reload(true);
   });
+
+  // Manage the DELETE button
+  document.getElementById("deleteBtn").addEventListener("click", function onDeleteClick(evt) {
+    if ( ! canEditDevice ) {
+      enoperm();
+      return;
+    }
+
+    evt.preventDefault();
+    $j('#deleteConfirm').modal('show');
+  });
+
+  // Load the delete confirmation modal into the DOM
+  getDelConfirmModal('ConfirmDeleteDevices');
+
+  // enable or disable buttons based on current selection and user rights
+  table.on('check.bs.table uncheck.bs.table ' +
+  'check-all.bs.table uncheck-all.bs.table',
+  function() {
+    selections = table.bootstrapTable('getSelections');
+
+    deleteBtn.prop('disabled', !(selections.length && canEditDevice));
+  });
+
+  // Process mouse clicks on the table cells
+  table.on('click-cell.bs.table', processClicks);
 }
 
 $j(document).ready(initPage);

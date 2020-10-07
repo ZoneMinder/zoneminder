b683d5326a44020d106c3519b210449b4dfb25aa
Isaac Connor <isaac@zoneminder.com>
2020-10-07 11:22:42 -0400

    Test for empty array in htmlOPtions when notifying about not selecting a value

--- web/includes/functions.php
+++ web/includes/functions.php
@@ -525,7 +525,7 @@ function htmlOptions($options, $values) {
       '>'.htmlspecialchars($text, ENT_COMPAT | ENT_HTML401, ini_get('default_charset'), false).'</option>
 ';
   } # end foreach options
-  if ( $values and ! $has_selected ) {
+  if ( $values and ((!is_array($values)) or count($values) ) and ! $has_selected ) {
     ZM\Warning('Specified value '.print_r($values, true).' not in contents: '.print_r($options, true));
   }
   return $options_html;

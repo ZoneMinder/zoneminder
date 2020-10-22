6c546f36ae1cf7c5565a38b4750236722087a4d2
Isaac Connor <isaac@zoneminder.com>
2020-10-22 10:43:35 -0400

    allow dashes and underscores in log file name

--- scripts/ZoneMinder/lib/ZoneMinder/Logger.pm
+++ scripts/ZoneMinder/lib/ZoneMinder/Logger.pm
@@ -159,7 +159,7 @@ sub new {
   ( $this->{fileName} = $0 ) =~ s|^.*/||;
   $this->{logPath} = $ZoneMinder::Config::Config{ZM_PATH_LOGS};
   $this->{logFile} = $this->{logPath}.'/'.$this->{id}.'.log';
-  ($this->{logFile}) = $this->{logFile} =~ /^([\w\.\/]+)$/;
+  ($this->{logFile}) = $this->{logFile} =~ /^([_\-\w\.\/]+)$/;
 
   $this->{trace} = 0;
 
@@ -210,7 +210,7 @@ sub initialise( @ ) {
   if ( my $logFile = $this->getTargettedEnv('LOG_FILE') ) {
     $tempLogFile = $logFile;
   }
-  ($tempLogFile) = $tempLogFile =~ /^([\w\.\/]+)$/;
+  ($tempLogFile) = $tempLogFile =~ /^([_\-\w\.\/]+)$/;
 
   my $tempLevel = INFO;
   my $tempTermLevel = $this->{termLevel};

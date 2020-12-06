eb4b7f248d488a1ac73eb5d2f6c1cfc092c0e416
Isaac Connor <isaac@zoneminder.com>
2020-12-05 19:50:58 -0500

    handle sort_field and limit not existing in _REQUEST

--- web/includes/functions.php
+++ web/includes/functions.php
@@ -1200,8 +1200,8 @@ function sortHeader($field, $querySep='&amp;') {
     '?view='.$view,
     'page=1'.(isset($_REQUEST['filter'])?$_REQUEST['filter']['query']:''),
     'sort_field='.$field,
-    'sort_asc='.($_REQUEST['sort_field'] == $field ? !$_REQUEST['sort_asc'] : 0),
-    'limit='.validInt($_REQUEST['limit']),
+    'sort_asc='.( ( isset($_REQUEST['sort_field']) and ( $_REQUEST['sort_field'] == $field ) ) ? !$_REQUEST['sort_asc'] : 0),
+    'limit='.(isset($_REQUEST['limit']) ? validInt($_REQUEST['limit']) : ''),
     (isset($_REQUEST['eid']) ? 'eid='.$_REQUEST['eid'] : '' ),
   ));
 }

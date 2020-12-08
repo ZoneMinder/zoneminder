bfe5bb0249d366ee48be7894a109826d1f556fec
Isaac Connor <isaac@zoneminder.com>
2020-12-08 11:19:22 -0500

    Include code to handle when we are already logged in but trying to auth with a mismatched auth_hash

--- web/includes/auth.php
+++ web/includes/auth.php
@@ -187,6 +187,26 @@ function getAuthUser($auth) {
         } // end if $auth == $authHash
       } // end foreach hour
     } // end foreach user
+
+    if ( isset($_SESSION['username']) ) {
+			# In a multi-server case, we might be logged in as another user and so the auth hash didn't work
+			$sql = 'SELECT * FROM Users WHERE Enabled = 1 AND Username != ?';
+
+			foreach ( dbFetchAll($sql, NULL, $values) as $user ) {
+				$now = time();
+				for ( $i = 0; $i < ZM_AUTH_HASH_TTL; $i++, $now -= 3600 ) { // Try for last TTL hours
+					$time = localtime($now);
+					$authKey = ZM_AUTH_HASH_SECRET.$user['Username'].$user['Password'].$remoteAddr.$time[2].$time[3].$time[4].$time[5];
+					$authHash = md5($authKey);
+
+					if ( $auth == $authHash ) {
+						return $user;
+					} // end if $auth == $authHash
+				} // end foreach hour
+			} // end foreach user
+    } // end if 
+  } // end if using auth hash
+
   } // end if using auth hash
   ZM\Error("Unable to authenticate user from auth hash '$auth'");
   return null;

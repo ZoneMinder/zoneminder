63c483ba92545d707da75737489b0dcc068fefff
Isaac Connor <isaac@zoneminder.com>
2020-12-08 11:27:18 -0500

    canEditMonitors is set in skin.js.php

--- web/skins/classic/views/js/watch.js.php
+++ web/skins/classic/views/js/watch.js.php
@@ -73,7 +73,6 @@ var statusRefreshTimeout = <?php echo 1000*ZM_WEB_REFRESH_STATUS ?>;
 var eventsRefreshTimeout = <?php echo 1000*ZM_WEB_REFRESH_EVENTS ?>;
 var imageRefreshTimeout = <?php echo 1000*ZM_WEB_REFRESH_IMAGE ?>;
 
-var canEditMonitors = <?php echo canEdit( 'Monitors' )?'true':'false' ?>;
 var canStreamNative = <?php echo canStreamNative()?'true':'false' ?>;
 var canViewControl = <?php echo canView( 'Control' )?'true':'false' ?>;
 

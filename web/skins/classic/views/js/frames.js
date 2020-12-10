e0ac68786f2b4ffdb3ab983f4dd0359c912b80e0
Andrew Bauer <zonexpertconsulting@outlook.com>
2020-12-09 18:48:58 -0600

    remove unneeded thumb animation functions from frames.js

--- web/skins/classic/views/js/frames.js
+++ web/skins/classic/views/js/frames.js
@@ -26,27 +26,6 @@ function processRows(rows) {
   return rows;
 }
 
-function thumbnail_onmouseover(event) {
-  var img = event.target;
-  img.src = '';
-  img.src = img.getAttribute('full_img_src');
-}
-
-function thumbnail_onmouseout(event) {
-  var img = event.target;
-  img.src = '';
-  img.src = img.getAttribute('img_src');
-}
-
-function initThumbAnimation() {
-  if ( WEB_ANIMATE_THUMBS ) {
-    $j('.colThumbnail img').each(function() {
-      this.addEventListener('mouseover', thumbnail_onmouseover, false);
-      this.addEventListener('mouseout', thumbnail_onmouseout, false);
-    });
-  }
-}
-
 function processClicks(event, field, value, row, $element) {
   if ( field == 'Score' ) {
     window.location.assign('?view=stats&eid='+row.EventId+'&fid='+row.FrameId);

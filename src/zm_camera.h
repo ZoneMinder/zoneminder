f842e9e2ede4152d5f2574cb248686fc93a8e069
Isaac Connor <isaac@zoneminder.com>
2021-01-07 09:36:33 -0500

    NULL=>nullptr

--- src/zm_camera.h
+++ src/zm_camera.h
@@ -110,10 +110,10 @@ public:
     //return (type == FFMPEG_SRC )||(type == REMOTE_SRC);
   }
 
-  virtual AVStream      *get_VideoStream() { return NULL; };
-  virtual AVStream      *get_AudioStream() { return NULL; };
-  virtual AVCodecContext     *get_VideoCodecContext() { return NULL; };
-  virtual AVCodecContext     *get_AudioCodecContext() { return NULL; };
+  virtual AVStream      *get_VideoStream() { return nullptr; };
+  virtual AVStream      *get_AudioStream() { return nullptr; };
+  virtual AVCodecContext     *get_VideoCodecContext() { return nullptr; };
+  virtual AVCodecContext     *get_AudioCodecContext() { return nullptr; };
   int            get_VideoStreamId() { return mVideoStreamId; };
   int            get_AudioStreamId() { return mAudioStreamId; };
 

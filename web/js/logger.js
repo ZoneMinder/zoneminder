72eaf267c1d964beca45db2c88777e58b7978aba
Andrew Bauer <zonexpertconsulting@outlook.com>
2021-01-08 10:45:59 -0600

    eslint

--- web/js/logger.js
+++ web/js/logger.js
@@ -141,37 +141,27 @@ function browserInfo() {
     if ((verOffset=nAgt.indexOf("Version")) != -1) {
       fullVersion = nAgt.substring(verOffset+8);
     }
-  }
-
   // In MSIE, the true version is after "MSIE" in userAgent
-  else if ((verOffset=nAgt.indexOf("MSIE")) != -1) {
+  } else if ((verOffset=nAgt.indexOf("MSIE")) != -1) {
     browserName = "Microsoft Internet Explorer";
     fullVersion = nAgt.substring(verOffset+5);
-  }
-
   // In Chrome, the true version is after "Chrome"
-  else if ((verOffset=nAgt.indexOf("Chrome")) != -1) {
+  } else if ((verOffset=nAgt.indexOf("Chrome")) != -1) {
     browserName = "Chrome";
     fullVersion = nAgt.substring(verOffset+7);
-  }
-
   // In Safari, the true version is after "Safari" or after "Version"
-  else if ((verOffset=nAgt.indexOf("Safari")) != -1) {
+  } else if ((verOffset=nAgt.indexOf("Safari")) != -1) {
     browserName = "Safari";
     fullVersion = nAgt.substring(verOffset+7);
     if ((verOffset=nAgt.indexOf("Version")) != -1) {
       fullVersion = nAgt.substring(verOffset+8);
     }
-  }
-
   // In Firefox, the true version is after "Firefox"
-  else if ((verOffset=nAgt.indexOf("Firefox")) != -1) {
+  } else if ((verOffset=nAgt.indexOf("Firefox")) != -1) {
     browserName = "Firefox";
     fullVersion = nAgt.substring(verOffset+8);
-  }
-
   // In most other browsers, "name/version" is at the end of userAgent
-  else if ( (nameOffset=nAgt.lastIndexOf(' ')+1) < (verOffset=nAgt.lastIndexOf('/')) ) {
+  } else if ( (nameOffset=nAgt.lastIndexOf(' ')+1) < (verOffset=nAgt.lastIndexOf('/')) ) {
     browserName = nAgt.substring(nameOffset, verOffset);
     fullVersion = nAgt.substring(verOffset+1);
     if (browserName.toLowerCase()==browserName.toUpperCase()) {

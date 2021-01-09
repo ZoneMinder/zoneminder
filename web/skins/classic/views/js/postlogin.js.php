64f755be4a641f33eb15835ceb84d0d266cc7f55
Andrew Bauer <zonexpertconsulting@outlook.com>
2021-01-09 14:40:38 -0600

    clean up postlogin js

--- web/skins/classic/views/js/postlogin.js.php
+++ web/skins/classic/views/js/postlogin.js.php
@@ -1,5 +1,4 @@
 <?php
-// $thisUrl is the base URL used to access ZoneMinder.
 //
 // If the user attempts to access a privileged view but is not logged in, then he may
 // be given the opportunity to log in via the login view.  In that case, the login view
@@ -7,28 +6,18 @@
 // view receives the postLoginQuery via the login form submission, and we can then
 // redirect the user to his original intended destination by appending it to the URL.
 //
+
+$redirectSuffix = '?view=console';
+if ( !empty($_SESSION['postLoginQuery']) ) {
+  parse_str($_SESSION['postLoginQuery'], $queryParams);
+  $redirectSuffix = '?' . http_build_query($queryParams);
+  zm_session_start();
+  unset($_SESSION['postLoginQuery']);
+  session_write_close();
+}
+
+// If we didn't redirect elsewhere, then don't show login page, go to console
+if ( $redirectSuffix == '?view=login' ) $redirectSuffix = '?view=console';
 ?>
 
-$j(
-	function () {
-		// Append '?(GET query)' to URL if the GET query is not empty.
-		var querySuffix = '<?php
-			if ( !empty($_SESSION['postLoginQuery']) ) {
-        parse_str($_SESSION['postLoginQuery'], $queryParams);
-        echo '?' . http_build_query($queryParams);
-        zm_session_start();
-        unset($_SESSION['postLoginQuery']);
-        session_write_close();
-      }
-			?>';
-
-    if ( querySuffix == '?view=login' || querySuffix == '' ) {
-      // If we didn't redirect elsewhere, then don't show login page, go to console
-      querySuffix = '?view=console';
-    }
-    var newUrl = querySuffix;
-console.log("Current location: " + window.location);
-console.log("Redirecting to (" + newUrl + ') from :' + thisUrl);
-		window.location.replace(newUrl);
-	}
-).delay( 500 );
+var redirectSuffix = '<?php echo $redirectSuffix ?>';

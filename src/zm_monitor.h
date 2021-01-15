3d234427e088dce198f0d9373f32e5df8009efd9
Isaac Connor <isaac@zoneminder.com>
2021-01-15 14:42:30 -0500

    initialize packetqueue, analysis_it, storage and videostore in Monitor constructor initializer. Only delete packetqeuue if it has been created.

--- src/zm_monitor.h
+++ src/zm_monitor.h
@@ -192,10 +192,6 @@ protected:
     timeval recording;      // used as both bool and a pointer to the timestamp when recording should begin
   } VideoStoreData;
 
-  VideoStore          *videoStore;
-  zm_packetqueue      *packetqueue;
-  packetqueue_iterator  *analysis_it;
-  Mutex mutex;
 
   class MonitorLink {
   protected:
@@ -377,6 +373,11 @@ protected:
   Camera      *camera;
   Event       *event;
   Storage     *storage;
+
+  VideoStore          *videoStore;
+  zm_packetqueue      *packetqueue;
+  packetqueue_iterator  *analysis_it;
+  Mutex mutex;
 
   int      n_zones;
   Zone      **zones;

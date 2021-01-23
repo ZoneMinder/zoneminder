7feb252f810ede027a6e92641affa5fb589baa02
Isaac Connor <isaac@zoneminder.com>
2021-01-22 20:24:12 -0500

    code style

--- src/zm_rtsp_server_unicast_server_media_subsession.cpp
+++ src/zm_rtsp_server_unicast_server_media_subsession.cpp
@@ -15,24 +15,33 @@
 //    ServerMediaSubsession for Unicast
 // -----------------------------------------
 UnicastServerMediaSubsession* UnicastServerMediaSubsession::createNew(
-    UsageEnvironment& env, StreamReplicator* replicator, const std::string& format)
-{
+    UsageEnvironment& env,
+    StreamReplicator* replicator,
+    //FramedSource *source,
+    const std::string& format
+    ) {
 	return new UnicastServerMediaSubsession(env, replicator, format);
+	//return new UnicastServerMediaSubsession(env, replicator, source, format);
 }
 
-FramedSource* UnicastServerMediaSubsession::createNewStreamSource(unsigned clientSessionId, unsigned& estBitrate)
-{
-	FramedSource* source = m_replicator->createStreamReplica();
-	return createSource(envir(), source, m_format);
+FramedSource* UnicastServerMediaSubsession::createNewStreamSource(
+    unsigned clientSessionId,
+    unsigned& estBitrate
+    ) {
+	FramedSource* replica = m_replicator->createStreamReplica();
+	return createSource(envir(), replica, m_format);
 }
 
 RTPSink* UnicastServerMediaSubsession::createNewRTPSink(
-    Groupsock* rtpGroupsock,  unsigned char rtpPayloadTypeIfDynamic, FramedSource* inputSource)
-{
-	return createSink(envir(), rtpGroupsock, rtpPayloadTypeIfDynamic, m_format);
+    Groupsock* rtpGroupsock,
+    unsigned char rtpPayloadTypeIfDynamic,
+    FramedSource* inputSource
+    ) {
+	return createSink(envir(), rtpGroupsock, rtpPayloadTypeIfDynamic, m_format, inputSource);
 }
 
-char const* UnicastServerMediaSubsession::getAuxSDPLine(RTPSink* rtpSink, FramedSource* inputSource)
-{
+char const* UnicastServerMediaSubsession::getAuxSDPLine(
+    RTPSink* rtpSink, FramedSource* inputSource
+    ) {
 	return this->getAuxLine(dynamic_cast<ZoneMinderDeviceSource*>(m_replicator->inputSource()), rtpSink->rtpPayloadType());
 }
--- src/zm_rtsp_server_unicast_server_media_subsession.h
+++ src/zm_rtsp_server_unicast_server_media_subsession.h
@@ -30,13 +30,15 @@ class UnicastServerMediaSubsession :
         StreamReplicator* replicator,
         const std::string& format)
 				:
-          OnDemandServerMediaSubsession(env, False),
+          OnDemandServerMediaSubsession(env, true
+              /* Boolean reuseFirstSource, portNumBits initialPortNum=6970, Boolean multiplexRTCPWithRTP=False */
+              ),
           BaseServerMediaSubsession(replicator),
           m_format(format) {};
 
 		virtual FramedSource* createNewStreamSource(unsigned clientSessionId, unsigned& estBitrate);
 		virtual RTPSink* createNewRTPSink(Groupsock* rtpGroupsock, unsigned char rtpPayloadTypeIfDynamic, FramedSource* inputSource);
-		virtual char const* getAuxSDPLine(RTPSink* rtpSink,FramedSource* inputSource);
+		virtual char const* getAuxSDPLine(RTPSink* rtpSink, FramedSource* inputSource);
 
 	protected:
 		const std::string m_format;

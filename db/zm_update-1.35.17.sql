159f61127478e670d64e14e08fa6c8ce2316c5ba
Isaac Connor <isaac@zoneminder.com>
2021-01-27 09:55:48 -0500

    Add Encoder if missing

--- db/zm_update-1.35.17.sql
+++ db/zm_update-1.35.17.sql
@@ -1,1 +1,12 @@
+SET @s = (SELECT IF(
+    (SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS WHERE table_schema = DATABASE()
+     AND table_name = 'Monitors'
+     AND column_name = 'Encoder'
+    ) > 0,
+"SELECT 'Column Encoder already exists in Monitors'",
+"ALTER TABLE `Monitors` ADD `Encoder`     enum('auto','h264','libx264','h264_omx','h264_vaapi','mjpeg','mpeg1','mpeg2')  AFTER `OutputCodec`"
+));
+
+PREPARE stmt FROM @s;
+EXECUTE stmt;
 ALTER TABLE `Monitors` MODIFY `Encoder`     enum('auto','h264','libx264', 'h264_omx', 'h264_vaapi', 'mjpeg','mpeg1','mpeg2');

e6358290b4167c8ab39339efb82dccd979efc0cf
Isaac Connor <isaac@zoneminder.com>
2021-02-02 17:30:42 -0500

    this=>self

--- scripts/ZoneMinder/lib/ZoneMinder/Control/onvif.pm
+++ scripts/ZoneMinder/lib/ZoneMinder/Control/onvif.pm
@@ -65,7 +65,7 @@ sub sendCmd {
   my $self = shift;
   my $cmd = shift;
   my $result = undef;
-  $this->printMsg($cmd, 'Tx');
+  $self->printMsg($cmd, 'Tx');
 
   my $req = HTTP::Request->new(GET=>'http://'.$self->{Monitor}->{ControlAddress}.'/'.$cmd);
   my $res = $self->{ua}->request($req);

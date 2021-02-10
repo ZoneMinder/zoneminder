2308bfab8e7833038c11d8f11d0071fa2071c75b
Isaac Connor <isaac@zoneminder.com>
2021-02-09 23:32:19 -0500

    Add groovy and hirsute to ubuntu builds

--- utils/do_debian_package.sh
+++ utils/do_debian_package.sh
@@ -80,7 +80,7 @@ fi;
 
 if [ "$DISTROS" == "" ]; then
   if [ "$RELEASE" != "" ]; then
-    DISTROS="xenial,bionic,focal"
+    DISTROS="xenial,bionic,focal,groovy,hirsute"
   else
     DISTROS=`lsb_release -a 2>/dev/null | grep Codename | awk '{print $2}'`;
   fi;

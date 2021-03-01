19d9812a3f68ce8040e225e295f062d52340b929
Isaac Connor <isaac@zoneminder.com>
2021-03-01 14:00:47 -0500

    Add a debug function to print out the start of the nal

--- src/zm_rtsp_server_frame.h
+++ src/zm_rtsp_server_frame.h
@@ -52,6 +52,17 @@ class NAL_Frame {
       }
       return false;
     }
+    void debug() {
+      if ( m_size <= 4 ) {
+      Debug(1, "NAL: %d: %.2x %.2x %.2x %.2x", m_size,
+          m_buffer[0], m_buffer[1], m_buffer[2], m_buffer[3]);
+      } else {
+        Debug(1, "NAL: %d: %.2x %.2x %.2x %.2x   %.2x %.2x %.2x %.2x ", m_size,
+            m_buffer[0], m_buffer[1], m_buffer[2], m_buffer[3],
+            m_buffer[4], m_buffer[5], m_buffer[6], m_buffer[7]
+            );
+      }
+    }
 
   private:
     unsigned char* m_buffer;

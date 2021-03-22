96a96f48656e1360e2406e8736d437af37826bad
Isaac Connor <isaac@zoneminder.com>
2021-03-22 17:45:44 -0400

    add jwt token as a std:;string

--- src/zm_rtsp_server_authenticator.h
+++ src/zm_rtsp_server_authenticator.h
@@ -45,10 +45,10 @@ class ZM_RtspServer_Authenticator : public xop::Authenticator {
 
       if (query.has("jwt_token")) {
         const QueryParameter *jwt_token = query.get("jwt_token");
-        user = zmLoadTokenUser(jwt_token->firstValue().c_str(), false);
+        user = zmLoadTokenUser(jwt_token->firstValue(), false);
       } else if (query.has("token")) {
         const QueryParameter *jwt_token = query.get("token");
-        user = zmLoadTokenUser(jwt_token->firstValue().c_str(), false);
+        user = zmLoadTokenUser(jwt_token->firstValue(), false);
       } else if (strcmp(config.auth_relay, "none") == 0) {
         if (query.has("username")) {
           std::string username = query.get("username")->firstValue();

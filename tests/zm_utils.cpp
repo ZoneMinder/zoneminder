46a4b615c8a3ccf126cd37a06cfc0f74c9e43873
Peter Keresztes Schmidt <carbenium@outlook.com>
2021-04-03 21:23:54 +0200

    tests/utils: Add tests for base64Encode
    Test vectors according to RFC4648

--- tests/zm_utils.cpp
+++ tests/zm_utils.cpp
@@ -144,3 +144,13 @@ TEST_CASE("join") {
   REQUIRE(join({"a", "b"}, ",") == "a,b");
   REQUIRE(join({"a", "b"}, "") == "ab");
 }
+
+TEST_CASE("base64Encode") {
+  REQUIRE(base64Encode("") == "");
+  REQUIRE(base64Encode("f") == "Zg==");
+  REQUIRE(base64Encode("fo") == "Zm8=");
+  REQUIRE(base64Encode("foo") == "Zm9v");
+  REQUIRE(base64Encode("foob") == "Zm9vYg==");
+  REQUIRE(base64Encode("fooba") == "Zm9vYmE=");
+  REQUIRE(base64Encode("foobar") == "Zm9vYmFy");
+}

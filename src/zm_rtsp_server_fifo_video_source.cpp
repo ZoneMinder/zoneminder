f8fc768486cd29dd981af96923b6f3c2a9af0166
Peter Keresztes Schmidt <carbenium@outlook.com>
2021-04-09 09:48:42 +0200

    FifoSource: Adapt to changed xop::AVFrame API

--- src/zm_rtsp_server_fifo_video_source.cpp
+++ src/zm_rtsp_server_fifo_video_source.cpp
@@ -23,12 +23,8 @@ ZoneMinderFifoVideoSource::ZoneMinderFifoVideoSource(
 }
 
 void ZoneMinderFifoVideoSource::PushFrame(const uint8_t *data, size_t size, int64_t pts) {
-  xop::AVFrame frame = {0};
-  frame.type = 0;
-  frame.size = size;
+  xop::AVFrame frame(data, size);
   frame.timestamp = av_rescale_q(pts, AV_TIME_BASE_Q, m_timeBase);
-  frame.buffer.reset(new uint8_t[size]);
-  memcpy(frame.buffer.get(), data, size);
   m_rtspServer->PushFrame(m_sessionId, m_channelId, frame);
 }
 

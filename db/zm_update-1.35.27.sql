c539969faa00de84b37c3049e22d9d670e65f975
Isaac Connor <isaac@zoneminder.com>
2021-04-23 12:54:41 -0400

    Cleanup

--- db/zm_update-1.35.27.sql
+++ db/zm_update-1.35.27.sql
@@ -1,5 +1,5 @@
 --
--- Add EventCloseMode moved from General Config
+-- Add ModectDuringPTZ
 --
 
 SET @s = (SELECT IF(
@@ -13,24 +13,3 @@ SET @s = (SELECT IF(
 
 PREPARE stmt FROM @s;
 EXECUTE stmt;
-
-UPDATE `Monitors` SET EventCloseMode = (SELECT Value from Config WHERE Name='ZM_EVENT_CLOSE_MODE');
-
-
---
--- Add Snapshot permission to Users
---
-
-SET @s = (SELECT IF(
-    (SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS WHERE table_schema = DATABASE()
-     AND table_name = 'Users'
-     AND column_name = 'Snapshots'
-    ) > 0,
-"SELECT 'Column Snapshots already exists in Users'",
-"ALTER TABLE `Users` ADD `Snapshots` enum('None','View','Edit') NOT NULL default 'None' AFTER `Devices`"
-));
-
-PREPARE stmt FROM @s;
-EXECUTE stmt;
-
-UPDATE `Users` SET `Snapshots` = `Events`;

69098ccdcb0f4c80f242d462185ce4f3faf3ef9a
Isaac Connor <isaac@zoneminder.com>
2021-04-29 14:49:30 -0400

    Implement changes() in Object

--- scripts/ZoneMinder/lib/ZoneMinder/Object.pm
+++ scripts/ZoneMinder/lib/ZoneMinder/Object.pm
@@ -29,6 +29,7 @@ use strict;
 use warnings;
 use Time::HiRes qw{ gettimeofday tv_interval }; 
 use Carp qw( cluck );
+use Array::Utils qw(:all);
 
 require ZoneMinder::Base;
 
@@ -845,6 +846,42 @@ sub find_sql {
 	#$log->debug("Loading Debug:$debug $object_type ($sql) (".join(',', map { ref $_ eq 'ARRAY' ? join(',', @{$_}) : $_ } @values).')' ) if $debug;
 	return \%sql;
 } # end sub find_sql
+
+sub changes {
+  my ( $self, $params ) = @_;
+
+  my $type = ref $self;
+  if ( ! $type ) {
+    my ( $caller, undef, $line ) = caller;
+    $log->error("No type in Object::changes. self:$self from  $caller:$line");
+  }
+  my $fields = eval ('\%'.$type.'::fields');
+  if (!$fields) {
+    $log->warn('Object::changes called on an object with no fields');
+    return;
+  } # end if
+  my @results;
+
+  foreach my $field (sort keys %$fields) {
+    next if ! exists $$params{$field};
+
+    if ( ref $$self{$field} eq 'ARRAY' ) {
+      my @second = ref $$params{$field} eq 'ARRAY' ? @{$$params{$field}} : ($$params{$field});
+      if (array_diff(@{$$self{$field}}, @second)) {
+        push @results, [ $field, $$self{$field}, $$params{$field} ];
+      }
+    } elsif (
+      (!defined($$self{$field}) and defined($$params{$field}))
+        or
+      (defined($$self{$field}) and !defined($$params{$field}))
+    ) {
+      push @results, $field;
+    } elsif ( defined($$self{$field}) and defined($$params{$field}) and ($$self{$field} ne $$params{$field}) ) {
+      push @results, $field;
+    }
+  }
+  return @results;
+}
 
 sub AUTOLOAD {
   my $type = ref($_[0]);

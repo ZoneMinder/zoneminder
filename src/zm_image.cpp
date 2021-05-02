ZoneMinder/zoneminder

commit 3f5dbb778070ef09a6daca9f8e62b9e435afd967

Default to rgba when subpixelorder is not specified

src/zm_image.cpp
@@ -160,19 +160,23 @@ Image::Image(int p_width, int p_height, int p_colours, int p_subpixelorder, uint
   subpixelorder(p_subpixelorder),
   buffer(p_buffer) {
 
-  if ( !initialised )
+  if (!initialised)
     Initialise();
   pixels = width * height;
   linesize = p_width * p_colours;
   size = linesize * height + padding;
   buffer = nullptr;
   holdbuffer = 0;
-  if ( p_buffer ) {
+  if (p_buffer) {
     allocation = size;
     buffertype = ZM_BUFTYPE_DONTFREE;
     buffer = p_buffer;
   } else {
     AllocImgBuffer(size);
+  }
+  if (!subpixelorder) {
+    // Default to RGBA when no subpixelorder is specified.
+    subpixelorder == ZM_SUBPIX_ORDER_RGBA;
   }
   imagePixFormat = AVPixFormat();
 
@@ -200,6 +204,10 @@ Image::Image(int p_width, int p_linesize, int p_height, int p_colours, int p_sub
     buffer = p_buffer;
   } else {
     AllocImgBuffer(size);
+  }
+  if (!subpixelorder) {
+    // Default to RGBA when no subpixelorder is specified.
+    subpixelorder == ZM_SUBPIX_ORDER_RGBA;
   }
   imagePixFormat = AVPixFormat();
 

commit e5ceed31cc624faccab5de48645a050b9075e342

Image: Fix a typo introduced in 3f5dbb778070ef09a6daca9f8e62b9e435afd967

src/zm_image.cpp
@@ -176,7 +176,7 @@ Image::Image(int p_width, int p_height, int p_colours, int p_subpixelorder, uint
   }
   if (!subpixelorder) {
     // Default to RGBA when no subpixelorder is specified.
-    subpixelorder == ZM_SUBPIX_ORDER_RGBA;
+    subpixelorder = ZM_SUBPIX_ORDER_RGBA;
   }
   imagePixFormat = AVPixFormat();
 
@@ -207,7 +207,7 @@ Image::Image(int p_width, int p_linesize, int p_height, int p_colours, int p_sub
   }
   if (!subpixelorder) {
     // Default to RGBA when no subpixelorder is specified.
-    subpixelorder == ZM_SUBPIX_ORDER_RGBA;
+    subpixelorder = ZM_SUBPIX_ORDER_RGBA;
   }
   imagePixFormat = AVPixFormat();
 
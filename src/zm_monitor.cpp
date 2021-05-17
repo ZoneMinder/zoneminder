24b3f4c1b75a78bd964ad1271197b3ea2491e949
Isaac Connor <isaac@zoneminder.com>
2021-05-17 16:32:19 -0400

    When de-interlacing, need to wait for decoder. Fixes race

--- src/zm_monitor.cpp
+++ src/zm_monitor.cpp
@@ -1850,7 +1850,7 @@ bool Monitor::Analyse() {
 
         /* try to stay behind the decoder. */
         if (decoding_enabled) {
-          while (!snap->image and !snap->decoded and !zm_terminate and !analysis_thread->Stopped()) {
+          while ((!snap->image or deinterlacing_value) and !snap->decoded and !zm_terminate and !analysis_thread->Stopped()) {
             // Need to wait for the decoder thread.
             Debug(1, "Waiting for decode");
             packet_lock->wait();

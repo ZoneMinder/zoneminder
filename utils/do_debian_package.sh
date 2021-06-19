a03b505324da3730c0c6de6d1172094db9c738b1
anon8675309 <anon8675309@github.com>
2021-06-19 18:11:57 -0500

    Attempting to build the lastest tag instead of a hard-coded one

--- utils/do_debian_package.sh
+++ utils/do_debian_package.sh
@@ -119,16 +119,16 @@ else
   if [ "$SNAPSHOT" == "stable" ]; then
     if [ "$BRANCH" == "" ]; then
       echo "About to run some basic commands for debugging..."
-      pwd; ls; git remote -v; git branch -v; git tag -l
+      pwd; ls; git remote -v; git branch -v; git status
+      echo "Listing all of the tags for this repo:"
+      git tag -l
       echo "About to get the hash of the most recent tag"
       REV=$(git rev-list --tags --max-count=1)
       echo "REV = $REV"
       BRANCH=`git describe --tags $(git rev-list --tags --max-count=1)`;
-      if [ -z "$BRANCH" ]; then
-        # This should only happen in CI environments where tag info isn't available
-        BRANCH=`grep ' release$' distros/fedora/zoneminder.spec | head -n 1 | sed -e 's/ release//g' -e 's/[^0-9]*//'`
-        echo "Building branch $BRANCH"
-      fi
+      echo "BRANCH = $BRANCH"
+      BRANCH=`git tag -l | sort | grep -v '^v.*' | tail -n 1`
+      echo "BRANCH = $BRANCH"
       if [ "$BRANCH" == "" ]; then
         echo "Unable to determine latest stable branch!"
         exit 0;

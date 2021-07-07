8df915e7a47c0b739d0ac39e3e5e7d2dff55d973
Isaac Connor <iconnor@connortechnology.com>
2021-07-07 09:45:53 -0400

    Fix merge.

--- db/zm_update-1.35.29.sql
+++ db/zm_update-1.35.29.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 SET @s = (SELECT IF(
     (SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS WHERE table_schema = DATABASE()
      AND table_name = 'Monitors'
@@ -42,19 +41,6 @@ SET @s = (SELECT IF(
     ) > 0,
 "SELECT 'FOREIGN KEY for ModelId already exists in Monitors'",
 "ALTER TABLE `Monitors` ADD FOREIGN KEY  (`ModelId`) REFERENCES `Models` (Id)"
-=======
---
--- Add AutoUnarchive action to Filters
---
-
-SET @s = (SELECT IF(
-    (SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS WHERE table_schema = DATABASE()
-     AND table_name = 'Filters'
-     AND column_name = 'AutoUnarchive'
-    ) > 0,
-"SELECT 'Column AutoUunarchive already exists in Filters'",
-"ALTER TABLE Filters ADD `AutoUnarchive` tinyint(3) unsigned NOT NULL default '0' AFTER `AutoArchive`"
->>>>>>> master
 ));
 
 PREPARE stmt FROM @s;

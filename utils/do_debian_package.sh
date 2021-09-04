3e3ce151d8971c61a73496ed33a00e8d583ca3e8
Isaac Connor <isaac@zoneminder.com>
2021-09-04 10:17:41 -0400

    Add impish to possible distros to build

--- utils/do_debian_package.sh
+++ utils/do_debian_package.sh
@@ -88,7 +88,7 @@ fi;
 
 if [ "$DISTROS" == "" ]; then
   if [ "$RELEASE" != "" ]; then
-    DISTROS="xenial,bionic,focal,groovy,hirsute"
+    DISTROS="xenial,bionic,focal,hirsute,impish"
   else
     DISTROS=`lsb_release -a 2>/dev/null | grep Codename | awk '{print $2}'`;
   fi;

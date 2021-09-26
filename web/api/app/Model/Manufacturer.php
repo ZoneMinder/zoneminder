c66489fb303de460fac00c33aeb659f84635db49
Isaac Connor <iconnor@connortechnology.com>
2021-09-25 20:49:30 -0400

    rough in api support for Models and Manufacturers

---
+++ web/api/app/Model/Manufacturer.php
@@ -0,0 +1,77 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * Manufacturer Model
+ *
+ * @property Event $Event
+ * @property Zone $Zone
+ */
+class Manufacturer extends AppModel {
+
+/**
+ * Use table
+ *
+ * @var mixed False or table name
+ */
+	public $useTable = 'Manufacturers';
+
+/**
+ * Primary key field
+ *
+ * @var string
+ */
+	public $primaryKey = 'Id';
+
+/**
+ * Display field
+ *
+ * @var string
+ */
+	public $displayField = 'Name';
+
+	public $recursive = -1;
+
+/**
+ * Validation rules
+ *
+ * @var array
+ */
+	public $validate = array(
+		'Id' => array(
+			'numeric' => array(
+				'rule' => array('numeric'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'Name' => array(
+			'notBlank' => array(
+				'rule' => array('notBlank'))),
+	);
+
+	//The Associations below have been created with all possible keys, those that are not needed can be removed
+
+/**
+ * hasMany associations
+ *
+ * @var array
+ */
+	public $hasMany = array(
+		'Model' => array(
+			'className' => 'Model',
+			'foreignKey' => 'ManufacturerId',
+			'dependent' => false,
+			'conditions' => '',
+			'fields' => '',
+			'order' => '',
+			'limit' => '',
+			'offset' => '',
+			'exclusive' => '',
+			'finderQuery' => '',
+			'counterQuery' => ''
+		)
+	);
+}

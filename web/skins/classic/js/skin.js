30d4900b45a56974614fc42cf5538311efb01cf1
Isaac Connor <isaac@zoneminder.com>
2021-11-09 14:32:06 -0500

    rough in fullscreen mode in watch view

--- web/skins/classic/js/skin.js
+++ web/skins/classic/js/skin.js
@@ -947,3 +947,29 @@ function initThumbAnimation() {
     });
   }
 }
+
+/* View in fullscreen */
+function openFullscreen(elem) {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) {
+    /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) {
+    /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
+
+/* Close fullscreen */
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) {
+    /* Safari */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) {
+    /* IE11 */
+    document.msExitFullscreen();
+  }
+}

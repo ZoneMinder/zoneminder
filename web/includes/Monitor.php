a57206ef5493b5cfabbaf09caecfe9ebb333b8ea
Isaac Connor <isaac@zoneminder.com>
2022-01-20 09:47:36 -0500

    Implement getMonitorStateHTML to synchronize it between montage and live view, cycle etc. Reuseable code.  Also the structure is required by MonitorStream.js

--- web/includes/Monitor.php
+++ web/includes/Monitor.php
@@ -719,5 +719,27 @@ class Monitor extends ZM_Object {
     }
     return $this->{'Manufacturer'};
   }
+  function getMonitorStateHTML() {
+    $html = '
+<div id="monitorStatus'.$this->Id().'" class="monitorStatus">
+  <div id="monitorState'.$this->Id().'" class="monitorState">
+    <span>'.translate('State').':<span id="stateValue'.$this->Id().'"></span></span>
+    <span id="viewingFPS'.$this->Id().'" title="'.translate('Viewing FPS').'"><span id="viewingFPSValue'.$this->Id().'"></span> fps</span>
+    <span id="captureFPS'.$this->Id().'" title="'.translate('Capturing FPS').'"><span id="captureFPSValue'.$this->Id().'"></span> fps</span>
+';
+    if ( $this->Function() == 'Modect' or $this->Function() == 'Mocord' ) {
+      $html .= '<span id="analysisFPS'.$this->Id().'" title="'.translate('Analysis FPS').'"><span id="analysisFPSValue'.$this->Id().'"></span> fps</span>
+      ';
+    }
+    $html .= '
+    <span id="rate'.$this->Id().'" class="hidden">'.translate('Rate').': <span id="rateValue'.$this->Id().'"></span>x</span>
+    <span id="delay'.$this->Id().'" class="hidden">'.translate('Delay').': <span id="delayValue'.$this->Id().'"></span>s</span>
+    <span id="level'.$this->Id().'" class="hidden">'.translate('Buffer').': <span id="levelValue'.$this->Id().'"></span>%</span>
+    <span id="zoom'.$this->Id().'">'. translate('Zoom').': <span id="zoomValue'.$this->Id().'"></span>x</span>
+  </div>
+</div>
+';
+    return $html;
+  }
 } // end class Monitor
 ?>

2d3f99eabbdb3279ae75903ff7056c0dfb6b7582
Isaac Connor <isaac@zoneminder.com>
2022-02-16 09:59:23 -0500

    rework zm_sendfile to try again if not all bytes were sent. According to the docs, this is entirely possible.

--- src/zm_sendfile.h
+++ src/zm_sendfile.h
@@ -3,22 +3,25 @@
 
 #ifdef HAVE_SENDFILE4_SUPPORT
 #include <sys/sendfile.h>
-int zm_sendfile(int out_fd, int in_fd, off_t *offset, size_t size) {
-  int err;
+ssize_t zm_sendfile(int out_fd, int in_fd, off_t *offset, size_t size) {
+  size_t remaining = size;
+  while (remaining) {
+    ssize_t err = sendfile(out_fd, in_fd, offset, remaining);
+    if (err < 0) {
+      return -errno;
+    }
+    remaining -= err;
+    offset += err;
+  }
 
-  err = sendfile(out_fd, in_fd, offset, size);
-  if ( err < 0 )
-    return -errno;
-
-  return err;
+  return size-remaining;
 }
 #elif HAVE_SENDFILE7_SUPPORT
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/uio.h>
-int zm_sendfile(int out_fd, int in_fd, off_t *offset, off_t size) {
-  int err;
-  err = sendfile(in_fd, out_fd, *offset, size, nullptr, &size, 0);
+ssize_t zm_sendfile(int out_fd, int in_fd, off_t *offset, off_t size) {
+  ssize_t err = sendfile(in_fd, out_fd, *offset, size, nullptr, &size, 0);
   if (err && errno != EAGAIN)
     return -errno;
 

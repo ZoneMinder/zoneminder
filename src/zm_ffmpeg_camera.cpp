1d39605476326462bf3aedc4ba25b47242a86912
Isaac Connor <isaac@zoneminder.com>
2022-04-22 11:42:31 -0400

    Use mOptions instead of Options().  Check for empty before parsing

--- src/zm_ffmpeg_camera.cpp
+++ src/zm_ffmpeg_camera.cpp
@@ -280,9 +280,11 @@ int FfmpegCamera::OpenFfmpeg() {
 #else
   // Handle options
   AVDictionary *opts = nullptr;
-  ret = av_dict_parse_string(&opts, Options().c_str(), "=", ",", 0);
-  if ( ret < 0 ) {
-    Warning("Could not parse ffmpeg input options '%s'", Options().c_str());
+  if (!mOptions.empty()) {
+    ret = av_dict_parse_string(&opts, mOptions.c_str(), "=", ",", 0);
+    if (ret < 0) {
+      Warning("Could not parse ffmpeg input options '%s'", mOptions.c_str());
+    }
   }
 
   // Set transport method as specified by method field, rtpUni is default

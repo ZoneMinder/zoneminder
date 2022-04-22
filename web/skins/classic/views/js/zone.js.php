d584b8ec2189bc8f23f43b798cc4efb35e97a8ec
Isaac Connor <isaac@zoneminder.com>
2022-04-22 15:54:33 -0400

    Remove CMD_ defines because they are now in skin.js.php

--- web/skins/classic/views/js/zone.js.php
+++ web/skins/classic/views/js/zone.js.php
@@ -94,16 +94,11 @@ var deleteString = "<?php echo translate('Delete') ?>";
 // Imported from watch.js.php and modified for new zone edit view
 //
 
-var CMD_PAUSE = <?php echo CMD_PAUSE ?>;
-var CMD_PLAY = <?php echo CMD_PLAY ?>;
-var CMD_STOP = <?php echo CMD_STOP ?>;
-var CMD_ANALYZE_ON = <?php echo CMD_ANALYZE_ON ?>;
-var CMD_ANALYZE_OFF = <?php echo CMD_ANALYZE_OFF ?>;
-var CMD_QUERY = <?php echo CMD_QUERY ?>;
 
 var SCALE_BASE = <?php echo SCALE_BASE ?>;
 
-var SOUND_ON_ALARM = <?php echo ZM_WEB_SOUND_ON_ALARM ?>;
+const SOUND_ON_ALARM = <?php echo ZM_WEB_SOUND_ON_ALARM ?>;
+const POPUP_ON_ALARM = false;
 
 var streamMode = "<?php echo $streamMode ?>";
 

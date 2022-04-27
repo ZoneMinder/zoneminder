65c9df9fcadbafde514e631722e7b6b82456b8b7
Isaac Connor <isaac@zoneminder.com>
2022-04-27 17:40:51 -0400

    Rough in lots of stuff from master.  setStreamScale to set src of stream img's.  Handle ajax errors better.  Send CMD_QUIT when changing img.src. These changes reduce errors from zms.

--- web/js/MonitorStream.js
+++ web/js/MonitorStream.js
@@ -15,24 +15,78 @@ function MonitorStream(monitorData) {
   };
   this.type = monitorData.type;
   this.refresh = monitorData.refresh;
+
+  this.element = null;
+  this.getElement = function() {
+    if (this.element) return this.element;
+    this.element = document.getElementById('liveStream'+this.id);
+    if (!this.element) {
+      console.error("No img for #liveStream"+this.id);
+    }
+    return this.element;
+  };
+
+  this.setStreamScale = function(newscale) {
+    const img = this.getElement();
+    if (!img) {
+      console.log("No img in setScale");
+      return;
+    }
+    const stream_frame = $j('#monitor'+this.id);
+    if (!newscale) {
+      newscale = parseInt(100*parseInt(stream_frame.width())/this.width);
+      console.log(newscale + " = " + this.width + " / " + stream_frame.width());
+    }
+    if (img.nodeName == 'IMG') {
+      if (newscale > 100) newscale = 100; // we never request a larger image, as it just wastes bandwidth
+      if (newscale < 0) newscale = 100;
+      const oldSrc = img.src;
+      if (!oldSrc) {
+        console.log('No src on img?!');
+        console.log(img);
+        return;
+      }
+      const newSrc = oldSrc.replace(/scale=\d+/i, 'scale='+newscale);
+      if (newSrc != oldSrc) {
+        this.streamCmdTimer = clearTimeout(this.streamCmdTimer);
+        this.statusCmdTimer = clearTimeout(this.statusCmdTimer);
+        // We know that only the first zms will get the command because the
+        // second can't open the commandQueue until the first exits
+        // This is necessary because safari will never close the first image
+        this.streamCommand(CMD_QUIT);
+        //this.statusCmdTimer = setTimeout(this.statusQuery.bind(this), statusRefreshTimeout);
+        console.log("Changing src to " + newSrc);
+        img.src = '';
+        img.src = newSrc;
+      }
+    }
+  }; // setStreamScale
+
   this.start = function(delay) {
     // Step 1 make sure we are streaming instead of a static image
-    var stream = $j('#liveStream'+this.id);
-    if (!stream.length) {
+    var stream = this.getElement();
+    if (!stream) {
       console.log('No live stream');
       return;
     }
-    stream = stream[0];
-    if ( !stream ) {
-      console.log('No live stream');
-      return;
-    }
-    if ( !stream.src ) {
+    if (!stream.src) {
       // Website Monitors won't have an img tag
       console.log('No src for #liveStream'+this.id);
       console.log(stream);
       return;
     }
+    if (stream.getAttribute('loading') == 'lazy') {
+      stream.setAttribute('loading', 'eager');
+    }
+    stream.addEventListener('load', (event) => {
+      console.log('Logo has been loaded! starting streamMcnd + '+delay + ' ' + statusRefreshTimeout);
+      clearTimeout(this.streamCmdTimer);
+      this.streamCmdTimer = setTimeout(this.streamCmdQuery.bind(this), delay);
+    });
+    stream.addEventListener('error', (event) => {
+      console.log('Logo has been stoppd! stopping streamMcnd');
+      clearTimeout(this.streamCmdTimer);
+    });
     src = stream.src.replace(/mode=single/i, 'mode=jpeg');
     if ( -1 == src.search('connkey') ) {
       src += '&connkey='+this.connKey;
@@ -42,7 +96,6 @@ function MonitorStream(monitorData) {
       stream.src = '';
       stream.src = src;
     }
-    setTimeout(this.streamCmdQuery.bind(this), delay);
   };
   this.stop = function() {
     if ( 0 ) {
@@ -104,7 +157,8 @@ function MonitorStream(monitorData) {
   };
 
   this.onFailure = function(jqxhr, textStatus, error) {
-    setTimeout(this.streamCmdQuery.bind(this), 1000*statusRefreshTimeout);
+    clearTimeout(this.streamCmdTimer);
+    this.streamCmdTimer = setTimeout(this.streamCmdQuery.bind(this), 10*statusRefreshTimeout);
     logAjaxFail(jqxhr, textStatus, error);
   };
 
@@ -115,6 +169,7 @@ function MonitorStream(monitorData) {
       console.log('No live stream');
       return;
     }
+    this.streamCmdTimer = clearTimeout(this.streamCmdTimer);
 
     if ( respObj.result == 'Ok' ) {
       if ( respObj.status ) {
@@ -173,9 +228,6 @@ function MonitorStream(monitorData) {
         if ( this.status.auth ) {
           if ( this.status.auth != auth_hash ) {
             // Try to reload the image stream.
-            if ( stream ) {
-              stream.src = stream.src.replace(/auth=\w+/i, 'auth='+this.status.auth);
-            }
             console.log("Changed auth from " + auth_hash + " to " + this.status.auth);
             auth_hash = this.status.auth;
           }
@@ -189,12 +241,12 @@ function MonitorStream(monitorData) {
           console.log('Reloading stream: ' + stream.src);
           src = stream.src.replace(/rand=\d+/i, 'rand='+Math.floor((Math.random() * 1000000) ));
           if ( src != stream.src ) {
+            //this.streamCommand(CMD_QUIT);
             stream.src = '';
             stream.src = src;
           } else {
             console.log("Failed to update rand on stream src");
           }
-        } else {
         }
       } else {
         console.log('No stream to reload?');
@@ -202,7 +254,16 @@ function MonitorStream(monitorData) {
     } // end if Ok or not
 
     this.lastAlarmState = this.alarmState;
-    setTimeout(this.streamCmdQuery.bind(this), statusRefreshTimeout);
+    this.streamCmdTimer = setTimeout(this.streamCmdQuery.bind(this), statusRefreshTimeout);
+  };
+
+  this.streamCommand = function(command) {
+    if (typeof(command) == 'object') {
+      for (const key in command) this.streamCmdParms[key] = command[key];
+    } else {
+      this.streamCmdParms.command = command;
+    }
+    this.streamCmdReq(this.streamCmdParms);
   };
 
   this.streamCmdQuery = function(resent) {

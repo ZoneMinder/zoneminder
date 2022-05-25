32e2db03fca9c13f9c0bd55205dfca71611e7f8f
Isaac Connor <isaac@zoneminder.com>
2022-05-25 15:54:04 -0400

    Update to use setScale instead of setStreamScale

--- web/skins/classic/views/js/zones.js
+++ web/skins/classic/views/js/zones.js
@@ -7,12 +7,12 @@ function AddNewZone(el) {
 var monitors = new Array();
 
 function initPage() {
-  for ( var i = 0, length = monitorData.length; i < length; i++ ) {
+  for ( let i = 0, length = monitorData.length; i < length; i++ ) {
     monitors[i] = new MonitorStream(monitorData[i]);
 
     // Start the fps and status updates. give a random delay so that we don't assault the server
-    var delay = Math.round( (Math.random()+0.5)*statusRefreshTimeout );
-    monitors[i].setStreamScale('0');
+    const delay = Math.round( (Math.random()+0.5)*statusRefreshTimeout );
+    monitors[i].setScale('0');
     monitors[i].start(delay);
   }
 
@@ -40,4 +40,3 @@ function streamCmdQuit() {
 }
 
 window.addEventListener('DOMContentLoaded', initPage);
-

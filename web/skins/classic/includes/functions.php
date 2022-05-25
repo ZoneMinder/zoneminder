7ad513b4e6d7d91aca3bbb914d7762d057a7f4eb
Isaac Connor <isaac@zoneminder.com>
2022-05-25 13:39:12 -0400

    Load ajaxQueue for all views

--- web/skins/classic/includes/functions.php
+++ web/skins/classic/includes/functions.php
@@ -883,6 +883,7 @@ function xhtmlFooter() {
 ?>
   <script src="<?php echo cache_bust('skins/'.$skin.'/js/jquery.min.js'); ?>"></script>
   <script src="skins/<?php echo $skin; ?>/js/jquery-ui-1.12.1/jquery-ui.min.js"></script>
+  <script src="<?php echo cache_bust('js/ajaxQueue.js') ?>"></script>
   <script src="<?php echo cache_bust('skins/'.$skin.'/js/bootstrap.min.js'); ?>"></script>
 <?php echo output_script_if_exists(array(
   'js/tableExport.min.js',

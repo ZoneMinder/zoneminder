06a59ff4afd49f34ba19e26d6251e0f72213e851
Isaac Connor <isaac@zoneminder.com>
2022-05-30 15:44:35 -0400

    Test against a constant because None==1 not zero. Fixes failed suspend motion detection during move

--- scripts/ZoneMinder/lib/ZoneMinder/Monitor.pm
+++ scripts/ZoneMinder/lib/ZoneMinder/Monitor.pm
@@ -239,6 +239,12 @@ $serial = $primary_key = 'Id';
     Longitude =>  undef,
     );
 
+  use constant CAPTURING_NONE     => 1;
+  use constant CAPTURING_ONDEMAND => 2;
+  use constant CAPTURING_ALWAYS   => 3;
+  use constant ANALYSING_ALWAYS   => 2;
+  use constant ANALYSING_NONE     => 1;
+
 sub Server {
 	return new ZoneMinder::Server( $_[0]{ServerId} );
 } # end sub Server
@@ -321,7 +327,9 @@ sub suspendMotionDetection {
   return 0 if ! ZoneMinder::Memory::zmMemVerify($self);
   return if $$self{Capturing} eq 'None' or $$self{Analysing} eq 'None';
   my $count = 50;
-  while ($count and ZoneMinder::Memory::zmMemRead($self, 'shared_data:analysing', 1)) {
+  while ($count and 
+    ( ZoneMinder::Memory::zmMemRead($self, 'shared_data:analysing', 1) != ANALYSING_NONE)
+  ) {
     ZoneMinder::Logger::Debug(1, 'Suspending motion detection');
     ZoneMinder::Memory::zmMonitorSuspend($self);
     usleep(100000);

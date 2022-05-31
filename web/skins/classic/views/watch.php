1fc9ddb54b4ce929480c828196f5cbafa7de0849
Isaac Connor <isaac@zoneminder.com>
2022-05-31 11:44:04 -0400

    Put live stream in a monitor div so that it lays out right

--- web/skins/classic/views/watch.php
+++ web/skins/classic/views/watch.php
@@ -84,13 +84,16 @@ if ( $monitor->Status() != 'Connected' and $monitor->Type() != 'WebSite' ) {
 }
 ?>
     <div id="content">
+<div class="monitor" id="monitor<?php echo $monitor->Id() ?>">
       <div id="imageFeed<?php echo $monitor->Id() ?>"
 <?php
 if ( $streamMode == 'jpeg' ) {
   echo 'title="Click to zoom, shift click to pan, ctrl click to zoom out"';
 }
 ?>
-><?php echo getStreamHTML($monitor, array('scale'=>$scale, 'mode'=>'single')); ?></div>
+><?php echo getStreamHTML($monitor, array('scale'=>$scale, 'mode'=>'single')); ?>
+</div>
+</div>
 
 
 <?php if ( $monitor->Type() != 'WebSite' ) { ?>

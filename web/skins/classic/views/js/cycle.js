8d7293202d56e9232115e1c8e217f3c514b56fcd
Isaac Connor <isaac@zoneminder.com>
2022-05-31 13:58:33 -0400

    use monitorStream more in cycle

--- web/skins/classic/views/js/cycle.js
+++ web/skins/classic/views/js/cycle.js
@@ -46,34 +46,19 @@ function cyclePrev() {
 function initCycle() {
   intervalId = setInterval(nextCycleView, cycleRefreshTimeout);
   monitor = new MonitorStream(monitorData[monIdx]);
-  applyScale();
+  monitor.setBottomElement(document.getElementById('buttons'));
+  monitor.setScale($j('#scale').val(), $j('#width').val(), $j('#height').val());
+  monitor.start();
 }
 
 function changeSize() {
   var width = $j('#width').val();
   var height = $j('#height').val();
-
-  // Scale the frame
-  monitor_frame = $j('#imageFeed'+monitor.id);
-  if ( !monitor_frame ) {
-    console.log('Error finding frame');
-    return;
-  }
-  let scale = 100;
-  if ( width != 'auto' && width != '100%') {
-    scale = parseInt(100*parseInt(width) / monitorData[monIdx].width);
-  }
-  monitor_frame.css('width', width);
-  monitor_frame.css('height', height);
-  if (scale > 100) scale = 100;
-  if (scale <= 0) scale = 100;
-
   $j('#scale').val('0');
   setCookie('zmCycleScale', '0', 3600);
   setCookie('zmCycleWidth', width, 3600);
   setCookie('zmCycleHeight', height, 3600);
-  applyScale();
-  monitor.setStreamScale(scale);
+  monitor.setScale($j('#scale').val(), $j('#width').val(), $j('#height').val());
 } // end function changeSize()
 
 function changeScale() {
@@ -83,44 +68,7 @@ function changeScale() {
   setCookie('zmCycleScale', scale, 3600);
   setCookie('zmCycleWidth', 'auto', 3600);
   setCookie('zmCycleHeight', 'auto', 3600);
-  applyScale();
-} // end function changeScale()
-
-function applyScale() {
-  var scale = $j('#scale').val();
-  var width = $j('#width').val();
-  var height = $j('#height').val();
-
-  // Scale the frame
-  monitor_frame = $j('#imageFeed'+monitor.id);
-  if ( !monitor_frame ) {
-    console.log('Error finding frame');
-    return;
-  }
-
-  let newWidth;
-  let newHeight;
-  if ( scale != '0' && scale != '' && scale != 'auto' ) {
-    newWidth = (( monitorData[monIdx].width * scale ) / SCALE_BASE)+'px';
-    newHeight = (( monitorData[monIdx].height * scale ) / SCALE_BASE)+'px';
-  } else {
-    var newSize = scaleToFit(monitorData[monIdx].width, monitorData[monIdx].height, monitor_frame, $j('#buttons'));
-    if (width != 'auto' || height != 'auto') {
-      newWidth = width;
-      newHeight = height;
-    } else {
-      newWidth = newSize.width+'px';
-      newHeight = newSize.height+'px';
-    }
-    scale = newSize.autoScale;
-  }
-  monitor_frame.width(newWidth);
-  monitor_frame.height(newHeight);
-  const monitor_image = $j('#imageFeed'+monitor.id + ' img');
-  monitor_image.width('100%');
-  monitor_image.height(height);
-
-  monitor.setStreamScale(scale);
+  monitor.setScale($j('#scale').val(), $j('#width').val(), $j('#height').val());
 } // end function changeScale()
 
 $j(document).ready(initCycle);

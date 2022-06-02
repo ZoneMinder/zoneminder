edfaffb88f7a66b501b2a43792b4d6f083a331b6
Isaac Connor <isaac@zoneminder.com>
2022-06-02 13:54:48 -0400

    Only set onclick if a new function is given

--- web/js/MonitorStream.js
+++ web/js/MonitorStream.js
@@ -253,10 +253,14 @@ function MonitorStream(monitorData) {
   };
 
   this.setup_onclick = function(func) {
-    this.onclick = func;
-    const el = this.getFrame();
-    if (!el) return;
-    el.addEventListener('click', this.onclick, false);
+    if (func) {
+      this.onclick = func;
+    }
+    if (this.onclick) {
+      const el = this.getFrame();
+      if (!el) return;
+      el.addEventListener('click', this.onclick, false);
+    }
   };
 
   this.disable_onclick = function() {

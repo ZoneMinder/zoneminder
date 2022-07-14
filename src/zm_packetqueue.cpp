318062951feb069f5462494dab0b9fb43025ae07
Isaac Connor <isaac@zoneminder.com>
2022-07-14 10:42:01 -0400

    packet_counts is always true here

--- src/zm_packetqueue.cpp
+++ src/zm_packetqueue.cpp
@@ -389,7 +389,7 @@ void PacketQueue::clear() {
     *iterator_it = pktQueue.begin();
   }  // end foreach iterator
 
-  if (packet_counts) delete[] packet_counts;
+  delete[] packet_counts;
   packet_counts = nullptr;
   max_stream_id = -1;
 

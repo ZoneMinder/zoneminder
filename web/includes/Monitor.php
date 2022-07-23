bfeee1ed0977aff731f360bd3d7acfb98d1cebbc
Isaac Connor <isaac@zoneminder.com>
2022-07-23 16:08:27 -0400

    rename Onvif_Alarm_Txt to ONVIF_Alarm_Text

--- web/includes/Monitor.php
+++ web/includes/Monitor.php
@@ -154,7 +154,7 @@ public static function getStatuses() {
     'ONVIF_Username'  =>  '',
     'ONVIF_Password'  =>  '',
     'ONVIF_Options'   =>  '',
-    'Onvif_Alarm_Txt'   =>  '',
+    'ONVIF_Alarm_Text'   =>  '',
     'ONVIF_Event_Listener'  =>  '0',
     'use_Amcrest_API'  =>  '0',
     'Device'  =>  '',

9385127b50f331796c317b4b4904b678ef9d1653
Doug Nazar <nazard@nazar.ca>
2022-07-30 22:27:41 -0400

    Consolidate temp_img_buffer resizing and use correct delete[] operator

--- src/zm_stream.h
+++ src/zm_stream.h
@@ -147,7 +147,7 @@ protected:
 
   CmdMsg msg;
 
-  unsigned char *temp_img_buffer;     // Used when encoding or sending file data
+  uint8_t *temp_img_buffer;     // Used when encoding or sending file data
   size_t temp_img_buffer_size;
 
 protected:
@@ -157,6 +157,7 @@ protected:
   Image *prepareImage(Image *image);
   bool checkCommandQueue();
   virtual void processCommand(const CmdMsg *msg)=0;
+  void reserveTempImgBuffer(size_t size);
 
 public:
   StreamBase(): 

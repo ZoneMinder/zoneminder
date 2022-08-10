e10c1e8d65e10853a81c530dfabb74477d188687
Isaac Connor <iconnor@connortechnology.com>
2022-08-10 09:18:15 -0400

    Fix deprecation warning due to implicit conversion to integer.  Make it explicit.

--- web/ajax/watch.php
+++ web/ajax/watch.php
@@ -65,7 +65,7 @@ foreach ( $rows as $row ) {
   $row['imgHtml'] = '<img id="thumbnail' .$event->Id(). '" src="' .$imgSrc. '" alt="Event '.$event->Id().'" width="' .validInt($event->ThumbnailWidth()). '" height="' .validInt($event->ThumbnailHeight()).'" stream_src="' .$streamSrc. '" still_src="' .$imgSrc. '" loading="lazy" />';
   $row['Name'] = validHtmlStr($row['Name']);
   $row['StartDateTime'] = $dateTimeFormatter->format(strtotime($row['StartDateTime']));
-  $row['Length'] = gmdate('H:i:s', $row['Length'] );
+  $row['Length'] = gmdate('H:i:s', intval($row['Length']));
 
   $returned_rows[] = $row;
 } # end foreach row matching search

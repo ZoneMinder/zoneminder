60241ab7dd2097d695600940702e5d0ed2f3b23d
Isaac Connor <isaac@zoneminder.com>
2022-08-11 15:23:35 -0400

    Use new packetqueue.stopping() to not wait when the packetqueue is stopping

--- src/zm_monitor.cpp
+++ src/zm_monitor.cpp
@@ -1824,13 +1824,13 @@ bool Monitor::Analyse() {
 
           /* try to stay behind the decoder. */
           if (decoding_enabled) {
-            while (!snap->decoded and !zm_terminate and !analysis_thread->Stopped()) {
+            while (!snap->decoded and !zm_terminate and !analysis_thread->Stopped() and !packetqueue.stopping()) {
               // Need to wait for the decoder thread.
               packetqueue.notify_all();
               Debug(1, "Waiting for decode");
               packet_lock->wait();
             }  // end while ! decoded
-            if (zm_terminate or analysis_thread->Stopped()) {
+            if (zm_terminate or analysis_thread->Stopped() or packetqueue.stopping()) {
               delete packet_lock;
               return false;
             }

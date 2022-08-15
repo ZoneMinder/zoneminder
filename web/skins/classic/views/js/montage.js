baa45bff4b686383eb4b7701bddd9daac7b306e9
Isaac Connor <isaac@zoneminder.com>
2022-08-15 13:02:55 -0400

    Don't setScale after layout, just do setSteamScale because setScale will alter widths and heights

--- web/skins/classic/views/js/montage.js
+++ web/skins/classic/views/js/montage.js
@@ -1,3 +1,4 @@
+const monitors = new Array();
 
 const VIEWING = 0;
 const EDITING = 1;
@@ -65,14 +66,9 @@ function selectLayout(new_layout_id) {
     //$j('#height').val('auto');
   }
 
-  var width = $j('#width').val();
-  var height = $j('#height').val();
-  var scale = $j('#scale').val();
-  for (var i = 0, length = monitors.length; i < length; i++) {
-    var monitor = monitors[i];
-    monitor.setScale(scale, width, height);
+  for (let i = 0, length = monitors.length; i < length; i++) {
+    monitors[i].setStreamScale();
   } // end foreach monitor
-  console.log("Done selectLayout");
 } // end function selectLayout(element)
 
 function changeHeight() {
@@ -235,7 +231,6 @@ function handleClick(evt) {
   }
 }
 
-const monitors = new Array();
 function initPage() {
   $j("#hdrbutton").click(function() {
     $j("#flipMontageHeader").slideToggle("slow");

9501da8a4108d3eff5cf1eb9af3b4332cdf5dd81
Isaac Connor <isaac@zoneminder.com>
2022-08-15 17:59:25 -0400

    Remove debug

--- web/includes/functions.php
+++ web/includes/functions.php
@@ -720,7 +720,6 @@ function canStreamIframe() {
 }
 
 function canStreamNative() {
-  ZM\Debug("ZM_WEB_CAN_STREAM:".ZM_WEB_CAN_STREAM.' isInternetExplorer: ' . isInternetExplorer() . ' isOldChrome:' . isOldChrome());
   // Old versions of Chrome can display the stream, but then it blocks everything else (Chrome bug 5876)
   return ( ZM_WEB_CAN_STREAM == 'yes' || ( ZM_WEB_CAN_STREAM == 'auto' && (!isInternetExplorer() && !isOldChrome()) ) );
 }

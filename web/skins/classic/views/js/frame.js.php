5bf1d9295684613312455e168def31db6467f910
Isaac Connor <isaac@zoneminder.com>
2022-08-19 08:56:03 -0400

    SCALE_BASE is const now in skin.js.php

--- web/skins/classic/views/js/frame.js.php
+++ web/skins/classic/views/js/frame.js.php
@@ -7,8 +7,6 @@
 
 var scale = '<?php echo validJsStr($scale); ?>';
 
-var SCALE_BASE = <?php echo SCALE_BASE ?>;
-
 var eid = <?php echo $eid ?>;
 var fid = <?php echo $fid ?>;
 var record_event_stats = <?php echo ZM_RECORD_EVENT_STATS ?>;

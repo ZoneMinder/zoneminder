85278293c2bfdc221962572765f8ad6f54bbb64e
Isaac Connor <isaac@zoneminder.com>
2022-09-04 13:54:18 -0400

    Make monitor rows in event stats a link if user can view monitors. Include path to event.

--- web/skins/classic/views/js/event.js.php
+++ web/skins/classic/views/js/event.js.php
@@ -41,7 +41,8 @@ var eventData = {
     DiskSpace: '<?php echo human_filesize($Event->DiskSpace(null)) ?>',
     Storage: '<?php echo validHtmlStr($Event->Storage()->Name()).( $Event->SecondaryStorageId() ? ', '.validHtmlStr($Event->SecondaryStorage()->Name()) : '' ) ?>',
     Archived: <?php echo $Event->Archived?'true':'false' ?>,
-    Emailed: <?php echo $Event->Emailed?'true':'false' ?>
+    Emailed: <?php echo $Event->Emailed?'true':'false' ?>,
+    Path: '<?php echo $Event->Path() ?>'
 <?php } ?>
 };
 
@@ -65,6 +66,7 @@ var eventDataStrings = {
     MaxScore: '<?php echo translate('AttrMaxScore') ?>',
     DiskSpace: '<?php echo translate('DiskSpace') ?>',
     Storage: '<?php echo translate('Storage') ?>',
+    Path: '<?php echo translate('Path') ?>',
     Archived: '<?php echo translate('Archived') ?>',
     Emailed: '<?php echo translate('Emailed') ?>'
 };

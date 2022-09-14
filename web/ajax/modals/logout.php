c4baddbf2ab410712f11d7c9d79e20a478042990
Isaac Connor <isaac@zoneminder.com>
2022-09-14 17:00:59 +0000

    Add fade to the logout modal which for some reason fixes it not showing after a hide

--- web/ajax/modals/logout.php
+++ web/ajax/modals/logout.php
@@ -19,7 +19,7 @@
 //
 global $CLANG;
 ?>
-<div id="modalLogout" class="modal" tabindex="-1" role="dialog">
+<div id="modalLogout" class="modal fade" tabindex="-1" role="dialog">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">

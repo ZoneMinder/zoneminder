3aa431020090216d352bf149803c29c9e0d85450
Isaac Connor <isaac@zoneminder.com>
2022-10-03 15:49:48 -0400

    Only perform actions on post. Doing them on GET allows doing actions from things like img tags which is not good.

--- web/index.php
+++ web/index.php
@@ -202,8 +202,12 @@ foreach ( getSkinIncludes('skin.php') as $includeFile ) {
   require_once $includeFile;
 }
 
-if ( isset($_REQUEST['action']) )
-  $action = detaintPath($_REQUEST['action']);
+if (isset($_POST['action'])) {
+  # Actions can only be performed on POST because we don't check csrf on GETs.
+  $action = detaintPath($_POST['action']);
+} else if (isset($_REQUEST['action'])) {
+  ZM\Error('actions can no longer be performed without POST.');
+}
 
 # The only variable we really need to set is action. The others are informal.
 isset($view) || $view = NULL;

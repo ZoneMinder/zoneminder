196ae7cf8904ba0d10b42ac2caab8edc0653a15c
Isaac Connor <isaac@zoneminder.com>
2022-10-12 19:12:36 -0400

    Add UpdatedOn field to Monitor_Status

---
+++ db/zm_update-1.37.25.sql
@@ -0,0 +1,11 @@
+SET @s = (SELECT IF(
+    (SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS WHERE table_schema = DATABASE()
+     AND table_name = 'Monitor_Status'
+     AND column_name = 'UpdatedOn'
+    ) > 0,
+"SELECT 'Column UpdatedOn already exists in Monitor_Status'",
+"ALTER TABLE `Monitor_Status` ADD `UpdatedOn`     datetime NOT NULL default NOW() AFTER CaptureBandwidth"
+));
+
+PREPARE stmt FROM @s;
+EXECUTE stmt;

a3a44e2f27e618784609040086486e6948168421
Isaac Connor <isaac@zoneminder.com>
2022-10-31 14:46:27 -0400

    Handle empty but defined REQUEST[action]

--- web/index.php
+++ web/index.php
@@ -196,7 +196,7 @@ foreach ( getSkinIncludes('skin.php') as $includeFile ) {
 if (isset($_POST['action'])) {
   # Actions can only be performed on POST because we don't check csrf on GETs.
   $action = detaintPath($_POST['action']);
-} else if (isset($_REQUEST['action'])) {
+} else if (isset($_REQUEST['action']) and $_REQUEST['action']) {
   ZM\Error('actions can no longer be performed without POST.');
 }
 

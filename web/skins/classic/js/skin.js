ecf91ff6a0163994d3707a421408ea860a3643e4
Isaac Connor <isaac@zoneminder.com>
2023-02-28 13:58:02 -0500

    Unhide material icons once the font has loaded

--- web/skins/classic/js/skin.js
+++ web/skins/classic/js/skin.js
@@ -1030,3 +1030,11 @@ function post(path, params, method='post') {
   document.body.appendChild(form);
   form.submit();
 }
+
+const font = new FontFaceObserver('Material Icons', { weight: 400 });
+font.load().then(function () {
+  console.log('Font is available');
+  $j('.material-icons').css('display','inline-block');
+}, function () {
+  console.log('Font is not available');
+});

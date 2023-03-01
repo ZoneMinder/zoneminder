11cfe8276f5abc3174cfe5b8a7cf7043ffe4f54d
Isaac Connor <isaac@zoneminder.com>
2023-03-01 13:34:18 -0500

    Add Forgot password button

--- web/skins/classic/views/login.php
+++ web/skins/classic/views/login.php
@@ -8,14 +8,18 @@ xhtmlHeaders(__FILE__, translate('Login'));
 if ( defined('ZM_OPT_USE_AUTH') and ZM_OPT_USE_AUTH ) {
 ?>
 		<form class="center-block" name="loginForm" id="loginForm" method="post" action="?view=login">
-			<input type="hidden" name="action" value="login"/>
       <input type="hidden" name="postLoginQuery" value="<?php echo isset($_SESSION['postLoginQuery']) ? validHtmlStr($_SESSION['postLoginQuery']) : ''; ?>" />
 
 			<div id="loginError" class="hidden alarm" role="alert">
 				<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
 				Invalid username or password.
 			</div>
-
+<?php
+  global $error_message;
+  if ( $error_message ) {
+   echo '<div id="error">'.$error_message.'</div>';
+  }
+?>
 			<div id="loginform">
 
         <h1><i class="material-icons md-36">account_circle</i> <?php echo validHtmlStr(ZM_WEB_TITLE) . ' ' . translate('Login') ?></h1>
@@ -24,7 +28,7 @@ if ( defined('ZM_OPT_USE_AUTH') and ZM_OPT_USE_AUTH ) {
 				<input type="text" id="inputUsername" name="username" class="form-control" autocapitalize="none" placeholder="Username" required autofocus autocomplete="username"/>
 	
 				<label for="inputPassword" class="sr-only"><?php echo translate('Password') ?></label>
-				<input type="password" id="inputPassword" name="password" class="form-control" placeholder="Password" required autocomplete="current-password"/>
+				<input type="password" id="inputPassword" name="password" class="form-control" placeholder="Password" autocomplete="current-password"/>
 <?php
 if (
   defined('ZM_OPT_USE_GOOG_RECAPTCHA') 
@@ -35,7 +39,12 @@ if (
   echo '<div class="g-recaptcha" data-sitekey="'.ZM_OPT_GOOG_RECAPTCHA_SITEKEY.'"></div>
     <script src="https://www.google.com/recaptcha/api.js" async defer></script>';
 } ?>
-				<button class="btn btn-lg btn-primary btn-block" type="submit"><?php echo translate('Login') ?></button>
+        <button class="btn btn-lg btn-primary btn-block" type="submit" name="action" value="login">
+        <?php echo translate('Login') ?>
+        </button>
+        <button class="btn btn-lg btn-primary btn-block" type="submit" name="action" value="forgotpassword">
+        <?php echo translate('Forgot Password') ?>
+        </button>
 			</div>
 		</form>
 <?php 

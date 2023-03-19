7779af74ad49a4ce82b145f1789a348ff2072173
Isaac Connor <isaac@zoneminder.com>
2023-03-19 11:31:12 -0400

    Populate missing serial field in Storage class

--- scripts/ZoneMinder/lib/ZoneMinder/Storage.pm
+++ scripts/ZoneMinder/lib/ZoneMinder/Storage.pm
@@ -47,11 +47,12 @@ use ZoneMinder::Database qw(:all);
 
 use POSIX;
 
-use vars qw/ $table $primary_key %fields $debug/;
-$debug = 0;
+use vars qw/ $serial $primark_key $table $primary_key %fields $debug/;
+$serial = $primary_key = 'Id';
+$debug = 1;
 $table = 'Storage';
 $primary_key = 'Id';
-%fields = map { $_ => $_ } qw( Id Name Path DoDelete ServerId Type Url DiskSpace Scheme );
+%fields = map { $_ => $_ } qw( Id Name Path DoDelete ServerId Type Url DiskSpace Scheme Enabled);
 
 sub Path {
   if ( @_ > 1 ) {

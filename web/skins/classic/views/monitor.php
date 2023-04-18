ZoneMinder/zoneminder

commit 5027bf207efb72ef69896b621dbadc204af2dd20

add h264_mmal as a decoder option

web/skins/classic/views/monitor.php
@@ -825,16 +825,20 @@ $decoders = array(
   'libx264' => 'libx264',
   'h264' => 'h264',
   'h264_cuvid' => 'h264_cuvid',
+  'h264_mmal'   => 'h264_mmal',
   'h264_omx' => 'h264_omx',
   'h264_qsv' => 'h264_qsv',
   'h264_vaapi' => 'h264_vaapi',
   'h264_v4l2m2m' => 'h264_v4l2m2m',
   'libx265' => 'libx265',
   'hevc_cuvid' => 'hevc_cuvid',
+  'hevc_nvmpi' => 'hevc_nvmpi',
   'hevc_qsv' => 'hevc_qsv',
+  'vp8_nvmpi' => 'vp8_nvmpi',
   'libvpx-vp9' => 'libvpx-vp9',
   'vp9_qsv' => 'vp9-qsv',
   'vp9_cuvid' => 'vp9_cuvid',
+  'vp9_nvmpi' => 'vp9_nvmpi',
   'vp9_v4l2m2m' => 'vp9_v4l2m2m',
   'libsvtav1' => 'libsvtav1',
   'libaom-av1'  => 'libaom-av1',

commit daa47f4eacf4a0313a088b597369f9e3727c39c3

views/monitor.php: Add back h264 NVMPI decoder
While moving decoder selection to be configuration based, the NVMPI based
H264 decoder was missed: add it back.

Fixes: 5027bf207 ("add h264_mmal as a decoder option")
Signed-off-by: AngeloGioacchino Del Regno <angelogioacchino.delregno@somainline.org>

web/skins/classic/views/monitor.php
@@ -825,6 +825,7 @@ $decoders = array(
   'libx264' => 'libx264',
   'h264' => 'h264',
   'h264_cuvid' => 'h264_cuvid',
+  'h264_nvmpi' => 'h264_nvmpi',
   'h264_mmal'   => 'h264_mmal',
   'h264_omx' => 'h264_omx',
   'h264_qsv' => 'h264_qsv',
14eb38a00b65d9f66be21f5c63fa9ee13741ce84
Isaac Connor <isaac@zoneminder.com>
2023-06-08 10:30:01 -0400

    add timeout value to debug, only send scale command if it is > 0

--- web/js/MonitorStream.js
+++ web/js/MonitorStream.js
@@ -51,7 +51,7 @@ function MonitorStream(monitorData) {
   };
   this.img_onload = function() {
     if (!this.streamCmdTimer) {
-      console.log('Image stream has loaded! starting streamCmd for '+this.connKey);
+      console.log('Image stream has loaded! starting streamCmd for '+this.connKey+' in '+statusRefreshTimeout + 'ms');
       this.streamCmdTimer = setTimeout(this.streamCmdQuery.bind(this), statusRefreshTimeout);
     }
   };
@@ -472,8 +472,10 @@ function MonitorStream(monitorData) {
             if (this.onplay) this.onplay();
           } // end if paused or delayed
           if ((this.status.scale !== undefined) && (this.status.scale !== undefined) && (this.status.scale != this.scale)) {
-            console.log("Stream not scaled, re-applying", this.scale, this.status.scale);
-            this.streamCommand({command: CMD_SCALE, scale: this.scale});
+            if (this.status.scale != 0) {
+              console.log("Stream not scaled, re-applying", this.scale, this.status.scale);
+              this.streamCommand({command: CMD_SCALE, scale: this.scale});
+            }
           }
 
           $j('#zoomValue'+this.id).text(this.status.zoom);

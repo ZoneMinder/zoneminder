6cad43a2f0341627a82f7c62df0d9d4670166d18
adhamiamirhossein <adhamiamirhossein@gmail.com>
2023-11-22 10:48:04 +0330

    fix: php 8.3 deprecated get_class method call without argument

--- web/includes/Group_Permission.php
+++ web/includes/Group_Permission.php
@@ -20,11 +20,11 @@ class Group_Permission extends ZM_Object {
   private $Monitors;
 
   public static function find( $parameters = array(), $options = array() ) {
-    return ZM_Object::_find(get_class(), $parameters, $options);
+    return ZM_Object::_find(self::class, $parameters, $options);
   }
 
   public static function find_one( $parameters = array(), $options = array() ) {
-    return ZM_Object::_find_one(get_class(), $parameters, $options);
+    return ZM_Object::_find_one(self::class, $parameters, $options);
   }
 
   public function MonitorPermission($mid) {

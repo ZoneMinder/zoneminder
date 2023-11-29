77fcc95a026bb52ad3f86748769018f3d048962e
Aaron Kling <webgeek1234@gmail.com>
2023-11-29 13:49:56 -0600

    Remove db triggers for intermediate event summaries

--- db/zm_update-1.37.48.sql
+++ db/zm_update-1.37.48.sql
@@ -1,1 +1,19 @@
 UPDATE Monitors SET ControlAddress='' WHERE ControlAddress='user:port@ip';
+
+DROP TRIGGER IF EXISTS Events_Hour_delete_trigger;
+DROP TRIGGER IF EXISTS Events_Hour_update_trigger;
+DROP TRIGGER IF EXISTS Events_Day_delete_trigger;
+DROP TRIGGER IF EXISTS Events_Day_update_trigger;
+DROP TRIGGER IF EXISTS Events_Week_delete_trigger;
+DROP TRIGGER IF EXISTS Events_Week_update_trigger;
+DROP TRIGGER IF EXISTS Events_Month_delete_trigger;
+DROP TRIGGER IF EXISTS Events_Month_update_trigger;
+DROP TRIGGER IF EXISTS event_insert_trigger;
+DROP TRIGGER IF EXISTS event_update_trigger;
+DROP TRIGGER IF EXISTS event_delete_trigger;
+
+DROP TABLE IF EXISTS `Events_Hour`;
+DROP TABLE IF EXISTS `Events_Day`;
+DROP TABLE IF EXISTS `Events_Week`;
+DROP TABLE IF EXISTS `Events_Month`;
+DROP TABLE IF EXISTS `Events_Archived`;

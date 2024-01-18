39e03e6a3cccc08df47bb39b663547c13160fe72
Isaac Connor <isaac@zoneminder.com>
2024-01-18 14:16:38 -0500

    Fix infinite loop in the event there is only the initial frame in the db.

--- src/zm_eventstream.cpp
+++ src/zm_eventstream.cpp
@@ -287,11 +287,12 @@ bool EventStream::loadEventData(uint64_t event_id) {
           FPSeconds(frame.offset).count(),
           FPSeconds(frame.delta).count(),
           frame.in_db);
-  }
+  } // end foreach db row
+
   if (event_data->end_time.time_since_epoch() != Seconds(0)) {
-    Debug(1, "end_time > last_timestamp, filling frames");
-    Microseconds delta = last_frame ? last_frame->delta : Microseconds( static_cast<int>(1000000 * base_fps / FPSeconds(event_data->duration).count()) );
+    Microseconds delta = (last_frame && (last_frame->delta > Microseconds(0))) ? last_frame->delta : Microseconds( static_cast<int>(1000000 * base_fps / FPSeconds(event_data->duration).count()) );
     if (!last_frame) {
+      // There were no frames in db
       auto frame = event_data->frames.emplace_back(
           1,
           event_data->start_time,
@@ -305,7 +306,7 @@ bool EventStream::loadEventData(uint64_t event_id) {
       event_data->frame_count ++;
     }
 
-    while (event_data->end_time > last_timestamp) {
+    while (event_data->end_time > last_timestamp and !zm_terminate) {
       last_timestamp += delta;
       last_id ++;
 

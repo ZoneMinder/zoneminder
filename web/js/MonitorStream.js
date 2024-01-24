8ca1436224e93f2c449f150ea8f6572f26848bd7
Isaac Connor <isaac@zoneminder.com>
2024-01-24 12:54:13 -0500

    If img has a fix width set, clear it when we change scale

--- web/js/MonitorStream.js
+++ web/js/MonitorStream.js
@@ -139,6 +139,7 @@ function MonitorStream(monitorData) {
     if (width && (width != '0px') && (img.style.width.search('%') == -1)) {
       monitor_frame.css('width', parseInt(width));
     }
+    if (img.style.width) img.style.width = 'auto';
     if (height && height != '0px') img.style.height = height;
 
     this.setStreamScale(newscale);

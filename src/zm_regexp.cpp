fd09feb5f45609518dd90e87a5de1c4467ef8249
David Aguilar <davvid@gmail.com>
2024-02-14 09:47:09 +0000

    regexp: port to pcre2
    Update pcre usage to be compatible with pcre2.

--- src/zm_regexp.cpp
+++ src/zm_regexp.cpp
@@ -26,31 +26,27 @@
 
 RegExpr::RegExpr( const char *pattern, int flags, int p_max_matches ) : max_matches( p_max_matches ), match_buffers( nullptr ), match_lengths( nullptr ), match_valid( nullptr )
 {
-  const char *errstr;
-  int erroffset = 0;
-  if ( !(regex = pcre_compile( pattern, flags, &errstr, &erroffset, 0 )) )
+  int errorcode;
+  PCRE2_SIZE erroffset = 0;
+  match_data = pcre2_match_data_create(max_matches, nullptr);
+  if ( !(regex = pcre2_compile( (PCRE2_SPTR8)pattern, strlen(pattern), flags, &errorcode, &erroffset, nullptr )) )
   {
-    Panic( "pcre_compile(%s): %s at %d", pattern, errstr, erroffset );
-  }
-
-  regextra = pcre_study( regex, 0, &errstr );
-  if ( errstr )
-  {
-    Panic( "pcre_study(%s): %s", pattern, errstr );
+    PCRE2_UCHAR buffer[256];
+    pcre2_get_error_message(errorcode, buffer, sizeof(buffer));
+    Panic( "pcre2_compile(%s): %s at %ld", pattern, buffer, erroffset );
   }
 
   if ( (ok = (bool)regex) )
   {
-    match_vectors = new int[3*max_matches];
-    memset( match_vectors, 0, sizeof(*match_vectors)*3*max_matches );
     match_buffers = new char *[max_matches];
     memset( match_buffers, 0, sizeof(*match_buffers)*max_matches );
-    match_lengths = new int[max_matches];
+    match_lengths = new PCRE2_SIZE[max_matches];
     memset( match_lengths, 0, sizeof(*match_lengths)*max_matches );
     match_valid = new bool[max_matches];
     memset( match_valid, 0, sizeof(*match_valid)*max_matches );
   } else {
-    match_vectors = nullptr;
+    pcre2_match_data_free(match_data);
+    match_data = nullptr;
   }
   match_string = "";
   n_matches = 0;
@@ -68,18 +64,22 @@ RegExpr::~RegExpr()
   delete[] match_valid;
   delete[] match_lengths;
   delete[] match_buffers;
-  delete[] match_vectors;
+  if ( match_data )
+  {
+    pcre2_match_data_free(match_data);
+  }
 }
 
 int RegExpr::Match( const char *subject_string, int subject_length, int flags )
 {
   match_string = subject_string;
 
-  n_matches = pcre_exec( regex, regextra, subject_string, subject_length, 0, flags, match_vectors, 2*max_matches );
+  int rc = pcre2_match( regex, (PCRE2_SPTR)subject_string, subject_length, 0, flags, match_data, nullptr );
+  n_matches = pcre2_get_ovector_count(match_data);
 
-  if ( n_matches <= 0 )
+  if ( rc < 0 || n_matches <= 0 )
   {
-    if ( n_matches < PCRE_ERROR_NOMATCH )
+    if ( rc <= PCRE2_ERROR_NOMATCH )
     {
       Error( "Error %d executing regular expression", n_matches );
     }
@@ -101,14 +101,15 @@ const char *RegExpr::MatchString( int match_index ) const
   }
   if ( !match_valid[match_index] )
   {
-    int match_len = match_vectors[(2*match_index)+1]-match_vectors[2*match_index];
+    PCRE2_SIZE* ovector = pcre2_get_ovector_pointer(match_data);
+    PCRE2_SIZE match_len = ovector[(2*match_index)+1]-ovector[2*match_index];
     if ( match_lengths[match_index] < (match_len+1) )
     {
       delete[] match_buffers[match_index];
       match_buffers[match_index] = new char[match_len+1];
       match_lengths[match_index] = match_len+1;
     }
-    memcpy( match_buffers[match_index], match_string+match_vectors[2*match_index], match_len );
+    memcpy( match_buffers[match_index], match_string+ovector[2*match_index], match_len );
     match_buffers[match_index][match_len] = '\0';
     match_valid[match_index] = true;
   }
@@ -121,7 +122,8 @@ int RegExpr::MatchLength( int match_index ) const
   {
     return( 0 );
   }
-  return( match_vectors[(2*match_index)+1]-match_vectors[2*match_index] );
+  PCRE2_SIZE* ovector = pcre2_get_ovector_pointer(match_data);
+  return( ovector[(2*match_index)+1]-ovector[2*match_index] );
 }
 
 #endif // HAVE_LIBPCRE

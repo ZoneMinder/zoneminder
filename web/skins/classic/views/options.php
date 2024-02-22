0cbce2a8a47b6e365da1a054a07459e70dffb6c9
Isaac Connor <isaac@zoneminder.com>
2024-02-22 09:33:32 -0500

    Apply bootstrap table to servers list.  Split the servers content into it's own file.  Add overflow:auto to the container which for some reason fixes button visibility.

--- web/skins/classic/views/options.php
+++ web/skins/classic/views/options.php
@@ -72,7 +72,7 @@ foreach ($tabs as $name=>$value) {
 ?>
         </ul>
       </nav>
-      <div class="container-fluid col-sm-offset-2 h-100 pr-0">
+      <div id="optionsContainer" class="container-fluid col-sm-offset-2">
 <?php 
 if ($tab == 'skins') {
 ?>
@@ -137,69 +137,7 @@ foreach (array_map('basename', glob('skins/'.$skin.'/css/*', GLOB_ONLYDIR)) as $
   // Have to do this 
   header('Location: '.$redirect);
 } else if ($tab == 'servers') {
-?>
-          <form name="serversForm" method="post" action="?">
-            <input type="hidden" name="view" value="<?php echo $view ?>"/>
-            <input type="hidden" name="tab" value="<?php echo $tab ?>"/>
-            <input type="hidden" name="action" value="delete"/>
-            <input type="hidden" name="object" value="server"/>
-            <div id="contentButtons">
-              <button type="button" id="NewServerBtn" value="<?php echo translate('AddNewServer') ?>" disabled="disabled"><?php echo translate('AddNewServer') ?></button>
-              <button type="submit" class="btn-danger" name="deleteBtn" value="Delete" disabled="disabled"><?php echo translate('Delete') ?></button>
-            </div>
-            <table id="contentTable" class="table table-striped">
-              <thead class="thead-highlight">
-                <tr>
-                  <th class="colMark"><?php echo translate('Mark') ?></th>
-                  <th class="colId"><?php echo translate('Id') ?></th>
-                  <th class="colName"><?php echo translate('Name') ?></th>
-                  <th class="colUrl"><?php echo translate('Url') ?></th>
-                  <th class="colPathToIndex"><?php echo translate('PathToIndex') ?></th>
-                  <th class="colPathToZMS"><?php echo translate('PathToZMS') ?></th>
-                  <th class="colPathToAPI"><?php echo translate('PathToApi') ?></th>
-                  <th class="colStatus"><?php echo translate('Status') ?></th>
-                  <th class="colMonitorCount"><?php echo translate('Monitors') ?></th>
-                  <th class="colCpuLoad"><?php echo translate('CpuLoad') ?></th>
-                  <th class="colMemory"><?php echo translate('Free').'/'.translate('Total') . ' ' . translate('Memory') ?></th>
-                  <th class="colSwap"><?php echo translate('Free').'/'.translate('Total') . ' ' . translate('Swap') ?></th>
-                  <th class="colStats"><?php echo translate('RunStats') ?></th>
-                  <th class="colAudit"><?php echo translate('RunAudit') ?></th>
-                  <th class="colTrigger"><?php echo translate('RunTrigger') ?></th>
-                  <th class="colEventNotification"><?php echo translate('RunEventNotification') ?></th>
-                </tr>
-              </thead>
-              <tbody>
-<?php
-          $monitor_counts = dbFetchAssoc('SELECT Id,(SELECT COUNT(Id) FROM Monitors WHERE Deleted!=1 AND ServerId=Servers.Id) AS MonitorCount FROM Servers', 'Id', 'MonitorCount');
-          foreach (ZM\Server::find() as $Server) {
-            $svr_opt = 'class="serverCol" data-sid="'.$Server->Id().'"';
-            ?>
-                <tr>
-                  <td class="colMark"><input type="checkbox" name="markIds[]" value="<?php echo $Server->Id() ?>" data-on-click-this="configureDeleteButton"<?php if ( !$canEdit ) { ?> disabled="disabled"<?php } ?>/></td>
-                  <td class="colId"><?php echo makeLink('#', $Server->Id(), $canEdit, $svr_opt ) ?></td>
-                  <td class="colName"><?php echo makeLink('#', validHtmlStr($Server->Name()), $canEdit, $svr_opt ) ?></td>
-                  <td class="colUrl"><?php echo makeLink('#', validHtmlStr($Server->Url()), $canEdit, $svr_opt ) ?></td>
-                  <td class="colPathToIndex"><?php echo makeLink('#', validHtmlStr($Server->PathToIndex()), $canEdit, $svr_opt ) ?></td>
-                  <td class="colPathToZMS"><?php echo makeLink('#', validHtmlStr($Server->PathToZMS()), $canEdit, $svr_opt ) ?></td>
-                  <td class="colPathToAPI"><?php echo makeLink('#', validHtmlStr($Server->PathToAPI()), $canEdit, $svr_opt ) ?></td>
-                  <td class="colStatus <?php if ( $Server->Status() == 'NotRunning' ) { echo 'danger'; } ?>">
-                      <?php echo makeLink('#', validHtmlStr($Server->Status()), $canEdit, $svr_opt) ?></td>
-                  <td class="colMonitorCount"><?php echo makeLink('#', validHtmlStr($monitor_counts[$Server->Id()]), $canEdit, $svr_opt) ?></td>
-                  <td class="colCpuLoad <?php if ( $Server->CpuLoad() > 5 ) { echo 'danger'; } ?>"><?php echo makeLink('#', $Server->CpuLoad(), $canEdit, $svr_opt) ?></td>
-                  <td class="colMemory <?php if ( (!$Server->TotalMem()) or ($Server->FreeMem()/$Server->TotalMem() < .1) ) { echo 'danger'; } ?>">
-                      <?php echo makeLink('#', human_filesize($Server->FreeMem()) . ' / ' . human_filesize($Server->TotalMem()), $canEdit, $svr_opt) ?></td>
-                  <td class="colSwap <?php if ( (!$Server->TotalSwap()) or ($Server->FreeSwap()/$Server->TotalSwap() < .1) ) { echo 'danger'; } ?>">
-                      <?php echo makeLink('#', human_filesize($Server->FreeSwap()) . ' / ' . human_filesize($Server->TotalSwap()) , $canEdit, $svr_opt) ?></td>
-                  <td class="colStats"><?php echo makeLink('#', $Server->zmstats() ? 'yes' : 'no', $canEdit, $svr_opt) ?></td>
-                  <td class="colAudit"><?php echo makeLink('#', $Server->zmaudit() ? 'yes' : 'no', $canEdit, $svr_opt) ?></td>
-                  <td class="colTrigger"><?php echo makeLink('#', $Server->zmtrigger() ? 'yes' : 'no', $canEdit, $svr_opt) ?></td>
-                  <td class="colEventNotification"><?php echo makeLink('#', $Server->zmeventnotification() ? 'yes' : 'no', $canEdit, $svr_opt) ?></td>
-                </tr>
-<?php } #end foreach Server ?>
-              </tbody>
-            </table>
-          </form>
-<?php
+  include('_options_servers.php');
 } else if ($tab == 'storage') { 
 ?>
           <form name="storageForm" method="post" action="?">

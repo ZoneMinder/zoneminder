beda6cd66d2ad57720f53b4f1365ba2fd159861d
Isaac Connor <isaac@zoneminder.com>
2024-03-05 11:47:39 -0500

    Remove unused linked monitor array population. All is done in javascript now

--- web/skins/classic/views/monitor.php
+++ web/skins/classic/views/monitor.php
@@ -1043,42 +1043,6 @@ echo htmlSelect('newMonitor[Decoder]', $decoders, $monitor->Decoder());
               <label><?php echo translate('LinkedMonitors'); echo makeHelpLink('OPTIONS_LINKED_MONITORS') ?></label>
               <input type="text" name="newMonitor[LinkedMonitors]" value="<?php echo $monitor->LinkedMonitors() ?>" data-on-input="updateLinkedMonitorsUI"/><br/>
               <div id="LinkedMonitorsUI"></div>
-<?php
-      $zones = ZM\Zone::find();
-      $zones_by_monitor_id = array();
-      foreach (ZM\Zone::find() as $zone) {
-        if (! isset($zones_by_monitor_id[$zone->MonitorId()]) ) {
-          $zones_by_monitor_id[$zone->MonitorId()] = array();
-        }
-        $zones_by_monitor_id[$zone->MonitorId()][] = $zone;
-      }
-      $monitor_options = array();
-      foreach ($monitors as $linked_monitor) {
-        if ( (!$monitor->Id() || ($monitor->Id() != $linked_monitor['Id'])) && visibleMonitor($linked_monitor['Id']) ) {
-          $monitor_options[$linked_monitor['Id']] = validHtmlStr($linked_monitor['Name']) . ' : ' . translate('All Zones');
-          if (isset($zones_by_monitor_id[$linked_monitor['Id']])) {
-            foreach ( $zones_by_monitor_id[$linked_monitor['Id']] as $zone) {
-              $monitor_options[$linked_monitor['Id'].':'.$zone->Id()] = validHtmlStr($linked_monitor['Name']). ' : ' . validHtmlStr($zone->Name()) . ' ('.$zone->Type().')';
-            }
-          }
-        }
-      }
-
-      $conjunctionTypes = ZM\getFilterQueryConjunctionTypes();
-      $obracketTypes = array();
-      $cbracketTypes = array();
-
-      $ops = array('or' => translate('or'), 'and' => translate('and'));
-  
-/*
-      echo htmlSelect(
-        'newMonitor[AvailableLinkedMonitors][]',
-        $monitor_options,
-        ( $monitor->LinkedMonitors() ? explode(',', $monitor->LinkedMonitors()) : array() ),
-        array('class'=>'chosen')
-      );
-*/
-?>
             </li>
 <?php
     }

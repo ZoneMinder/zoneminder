2e959124eea0d247d0565b90e53df8ca4992eb78
IgorA100 <igora100@gmail.com>
2024-03-15 01:11:22 +0300

    Change ID button

--- web/skins/classic/views/console.php
+++ web/skins/classic/views/console.php
@@ -225,7 +225,7 @@ echo $navbar ?>
         </button>
       </div>
         
-        &nbsp;<a href="#" data-flip-сontrol-object="#fbpanel"><i id="fbflip00" class="material-icons" data-icon-visible="filter_alt_off" data-icon-hidden="filter_alt"></i></a>
+        &nbsp;<a href="#" data-flip-сontrol-object="#fbpanel"><i id="fbflip" class="material-icons" data-icon-visible="filter_alt_off" data-icon-hidden="filter_alt"></i></a>
     
     </div><!-- contentButtons -->
 <?php

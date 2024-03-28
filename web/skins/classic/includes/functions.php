030e6bbf89effac12a9df0a02e2249175de8e6ff
IgorA100 <igora100@gmail.com>
2024-03-28 18:07:40 +0300

    Moved applyChosen() function to \skins\classic\js\skin.js
    cspNonce applied to entire main JS skin file

--- web/skins/classic/includes/functions.php
+++ web/skins/classic/includes/functions.php
@@ -1020,18 +1020,7 @@ function xhtmlFooter() {
   }
   $skinJsFile = getSkinFile('js/skin.js');
 ?>
-  <script src="<?php echo cache_bust($skinJsFile) ?>"></script>
-  <script nonce="<?php echo $cspNonce; ?>">
-    function applyChosen() {
-      const limit_search_threshold = 10;
-
-      $j('.chosen').chosen('destroy');
-      $j('.chosen').not('.chosen-full-width, .chosen-auto-width').chosen({disable_search_threshold: limit_search_threshold});
-      $j('.chosen.chosen-full-width').chosen({disable_search_threshold: limit_search_threshold, width: "100%"});
-      $j('.chosen.chosen-auto-width').chosen({disable_search_threshold: limit_search_threshold, width: "auto"});
-    }
-    $j(document).ready(applyChosen);
-  </script>
+  <script nonce="<?php echo $cspNonce; ?>" src="<?php echo cache_bust($skinJsFile) ?>"></script>
   </body>
 </html>
 <?php

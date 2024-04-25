23f5e022737eb6a1474783a6feaed698a926c852
Isaac Connor <isaac@zoneminder.com>
2024-04-25 09:46:57 -0400

    Also check for Snapshots_Events in case both exist.

--- db/zm_update-1.37.59.sql
+++ db/zm_update-1.37.59.sql
@@ -4,11 +4,9 @@
 
 SELECT 'Checking For Snapshot_Events Table which should be Snapshots_Events';
 SET @s = (SELECT IF(
-    (SELECT COUNT(*)
-    FROM INFORMATION_SCHEMA.TABLES
-    WHERE table_name = 'Snapshot_Events'
-    AND table_schema = DATABASE()
-    ) > 0,
+    ((SELECT COUNT(*) FROM INFORMATION_SCHEMA.TABLES WHERE table_name = 'Snapshot_Events' AND table_schema = DATABASE()) > 0)
+    AND 
+    ((SELECT COUNT(*) FROM INFORMATION_SCHEMA.TABLES WHERE table_name = 'Snapshots_Events' AND table_schema = DATABASE()) = 0),
 "ALTER TABLE Snapshot_Events RENAME TO Snapshots_Events",
 "SELECT 'Snapshot_Event table does not exist, good.'"
 ));

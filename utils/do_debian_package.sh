399bc7e6e07e24554fc6605c17b3cf3226aa41a9
Isaac Connor <isaac@zoneminder.com>
2024-05-08 14:30:58 -0400

    Only look for version.txt in the root

--- utils/do_debian_package.sh
+++ utils/do_debian_package.sh
@@ -142,7 +142,7 @@ if [ "$SNAPSHOT" == "stable" ]; then
     BRANCH=`git describe --tags $(git rev-list --tags --max-count=1)`;
     if [ -z "$BRANCH" ]; then
       # This should only happen in CI environments where tag info isn't available
-      BRANCH=$(cat "$(find . -name 'version' -o -name 'version.txt')")
+      BRANCH=$(cat "$(find . -maxdepth 1 -name 'version' -o -name 'version.txt')")
       echo "Building branch $BRANCH"
     fi
     if [ "$BRANCH" == "" ]; then
@@ -180,7 +180,7 @@ fi;
 echo "git pull..."
 git pull
 # Grab the ZoneMinder version from the contents of the version file
-VERSION=$(cat "$(find . -name 'version' -o -name 'version.txt')")
+VERSION=$(cat "$(find . -maxdepth 1 -name 'version' -o -name 'version.txt')")
 if [ -z "$VERSION" ]; then
   exit 1;
 fi;

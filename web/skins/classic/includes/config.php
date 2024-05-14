ebe84d45206488fa66dbbbe88c4115be7de6fcc4
IgorA100 <igora100@gmail.com>
2024-05-14 21:02:33 +0300

    Changed the values in $scales (config.php)

--- web/skins/classic/includes/config.php
+++ web/skins/classic/includes/config.php
@@ -45,16 +45,17 @@ $scales = array(
   # We use 0 instead of words because we are saving this in the monitor
   # and use this array to populate the default scale option
     '0' => translate('Auto'),
-    '400' => '4x',
-    '300' => '3x',
-    '200' => '2x',
-    '150' => '1.5x',
-    '100' => translate('Actual'),
-    '75' => '3/4x',
-    '50' => '1/2x',
-    '33' => '1/3x',
-    '25' => '1/4x',
-    '12.5' => '1/8x',
+  #  '400' => '4x',
+  #  '300' => '3x',
+  #  '200' => '2x',
+  #  '150' => '1.5x',
+  #  '100' => translate('Actual'),
+  #  '75' => '3/4x',
+  #  '50' => '1/2x',
+  #  '33' => '1/3x',
+  #  '25' => '1/4x',
+  #  '12.5' => '1/8x',
+    'fit_to_width' => translate('Fit to width'),
 );
 
 if ( isset($_REQUEST['view']) && ($_REQUEST['view'] == 'montage') ) {

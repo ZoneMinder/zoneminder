d8647ac2bbf5914ef89f46eed7f14f2b29f6ba82
Isaac Connor <isaac@zoneminder.com>
2024-05-22 09:14:21 -0400

    Fre update analysis image when encoding as it won't be used anyways. Should reduce ram use while recording

--- src/zm_monitor.cpp
+++ src/zm_monitor.cpp
@@ -2144,23 +2144,24 @@ bool Monitor::Analyse() {
 
     if (event) {
       event->AddPacket(snap);
-    }
+    } else {
 
-    // In the case where people have pre-alarm frames, the web ui will generate the frame images
-    // from the mp4. So no one will notice anyways.
-    if (snap->image) {
-      if (videowriter == PASSTHROUGH) {
-        if (!savejpegs) {
-          Debug(1, "Deleting image data for %d", snap->image_index);
-          // Don't need raw images anymore
-          delete snap->image;
-          snap->image = nullptr;
+      // In the case where people have pre-alarm frames, the web ui will generate the frame images
+      // from the mp4. So no one will notice anyways.
+      if (snap->image) {
+        if (videowriter == PASSTHROUGH) {
+          if (!savejpegs) {
+            Debug(1, "Deleting image data for %d", snap->image_index);
+            // Don't need raw images anymore
+            delete snap->image;
+            snap->image = nullptr;
+          }
         }
-      }
-      if (snap->analysis_image and !(savejpegs & 2)) {
-        Debug(1, "Deleting analysis image data for %d", snap->image_index);
-        delete snap->analysis_image;
-        snap->analysis_image = nullptr;
+        if (snap->analysis_image and !(savejpegs & 2)) {
+          Debug(1, "Deleting analysis image data for %d", snap->image_index);
+          delete snap->analysis_image;
+          snap->analysis_image = nullptr;
+        }
       }
     }
     // Free up the decoded frame as well, we won't be using it for anything at this time.

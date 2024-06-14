742f36c58c5116809b3fee12c94043933bb749dd
Isaac Connor <isaac@zoneminder.com>
2024-06-14 18:34:28 -0400

    Redo the event thread. Instead of analysis adding packets to an event specific queue, just pass in the iterator and let the event thread do it's own locking. This allows us to free ram in packet in the event, and not segfault.

--- src/zm_event.cpp
+++ src/zm_event.cpp
@@ -42,12 +42,14 @@ Event::PreAlarmData Event::pre_alarm_data[MAX_PRE_ALARM_FRAMES] = {};
 
 Event::Event(
   Monitor *p_monitor,
+  packetqueue_iterator *p_packetqueue_it,
   SystemTimePoint p_start_time,
   const std::string &p_cause,
   const StringSetMap &p_noteSetMap
 ) :
   id(0),
   monitor(p_monitor),
+  packetqueue_it(p_packetqueue_it),
   start_time(p_start_time),
   end_time(p_start_time),
   cause(p_cause),
@@ -73,6 +75,8 @@ Event::Event(
   createNotes(notes);
 
   SystemTimePoint now = std::chrono::system_clock::now();
+
+  packetqueue = monitor->GetPacketQueue();
 
   if (start_time.time_since_epoch() == Seconds(0)) {
     Warning("Event has zero time, setting to now");
@@ -148,6 +152,8 @@ Event::~Event() {
     // Should be.  Issuing the stop and then getting the lock
     thread_.join();
   }
+  packetqueue->free_it(packetqueue_it);
+  delete packetqueue_it;
 
   /* Close the video file */
   // We close the videowriter first, because if we finish the event, we might try to view the file, but we aren't done writing it yet.
@@ -306,15 +312,6 @@ void Event::updateNotes(const StringSetMap &newNoteSetMap) {
     dbQueue.push(std::move(sql));
   }  // end if update
 }  // void Event::updateNotes(const StringSetMap &newNoteSetMap)
-
-void Event::AddPacket(const std::shared_ptr<ZMPacket>&packet) {
-  {
-    std::unique_lock<std::mutex> lck(packet_queue_mutex);
-
-    packet_queue.push(std::move(packet));
-  }
-  packet_queue_condition.notify_one();
-}
 
 void Event::AddPacket_(const std::shared_ptr<ZMPacket>&packet) {
   have_video_keyframe = have_video_keyframe ||
@@ -686,22 +683,11 @@ void Event::Run() {
 
   // The idea is to process the queue no matter what so that all packets get processed.
   // We only break if the queue is empty
-  while (true) {
-    std::shared_ptr<ZMPacket> packet = nullptr;
-    {
-      std::unique_lock<std::mutex> lck(packet_queue_mutex);
+  while (!terminate_) {
+    ZMLockedPacket *packet_lock = packetqueue->get_packet_and_increment_it(packetqueue_it);
+    if (packet_lock) {
+      std::shared_ptr<ZMPacket> packet = packet_lock->packet_;
 
-      if (packet_queue.empty()) {
-        if (terminate_ or zm_terminate) break;
-        packet_queue_condition.wait(lck);
-        // Necessary because we don't hold the lock in the while condition
-      }
-      if (!packet_queue.empty()) {
-        packet = packet_queue.front();
-        packet_queue.pop();
-      }
-    }  // end lock scope
-    if (packet) {
       Debug(1, "Adding packet %d", packet->image_index);
       this->AddPacket_(packet);
 
@@ -720,7 +706,11 @@ void Event::Run() {
           packet->analysis_image = nullptr;
         }
       } // end if packet->image
-    }
+      delete packet_lock;
+    } else {
+      Warning("Unable to get packet lock");
+      return;
+    } // end if packet_lock
   }  // end while
 }  // end Run()
 

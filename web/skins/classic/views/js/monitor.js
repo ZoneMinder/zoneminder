d1e8975e411e036be4e3e77e56ea8b8edc6d78f4
Isaac Connor <isaac@zoneminder.com>
2024-07-25 12:37:31 -0400

    FIx WallClockTimeStamps not being hidden when not passthrough

--- web/skins/classic/views/js/monitor.js
+++ web/skins/classic/views/js/monitor.js
@@ -170,10 +170,10 @@ function initPage() {
     el.onchange = function() {
       if (this.value == 1 /* Encode */) {
         $j('.OutputCodec').show();
-        $j('.WallClockTimestamps').hide();
+        $j('.WallClockTimeStamps').hide();
         $j('.Encoder').show();
       } else {
-        $j('.WallClockTimestamps').show();
+        $j('.WallClockTimeStamps').show();
         $j('.OutputCodec').hide();
         $j('.Encoder').hide();
       }

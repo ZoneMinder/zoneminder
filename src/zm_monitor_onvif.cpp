ZoneMinder/zoneminder

commit a0a95d887c0328294b2253c91cf81d00c7407700

Don't crash when alarms is empty

src/zm_monitor_onvif.cpp
@@ -188,8 +188,8 @@ void Monitor::ONVIF::WaitForMessage() {
           if (last_value.find("false") == 0) {
             Info("Triggered off ONVIF");
             alarms.erase(last_topic);
-            if(alarms.empty()) {
-              Debug(1, "ONVIF Alarms Empty: Alarms count is %zu, alarmed is %s", alarms.size(), alarmed ? "true": "false");
+              Debug(1, "ONVIF Alarms Empty: Alarms count is %zu, alarmed is %s, empty is %d ", alarms.size(), alarmed ? "true": "false", alarms.empty());
+            if (alarms.empty()) {
               alarmed = false;
             }
             if (!parent->Event_Poller_Closes_Event) { //If we get a close event, then we know to expect them.
@@ -284,13 +284,14 @@ int SOAP_ENV__Fault(struct soap *soap, char *faultcode, char *faultstring, char 
 #endif
 
 void Monitor::ONVIF::SetNoteSet(Event::StringSet &noteSet) {
-  std::string note = "";
+  if (alarms.empty()) return;
   #ifdef WITH_GSOAP
+    std::string note = "";
     for (auto it = alarms.begin(); it != alarms.end(); ++it) {
       note = it->first + "/" + it->second;
       noteSet.insert(note);
     }
   #endif
-  return ;
+  return;
 }
 

commit 1136ef9ebe982a9966db343b15b27508d7508285

Fix alarms() out of scope
The string alarms() is only valid when the define WITH_GSOAP is present.

Fixes: a0a95d887c03 ("Don't crash when alarms is empty")
Signed-off-by: Bryan O'Donoghue <bryan.odonoghue@nexus-software.ie>

src/zm_monitor_onvif.cpp
@@ -284,8 +284,9 @@ int SOAP_ENV__Fault(struct soap *soap, char *faultcode, char *faultstring, char 
 #endif
 
 void Monitor::ONVIF::SetNoteSet(Event::StringSet &noteSet) {
-  if (alarms.empty()) return;
   #ifdef WITH_GSOAP
+    if (alarms.empty()) return;
+
     std::string note = "";
     for (auto it = alarms.begin(); it != alarms.end(); ++it) {
       note = it->first + "/" + it->second;
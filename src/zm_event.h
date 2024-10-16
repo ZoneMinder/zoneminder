c4dbbbccc57cd7c7467e89ec1ed63974e6f3b436
Isaac Connor <isaac@zoneminder.com>
2024-10-16 10:22:25 -0400

    Revert change of tot_score and avg_score to unsigned ints. Make them ints so that comparison with other scores is ok

--- src/zm_event.h
+++ src/zm_event.h
@@ -85,8 +85,8 @@ class Event {
     int        frames;
     int        alarm_frames;
     bool alarm_frame_written;
-    unsigned int  tot_score;
-    unsigned int  max_score;
+    int  tot_score;
+    int  max_score;
     std::string path;
     std::string snapshot_file;
     bool snapshot_file_written;

0a2e41bedd0a1a3460b203c59f2ccd9ee640649a
Isaac Connor <isaac@zoneminder.com>
2025-01-09 17:19:20 -0500

    Add EncoderHWAccel fields

---
+++ db/zm_update-1.37.67.sql
@@ -0,0 +1,24 @@
+
+SET @s = (SELECT IF(
+    (SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS WHERE table_schema = DATABASE()
+      AND table_name = 'Monitors'
+      AND column_name = 'EncoderHWAccelName'
+    ) > 0,
+    "SELECT 'Column EncoderHWAccelName already exists in Monitors'",
+    "ALTER TABLE Monitors ADD `EncoderHWAccelName`  varchar(64) AFTER `Encoder`"
+    ));
+
+PREPARE stmt FROM @s;
+EXECUTE stmt;
+
+SET @s = (SELECT IF(
+    (SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS WHERE table_schema = DATABASE()
+      AND table_name = 'Monitors'
+      AND column_name = 'EncoderHWAccelDevice'
+    ) > 0,
+    "SELECT 'Column EncoderHWAccelDevice already exists in Monitors'",
+    "ALTER TABLE Monitors ADD `EncoderHWAccelDevice` varchar(255) AFTER `EncoderHWAccelName`"
+    ));
+
+PREPARE stmt FROM @s;
+EXECUTE stmt;

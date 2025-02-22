da0a36a6239bac7d2bba058ededae61b35007572
IgorA100 <igora100@gmail.com>
2025-02-22 20:13:25 +0300

    Added support for selecting Secondary stream for RTSP2Web (zone.js.php)

--- web/skins/classic/views/js/zone.js.php
+++ web/skins/classic/views/js/zone.js.php
@@ -73,6 +73,7 @@ monitorData[monitorData.length] = {
   'janusEnabled':<?php echo $monitor->JanusEnabled() ?>,
   'RTSP2WebEnabled': <?php echo $monitor->RTSP2WebEnabled() ?>,
   'RTSP2WebType': '<?php echo $monitor->RTSP2WebType() ?>',
+  'RTSP2WebStream': '<?php echo $monitor->RTSP2WebStream() ?>',
   'url': '<?php echo $monitor->UrlToIndex( ZM_MIN_STREAMING_PORT ? ($monitor->Id() + ZM_MIN_STREAMING_PORT) : '') ?>',
   'url_to_zms': '<?php echo $monitor->UrlToZMS( ZM_MIN_STREAMING_PORT ? ($monitor->Id() + ZM_MIN_STREAMING_PORT) : '') ?>',
   'type': '<?php echo $monitor->Type() ?>',

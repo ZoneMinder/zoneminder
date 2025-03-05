60189eff7209a12a486956db9a38cf9015978edd
IgorA100 <igora100@gmail.com>
2025-03-06 00:24:17 +0300

    When scaling the browser window, save the browser size in cookies (skin.js)
    Saved in JSON
    innerWidth: window.innerWidth
    innerHeight: window.innerHeight
    outerWidth: window.outerWidth
    outerHeight: window.outerHeight
    
    Request: #4228

--- web/skins/classic/js/skin.js
+++ web/skins/classic/js/skin.js
@@ -687,9 +687,19 @@ video.appendChild(track);
 var resizeTimer;
 
 function endOfResize(e) {
-  clearTimeout(resizeTimer);
-  resizeTimer = setTimeout(changeScale, 250);
+  setCookie('zmBrowserSizes', JSON.stringify({
+    innerWidth: window.innerWidth,
+    innerHeight: window.innerHeight,
+    outerWidth: window.outerWidth,
+    outerHeight: window.outerHeight,
+  }));
+  if (typeof changeScale !== 'undefined' && $j.isFunction(changeScale)) {
+    //Only for scaleToFit
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(changeScale, 250);
+  }
 }
+$j(window).on('resize', endOfResize);
 
 /* scaleToFit
  *
@@ -700,7 +710,7 @@ function endOfResize(e) {
  * scaleEl is the thing to be scaled, should be a jquery object and should have height
  * */
 function scaleToFit(baseWidth, baseHeight, scaleEl, bottomEl, container, panZoomScale = 1) {
-  $j(window).on('resize', endOfResize); //set delayed scaling when Scale to Fit is selected
+  //$j(window).on('resize', endOfResize); //set delayed scaling when Scale to Fit is selected
   if (!container) container = $j('#content');
   if (!container) {
     console.error("No container found");

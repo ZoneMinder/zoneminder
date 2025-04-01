b2599c36c1191835c47b6ff29b12754b55f9022f
Isaac Connor <isaac@zoneminder.com>
2025-04-01 09:59:52 -0700

    don't use synch

--- src/zm_untether_speedai.cpp
+++ src/zm_untether_speedai.cpp
@@ -330,12 +330,12 @@ const nlohmann::json SpeedAI::receive_detections(Job *job, float object_threshol
   {
     Debug(1, "getting receive lock");
     //std::unique_lock<std::mutex> lck(mutex_);
-    err = uai_module_synchronize(module_, &job->event);
+    err = uai_module_wait(module_, &job->event, 10);
+    //err = uai_module_synchronize(module_, &job->event);
   }
 #else
   err = uai_module_synchronize(module_, &job->event);
 #endif
-  //UaiErr err = uai_module_wait(module_, &job->event, 10);
   if (err != UAI_SUCCESS) {
     Warning("SpeedAI Failed wait %d, %s", err, uai_err_string(err));
     return coco_object;

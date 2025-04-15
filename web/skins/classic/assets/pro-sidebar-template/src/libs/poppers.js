82aa87d94a5c4907c683e9a168aff3511645ee2d
IgorA100 <igor101@front.ru>
2025-04-15 18:00:36 +0300

    Added pro-sidebar-template from https://github.com/azouaoui-med/pro-sidebar-template

---
+++ web/skins/classic/assets/pro-sidebar-template/src/libs/poppers.js
@@ -0,0 +1,44 @@
+import { SUB_MENU_ELS } from './constants';
+import Popper from './popper';
+
+class Poppers {
+  subMenuPoppers = [];
+
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    SUB_MENU_ELS.forEach((element) => {
+      this.subMenuPoppers.push(new Popper(element, element.lastElementChild));
+      this.closePoppers();
+    });
+  }
+
+  togglePopper(target) {
+    if (window.getComputedStyle(target).visibility === 'hidden') {
+      target.style.visibility = 'visible';
+      target.style.height = '100%'; 
+    } else {
+      target.style.visibility = 'hidden';
+      target.style.height = 0; 
+    }
+  }
+
+  updatePoppers() {
+    this.subMenuPoppers.forEach((element) => {
+      if (!element.classList.contains(open)) {
+        element.instance.state.elements.popper.style.display = 'none';
+      }
+      element.instance.update();
+    });
+  }
+
+  closePoppers() {
+    this.subMenuPoppers.forEach((element) => {
+      element.hide();
+    });
+  }
+}
+
+export default Poppers;

cb7d7f4a29e424b036457fe1dd94f1facdadf38e
IgorA100 <igora100@gmail.com>
2025-05-02 23:08:21 +0300

    Added a comment to the condition if ($l->Name() == 'Freeform') $l->Name('Auto'); (montage.php)

--- web/skins/classic/views/montage.php
+++ web/skins/classic/views/montage.php
@@ -85,6 +85,10 @@ $presetLayoutsNames = array( //Order matters!
 /* Create an array "Name"=>layouts to make it easier to find IDs by name */
 $layoutsByName = array();
 foreach ($layouts as $l) {
+  /* IMPORTANT! 
+  * Before GridStack integration, instead of the layer name "Auto", we used "Freeform" in the DB.
+  * Before deleting this check, you need to replace "Freeform" with "Auto" in the DB of already installed ZM !!!
+  */
   if ($l->Name() == 'Freeform') $l->Name('Auto');
   $layoutsByName[$l->Name()] = $l;
 }

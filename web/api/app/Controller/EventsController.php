c3d4b12286d126475de17fbdd6888211ec688206
Isaac Connor <isaac@zoneminder.com>
2025-07-11 17:40:54 -0400

    Do pagination if either page or limit is specified. Otherwise return all

--- web/api/app/Controller/EventsController.php
+++ web/api/app/Controller/EventsController.php
@@ -81,15 +81,8 @@ class EventsController extends AppController {
       // ICON: 2023-11-16: MontageReview now uses API and unless we specifiy a limit in params, there should be no limit
       // TODO: Implement request based limits.
 
-      # 'limit' => '100',
-      
-
-      'order' => array('StartDateTime'),
       'paramType' => 'querystring',
     );
-    if ($this->request->query('limit')) {
-      $settings['limit'] = $this->request->query('limit');
-    }
 
     if ( isset($conditions['GroupId']) ) {
       $settings['joins'] = array(
@@ -105,8 +98,8 @@ class EventsController extends AppController {
     }
     $settings['conditions'] = array($conditions, $mon_options);
 
-    if ($this->request->query('paginate')) {
-      $this->Paginator->settings = $settings;
+    $this->Paginator->settings = $settings;
+    if ($this->request->query('limit') or $this->request->query('page')) {
       $events = $this->Paginator->paginate('Event');
     } else {
       $events = $this->Event->find('all', $settings);

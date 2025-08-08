1daf5a40f465b63d1f0e53cb08e58b067f1762f8
Isaac Connor <isaac@zoneminder.com>
2025-08-08 14:50:38 -0700

    Rough in memx support

---
+++ src/zm_mx_accl.cpp
@@ -0,0 +1,314 @@
+//
+// ZoneMinder MxAccl::MxAccl Class Implementation
+// Copyright (C) 2024 ZoneMinder Inc
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+
+
+#include "zm_logger.h"
+#include "zm_ffmpeg.h"
+#include "zm_monitor.h"
+#include "zm_signal.h"
+#include "zm_vector2.h"
+
+#include "zm_mx_accl.h"
+
+#include <cstring>
+#include <algorithm>
+#include <filesystem>
+#include <iostream>
+#include <memory>
+#include <random>
+#include <vector>
+
+
+#define USE_THREAD 0
+#define BATCH_SIZE 10
+
+#ifdef HAVE_MX_ACCL_H
+static const char *coco_classes[] = {"person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck", "boat",
+  "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+  "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack",
+  "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball",
+  "kite", "baseball bat", "baseball glove", "skateboard", "surfboard", "tennis racket",
+  "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple",
+  "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair",
+  "couch", "potted plant", "bed", "dining table", "toilet", "tv", "laptop", "mouse", "remote",
+  "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator", "book",
+  "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"};
+
+MxAccl::MxAccl() :
+  accl(),
+  confidence(0.5),
+  terminate_(false),
+  inSize(0),
+  outSize(0),
+  count(0)
+{
+}
+
+MxAccl::~MxAccl() {
+  terminate_ = true;
+  if (thread_.joinable()) thread_.join();
+  while (jobs.size()) {
+    delete jobs.front();
+    jobs.pop_front();
+  }
+
+  accl->stop();
+}
+
+bool MxAccl::setup(
+    const std::string &model_type,
+    const std::string &model_file
+    ) {
+  // Load and launch module_
+  Debug(1, "MxAccl: Loading model %s", model_file.c_str());
+
+  std::vector<int> device_ids = {0};
+  std::array<bool, 2> use_model_shape = {false, false};
+
+  // Create the accelerator object and load the DFP model
+  accl = new MX::Runtime::MxAccl(filesystem::path(model_file), device_ids, use_model_shape);
+
+  model_info = accl->get_model_info(0);
+  Debug(1, "model_info in_featermaps: %d out_featuremaps: %d",
+      model_info.num_in_featuremaps,
+      model_info.num_out_featuremaps
+      );
+  input_height = model_info.in_featuremap_shapes[0][0];
+  input_width = model_info.in_featuremap_shapes[0][1];
+  input_depth = model_info.in_featuremap_shapes[0][2];
+  input_channels = model_info.in_featuremap_shapes[0][3];
+
+  yolov8 = new YOLOv8( input_width, input_height, input_channels, 0.5, 0.5);
+
+
+  /*
+  output_height = model_info.out_featuremap_shapes[0][0];
+  output_width = model_info.out_featuremap_shapes[0][1];
+  output_depth = model_info.out_featuremap_shapes[0][2];
+  output_channels = model_info.out_featuremap_shapes[0][3];
+  */
+
+  for (int i=0; i<model_info.num_out_featuremaps; i++) {
+    Debug(1, "ofmap shape = (%" PRId64 ", %" PRId64 ", %" PRId64 ", %" PRId64 ")",
+        model_info.out_featuremap_shapes[i][0],
+        model_info.out_featuremap_shapes[i][1],
+        model_info.out_featuremap_shapes[i][2],
+        model_info.out_featuremap_shapes[i][3]
+        );
+  }
+
+  Debug(1, "ifmap shape = (%d, %d, %d, %d)", input_height, input_width, input_depth, input_channels);
+  image_size = av_image_get_buffer_size(AV_PIX_FMT_RGB24, input_width, input_height, 0);
+
+
+  auto in_cb = std::bind(&MxAccl::in_callback_func, this, std::placeholders::_1, std::placeholders::_2);
+  auto out_cb = std::bind(&MxAccl::out_callback_func, this, std::placeholders::_1, std::placeholders::_2);
+  accl->connect_stream(in_cb, out_cb, 21, 0);
+
+  // Start the accelerator after connecting streams
+  accl->start();
+
+  return true;
+}
+
+MxAccl::Job * MxAccl::send_image(Job *job, Image *image) {
+  if (!job) {
+    Error("No job");
+    return nullptr;
+  }
+  av_frame_ptr frame = av_frame_ptr(av_frame_alloc());
+  image->PopulateFrame(frame.get());
+  return send_frame(job, frame.get());
+}
+
+MxAccl::Job * MxAccl::send_packet(Job *job, std::shared_ptr<ZMPacket> packet) {
+  AVFrame *avframe = packet->in_frame.get();
+  if (!avframe) {
+    Error("NO inframe in packet %d, out of mem?", packet->image_index);
+    return nullptr;
+  }
+  return send_frame(job, avframe);
+}
+
+MxAccl::Job * MxAccl::get_job() {
+  Job *job = new Job(accl);
+  job->scaled_frame->width = input_width;
+  job->scaled_frame->height = input_height;
+
+  if (av_frame_get_buffer(job->scaled_frame.get(), 32)) {
+    Error("cannot allocate scaled frame buffer");
+    return nullptr;
+  }
+  for (int i = 0; i < model_info.num_in_featuremaps; ++i) {
+    float *pData = new float[model_info.in_featuremap_sizes[i]];
+    memset(pData, 0, model_info.in_featuremap_sizes[i] * sizeof(float));
+    job->accl_input_data.push_back(pData);
+  }
+  for (int i = 0; i < model_info.num_out_featuremaps; ++i) {
+    float *pData = new float[model_info.out_featuremap_sizes[i]];
+    memset(pData, 0, model_info.out_featuremap_sizes[i] * sizeof(float));
+    job->accl_output_data.push_back(pData);
+  }
+
+  std::lock_guard<std::mutex> lck(mutex_);
+  jobs.push_back(job);
+  return job;
+}
+
+MxAccl::Job * MxAccl::send_frame(MxAccl::Job *job, AVFrame *avframe) {
+  count++;
+  Debug(1, "MxAccl::send_frame %d", count);
+
+  SystemTimePoint starttime = std::chrono::system_clock::now();
+
+  job->sw_scale_ctx = sws_getCachedContext(job->sw_scale_ctx,
+      avframe->width, avframe->height, static_cast<AVPixelFormat>(avframe->format),
+      input_width, input_height, AV_PIX_FMT_RGB24,
+      SWS_BICUBIC, nullptr, nullptr, nullptr);
+
+  int ret = sws_scale(job->sw_scale_ctx, (const uint8_t * const *)avframe->data,
+      avframe->linesize, 0, avframe->height, job->scaled_frame->data, job->scaled_frame->linesize);
+  if (ret < 0) {
+    Error("cannot do sw scale: inframe data 0x%lx, linesize %d/%d/%d/%d, height %d to %d linesize",
+        (unsigned long)avframe->data, avframe->linesize[0], avframe->linesize[1],
+        avframe->linesize[2], avframe->linesize[3], avframe->height, job->scaled_frame->linesize[0]);
+    return nullptr;
+  }
+
+  job->m_width_rescale = ((float)input_width / (float)avframe->width);
+  job->m_height_rescale = ((float)input_height / (float)avframe->height);
+  job->lock();
+  {
+    Debug(1, "Locking");
+    std::lock_guard<std::mutex> lck(mutex_);
+    send_queue.push_back(job);
+    condition_.notify_all();
+  }
+  Debug(4, "Waiting for inference");
+  job->wait();
+  job->unlock();
+  Debug(4, "Done Waiting");
+
+  SystemTimePoint endtime = std::chrono::system_clock::now();
+  if (endtime - starttime > Milliseconds(60)) {
+    Warning("scale is too slow: %.3f seconds", FPSeconds(endtime - starttime).count());
+  } else {
+    Debug(1, "scale took: %.3f seconds", FPSeconds(endtime - starttime).count());
+  }
+  return job;
+}  // Job * MxAccl::send_frame(AVFrame *frame)
+
+const nlohmann::json MxAccl::receive_detections(Job *job, float object_threshold) {
+  // Convert yolov8 result to nlohmann::json
+  nlohmann::json predictions = nlohmann::json::array();
+  std::queue<BBox> bboxes = job->result.bboxes;
+
+  // How bout an interator
+  while (!bboxes.empty()) {
+    BBox bbox = bboxes.front();
+    bboxes.pop();
+
+#if 0
+    int l = static_cast<int>((bbox.x_min - padding_width_) / letterbox_ratio_);
+    int t = static_cast<int>((bbox.y_min - padding_height_) / letterbox_ratio_);
+    int r = static_cast<int>((bbox.x_max - padding_width_) / letterbox_ratio_);
+    int b = static_cast<int>((bbox.y_max - padding_height_) / letterbox_ratio_);
+
+#else
+    float l = bbox.x_min;
+    float t = bbox.y_min;
+    float r = bbox.x_max;
+    float b = bbox.y_max;
+#endif
+     // coordinates must be scaled to true image dimensions
+    l = l / job->m_width_rescale;
+    t = t / job->m_height_rescale;
+    r = r / job->m_width_rescale;
+    b = b / job->m_height_rescale;
+
+    std::array<float, 4> cv_bbox = {l, t, r, b};
+
+    // map class_id to class name
+    std::string class_name = coco_classes[bbox.class_index];
+    predictions.push_back({{"class_name", class_name}, {"bbox", cv_bbox}, {"score", bbox.class_score}});
+  }
+
+  return predictions;
+}
+
+bool MxAccl::in_callback_func(vector<const MX::Types::FeatureMap *> dst, int channel_idx) {
+  Debug(4, "MxAccl locking, queue size %zu", send_queue.size());
+
+  Job *job = nullptr;
+  {
+    std::unique_lock<std::mutex> lck(mutex_);
+    while (!send_queue.size() and ! zm_terminate) {
+      Debug(1, "MxAccl waiting, queue size %zu", send_queue.size());
+      condition_.wait(lck);
+    }
+    if (!send_queue.size()) return false;
+    job = send_queue.front();
+  }
+
+  SystemTimePoint starttime = std::chrono::system_clock::now();
+  // Compute letterbox padding for YOLO model
+  yolov8->ComputePadding(job->scaled_frame->width, job->scaled_frame->height);
+  yolov8->PreProcess(job->scaled_frame->data[0], job->scaled_frame->width, job->scaled_frame->height, job->accl_input_data);
+  // Set preprocessed input data for accelerator
+  for (int in_idx = 0; in_idx < model_info.num_in_featuremaps; ++in_idx) {
+    dst[in_idx]->set_data(job->accl_input_data[in_idx]);
+  }
+  SystemTimePoint endtime = std::chrono::system_clock::now();
+  if (endtime - starttime > Milliseconds(60)) {
+    Warning("in_callback is too slow: %.3f seconds", FPSeconds(endtime - starttime).count());
+  } else {
+    Debug(1, "in_callback took: %.3f seconds", FPSeconds(endtime - starttime).count());
+  }
+  condition_.notify_one();
+  return true;
+}
+
+bool MxAccl::out_callback_func(vector<const MX::Types::FeatureMap *> src, int channel_idx) {
+  YOLOv8Result result;
+
+  Job *job;
+  {
+    std::unique_lock<std::mutex> lck(mutex_);
+    while (!send_queue.size() and !zm_terminate) {
+      Debug(1, "MxAccl waiting, queue size %zu", send_queue.size());
+      condition_.wait(lck);
+    }
+    if (!send_queue.size()) return false;
+    job = send_queue.front();
+  }
+
+  // Retrieve output data from accelerator
+  for (int out_idx = 0; out_idx < model_info.num_out_featuremaps; ++out_idx) {
+    src[out_idx]->get_data(job->accl_output_data[out_idx]);
+  }
+  // Set confidence threshold and process detection results
+  yolov8->SetConfidenceThreshold(confidence);
+  yolov8->PostProcess(job->accl_output_data, result);
+  job->result = std::move(result);
+
+  job->notify();
+  return true;
+}
+
+#endif
---
+++ src/zm_mx_accl.h
@@ -0,0 +1,129 @@
+#ifndef ZM_MX_ACCL_H
+#define ZM_MX_ACCL_H
+
+#include <nlohmann/json.hpp>
+
+#include <condition_variable>
+#include <list>
+#include <mutex>
+#include <atomic>
+#include <memory>
+#include <thread>
+
+extern "C" {
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libavfilter/buffersink.h>
+#include <libavfilter/buffersrc.h>
+#include <libavutil/opt.h>
+#include <libswscale/swscale.h>
+}
+
+#ifdef HAVE_MX_ACCL_H
+#include <memx/accl/MxAccl.h>
+#include "zm_memx_yolov8.h"
+
+class MxAccl {
+  private:
+    MX::Runtime::MxAccl *accl;
+    MX::Types::MxModelInfo model_info;
+    int32_t input_height, input_width, input_depth, input_channels, input_format;
+    int32_t output_height, output_width, output_depth, output_channels, output_format;
+    YOLOv8 *yolov8;
+    float confidence;
+
+    std::mutex  mutex_;
+    std::condition_variable condition_;
+
+    std::atomic<bool> terminate_;
+    std::thread thread_;
+
+
+    size_t batchSize;
+    size_t inSize;
+    size_t outSize;
+
+    int image_size;
+    int count;
+
+  public:
+    class Job {
+      private:
+         MX::Runtime::MxAccl *m_accl;
+      public:
+
+        int index;
+        av_frame_ptr scaled_frame;
+        float m_width_rescale;
+        float m_height_rescale;
+        SwsContext *sw_scale_ctx;
+
+        std::mutex  mutex_;
+        std::unique_lock<std::mutex> lck_;
+        std::condition_variable condition_;
+
+        std::vector<float *> accl_input_data;
+        std::vector<float *> accl_output_data;
+        YOLOv8Result result;
+
+        Job(MX::Runtime::MxAccl *p_accl) :
+          m_accl(p_accl),
+          index(0),
+          m_width_rescale(1.0),
+          m_height_rescale(1.0),
+          sw_scale_ctx(nullptr),
+          lck_(mutex_, std::defer_lock)
+          {
+            scaled_frame = av_frame_ptr(zm_av_frame_alloc());
+            scaled_frame->format = AV_PIX_FMT_RGB24;
+          };
+        ~Job() {
+          if (sw_scale_ctx) {
+            sws_freeContext(sw_scale_ctx);
+          }
+          for (auto ptr : accl_input_data) {
+            if (ptr) delete ptr;
+            ptr = nullptr;
+          }
+          for (auto ptr : accl_output_data) {
+            if (ptr) delete ptr;
+            ptr = nullptr;
+          }
+        };
+        void setFrame(AVFrame *frame) {
+        }
+        Job(Job &&in) :
+          m_accl(in.m_accl),
+          index(in.index),
+          scaled_frame(std::move(in.scaled_frame)),
+          m_width_rescale(in.m_width_rescale),
+          m_height_rescale(in.m_height_rescale)
+        {
+          Debug(1, "In move");
+          //in.scaled_frame = nullptr;
+        }
+        void lock() { lck_.lock(); }
+        void unlock() { lck_.unlock(); };
+
+        void wait() { condition_.wait(lck_); }
+        void notify() { condition_.notify_all(); }
+    };
+    std::list<Job *> jobs;
+    std::list<Job *> send_queue;
+
+    explicit MxAccl();
+    ~MxAccl();
+    bool setup( const std::string &model_type, const std::string &model_file);
+    void Run();
+    Job * get_job();
+    Job * send_packet(Job *job, std::shared_ptr<ZMPacket>);
+    Job * send_image(Job *job, Image *image);
+    Job * send_frame(Job *job, AVFrame *);
+
+    bool in_callback_func(vector<const MX::Types::FeatureMap *> dst, int channel_idx);
+    bool out_callback_func(vector<const MX::Types::FeatureMap *> src, int channel_idx);
+
+    const nlohmann::json receive_detections(Job *job, float threshold);
+};
+#endif
+#endif

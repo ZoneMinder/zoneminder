401864b78a1ed7c04bf01e9507b203ad38234821
Isaac Connor <isaac@zoneminder.com>
2025-08-17 15:55:13 -0400

    Fix missing )

--- web/api/app/Model/Monitor.php
+++ web/api/app/Model/Monitor.php
@@ -164,7 +164,7 @@ class Monitor extends AppModel {
 
   public function daemonControl($monitor, $command, $daemon=null) {
     ZM\Warning(print_r($monitor, true));
-    if (isset($monitor['Function'] and ($monitor['Function'] == 'None') and $command != 'stop') {
+    if (isset($monitor['Function']) and ($monitor['Function'] == 'None') and $command != 'stop') {
       ZM\Warning("Calling daemonControl with command $command when Function == None");
       return;
     }

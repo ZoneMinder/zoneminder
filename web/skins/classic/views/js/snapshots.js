4aa71a53b6c1db148263711543a68f9127414569
Isaac Connor <isaac@zoneminder.com>
2025-10-16 09:54:31 -0400

    Use new strip_html function instead of bad regexp

--- web/skins/classic/views/js/snapshots.js
+++ web/skins/classic/views/js/snapshots.js
@@ -42,17 +42,17 @@ function ajaxRequest(params) {
     data: params.data,
     timeout: 0
   })
-      .done(function(data) {
-        ajax = null;
-        var rows = processRows(data.rows);
-        // rearrange the result into what bootstrap-table expects
-        params.success({total: data.total, totalNotFiltered: data.totalNotFiltered, rows: rows});
-      })
-      .fail(function(jqXHR) {
-        ajax = null;
-        logAjaxFail(jqXHR);
-        table.bootstrapTable('refresh');
-      });
+    .done(function(data) {
+      ajax = null;
+      var rows = processRows(data.rows);
+      // rearrange the result into what bootstrap-table expects
+      params.success({total: data.total, totalNotFiltered: data.totalNotFiltered, rows: rows});
+    })
+    .fail(function(jqXHR) {
+      ajax = null;
+      logAjaxFail(jqXHR);
+      table.bootstrapTable('refresh');
+    });
 }
 
 function processRows(rows) {
@@ -73,7 +73,8 @@ function getIdSelections() {
   var table = $j('#snapshotTable');
 
   return $j.map(table.bootstrapTable('getSelections'), function(row) {
-    return row.Id.replace(/(<([^>]+)>)/gi, ''); // strip the html from the element before sending
+    // row.Id looks like <a href="?view=snapshot&amp;id=70">70</a> but sadly is not a dom element.
+    return strip_html(row.Id); // strip the html from the element before sending
   });
 }
 
@@ -289,7 +290,7 @@ function initPage() {
   table.bootstrapTable('resetSearch');
   // The table is initially given a hidden style, so now that we are done rendering, show it
   table.show();
-}
+} // initPage
 
 $j(document).ready(function() {
   initPage();

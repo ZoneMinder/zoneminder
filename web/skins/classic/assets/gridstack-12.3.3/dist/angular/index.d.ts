0812e93cd63d648ea04f08ae434ed047b8692ab9
IgorA100 <igora100@gmail.com>
2025-10-29 22:53:52 +0300

    Removed extra files

--- web/skins/classic/assets/gridstack-12.3.3/dist/angular/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './lib/types';
-export * from './lib/base-widget';
-export * from './lib/gridstack-item.component';
-export * from './lib/gridstack.component';
-export * from './lib/gridstack.module';

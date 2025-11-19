344cae678200bcdf875013e1a83242f7b3cd908a
Isaac Connor <isaac@zoneminder.com>
2025-11-19 16:39:11 -0500

    Drop various foreign keys around Events table.  We do this in 1.37 for performance/locking reasons. We are doing it here for zmrecover.pl

--- db/zm_update-1.36.37.sql
+++ db/zm_update-1.36.37.sql
@@ -1,1 +1,78 @@
 UPDATE `Config` SET Value='https://telemetry.zoneminder.com/index.php' WHERE Value='https://zmanon:2b2d0b4skps@telemetry.zoneminder.com/zmtelemetry/testing5';
+
+--
+-- Drop Foreign Keys on Events table.
+--
+
+SELECT 'Checking for FOREIGN KEYS on Events Table';
+SET @s = (SELECT IF(
+  (SELECT COUNT(*)
+  FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE
+  WHERE TABLE_NAME = 'Frames'
+  AND REFERENCED_TABLE_SCHEMA = DATABASE()
+  AND REFERENCED_TABLE_NAME = 'Events'
+  AND CONSTRAINT_NAME = 'Frames_ibfk_1'
+  ) > 0,
+"ALTER TABLE Frames DROP FOREIGN KEY Frames_ibfk_1",
+"SELECT 'FOREIGN KEY In Frames table already removed or has a different name.'"
+));
+
+PREPARE stmt FROM @s;
+EXECUTE stmt;
+
+SET @s = (SELECT IF(
+  (SELECT COUNT(*)
+  FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE
+  WHERE TABLE_NAME = 'Stats'
+  AND REFERENCED_tABLE_SCHEMA = DATABASE()
+  AND REFERENCED_TABLE_NAME = 'Events'
+  AND CONSTRAINT_NAME = 'Stats_ibfk_1'
+  ) > 0,
+"ALTER TABLE Stats DROP FOREIGN KEY Stats_ibfk_1",
+"SELECT 'FOREIGN KEY In Stats table already removed or has a different name.'"
+));
+
+PREPARE stmt FROM @s;
+EXECUTE stmt;
+
+SET @s = (SELECT IF(
+  (SELECT COUNT(*)
+  FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE
+  WHERE TABLE_NAME = 'Stats'
+  AND REFERENCED_tABLE_SCHEMA = DATABASE()
+  AND REFERENCED_TABLE_NAME = 'Events'
+  AND CONSTRAINT_NAME = 'Stats_ibfk_2'
+  ) > 0,
+"ALTER TABLE Stats DROP FOREIGN KEY Stats_ibfk_2",
+"SELECT 'FOREIGN KEY In Stats table already removed or has a different name.'"
+));
+
+PREPARE stmt FROM @s;
+EXECUTE stmt;
+
+SET @s = (SELECT IF(
+  (SELECT COUNT(*)
+  FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE
+  WHERE TABLE_NAME = 'Stats'
+  AND REFERENCED_tABLE_SCHEMA = DATABASE()
+  AND REFERENCED_TABLE_NAME = 'Events'
+  AND CONSTRAINT_NAME = 'Stats_ibfk_3'
+  ) > 0,
+"ALTER TABLE Stats DROP FOREIGN KEY Stats_ibfk_3",
+"SELECT 'FOREIGN KEY In Stats table already removed or has a different name.'"
+));
+
+PREPARE stmt FROM @s;
+EXECUTE stmt;
+
+SET @s = (SELECT IF(
+  (SELECT COUNT(*)
+  FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE
+  WHERE TABLE_NAME = 'Stats'
+  AND REFERENCED_tABLE_SCHEMA = DATABASE()
+  AND REFERENCED_TABLE_NAME = 'Events'
+  AND CONSTRAINT_NAME = 'Stats_ibfk_4'
+  ) > 0,
+"ALTER TABLE Stats DROP FOREIGN KEY Stats_ibfk_4",
+"SELECT 'FOREIGN KEY In Stats table already removed or has a different name.'"
+));

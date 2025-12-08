c393083f8c83057326ed5470929d1eaa27399cb2
Simpler1 <kevin.bezold@gmail.com>
2025-12-08 15:59:53 -0500

    array('0'=>translate('No Tag'))

--- web/includes/Filter.php
+++ web/includes/Filter.php
@@ -979,7 +979,7 @@ class Filter extends ZM_Object {
         }
       }
     }
-    $availableTags = array(translate('No Tag'));
+    $availableTags = array('0'=>translate('No Tag'));
     foreach ( dbFetchAll('SELECT Id, Name FROM Tags ORDER BY lower(`Name`) ASC') AS $tag ) {
       $availableTags[$tag['Id']] = validHtmlStr($tag['Name']);
     }
@@ -1115,7 +1115,7 @@ class Filter extends ZM_Object {
     for ( $i = 0; $i < 7; $i++ ) {
       $weekdays[$i] = date('D', mktime(12, 0, 0, 1, $i+1, 2001));
     }
-    $availableTags = array(translate('No Tag'));
+    $availableTags = array('0'=>translate('No Tag'));
     foreach ( dbFetchAll('SELECT Id, Name FROM Tags ORDER BY lower(`Name`) ASC') AS $tag ) {
       $availableTags[$tag['Id']] = validHtmlStr($tag['Name']);
     }

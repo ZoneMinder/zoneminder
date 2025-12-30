a71d5d9c3cbf48c4d423cdf15227f3dcbd433778
Steve Gilvarry <SteveGilvarry@users.noreply.github.com>
2025-12-30 16:10:32 +1100

    Update Videojs to v8

--- web/includes/functions.php
+++ web/includes/functions.php
@@ -208,19 +208,28 @@ function getVideoStreamHTML($id, $src, $width, $height, $format, $title='') {
   switch ($mimeType) {
     case 'video/mp4' :
       global $rates;
-      return '<video autoplay id="videoobj" class="video-js vjs-default-skin"'
+      $playbackRates = implode(',',
+        array_map(function($r){return $r/100;},
+          array_filter(
+            array_keys($rates),
+            function($r){return $r >= 0 ? true : false;}
+          )));
+      
+      return '<video id="videoobj" class="video-js"'
         .($width ? ' width="'.$width.'"' : '').($height ? ' height="'.$height.'"' : '').'
             style="transform: matrix(1, 0, 0, 1, 0, 0);"
-            data-setup=\'{ "controls": true, "autoplay": true, "preload": "auto", "playbackRates": [ '. implode(',',
-              array_map(function($r){return $r/100;},
-                array_filter(
-                  array_keys($rates),
-                  function($r){return $r >= 0 ? true : false;}
-                ))).']}\' 
-          >
+            controls autoplay preload="auto">
           <source src="'. $src.'" type="video/mp4">
           Your browser does not support the video tag.
-          </video>';
+        </video>
+        <script>
+          videojs("videoobj", {
+            controls: true,
+            autoplay: true,
+            preload: "auto",
+            playbackRates: [' . $playbackRates . ']
+          });
+        </script>';
     default:
     return '<embed'. ( isset($mimeType)?(' type="'.$mimeType.'"'):'' ). '
       src="'.$src.'"
--- web/skins/classic/includes/export_functions.php
+++ web/skins/classic/includes/export_functions.php
@@ -240,14 +240,27 @@ function exportEventImages($event, $exportDetail, $exportFrames, $myfilelist) {
     } # end if passthrough
 ?>
     <div id="videoFeed">
-      <video id="videoobj" class="video-js vjs-default-skin" style="transform: matrix(1, 0, 0, 1, 0, 0);"
+      <video id="videoobj" class="video-js" style="transform: matrix(1, 0, 0, 1, 0, 0);"
         width="<?php echo $event->Width() ?>"
         height="<?php echo $event->Height() ?>"
-        data-setup='{ "controls": true, "autoplay": true, "preload": "auto", "plugins": { "zoomrotate": { "zoom": "<?php echo $Zoom ?>"}}}'>
+        controls autoplay preload="auto">
         <source src="<?php echo $event->DefaultVideo(); ?>" type="video/mp4">
         <track id="monitorCaption" kind="captions" label="English" srclang="en" src='data:plain/text;charset=utf-8,"WEBVTT\n\n 00:00:00.000 --> 00:00:01.000 ZoneMinder"' default>
         Your browser does not support the video tag.
       </video>
+      <script>
+        (function() {
+          var player = videojs('videoobj', {
+            controls: true,
+            autoplay: true,
+            preload: 'auto'
+          });
+          player.zoomrotate({
+            zoom: <?php echo $Zoom ?>,
+            rotate: <?php echo $Rotation ?>
+          });
+        })();
+      </script>
     </div><!--videoFeed-->
 <?php
 	} else { // end if DefaultVideo

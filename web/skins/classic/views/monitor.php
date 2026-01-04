d4b05782f89948c4f8a901e79574c2812080312e
Isaac Connor <isaac@zoneminder.com>
2026-01-04 11:23:22 -0500

    Add a dummy password field at the top of the form to prevent chrome from offering to save passwords

--- web/skins/classic/views/monitor.php
+++ web/skins/classic/views/monitor.php
@@ -453,6 +453,7 @@ if (canEdit('Monitors')) {
     <!-- BEGIN ITEM LIST -->
     <div class="container-fluid" id="monitor">
       <form name="contentForm" id="contentForm" method="post" action="?view=monitor" autocomplete="off">
+        <input type="password" name="dummy_password" style="display:none;"/><?php #to prevent chrome from saving passwords ?>
         <input type="hidden" name="tab" value="<?php echo $tab?>"/>
         <input type="hidden" name="mid" value="<?php echo $monitor->Id() ? $monitor->Id() : $mid ?>"/>
         <input type="hidden" name="origMethod" value="<?php echo (null !== $monitor->Method())?validHtmlStr($monitor->Method()):'' ?>"/>

d5531180f42eaf514d6db2cca7987a4e6bf58033
Isaac Connor <isaac@zoneminder.com>
2026-01-31 12:43:03 -0500

    Add av1 support

--- src/zm_rtsp_server_fifo_video_source.h
+++ src/zm_rtsp_server_fifo_video_source.h
@@ -15,6 +15,7 @@
 namespace xop {
   class H264Source;
   class H265Source;
+  class AV1Source;
 }
 
 #if HAVE_RTSP_SERVER
@@ -27,9 +28,10 @@ class ZoneMinderFifoVideoSource: public ZoneMinderFifoSource {
   int setWidth(int width) { return m_width=width; };
   int setHeight(int height) { return m_height=height; };
 
-  // Set xop sources for SPS/PPS updates
+  // Set xop sources for SPS/PPS/sequence header updates
   void setH264Source(xop::H264Source *source) { m_h264Source = source; }
   void setH265Source(xop::H265Source *source) { m_h265Source = source; }
+  void setAV1Source(xop::AV1Source *source) { m_av1Source = source; }
 
   ZoneMinderFifoVideoSource(
     std::shared_ptr<xop::RtspServer>& rtspServer,
@@ -45,6 +47,7 @@ class ZoneMinderFifoVideoSource: public ZoneMinderFifoSource {
   int m_height;
   xop::H264Source *m_h264Source = nullptr;
   xop::H265Source *m_h265Source = nullptr;
+  xop::AV1Source *m_av1Source = nullptr;
 };
 #endif // HAVE_RTSP_SERVER
 

d67f23b2795516ce1a9c516713e5fc9f20e6fd1d
Isaac Connor <isaac@zoneminder.com>
2026-03-10 14:18:44 -0700

    Remove General header

--- web/skins/classic/includes/functions.php
+++ web/skins/classic/includes/functions.php
@@ -309,10 +309,7 @@ function buildSidebarMenu() {
   global $view;
   global $user;
   if ( $user and $user->Username() ) {
-  $menuForAuthUser = '
-            <li class="menu-header"><span> GENERAL </span></li> ' .
-    renderMenuItems($forLeftBar = true)
-  ;
+    $menuForAuthUser = renderMenuItems($forLeftBar = true) ;
   } else { // USER IS NOT AUTHORIZED!
     $menuForAuthUser = '';
   }
@@ -378,6 +375,8 @@ function buildSidebarMenu() {
               </a>
             </li>
 ';
+  } else if (defined(ZM_SIDEBAR_FOOTER) and ZM_SIDEBAR_FOOTER) {
+    $menu .- ZM_SIDEBAR_FOOTER;
   }
   $menu .= '
           </ul>

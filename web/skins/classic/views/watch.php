91a94d1dbef1b4aeef99ca6937369f500e932f60
IgorA100 <igora100@gmail.com>
2026-03-23 17:15:53 +0300

    Added a link to the PR (watch.php)

--- web/skins/classic/views/watch.php
+++ web/skins/classic/views/watch.php
@@ -408,7 +408,7 @@ echo htmlSelect('cyclePeriod', $cyclePeriodOptions, $period, array('id'=>'cycleP
  ?>
           </ul>
         </nav>
-        <div style="padding-right:1px; margin-right: -1px;"></div> <--! REQUIRED -->
+        <div style="padding-right:1px; margin-right: -1px;"></div> <--! REQUIRED https://github.com/ZoneMinder/zoneminder/pull/4721 -->
         <div id="wrapperMonitor" class="container-fluid col">
           <div id="monitor" class="monitor hidden-shift"
 >

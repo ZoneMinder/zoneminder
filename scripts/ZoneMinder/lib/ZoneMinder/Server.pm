b8183e2642ea6197df930deb5c375541b74a715f
Daniel Caujolle-Bert <f1rmb.daniel@naboo-homelinux.org>
2026-04-10 16:17:03 +0200

    Use qx() lc() and chomp() for command execution and result. Use eq in the if() statement, instead of '=='.

--- scripts/ZoneMinder/lib/ZoneMinder/Server.pm
+++ scripts/ZoneMinder/lib/ZoneMinder/Server.pm
@@ -128,8 +128,10 @@ sub CpuUsage {
   } else {
     # Get CPU utilization percentages
     my $top_output = '';
+    my $uname_output = lc(qx($ZoneMinder::Config{ZM_PATH_UNAME} -s));
+    chomp($uname_output);
     ## FreeBSD
-    if ($ZoneMinder::Config{ZM_PATH_UNAME} == 'freebsd') {
+    if ($uname_output eq "freebsd") {
       $top_output = `top -b -n 1 | grep "^CPU" | sed 's/%//g' | awk '{print \$2, \$6, \$4, \$10}'`;
     } else {
       $top_output = `top -b -n 1 | grep -i "^%Cpu(s)" | awk '{print \$2, \$4, \$6, \$8}'`;

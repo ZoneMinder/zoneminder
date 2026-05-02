c702d2374b758cf4cd4f0b588e6efe66f56fbc76
IgorA100 <igora100@gmail.com>
2026-05-02 17:20:19 +0300

    To avoid problems when modifying the database, we'll continue to use the "*_bin" collation instead of "*_unicode_ci" (zm_update-1.39.6.sql).

--- db/zm_update-1.39.6.sql
+++ db/zm_update-1.39.6.sql
@@ -1,6 +1,6 @@
 --
 -- This updates a 1.39.5 database to 1.39.6
 --
--- Make changes to the Tags table and change the Name column's COLLATE to utf8mb4_unicode_ci
+-- Make changes to the Tags table and change the Name column's COLLATE to utf8mb4_bin
 --
-ALTER TABLE `Tags` MODIFY `Name` VARCHAR(64) COLLATE `utf8mb4_unicode_ci` NOT NULL DEFAULT '';
+ALTER TABLE `Tags` MODIFY `Name` VARCHAR(64) COLLATE `utf8mb4_bin` NOT NULL DEFAULT '';

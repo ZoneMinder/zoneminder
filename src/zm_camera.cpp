7b21371083c509da1c7cfd301e920688ae51d309
Isaac Connor <isaac@zoneminder.com>
2026-05-03 12:24:43 -0400

    Reapply "fix: replace ZM_COLOUR system with AVPixelFormat for format dispatch"
    This reverts commit 60b6b37c60796453a7d308e5f14be5c9da130c08.

--- src/zm_camera.cpp
+++ src/zm_camera.cpp
@@ -41,6 +41,7 @@ Camera::Camera(
   height(p_height),
   colours(p_colours),
   subpixelorder(p_subpixelorder),
+  pixelFormat(zm_pixformat_from_colours(p_colours, p_subpixelorder)),
   brightness(p_brightness),
   hue(p_hue),
   colour(p_colour),
@@ -63,9 +64,9 @@ Camera::Camera(
   mLastAudioDTS(AV_NOPTS_VALUE),
   bytes(0),
   mIsPrimed(false) {
-  linesize = width * colours;
+  linesize = FFALIGN(av_image_get_linesize(pixelFormat, width, 0), 32);
   pixels = width * height;
-  imagesize = static_cast<unsigned long long>(height) * linesize;
+  imagesize = av_image_get_buffer_size(pixelFormat, width, height, 32);
 
   Debug(2, "New camera id: %d width: %d line size: %d height: %d colours: %d subpixelorder: %d capture: %d, size: %llu",
         monitor->Id(), width, linesize, height, colours, subpixelorder, capture, imagesize);
@@ -101,7 +102,7 @@ AVStream *Camera::getVideoStream() {
       mVideoStream->time_base = (AVRational) {1, 1000000}; // microseconds as base frame rate
       mVideoStream->codecpar->width = width;
       mVideoStream->codecpar->height = height;
-      mVideoStream->codecpar->format = GetFFMPEGPixelFormat(colours, subpixelorder);
+      mVideoStream->codecpar->format = pixelFormat;
       mVideoStream->codecpar->codec_type = AVMEDIA_TYPE_VIDEO;
       mVideoStream->codecpar->codec_id = AV_CODEC_ID_NONE;
       Debug(1, "Allocating avstream %p %p %d", mVideoStream, mVideoStream->codecpar, mVideoStream->codecpar->codec_id);

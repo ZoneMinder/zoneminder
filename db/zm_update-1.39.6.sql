4ff7020ca66582683d50a50dc5f526127881e872
IgorA100 <igora100@gmail.com>
2026-05-06 17:04:41 +0300

    Update zm_update-1.39.6.sql

--- db/zm_update-1.39.6.sql
+++ db/zm_update-1.39.6.sql
@@ -3,4 +3,4 @@
 --
 -- Make changes to the Tags table and change the Name column's COLLATE to utf8mb4_bin
 --
-ALTER TABLE `Tags` MODIFY `Name` VARCHAR(64) COLLATE `utf8mb4_bin` NOT NULL DEFAULT '';
+ALTER TABLE `Tags` MODIFY `Name` VARCHAR(64) CHARACTER SET `utf8mb4` COLLATE `utf8mb4_bin` NOT NULL DEFAULT '';

d1556ea4cc1b50d741486b2a19e1622088ce88c4
IgorA100 <igora100@gmail.com>
2026-05-10 20:05:08 +0300

    Add Component, TimeKey, and Level to the primary index to increase query processing speed.  zm_update-1.39.10.sql

---
+++ db/zm_update-1.39.10.sql
@@ -0,0 +1,6 @@
+--
+-- This updates a 1.39.9 database to 1.39.10
+--
+-- Add Component, TimeKey, and Level to the primary index to increase query processing speed.
+--
+ALTER TABLE `Logs` DROP PRIMARY KEY, ADD PRIMARY KEY (`Id`, `Component`, `TimeKey`, `Level`) USING BTREE;

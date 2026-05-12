94b58144807678aacf37032082f0a0622a9c884c
IgorA100 <igora100@gmail.com>
2026-05-12 21:37:39 +0300

    Added ZMSEnabled column to the Monitors table (zm_update-1.39.11.sql)

---
+++ db/zm_update-1.39.11.sql
@@ -0,0 +1,6 @@
+--
+-- This updates a 1.39.10 database to 1.39.11
+--
+-- Added the ability to disable ZMS
+--
+ALTER TABLE `Monitors` ADD COLUMN `ZMSEnabled` BOOLEAN NOT NULL default true AFTER `Decoding`

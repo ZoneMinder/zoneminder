1678c13993abded32e462c1ddb5e7e8e179aa412
IgorA100 <igora100@gmail.com>
2026-05-17 01:27:19 +0300

    Option to generate 'view=view_hls' (Event.php)

--- web/includes/Event.php
+++ web/includes/Event.php
@@ -274,7 +274,11 @@ class Event extends ZM_Object {
     if ( $this->{'DefaultVideo'} and $args['mode'] != 'jpeg' ) {
       $streamSrc .= $Server->PathToIndex();
       $args['eid'] = $this->{'Id'};
-      $args['view'] = 'view_video';
+      if (strtolower($args['mode']) == 'mp4') {
+        $args['view'] = 'view_video';
+      } elseif (strtolower($args['mode']) == 'mp4hls') {
+        $args['view'] = 'view_hls';
+      }
     } else {
       $streamSrc .= $Server->PathToZMS();
 

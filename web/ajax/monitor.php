ZoneMinder/zoneminder

commit 9561ed894738a707814f0b25a11420241b89b29d

If the monitor name has changed, you must change the symbolic links in all repositories that contain folders for recording events for this monitor. (monitor.php)

web/includes/actions/monitor.php
@@ -175,20 +175,28 @@ if ($action == 'save') {
       $oldMonitor = clone $monitor;
 
       if ($monitor->save($changes)) {
-        # Leave old symlinks on old storage areas, as old events will still be there. Only delete the link if the name has changed
+        # If the name has changed, it must also be changed in all storage areas.
         if (isset($changes['Name'])) {
-          $link_path = $oldMonitor->Storage()->Path().'/'.basename($oldMonitor->Name());
-          if (file_exists($link_path)) {
-            ZM\Debug("Deleting old link  ".$link_path);
-            unlink($link_path);
-          } else {
-            ZM\Debug("Old link didn't exist at ".$link_path);
-          }
           $saferName = basename($newMonitor['Name']);
-          $link_path = $monitor->Storage()->Path().'/'.$saferName;
-          if (($saferName != $mid) and !@symlink($mid, $link_path)) {
-            if (!(file_exists($link_path) and is_link($link_path))) {
-              ZM\Warning('Unable to symlink ' . $monitor->Storage()->Path().'/'.$mid . ' to ' . $link_path);
+          require_once('includes/Storage.php');
+          foreach (ZM\Storage::find() as $Storage) {
+            # Let's remove old symlinks
+            $old_link_path = $Storage->Path().'/'.basename($oldMonitor->Name());
+            if (file_exists($old_link_path)) {
+              ZM\Debug("Deleting old link in storage '" . $Storage->Name() . "' " . $old_link_path);
+              unlink($old_link_path);
+            } else {
+              ZM\Debug("Old link didn't exist in storage '" . $Storage->Name() . "' at ".$old_link_path);
+            }
+
+            # Let's create new symlinks
+            $dir = $Storage->Path().'/'.$mid;
+            if (is_dir($dir)) { # Let's check if there is a folder with events in this storage
+              $link_path = $Storage->Path().'/'.$saferName;
+              if (!is_link($link_path) && !@symlink($mid, $link_path)) {
+                # It is necessary to check is_link() to avoid unnecessary warnings, since different repositories can have the same path and this is not prohibited by the configuration.
+                ZM\Warning('Unable to symlink in storage "' . $Storage->Name() . '" ' . $dir . ' to ' . $link_path);
+              }
             }
           }
         }

commit c2820d13173c38f2390cdd039bd0a9bc5dfbc6d3

- Don't create symlinks if the monitor name is equal to its ID. This was previously broken in https://github.com/IgorA100/zoneminder/commit/9561ed894738a707814f0b25a11420241b89b29d - Moved $oldLinkPathFound analysis outside the storages loop - More correct check for an empty $_REQUEST['monitorName'] value

web/ajax/monitor.php
@@ -16,7 +16,7 @@ if ( canView('Monitors') ) {
     $badChars = [];
     $message = '';
 
-    if (isset($_REQUEST['monitorName']) && !empty($_REQUEST['monitorName'])) {
+    if (isset($_REQUEST['monitorName']) && $_REQUEST['monitorName'] !== '') {
       $monitorName = $_REQUEST['monitorName'];
       $trimmedMonitorName = trim($monitorName);
       $cleanedMonitorName = preg_replace($filterRegexp, '', $trimmedMonitorName);

web/includes/actions/monitor.php
@@ -201,19 +201,19 @@ if ($action == 'save') {
               } 
               closedir($dirStoragePath);
             }
-            if (!$oldLinkPathFound) {
-              ZM\Debug("Old link at '" . $oldLinkPath . "' was not found in any of the storages");
-            }
 
             # Let's create new symlinks
             $dir = $Storage->Path().'/'.$mid;
-            if (is_dir($dir)) { # Let's check if there is a folder with events in this storage
+            if (is_dir($dir) && (string)$saferName !== (string)$mid) { # Let's check if there is a folder with events in this storage
               $linkPath = $Storage->Path().'/'.$saferName;
               if (!is_link($linkPath) && !@symlink($mid, $linkPath)) {
                 # It is necessary to check is_link() to avoid unnecessary warnings, since different repositories can have the same path and this is not prohibited by the configuration.
                 ZM\Warning('Unable to symlink in storage "' . $Storage->Name() . '" ' . $dir . ' to ' . $linkPath);
               }
             }
+          }
+          if (!$oldLinkPathFound) {
+            ZM\Debug("Old link at '" . $oldLinkPath . "' was not found in any of the storages");
           }
         }
 
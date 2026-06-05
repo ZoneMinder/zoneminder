afb81fe72022acbef09e1c9a969e344e3e6180d9
IgorA100 <igora100@gmail.com>
2026-06-05 12:13:03 +0300

    Write "is not" instead of "has not" (view_hls.php)

--- web/views/view_hls.php
+++ web/views/view_hls.php
@@ -43,7 +43,7 @@ $m3u8_content_check = file_get_contents($m3u8_path);
 if ($m3u8_content_check === false) {
   header('HTTP/1.1 204 No Content');
   header('Cache-Control: no-cache');
-  ZM\Debug('HLS manifest ' . $m3u8_path .' has not available yet');
+  ZM\Debug('HLS manifest ' . $m3u8_path . ' is not available yet');
   exit;
 } else if (strpos($m3u8_content_check, '#EXTINF:') === false) {
   header('HTTP/1.1 204 No Content');

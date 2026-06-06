ZoneMinder/zoneminder

commit d5f71b17b0473bb9f1c64d2223a08391b4572b38

Interrupt getTracksFromStream() if the stream has been stopped. (skin.js)

web/skins/classic/js/skin.js
@@ -2931,11 +2931,12 @@ async function getTracksFromStream(videoFeedStream) {
 
   if (stream) {
     const timeoutStreamActive = 20000;
-    const streamActive = await waitUntil(() => stream.active, timeoutStreamActive ); // We are waiting for the stream to become active.
-    if (streamActive !== false) {
+    const startTime = Date.now();
+    const streamActive = await waitUntil(() => (stream.active || !videoFeedStream.started), timeoutStreamActive, stream.active); // We are waiting for the stream to become active.
+    if (streamActive !== false && videoFeedStream.started) {
       console.debug(`Stream for monitor with ID=${mid} became active within ${(streamActive/1000).toFixed(2)} seconds.`);
     } else {
-      console.warn(`Within ${(timeoutStreamActive/1000).toFixed(2)} seconds, the stream for monitor with ID=${mid} did not become active.`);
+      console.warn(`Within ${((Date.now() - startTime)/1000).toFixed(2)} seconds, the stream for monitor with ID=${mid} did not become active.`);
       return;
     }
 

commit 00e2ac4ab1c530558d547aeaf99a481c61508b42

Fixed a regression in retrieving tracks from a stream. (skin.js)
PR https://github.com/ZoneMinder/zoneminder/commit/d5f71b17b0473bb9f1c64d2223a08391b4572b38 broke retrieving tracks from a stream for recorded events.
Sorry for the annoying error :(

web/skins/classic/js/skin.js
@@ -3189,8 +3189,8 @@ async function getTracksFromStream(videoFeedStream) {
   if (stream) {
     const timeoutStreamActive = 20000;
     const startTime = Date.now();
-    const streamActive = await waitUntil(() => (stream.active || !videoFeedStream.started), timeoutStreamActive, stream.active); // We are waiting for the stream to become active.
-    if (streamActive !== false && videoFeedStream.started) {
+    const streamActive = await waitUntil(() => (stream.active || videoFeedStream.started === false), timeoutStreamActive, stream.active); // We are waiting for the stream to become active.
+    if (streamActive !== false && videoFeedStream.started !== false) {
       console.debug(`Stream for monitor with ID=${mid} became active within ${(streamActive/1000).toFixed(2)} seconds.`);
     } else {
       console.warn(`Within ${((Date.now() - startTime)/1000).toFixed(2)} seconds, the stream for monitor with ID=${mid} did not become active.`);
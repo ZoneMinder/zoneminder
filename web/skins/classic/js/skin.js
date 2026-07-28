644109d601be0efbed1026e0d24605b65198f793
IgorA100 <igora100@gmail.com>
2026-07-28 12:49:24 +0300

    Fix: ESLint (skin.js)

--- web/skins/classic/js/skin.js
+++ web/skins/classic/js/skin.js
@@ -3764,7 +3764,7 @@ function generateUUID() {
     bytes[6] = (bytes[6] & 0x0f) | 0x40;
     bytes[8] = (bytes[8] & 0x3f) | 0x80;
 
-    const hex = Array.from(bytes, b => b.toString(16).padStart(2, '0'));
+    const hex = Array.from(bytes, (b) => b.toString(16).padStart(2, '0'));
 
     return (
       hex.slice(0, 4).join('') + '-' +

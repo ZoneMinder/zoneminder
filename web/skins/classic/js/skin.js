336e00ac6943bd3bf7c8230d86e567a4dfc9bde7
IgorA100 <igora100@gmail.com>
2026-08-22 00:50:40 +0300

    Update skin.js

--- web/skins/classic/js/skin.js
+++ web/skins/classic/js/skin.js
@@ -3386,7 +3386,7 @@ async function getTracksFromStream(videoFeedStream) {
     console.warn(`"captureStream" NOT found in STREAM for monitor ID=${mid} or not supported by the browser.`);
     streamCaptureNotSupported = true; // This will enable the volume control if the browser does not support captureStream (for example, Safari)
   }
-  if (!isCurrentPlaybackSession(videoFeedStream, playbackSessionId)) {
+  if (!(("sessionActive" in videoFeedStream) ? videoFeedStream.sessionActive(playbackSessionId) : isCurrentPlaybackSession(videoFeedStream, playbackSessionId))) {
     console.debug(`RACE [${playbackSessionId}] getTracksFromStream() aborted`);
     stopMediaStreamTracks(stream);
     dispatchTracksReceived(videoFeedStream, {
@@ -3400,7 +3400,7 @@ async function getTracksFromStream(videoFeedStream) {
     const timeoutStreamActive = 20000;
     const startTime = Date.now();
     const streamActive = await waitUntil(() => (stream.active || videoFeedStream.started === false), timeoutStreamActive); // We are waiting for the stream to become active.
-    if (!isCurrentPlaybackSession(videoFeedStream, playbackSessionId)) {
+    if (!(("sessionActive" in videoFeedStream) ? videoFeedStream.sessionActive(playbackSessionId) : isCurrentPlaybackSession(videoFeedStream, playbackSessionId))) {
       console.debug(`RACE [${playbackSessionId}] waitUntil() aborted`);
       stopMediaStreamTracks(stream);
       dispatchTracksReceived(videoFeedStream, {
